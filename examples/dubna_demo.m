e6cfe49c5d2d117b847d41ef69e8e4576bc1f880
ralf.hielscher <ralf.hielscher@localhost>
2007-11-07 13:26:28 +0000

    Ralf Hielscher: initial import, current version is 0.3 + some minor bugfixes

---
+++ examples/dubna_demo.m
@@ -0,0 +1,87 @@
+%% MTEX - Analysing Neutron Pole Figures (Dubna)
+%
+% Detailed demonstration of the MTEX toolbox at the Dubna data example.
+%
+% The data where meassured by Florian Wobbe at Dubna 2005 of a Quarz
+% specimen using Neutron diffraction.
+
+
+%% Import diffraction data
+%
+
+% specify scrystal and specimen symmetry
+cs = symmetry('-3m',[1.4,1.4,1.5]);
+ss = symmetry('tricline');
+
+% specify file names
+fname = {...
+  [mtexDataPath '/dubna/Q(10-10)_amp.cnv'],...
+  [mtexDataPath '/dubna/Q(10-11)(01-11)_amp.cnv'],...
+  [mtexDataPath '/dubna/Q(11-22)_amp.cnv']};
+
+% specify crystal directions
+h = {Miller(1,0,-1,0,cs),[Miller(0,1,-1,1,cs),Miller(1,0,-1,1,cs)],Miller(1,1,-2,2,cs)};
+
+% specify structure coefficients
+c = {1,[0.52 ,1.23],1};
+
+% import pole figure data
+pf = loadPoleFigure(fname,h,cs,ss,'superposition',c)
+
+%% plot pole figures
+
+figure('position',[359 450 749 249])
+plot(pf)
+
+%% Analyse pole figures
+
+clf
+hist(pf)
+
+%% Correct pole figures
+
+pf_corrected = delete(pf,getTheta(getr(pf)) >= 70*degree &...
+  getTheta(getr(pf)) <= 75*degree);
+
+plot(pf_corrected)
+
+%% Rotate pole figures
+
+pf_rotated = rotate(pf_corrected,axis2quat(xvector,45*degree));
+plot(pf_rotated)
+
+%% Recalculate ODF
+%
+% we use here the option 'background' to specify the approximative
+% background radiation and to increase the accuracy of the reconstruction
+
+odf = calcODF(pf_corrected,'resolution',10*degree,'background',10,'iter_max',6)
+
+
+%% Error analysis
+
+% calc RP1 error
+calcerror(pf_corrected,odf,'RP',1)
+
+% difference plot
+plotDiff(pf,odf)
+
+
+%% Recalculate c-axis pole figures
+
+plotpdf(odf,Miller(0,0,1,cs))
+
+%% Plot inverse pole figure
+
+plotipdf(odf,vector3d(1,1,2))
+
+%% plot recalculated ODF
+
+figure('position',[15 111 720 308])
+plot(odf,'sections',18,'resolution',5*degree)
+
+%% rotate ODF back
+
+odfrotated = rotate(odf,axis2quat(xvector,45*degree));
+plotpdf(odfrotated,getMiller(pf(1)));
+
---
+++ examples/geometry_demo.m
@@ -0,0 +1,91 @@
+%% Demo of the Crystal Geometry Tools
+%
+%% the class vector3d
+
+% create
+v1 = vector3d(1,1,1);
+v2 = [xvector,yvector,zvector];
+
+% operate with vectors
+v3 = 2*v1 + 3*v2;
+v1 == v2;
+
+% transform to spherical coordinates
+[theta,rho] = polar(v3);
+v3 = sph2vec(theta,rho);
+
+% plot
+plot(v3,'FontSize',20)
+
+
+
+%% quaternion
+
+% create 
+q = quaternion(1,0,0,0);
+q = euler2quat(pi/2,0,0,'BUNGE');
+q = axis2quat(zvector,pi/2);
+
+% operate with quaternions
+q*q;
+q*v3;
+plot(q*v3,'FontSize',20)
+
+% transform
+[phi1,Phi,phi2] = quat2euler(q,'BUNGE');
+rotangle(q);
+
+
+%% groups of quaternions - symmetries
+%--------------------------------------------------------------------------
+
+% create
+SS = symmetry;
+CS = symmetry('-3m',[1,1,3])
+CS = symmetry('cubic')
+
+% operate
+CS * zvector;
+symetriceVec(CS,zvector);
+
+%% the class S2grid
+%-------------------------------------------------------------------------
+
+% create
+S2G = S2Grid(v3);
+S2G = S2Grid(100);
+S2G = S2Grid('RESOLUTION',5*pi/180)
+
+% operations
+rotate(S2G,q);
+delete(S2G,getTheta(S2G)==pi/2);
+subGrid(S2G,getTheta(S2G)<=80*degree);
+
+% plot
+plot(S2G)
+
+% operations: 
+% *subgrid
+% *transformation to spherical coordinates
+
+%% groups of quaternions - grids on SO(3)
+%--------------------------------------------------------------------------
+
+% create 
+SO3G = SO3Grid(q);
+SO3G = SO3Grid(5*pi/180,CS,SS);
+SO3G = SO3Grid(500,CS,SS)
+
+
+% operate
+plot(SO3G * xvector)
+%%
+plot(SO3G * symetriceVec(CS,xvector))
+
+%%
+% plot
+plot(SO3G,'RODRIGUEZ')
+
+
+
+
