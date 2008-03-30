eba30c18c0681833cbcf39814627f201caea912f
Hielscher <ralfHielscher@users.noreply.github.com>
2008-03-30 18:23:36 +0000

    Ralf Hielscher: new tests

--- tests/check_eulerquat.m
+++ tests/check_eulerquat.m
@@ -3,14 +3,37 @@
 % compare subGrid function with the max_angle option to SO3Grid
 %
 
-q = quaternion(SO3Grid(1000,symmetry,symmetry));
+q = quaternion(SO3Grid(10000,symmetry,symmetry));
 
 [alpha,beta,gamma] = quat2euler(q);
 
 qq = euler2quat(alpha,beta,gamma);
 
-if mean(abs(dot(q,qq))) < 0.9
-  hist(abs(dot(q,qq)));
+e  = abs(dot(q,qq));
+if mean(e) < 0.9
+  hist(e);
   error('Error in euler - quaternion conversion');
+else
+  disp('Euler <-> quaternion conversion is ok!')
 end
-disp('Euler - quaternion conversion is ok!')
+
+
+e = 0;
+for i = 1:length(q)
+  
+  q1 = q(i);
+  q2 = mat2quat(quat2mat(q(i)));
+  e(i) = abs(dot(q2,q1));
+  
+end
+
+qq = mat2quat(quat2mat(q));
+e  = abs(dot(q,qq));
+
+if mean(e) < 0.9
+  hist(abs(e));
+  error('Error in matrix - quaternion conversion');
+else
+  disp('matrix <-> quaternion conversion is ok!')
+end
+  
