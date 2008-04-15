1d5219a63be4d36e3911287170a64ecf25324d63
Hielscher <ralfHielscher@users.noreply.github.com>
2008-04-15 12:38:47 +0000

    ralf hielscher: rewrite of plot function

--- geometry/@SO3Grid/plot.m
+++ geometry/@SO3Grid/plot.m
@@ -7,11 +7,16 @@ function plot(SO3G,varargin)
 %% Options
 %  RODRIGUEZ  - plot in rodriguez space
 
-if check_option(varargin,'RODRIGUEZ') || GridLength(SO3G) > 50
-	r = quat2rodriguez(SO3G.Grid);
+if check_option(varargin,'RODRIGUEZ') || sum(GridLength(SO3G)) > 50
+  
+  for i = 1:length(SO3G)
+    r = quat2rodriguez(SO3G(i).Grid);
 
-	scatter3(getx(r),gety(r),getz(r));
-
+    scatter3(getx(r),gety(r),getz(r)); 
+    hold all
+  end
+  hold off
+  
 else
 
   colororder = ['b','g','r','c','m','k','y'];
