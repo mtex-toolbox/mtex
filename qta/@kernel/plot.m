c2d6503700975da3b34658a8972ad65420e06aa4
Hielscher <ralfHielscher@users.noreply.github.com>
2008-04-15 12:39:09 +0000

    ralf hielscher: rewrite of plot function

--- qta/@kernel/plot.m
+++ qta/@kernel/plot.m
@@ -5,7 +5,6 @@ function plot(kk,options,varargin)
 %% Options
 %  K, RK, RADON, RRK, FOURIER, FOURIER_LOGLOG, EVEN, ODD, EVEN-ODD
 
-colororder = ['b','g','r','c','m','k','y'];
 charorder =  ['.','o','+','s','^','d','x'];
 
 if nargin == 1, options = 'K';end
@@ -18,12 +17,12 @@ for i = 1:length(kk)
   switch upper(options)
     case 'K'
 			omega = linspace(-pi,pi,1000);
-			optionplot(omega,kk(i).K(cos(omega/2)),varargin{:},'color',colororder(i),'LineWidth',2);
+			optionplot(omega,kk(i).K(cos(omega/2)),varargin{:},'LineWidth',2);
 			set(gcf,'Name',['kernel ',inputname(1),' on SO(3)']);
 			xlim([-pi,pi]);
 		case {'RK','RADON'}
 			omega = linspace(-pi,pi,1000);
-			optionplot(omega,max(0,kk(i).RK(cos(omega))),varargin{:},'color',colororder(i),'LineWidth',2);
+			optionplot(omega,max(0,kk(i).RK(cos(omega))),varargin{:},'LineWidth',2);
 			set(gcf,'Name',['Randon transformed kernel ',inputname(1),' on S^2']);
 			xlim([-pi,pi]);
 		case 'RRK'
@@ -41,10 +40,8 @@ for i = 1:length(kk)
       l = get_option(varargin,'bandwidth',l);
 			A = zeros(1,l);
 			A(1:min(l,length(kk(i).A))) = kk(i).A(1:min(l,length(kk(i).A)));
-      optionplot(0:length(A)-1,A./(2*(0:length(A)-1)+1),varargin{:},'marker',charorder(i),'color',colororder(i),'MarkerSize',10);
+      optionplot(0:length(A)-1,A./(2*(0:length(A)-1)+1),varargin{:},'marker',charorder(i),'MarkerSize',10);
       
-%      if check_option(varargin,'logarithmic'), A = log(A)/log(10);end
-%			plot(A,charorder(i),'color',colororder(i),'MarkerSize',10)
       set(gcf,'Name',['Fourier coefficients of the kernel ',inputname(1)]);
     case 'FOURIER_LOGLOG'
 			l = length(kk(1).A);
@@ -53,24 +50,24 @@ for i = 1:length(kk)
       end
 			A = zeros(1,l);
 			A(1:length(kk(i).A)) = kk(i).A;
-			loglog(abs(A./(2*(0:length(A)-1)+1)),charorder(i),'color',colororder(i),'MarkerSize',10);
+			loglog(abs(A./(2*(0:length(A)-1)+1)),charorder(i),'MarkerSize',10);
       set(gcf,'Name',['Fourier coefficients of the kernel ',inputname(1)]);
 		case 'EVEN'
 			omega = linspace(-pi,pi,1000);
-			optionplot(omega,GK(kk(i),cos(omega/2)),varargin{:},'color',colororder(i),'LineWidth',2);
+			optionplot(omega,GK(kk(i),cos(omega/2)),varargin{:},'LineWidth',2);
       set(gcf,'Name',['Even part of the kernel ',inputname(1)]);
 		case 'ODD'
 			omega = linspace(-pi,pi,1000);
-			optionplot(omega,UK(kk(i),cos(omega/2)),varargin{:},'color',colororder(i),'LineWidth',2);
+			optionplot(omega,UK(kk(i),cos(omega/2)),varargin{:},'LineWidth',2);
       set(gcf,'Name',['Odd part of the kernel ',inputname(1)]);
 		case 'EVEN_ODD'
 			omega = linspace(-pi,pi,1000);
-			optionplot(omega,GK(kk(i),cos(omega/2))-UK(kk(i),cos(omega/2)),varargin{:},'color',colororder(i));
+			optionplot(omega,GK(kk(i),cos(omega/2))-UK(kk(i),cos(omega/2)),varargin{:});
       set(gcf,'Name',['Difference between even and odd part of the kernel ',inputname(1)]);
     otherwise
       error('unknown option');
   end
-  hold on
+  hold all
 end
 
 if check_option(varargin,'Legend')
