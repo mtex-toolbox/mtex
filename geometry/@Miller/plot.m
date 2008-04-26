6552c6716e5dfd2e0b7e5ff88d3ec419b5be9f91
ralf.hielscher <ralf.hielscher@localhost>
2008-04-26 18:06:12 +0000

    Ralf Hielscher: minor changes

--- geometry/@Miller/plot.m
+++ geometry/@Miller/plot.m
@@ -8,32 +8,29 @@ function plot(m,varargin)
 %  ALL     - plot symmetrically equivalent directions
 %  NO_CHAR - no description
 
-
-%if any(strcmpi(get(gcf,'Tag'),{'IPDF'}))  
-%  hold on
-%else
-%  clf;
-%  set(gcf,'Name','Miller indece in a Schmidt net');  
-%end
-
+% store hold status
 washold = ishold;
 
 for i = 1:numel(m)
 
+  % all symmetrically equivalent?
   if check_option(varargin,'ALL')  
     mm = vec2Miller(symvec(m(i),varargin{:}),m(i).CS);
   else
     mm = m(i);
   end
   
+  % generate strings
   s = cell(1,numel(mm));
   for ii = 1:numel(mm)
     s{ii} = char(char(mm(ii),'latex'));
   end
   
+  % plot
   plot(S2Grid(vector3d(mm)),varargin{:},'data',s);
   
   hold all
 end
 
+% revert old hold status
 if ~washold, hold off;end
