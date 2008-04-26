5da41d8d64bd00b228efb7539041cc7c86221553
ralf.hielscher <ralf.hielscher@localhost>
2008-04-26 18:18:00 +0000

    Ralf Hielscher: added randsample

---
+++ tools/statistic_tools/mtexrandsample.m
@@ -0,0 +1,88 @@
+function y = mtexrandsample(n, k, replace, w)
+% mtexrandsample is a reimplementation of the MATLAB randsample function
+%
+
+error(nargchk(2, 4, nargin));
+
+if numel(n) == 1
+  population = [];
+else
+  population = n(:);
+  n = numel(population);
+end
+
+if nargin < 3, replace = false;end
+
+if nargin < 4
+  w = [];
+elseif ~isempty(w)
+  if length(w) ~= n
+    if isempty(population)
+      error('stats:randsample:InputSizeMismatch',...
+        'W must have length equal to N.');
+    else
+      error('stats:randsample:InputSizeMismatch',...
+        'W must have the same length as the population.');
+    end
+  else
+    p = w(:)' / sum(w);
+  end
+end
+
+switch replace
+  
+  % Sample with replacement
+  case {true, 'true', 1}
+    if isempty(w)
+      y = ceil(n .* rand(k,1));
+    else
+      [dum, y] = histc(rand(k,1),[0 cumsum(p)]);
+    end
+    
+    % Sample without replacement
+  case {false, 'false', 0}
+    if k > n
+      if isempty(population)
+        error('stats:randsample:SampleTooLarge',...
+          'K must be less than or equal to N for sampling without replacement.');
+      else
+        error('stats:randsample:SampleTooLarge',...
+          'K must be less than or equal to the population size.');
+      end
+    end
+    
+    if isempty(w)
+      % If the sample is a sizeable fraction of the population,
+      % just randomize the whole population (which involves a full
+      % sort of n random values), and take the first k.
+      if 4*k > n
+        rp = randperm(n);
+        y = rp(1:k);
+        
+        % If the sample is a small fraction of the population, a full sort
+        % is wasteful.  Repeatedly sample with replacement until there are
+        % k unique values.
+      else
+        x = zeros(1,n); % flags
+        sumx = 0;
+        while sumx < k
+          x(ceil(n * rand(1,k-sumx))) = 1; % sample w/replacement
+          sumx = sum(x); % count how many unique elements so far
+        end
+        y = find(x > 0);
+        y = y(randperm(k));
+      end
+    else
+      error('stats:randsample:NoWeighting',...
+        'Weighted sampling without replacement is not supported.');
+    end
+  otherwise
+    error('stats:randsample:BadReplaceValue',...
+      'REPLACE must be either true or false.');
+end
+
+if ~isempty(population)
+  y = population(y);
+else
+  y = y(:);
+end
