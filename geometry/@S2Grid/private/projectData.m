eb0b30b80d2f8f0fbf3c26bcdf46b92ed58d5f0e
Hielscher <ralfHielscher@users.noreply.github.com>
2008-04-28 21:16:26 +0000

    Ralf Hielscher: remember projection

--- geometry/@S2Grid/private/projectData.m
+++ geometry/@S2Grid/private/projectData.m
@@ -1,21 +1,39 @@
 function [X,Y,bounds] = projectData(theta,rho,varargin)
 
-% projection
-if check_option(varargin,'PLAIN')
-
-  X = rho; Y = theta; 
-    
-elseif check_option(varargin,'EDIST') % equal distance
-  
-  [X,Y] = stereographicProj(theta,rho);
-  bounds = [-2, -2, 4, 4];
-  
-else % equal area
-  
-  [X,Y] = SchmidtProj(theta,rho);
-  bounds = [-1.4142, -1.4142, 2*1.4142 , 2*1.4142];
-  
+% get projection
+if isappdata(gcf,'projection')  
+  projection = getappdata(gcf,'projection');
+else
+  projection = 'earea';
 end
+projection = get_option(varargin,'projection',projection);
+
+
+% project data
+switch lower(projection)
+  
+  case 'plain'
+
+    X = rho; Y = theta;
+    
+  case 'edist' % equal distance
+  
+    [X,Y] = stereographicProj(theta,rho);
+    bounds = [-2, -2, 4, 4];
+  
+  case 'earea' % equal area
+    
+    [X,Y] = SchmidtProj(theta,rho);
+    bounds = [-1.4142, -1.4142, 2*1.4142 , 2*1.4142];
+
+  otherwise
+    
+    error('Unknown Projection!')
+    
+end
+
+% store projection
+setappdata(gcf,'projection',projection);
 
 % bounding box 
 if check_option(varargin,{'plain','contour','contour','smooth'})
