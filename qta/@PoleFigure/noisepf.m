f094e4212684469b58b3ed8e7000e57893ca28cd
ralf.hielscher <ralf.hielscher@localhost>
2008-04-28 21:37:29 +0000

    Ralf Hielscher: minor changes

--- qta/@PoleFigure/noisepf.m
+++ qta/@PoleFigure/noisepf.m
@@ -19,10 +19,11 @@ function pdfn = noisepf(pdf,fak,background,varargin)
 % ODF/simulatePoleFigure
 
 if nargin == 2, background = 0;end
+if numel(fak) == 1, fak = repmat(fak,numel(pdf),1);end
 
 pdfn = pdf;
 for i = 1:length(pdf)
-    data = randp(fak*pdf(i).data + background) - background;
+    data = randp(fak(i)*pdf(i).data + background) - background;
 		if check_option(varargin,'NONNEGATIV')
 			data(data < 0) = 0;
 		end
