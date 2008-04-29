c15678e4ecc163b1db86be003982429ed4813537
ralf.hielscher <ralf.hielscher@localhost>
2008-04-29 06:02:21 +0000

    Ralf Hielscher: bug fix in text alignment

--- tools/plot_tools/smarttext.m
+++ tools/plot_tools/smarttext.m
@@ -4,20 +4,21 @@ function s = smarttext(x,y,s,box,varargin)
 arg{1} = 'HorizontalAlignment';
 arg{3} = 'VerticalAlignment';
 
-if box(1) + (box(2) - box(1))*2/3 < x
+mystop;
+if box(1) + (box(3) - box(1))*2/3 < x
   arg{2} = 'Right';
   x = x - 0.05;
-elseif box(1) + (box(2) - box(1))*1/3 < x
+elseif box(1) + (box(3) - box(1))*1/3 < x
   arg{2} = 'Center';
 else
   arg{2} = 'Left';
   x = x + 0.05;
 end
 
-if box(3) + (box(4) - box(3))*1/3 > y
+if box(2) + (box(4) - box(2))*1/3 > y
   arg{4} = 'Bottom';
   y = y + 0.05;
-elseif (box(3) + (box(4) - box(3))*2/3 > y) && ~strcmp(arg{2},'Center')
+elseif (box(2) + (box(4) - box(2))*2/3 > y) && ~strcmp(arg{2},'Center')
   arg{4} = 'Middle';
 else
   arg{4} = 'Top';
