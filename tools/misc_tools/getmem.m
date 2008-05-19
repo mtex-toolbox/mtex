f4c928c962c04adda5da6d0f60ab58b8dedf9ea3
ralf.hielscher <ralf.hielscher@localhost>
2008-05-19 05:20:19 +0000

    Ralf Hielscher: vista port

--- tools/misc_tools/getmem.m
+++ tools/misc_tools/getmem.m
@@ -1,6 +1,13 @@
 function m = getmem
 % return total system memory in kb
 
-[r,s] = system('free');
-
-m = sscanf(s(strfind(s,'Mem:')+5:end),'%d',1);
+try
+  m = memory;
+  m = m.MemAvailableAllArrays / 1024;
+catch %#ok<CTCH>
+  [r,s] = system('free');
+  m = sscanf(s(strfind(s,'Mem:')+5:end),'%d',1);
+  if isempty(m)
+    m = 300 * 1024;
+  end
+end
