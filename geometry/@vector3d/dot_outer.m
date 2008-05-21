a85f6a1fcd7b3e81ccc830e7ef2d0d8116ab4e06
ralf.hielscher <ralf.hielscher@localhost>
2008-05-21 10:55:53 +0000

    Ralf Hielscher: minor changes

--- geometry/@vector3d/dot_outer.m
+++ geometry/@vector3d/dot_outer.m
@@ -7,15 +7,12 @@ function d = dot_outer(v1,v2)
 
 if ~isempty(v1) && ~isempty(v2) 
 			
-	d = reshape(v1.x,[],1) * reshape(v2.x,1,[]) + ...
-		reshape(v1.y,[],1) * reshape(v2.y,1,[]) + ...
-		reshape(v1.z,[],1) * reshape(v2.z,1,[]);
+	d = v1.x(:) * v2.x(:).' + v1.y(:) * v2.y(:).' + v1.z(:) * v2.z(:).';
 
   % eliminate wrong values
   d(d>1) = 1;
   d(d<-1) = -1;
   
-	d = reshape(d,[numel(v1),numel(v2)]);
 else	
 	d  = [];	
 end
