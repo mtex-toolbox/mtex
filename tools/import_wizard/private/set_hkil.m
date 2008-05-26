d4d75a1d3cffcea999da90aafc7a740f2708905c
Hielscher <ralfHielscher@users.noreply.github.com>
2008-05-26 15:04:21 +0000

    new assert function

--- tools/import_wizard/private/set_hkil.m
+++ tools/import_wizard/private/set_hkil.m
@@ -7,7 +7,7 @@ k = str2num(get(handles.miller{2}, 'String')); %#ok<ST2NM>
 l = str2num(get(handles.miller{4}, 'String')); %#ok<ST2NM>
 c = str2num(get(handles.structur, 'String')); %#ok<ST2NM>
 
-assert(all([length(h),length(k),length(l)] == length(c)));
+mtex_assert(all([length(h),length(k),length(l)] == length(c)));
 
 appdata.pf(ip) = set(appdata.pf(ip),'h',Miller(h,k,l,getCS(appdata.pf)));
 appdata.pf(ip) = set(appdata.pf(ip),'c',c);
