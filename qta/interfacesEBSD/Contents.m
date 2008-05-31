0339fe215900b1276e2f721beea198ca1e9380a8
ralf.hielscher <ralf.hielscher@localhost>
2008-05-31 05:10:56 +0000

    more help

--- qta/interfacesEBSD/Contents.m
+++
@@ -1,6 +0,0 @@
-% 
-%
-% Files
-%   loadEBSD         - import ebsd data 
-%   loadEBSD_generic - load pole figure data from (alpha,beta,gamma) files
-%   loadEBSD_txt     - load pole figure data from (alpha,beta,gamma) files
