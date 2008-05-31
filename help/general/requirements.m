3cf3b6658fb79328a0fb4bbfbe3a061cd85dd53b
Hielscher <ralfHielscher@users.noreply.github.com>
2008-05-31 05:08:10 +0000

    more help

--- help/general/requirements.m
+++
@@ -1,8 +0,0 @@
-%% MTEX Requirenments
-%
-% in order to use the MTEX toolbox you need a system running 
-%
-% * one of the operating systems Linux, BSD or MAC OSX
-% * MATLAB version 6 or higher 
-% * the fast Fourier library <http://www.fftw.org FFTW 3> 
-% * the non equispaced fast Fourier library <http://www-user.tu-chemnitz.de/~potts/nfft/ NFFT 3>
