8ff245c3702d67a34d4a81a7ec10d16d27db1f28
ralf.hielscher <ralf.hielscher@localhost>
2008-06-01 21:10:45 +0000

    bug fix

--- qta/interfaces/loadPoleFigure_xrdml.m
+++ qta/interfaces/loadPoleFigure_xrdml.m
@@ -14,82 +14,95 @@ function pf = loadPoleFigure_xrdml(fname,varargin)
 % interfaces_index
 
 try
-  [fdir,fname,ext] = fileparts(fname);
-  mtex_assert(strcmpi(ext,{'.xml','.xrdml'}));
+  [fdir,fn,ext] = fileparts(fname);
+  mtex_assert(any(strcmpi(ext,{'.xml','.xrdml'})));
   doc = xmlread(fname);
 catch
   error('file not found or format xrdml does not match file %s',fname);
 end
 
-
 xRoot = doc.getDocumentElement;
 
 if strcmp(xRoot.getTagName,'xrdMeasurements')
-    %begin import
+  
+  %begin import
   nodelist = xRoot.getElementsByTagName('xrdMeasurement');
-  pf = [];
-    
-    %loop over measurements
+  
+  %loop over pole figures
   for i=0:nodelist.getLength-1
+    
     d=[];
     current_theta = [];
     current_rho = [];
     
-      %read current measurement
+    %read current measurement
     xrdMeasurement = nodelist.item(i);
     scan = xrdMeasurement.getElementsByTagName('scan');  
     
-      %read hkl from first entry
-    hkl_node = scan.item(0).getElementsByTagName('hkl').item(0).getChildNodes; hkl = ['hkl']; h_ =[];
-    for k=1:3, h_ = [h_, ...
-      str2num(hkl_node.getElementsByTagName(hkl(k)).item(0).getFirstChild.getNodeValue)];  end 
-    h = Miller(h_(1),h_(2),h_(3));
+    %read hkl from first entry
+    hkl_node = scan.item(0).getElementsByTagName('hkl').item(0).getChildNodes; 
+    hkl = 'hkl';
+    for k=1:3
+      h(k) = str2num(hkl_node.getElementsByTagName(...
+        hkl(k)).item(0).getFirstChild.getNodeValue);  
+    end
+    h = Miller(h(1),h(2),h(3));
     
     step_axis = xrdMeasurement.getAttribute('measurementStepAxis');
 
-      %loop over all scan-data sets
-    for i=0:scan.getLength-1      
-      val = str2num(scan.item(i).getElementsByTagName('intensities').item(0).getFirstChild.getNodeValue);
+    %loop over all scan-data sets
+    for ii=0:scan.getLength-1
+      
+      val = str2num(scan.item(ii).getElementsByTagName('intensities').item(0).getFirstChild.getNodeValue);
       d = [d,val];
-     
-        %get start and stop position
-      pos = scan.item(i).getElementsByTagName('positions');
-      rot_start = scan.item(i).getElementsByTagName('startPosition').item(0);
-      	%check units
+      
+      %get start and stop position
+      pos = scan.item(ii).getElementsByTagName('positions');
+      rot_start = scan.item(ii).getElementsByTagName('startPosition').item(0);
+      
+      %check units
       if strcmp(rot_start.getParentNode.getAttribute('unit'),'deg')
-        isrho_deg=degree; 
-      else, isrho_deg=1; end; 
-      start = str2num(rot_start.getFirstChild.getNodeValue);
-      stop = str2num(scan.item(i).getElementsByTagName('endPosition').item(0).getFirstChild.getNodeValue);
+        isrho_deg=degree;
+      else
+        isrho_deg=1;
+      end;
+      
+      start = isrho_deg * str2num(rot_start.getFirstChild.getNodeValue);
+      stop = isrho_deg * str2num(scan.item(ii).getElementsByTagName('endPosition').item(0).getFirstChild.getNodeValue);
      
-        %get theta
+      %get theta
       for k=0:pos.getLength-1
         if strcmp(pos.item(k).getAttribute('axis'), char(step_axis))
-            %check units
+          
+          %check units
           if strcmp(pos.item(k).getAttribute('unit'),'deg')
             istheta_deg=degree;
-          else, istheta_deg=1; end;   
-            %get theta
+          else
+            istheta_deg=1; 
+          end;
+          
+          %get theta
           theta = str2double(pos.item(k).getElementsByTagName('commonPosition').item(0).getFirstChild.getNodeValue);
           current_theta = [ current_theta; ...
-            ones(length(val),1)*istheta_deg*theta];
+            repmat(pi/2-istheta_deg*theta,length(val),1)];
         end
       end
       
-        %get rho, mode="Continuous"
-      step = abs(start-stop)/(length(val)-1);
-      for k=0:length(val)-1
-        current_rho = [ current_rho; (start+k*step)*isrho_deg ];
-      end      
+      %get rho, mode="Continuous"
+      current_rho = [current_rho,linspace(start,stop,length(val))];
+
     end
     
-      %setup sphere 
-    r = S2Grid(sph2vec(current_theta, current_rho),'hemisphere')
-      %add polefigure
-    pf = [pf,PoleFigure(h,r,d,symmetry('cubic'),symmetry,varargin{:})]
+    %setup sphere
+    r = S2Grid(sph2vec(current_theta(:), current_rho(:)),'reduced');
+    
+    %add polefigure
+    pf(i+1) = PoleFigure(h,r,d,symmetry('cubic'),symmetry,varargin{:});
+    
   end
+  
 else
-  error('format xrdml does not match file %s',fname);
+  error('Format XRDM does not match file %s.',fname);
 end
 
 
