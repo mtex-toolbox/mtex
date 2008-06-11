9075042b4fa94e4e452c34955fd5f937c0034af4
Hielscher <ralfHielscher@users.noreply.github.com>
2008-06-11 15:54:02 +0000

    better installation check

--- tools/misc_tools/check_installation.m
+++ tools/misc_tools/check_installation.m
@@ -4,16 +4,46 @@ global mtex_path;
 
 %% check for mex files
 try
-  S3G = SO3Grid(100,symmetry,symmetry);
-  dot_outer(S3G,idquaternion,'epsilon',pi/10);
+  fast_check_mex;
 catch
-  warning('%s\n%s','--MTEX-- Error using mex files!',...
-    'Recompile now mex files.');
-  opwd = pwd;
-  cd([mtex_path '/c/mex']);
-  mex_install;
-  cd(opwd);
-  disp('Done!')
+  warning('%s\n','--MTEX-- Error using mex files!');
+  disp('Trying now with older version.');
+  
+  try
+    rmpath([mtex_path '/c/mex']);
+    addpath([mtex_path '/c/mex/v7.1']);
+    fast_check_mex;    
+    disp('Hurray - everythink works!')
+    try
+      copyfile([mtex_path '/c/mex/v7.1/*.*'],[mtex_path '/c/mex'],'f');
+      rmpath([mtex_path '/c/mex/v7.1']);
+      addpath([mtex_path '/c/mex']);
+    catch
+      disp('Befor starting the next session copy the mex files from ');
+      disp([' ' mtex_path '/c/mex/v7.1']);
+      disp(' to ');
+      disp([' ', mtex_path '/c/mex']);
+    end    
+  catch
+    
+    warning('%s\n','--MTEX-- Error using mex files!');
+    disp('Recompile now mex files.');
+    rmpath([mtex_path '/c/mex/v7.1']);
+    addpath([mtex_path '/c/mex']);
+    opwd = pwd;
+    cd([mtex_path '/c/mex']);
+    mex_install;
+    cd(opwd);
+    
+    try
+      fast_check_mex;
+      disp('Hurray - everythink works!')
+    catch
+      warning('%s\n%s','--MTEX-- Error using mex files!',...
+      'Contact author for help!');
+    end
+
+  end
 end
 
 %% check for binaries
@@ -40,3 +70,9 @@ catch
     'Download the binaries that corresponds to your system, recompile the binaries yourself, or ask the maintainer!');
     
 end
+
+
+function fast_check_mex
+
+S3G = SO3Grid(100,symmetry,symmetry);
+dot_outer(S3G,idquaternion,'epsilon',pi/10);
