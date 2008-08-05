b42de46e401cd22c5fa1535d8c6e1ec707be0e0b
ralf.hielscher <ralf.hielscher@localhost>
2008-08-05 04:51:11 +0000

    some minor bugfixes in plotting

--- geometry/@vector3d/plot.m
+++ geometry/@vector3d/plot.m
@@ -11,10 +11,13 @@ if length(v) > 20 || check_option(varargin,'data')
   
 else
     
-  % convert to cell
-  s = cell(1,numel(v));
-  for i = 1:numel(v), s{i} = subsref(v,i); end  
+  if ~check_option(varargin,'data')
+    % convert to cell
+    s = cell(1,numel(v));
+    for i = 1:numel(v), s{i} = subsref(v,i); end
+    varargin = {'data',s,varargin{:}};    
+  end
   
-  plot(S2Grid(v),'data',s,varargin{:},'grid');
+  plot(S2Grid(v),varargin{:},'grid');
 
 end
