98c0b97c18d73cf556718846d15e32bdf0d463e0
Hielscher <ralfHielscher@users.noreply.github.com>
2008-10-07 07:56:06 +0000

    ralf hielscher: deleted some obsolete files

--- qta/@kernel/private/GWkern.m
+++
@@ -1,9 +0,0 @@
-function p=GWkern(kappa,l)
-% Legendre coefficients of the Gauss-Weierstrass kernel
-% up to order l with coefficient kappa
-
-p(1) = 1;
-for i=1:l
-    p(i+1) = (2*i+1)*exp(-i*(i+1)*kappa);
-end
-%p = p(1:sum(p > 1E-10));
--- qta/@kernel/private/MFkern.m
+++
@@ -1,9 +0,0 @@
-function m = MFkern(h,l)
-% Legendrekoeffizienten des von Mieses Fischer kerns bis
-% zur Entwicklungsstufe l zum Parameter h
-
-m(1) = 1;
-for i=1:l
-    m(i+1) = (besseli(i,h)-besseli(i+1,h))/(besseli(0,h)-besseli(1,h));
-end
-%m = m(1:length(find(m > 1E-10)));
