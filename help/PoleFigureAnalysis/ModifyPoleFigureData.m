4ca052182e1fc8d6c4d6fa3edac8428546aa5b26
ralf.hielscher <ralf.hielscher@localhost>
2008-11-10 22:38:01 +0000

    ralf hielscher: reorganize help

---
+++ help/PoleFigureAnalysis/ModifyPoleFigureData.m
@@ -0,0 +1,44 @@
+%% Modify Pole Figure Data
+% 
+% This section explains how one can manipulate pole figure data in MTEX.
+
+%% Import diffraction data
+%
+% Let us therefore import some data and plot them.
+
+% specify scrystal and specimen symmetry
+cs = symmetry('-3m',[1.4,1.4,1.5]);
+ss = symmetry('triclinic');
+
+% specify file names
+fname = {...
+  [mtexDataPath '/dubna/Q(10-10)_amp.cnv'],...
+  [mtexDataPath '/dubna/Q(10-11)(01-11)_amp.cnv'],...
+  [mtexDataPath '/dubna/Q(11-22)_amp.cnv']};
+
+% specify crystal directions
+h = {Miller(1,0,-1,0,cs),[Miller(0,1,-1,1,cs),Miller(1,0,-1,1,cs)],Miller(1,1,-2,2,cs)};
+
+% specify structure coefficients
+c = {1,[0.52 ,1.23],1};
+
+% import pole figure data
+pf = loadPoleFigure(fname,h,cs,ss,'superposition',c)
+
+% plot raw data
+figure('position',[359 450 749 249])
+plot(pf)
+
+
+%% Correct pole figures
+
+pf_corrected = delete(pf,getTheta(getr(pf)) >= 70*degree &...
+  getTheta(getr(pf)) <= 75*degree);
+
+plot(pf_corrected)
+
+%% Rotate pole figures
+
+pf_rotated = rotate(pf_corrected,axis2quat(xvector,45*degree));
+plot(pf_rotated,'reduced')
+
