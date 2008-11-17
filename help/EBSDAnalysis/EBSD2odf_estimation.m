b600947399f87242126fecf260532222a4ca957f
Hielscher <ralfHielscher@users.noreply.github.com>
2008-11-17 07:19:31 +0000

    ralf hielscher: help improvements

--- help/EBSDAnalysis/EBSD2odf_estimation.m
+++ help/EBSDAnalysis/EBSD2odf_estimation.m
@@ -1,21 +1,32 @@
 %% ODF Estimation from EBSD data
 %
-% 
-% MTEX allows to estimate ODFs from any kind of EBSD data.
+% This page describes how to use MTEX to estimate an ODF from single
+% orientation measurements.
 %  
 %
 %% Syntax
 %
 % Starting point of any ODF estimation from EBSD data is a 
 % <EBSD_index.html EBSD> object which is in general created by
-ebsd = loadEBSD(data_files) 
+%
+ebsd = loadEBSD([mtexDataPath,'/aachen_ebsd/85_829grad_07_09_06.txt'],...
+  symmetry('m-3m'),symmetry('-1'),...
+  'layout', [5 6 7 2], 'xy', [3 4],'Bunge',  'phase', 1);
+plot(ebsd)
+
 %%
 % See [[interfacesEBSD_index.html,EBSD interfaces]] for more infomations how to import
 % EBSD data and to create a EBSD object. ODF estimation from
 % a EBSD object is done by the function 
 % <EBSD_calcODF.html calcODF>. The most simplest
 % syntax is
+
 odf = calcODF(ebsd)
+
+%% 
+% You may want to plot some pole figures of the estimated ODF:
+
+plotpdf(odf,[Miller(1,0,0),Miller(1,1,0),Miller(1,1,1)],'reduced','silent','position',[10 10 600 200])
 
 %% Kernel Density Estimation
 %
@@ -41,11 +52,19 @@ odf = calcODF(ebsd)
 % great impact in the resulting ODF. In MTEX these parameters can be
 % specified as options to the function <EBSD_calcODF.html calcODF>.
 %
+% In order to change the default halfwidth of the kernel function psi the
+% option *halfwidth* has to be used, e.g.
+
+odf = calcODF(ebsd,'halfwidth',10*degree);
+plotpdf(odf,[Miller(1,0,0),Miller(1,1,0),Miller(1,1,1)],'reduced','silent')
+
 %% Estimation of Fourier Coefficients
 %
 % Once, a ODF has been estimated from EBSD data it is straight forward to
 % calculate Fourier coefficients. E.g. by
+
 Fourier(odf,'order',4)
+
 %%
 % However this is a biased estimator of the Fourier coefficents which
 % underestimates the true Fourier coefficients by a factor that
--- help/PoleFigureAnalysis/ModifyPoleFigureData.m
+++ help/PoleFigureAnalysis/ModifyPoleFigureData.m
@@ -1,40 +1,69 @@
 %% Modify Pole Figure Data
 % 
-% This section explains how one can manipulate pole figure data in MTEX.
+% This section explains how to manipulate pole figure data in MTEX.
 
 %% Import diffraction data
 %
 % Let us therefore import some data and plot them.
 
-% specify scrystal and specimen symmetry
-cs = symmetry('-3m',[1.4,1.4,1.5]);
-ss = symmetry('triclinic');
 
-% specify file names
-fname = {...
-  [mtexDataPath '/dubna/Q(10-10)_amp.cnv'],...
-  [mtexDataPath '/dubna/Q(10-11)(01-11)_amp.cnv'],...
-  [mtexDataPath '/dubna/Q(11-22)_amp.cnv']};
+% specify crystal and specimen symmetries
+cs = symmetry('m-3m');
+ss = symmetry('-1');
+
+% specify file name
+fname = [mtexDataPath '/geesthacht/ST42-104-110.dat'];
 
 % specify crystal directions
-h = {Miller(1,0,-1,0,cs),[Miller(0,1,-1,1,cs),Miller(1,0,-1,1,cs)],Miller(1,1,-2,2,cs)};
+h = { ...
+  Miller(1,0,4,cs), ...
+  Miller(1,0,4,cs), ...
+  Miller(1,1,0,cs), ...
+  Miller(1,1,0,cs), ...
+  };
 
-% specify structure coefficients
-c = {1,[0.52 ,1.23],1};
+% import the data
+pf = loadPoleFigure(fname,h,cs,ss);
 
-% import pole figure data
-pf = loadPoleFigure(fname,h,cs,ss,'superposition',c)
-
-% plot raw data
-figure('position',[359 450 749 249])
+% plot imported polefigure
 plot(pf)
 
-%% Correct Pole figure data
 
+%% Splitting and Reordering of Pole Figures
+% As we can see the first and the third pole figure complete pole figures
+% and the second and the fourth pole figure contain some values for
+% background correction. Let us therefore split the pole figures into
+% these two groups. 
+
+pf_complete = pf([1 3]);
+pf_background= pf([2 4]);
+
+%%
+% Actually it is possible to work with pole figures as with simple numbers.
+% E.g. it is possible to 
+
+
+%% Correct Pole figure data
+% In order to correct the pole figures we have use the command
+% <PoleFigure_correct.html correct>, which also allows for defocusing
+% correction.
+
+pf = correct(pf_complete,'background',pf_background);
+plot(pf)
+
+%% Modify certain pole figure values
+
+ 
 
 
 %% Remove certain measurements from the data
-%
+% A comon problem in pole figure analysis is to detect outiers and to
+% remove them from the data. In order to demonstrate this procedure in MTEX
+% let us first add outlier to the data.
+
+
+
+
 
 % get theta angle
 theta = get(pf,'theta');
@@ -45,7 +74,17 @@ pf_corrected = delete(pf,theta > 69*degree & theta < 76*degree);
 plot(pf_corrected)
 
 %% Rotate pole figures
+% Sometimes it is neccasary to rotate the pole figures. In order to do this
+% with MTEX on has first to define a rotation, e.e. by
 
-pf_rotated = rotate(pf_corrected,axis2quat(xvector,45*degree));
+% This defines a rotation around the x-axis about 100 degree
+q = axis2quat(xvector,100*degree);
+
+%%
+% Second, the command <PoleFigure_rotate rotate> can be used to rotate the
+% pole figure data.
+pf_rotated = rotate(pf,q);
 plot(pf_rotated,'reduced')
+
+
 
