fcbfd5caf4f24a74f30e7b1ba4946be281475f1d
ralf.hielscher <ralf.hielscher@localhost>
2009-02-23 05:38:04 +0000

    ralf hielscher: better ebsd coloring

---
+++ qta/@EBSD/private/euler2rgb.m
@@ -0,0 +1,18 @@
+function c = euler2rgb(S3G,varargin)
+% converts orientations to rgb values
+
+% get reference orientation
+q0 = get_option(varargin,'center',idquaternion);
+S3G = S3G * inverse(q0);
+
+% restrict to fundamental region
+q = quaternion(getFundamentalRegion(S3G));
+
+% convert to euler angles angles
+[phi1,Phi,phi2] = quat2euler(q(:),'Bunge');
+
+phi1 = mod(-phi1,pi/2) *2 ./ pi;
+Phi = mod(-Phi,pi/2); Phi = Phi./max(Phi(:));
+phi2 = mod(-phi2,pi/2)*2 ./ pi;
+
+c = [phi1(:),Phi(:),phi2(:)];
