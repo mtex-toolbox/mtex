f950e62752ad4c8a3ce7a3e2ac519e324a75232a
Hielscher <ralfHielscher@users.noreply.github.com>
2009-02-23 05:38:32 +0000

    ralf hielscher: minor changes

--- tests/check_volume.m
+++ tests/check_volume.m
@@ -1,14 +1,23 @@
+%%
+
 cs = symmetry('m-3m');
 ss = symmetry('mmm');
 
+odf = unimodalODF(idquaternion,cs,ss,'halfwidth',5*degree);
+
 r = linspace(0,60*degree,20);
 for i = 1:length(r)
-  v(i,1) = volume(uniformODF(cs,ss),idquaternion,r(i)); disp(v(i))
+  v(i,1) = volume(odf,idquaternion,r(i)); disp(v(i))
+  v(i,2) = volume(uniformODF(cs,ss),idquaternion,r(i)); disp(v(i))
 end
 
-v(:,2) = length(cs)*(r - sin(r))./pi;
+v(:,3) = length(cs)*(r - sin(r))./pi;
 
 plot(r/degree,v);
+
+%%
+
+
 
 %r = linspace(0,100*degree,20);
 %for i = 1:length(r)
