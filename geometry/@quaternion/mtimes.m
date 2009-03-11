cd24d26e6167bc3f3067f3c42772e3825120a6bf
Hielscher <ralfHielscher@users.noreply.github.com>
2009-03-11 23:45:11 +0000

    ralf hielscher: minor change

--- geometry/@quaternion/mtimes.m
+++ geometry/@quaternion/mtimes.m
@@ -18,18 +18,7 @@ if isa(q1,'quaternion') && isa(q2,'quaternion')
 elseif isa(q1,'quaternion') && isa(q2,'vector3d')
 	
 	[x,y,z] = double(q2);
-  
-  
-  [nx,ny,nz] = quaternion_mtimes(q1.a,q1.b,q1.c,q1.d,x,y,z);
-% 	n = q1.b.^2 + q1.c.^2 + q1.d.^2;
-% 	s = q1.b*x + q1.c*y + q1.d*z;
-% 	l = length(q1.a);
-% 	a = spdiags(q1.a,0,l,l);
-%     
-% 	nx = 2*a * (q1.c * z - q1.d * y) + 2*spdiags(q1.b,0,l,l)*s + (q1.a.^2 - n) * x;
-% 	ny = 2*a * (q1.d * x - q1.b * z) + 2*spdiags(q1.c,0,l,l)*s + (q1.a.^2 - n) * y;
-% 	nz = 2*a * (q1.b * y - q1.c * x) + 2*spdiags(q1.d,0,l,l)*s + (q1.a.^2 - n) * z;
-	
+  [nx,ny,nz] = quaternion_mtimes(q1.a(:),q1.b(:),q1.c(:),q1.d(:),x(:).',y(:).',z(:).');
 	q = vector3d(nx,ny,nz);
 	
 elseif isa(q1,'quaternion') && isa(q2,'double') % einfache Zahl   
