9abcef26c32ca7de4deb76c10437d3f0166347ea
Hielscher <ralfHielscher@users.noreply.github.com>
2009-04-02 08:03:18 +0000

    ralf hielscher: improved string2miller

--- qta/interfaces/tools/string2Miller.m
+++ qta/interfaces/tools/string2Miller.m
@@ -7,12 +7,30 @@ m = Miller(1,0,0);
 % extract filename
 s = s(max([1,1+strfind(s,'/')]):end);
 
-% extract indice
-s = regexp(s,'( ?-?\d){3,4}','match');
-r = ~isempty(s);
+% first try to extract 4 Miller indice
+e = regexp(fliplr(s),'([0123456]-? ?){4}','match');
+r = ~isempty(e);
 
-for i = 1:length(s)
-  ss = regexp(char(s{i}),'-?\d','match');
+ok = 1;
+for i = 1:length(e)
+  ss = regexp(fliplr(char(e{i})),'-?\d','match');
   ss = cellfun(@(x) str2double(x),ss,'UniformOutput',0);
-  m(i) = Miller(ss{:});
+  [m(i),er] = Miller(ss{:});
+  ok = ok & ~er;
 end
+m = fliplr(m);
+if ~isempty(e) && ok, return;end
+
+% if this fails try to extract only 3 Miller indice
+
+  e = regexp(fliplr(s),'([0123456] ?){3}','match');
+  r = ~isempty(e);
+
+  for i = 1:length(e)
+    ss = regexp(fliplr(char(e{i})),'-?\d','match');
+    ss = cellfun(@(x) str2double(x),ss,'UniformOutput',0);
+    m(i) = Miller(ss{:});
+  end
+end
+
+
