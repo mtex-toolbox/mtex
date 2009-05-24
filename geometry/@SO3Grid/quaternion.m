77f7e00c1b4a6bba6494f8f4d5c47b4497211ca0
Hielscher <ralfHielscher@users.noreply.github.com>
2009-05-24 19:32:20 +0000

    Florian Bachmann: bug fixes and one new function in SO3Grid

--- geometry/@SO3Grid/quaternion.m
+++ geometry/@SO3Grid/quaternion.m
@@ -12,8 +12,8 @@ function N = quaternion(SO3G,i,j)
 if nargin == 1  
   for i = 1:length(SO3G)
     SO3G(i).Grid = reshape(SO3G(i).Grid * SO3G(i).center,1,[]);
-    N = [SO3G.Grid];
   end
+  N = [SO3G.Grid];
 elseif nargin == 2
   N = reshape(SO3G.Grid(i)*SO3G.center,size(i));
 else
