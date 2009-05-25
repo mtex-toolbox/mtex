492aa50966830896e9607bbe88743a7f1de5f1ef
ralf.hielscher <ralf.hielscher@localhost>
2009-05-25 05:25:34 +0000

    Florian Bachmann: performance improvement

--- geometry/@S1Grid/GridLength.m
+++ geometry/@S1Grid/GridLength.m
@@ -1,10 +1,4 @@
 function l = GridLength(G)
 % return number of points
 
-l = zeros(1,length(G));
-% todo -> make it faster
-for i = 1:length(G)
-	l(i) = numel(G(i).points);
-end
-%l = arrayfun(@(x) numel(x.points),struct(G));
-%l = reshape(l,1,[]);
+l = cellfun('prodofsize',{G.points});
