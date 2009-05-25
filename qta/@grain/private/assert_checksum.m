11b0ae8c8493e3921262a75adb907ce09fae454c
ralf.hielscher <ralf.hielscher@localhost>
2009-05-25 06:43:23 +0000

    Florian Bachmann: grain support merged

---
+++ qta/@grain/private/assert_checksum.m
@@ -0,0 +1,22 @@
+function [members_ebsd members_grains ids] = assert_checksum(grains,ebsd)
+
+
+
+checksum = (unique([grains.checksum]));  
+checksums = strcat('grain_id', cellstr(dec2hex(checksum)));
+
+if length(checksums) > 1  
+	error('operation with grain compositions not supported');
+elseif any(strcmpi(checksums, fields(struct(ebsd).options)))
+  ids = get(ebsd,checksums{:});
+  gids = [grains.id];
+ 	members_ebsd = ismember(ids,gids);
+  if nargout > 1
+    ids = ids(members_ebsd);
+    members_grains = ismember(gids, ids);
+  end
+  return
+else
+  error(['assure that you are operating '...
+      'with the correct grains corresponding to your ebsd data']);
+end
