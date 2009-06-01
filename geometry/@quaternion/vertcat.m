a25f8ea234fc961dc2a46459c79a898835ed9d21
fl.bachmann <famb@users.noreply.github.com>
2009-06-01 11:04:03 +0000

    quaternion concatenation speedup

--- geometry/@quaternion/vertcat.m
+++ geometry/@quaternion/vertcat.m
@@ -1,11 +1,9 @@
 function q = vertcat(varargin)
 % implements [q1;q2;q3..]
 
-
-q = varargin{1};
-
-q.a = cellfun(@(q) q.a.',varargin).';
-q.b = cellfun(@(q) q.b.',varargin).';
-q.c = cellfun(@(q) q.c.',varargin).';
-q.d = cellfun(@(q) q.d.',varargin).';
-
+q = quaternion;
+qs = cat(1,varargin{:});
+q.a = vertcat(qs.a);
+q.b = vertcat(qs.b);
+q.c = vertcat(qs.c);
+q.d = vertcat(qs.d);
