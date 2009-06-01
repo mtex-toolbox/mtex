1c976fab2a93c87540e8d2eb328ac4d5f10f70e2
fl.bachmann <fl.bachmann@localhost>
2009-06-01 11:20:52 +0000

    fix of quaternion\times.m error in r581

--- geometry/@quaternion/times.m
+++ geometry/@quaternion/times.m
@@ -17,7 +17,7 @@ if isa(q1,'quaternion') && isa(q2,'quaternion')
      c = q1.a .* q2.c + q1.c .* q2.a + q1.d .* q2.b - q2.d .* q1.b;
      d = q1.a .* q2.d + q1.d .* q2.a + q1.b .* q2.c - q2.b .* q1.c;
      q = q1;
-     q1.a = a; q1.b = b; q1.c = c; q1.d = d;
+     q.a = a; q.b = b; q.c = c; q.d = d;
 
 elseif isa(q1,'quaternion') && isa(q2,'double')
     q = quaternion(q1.a .* q2,q1.b .* q2,q1.c .* q2,q1.d .* q2);
