f265b7a0fbbba62c28d13896925145b282d45e1a
fl.bachmann <famb@users.noreply.github.com>
2009-06-03 11:11:34 +0000

    rodrigues colorcoding

--- tools/plot_tools/orientation2color.m
+++ tools/plot_tools/orientation2color.m
@@ -6,7 +6,13 @@ function d = orientation2color(grid,coloring,varargin)
 % type   - as string: BUNGE, ANGLE, SIGMA, IHS, IPDF
 %
 
-if isa(grid,'SO3Grid')
+model = {'bunge','angle','sigma','ihs','ipdf',...
+    'rodrigues','rodriguesquat','rodriguesinverse'};
+
+if nargin == 0
+  d = model;
+  return;
+elseif isa(grid,'SO3Grid')
   cs = get(grid,'CS');
   ss = get(grid,'SS');
 else
@@ -16,16 +22,27 @@ else
 end
 
 switch coloring
-  case 'bunge'
+  case model(1)
     d = euler2rgb(grid,varargin{:});
-  case 'angle'
+  case model(2)
     d = quat2rgb(grid,cs,varargin{:});
-  case 'sigma'
+  case model(3)
     d = sigma2rgb(grid,varargin{:});
-  case 'ihs'
+  case model(4)
     d = euler2rgb(grid,cs,ss,varargin{:});
     d = rgb2hsv(d);
-  case 'ipdf'     % colorcoding according according to ipdf    
+  case model(5) % colorcoding according according to ipdf    
     h = quat2ipdf(grid,varargin{:});
+    d = ipdf2rgb(h,cs,varargin{:});
+  case model(6:8)
+    switch coloring
+      case model(6)  
+        q = getFundamentalRegion(grid);
+      case model(7)
+        q = quaternion(grid);
+      case model(8)
+        q = inverse(getFundamentalRegion(grid));
+    end
+    h = quat2rodrigues(q);
     d = ipdf2rgb(h,cs,varargin{:});
 end
