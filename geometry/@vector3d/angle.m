160b7ac76325438d177e6ea555ac4213dd420cbc
ralf.hielscher <ralf.hielscher@localhost>
2009-06-06 05:06:10 +0000

    make angle more robust - no more imaginary results

--- geometry/@vector3d/angle.m
+++ geometry/@vector3d/angle.m
@@ -10,7 +10,9 @@ function a = angle(v1,v2,varargin)
 %  axial  - include [[AxialDirectional.html,antipodal symmetry]]
 
 if check_option(varargin,'axial')
-  a = acos(abs(dot(v1./norm(v1),v2./norm(v2))));
+  a = abs(dot(v1./norm(v1),v2./norm(v2)));
 else
-  a = acos(dot(v1./norm(v1),v2./norm(v2)));
+  a = dot(v1./norm(v1),v2./norm(v2));
 end
+
+a = acos(max(-1,min(1,a)));
