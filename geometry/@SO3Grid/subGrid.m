ebbcf2f1e5707a3a8bbc2849814622edea984976
fl.bachmann <famb@users.noreply.github.com>
2009-06-10 23:26:14 +0000

    subgrid errata? wrong indexing

--- geometry/@SO3Grid/subGrid.m
+++ geometry/@SO3Grid/subGrid.m
@@ -16,7 +16,7 @@ function G = subGrid(G,q,epsilon,varargin)
 
 if nargin >= 3
   ind = find(G,q,epsilon,varargin{:});
-  ind = any(ind,2);
+  %ind = any(ind,2); reshape?
 elseif islogical(q) 
   ind = q;
 else
