d0e3dcc160300eeecb8d2c0cc65f5d413ebbbbf9
Hielscher <ralfHielscher@users.noreply.github.com>
2009-06-19 15:14:22 +0000

    updated TODO list for MTEX 2.0

--- help/general/TODO.m
+++ help/general/TODO.m
@@ -14,6 +14,10 @@
 % * document new functionality
 % * work on compatibility issues, i.e. make an directory that will be added
 % to path when necassary
+% * m-file generated from import wizard should contain also some sceme what
+% todo next, i.e. append something like [grains ebsd] = segment2s(ebsd);
+% grains = copyproperty(grains,ebsd,'all')
+% * say explicetly in generic wizard which columns has to be specified
 %
 %% Sudent projects
 %
