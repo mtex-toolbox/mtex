c187128818f9c695099a0e05fba52797137c6b75
fl.bachmann <fl.bachmann@localhost>
2009-06-22 16:23:01 +0000

    enhanced grain generation

--- qta/@grain/private/setproperty.m
+++
@@ -1,9 +0,0 @@
-function gr = setproperty(gr,vname,prop)
-
-if numel(prop) == numel(gr)
-  for k=1:numel(gr)
-    gr(k).properties.(vname) = prop(k);
-  end
-else
-  error('dimension missmatch')
-end
