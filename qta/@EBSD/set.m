c83752a1a9ed498602cfaa9ec20a33e8bdd9b1da
Hielscher <ralfHielscher@users.noreply.github.com>
2009-06-26 18:36:17 +0000

    bug fix

--- qta/@EBSD/set.m
+++ qta/@EBSD/set.m
@@ -6,8 +6,10 @@ if any(strcmp(vname,fields(obj)))
   for i = 1:numel(obj)
 
     % is value is a cell spread it over all elements of obj
-    if iscell(value)
+    if iscell(value) && length(value) == length(obj)
       ivalue = value{i};
+    elseif iscell(value)
+      ivalue = value{1};
     else
       ivalue = value;
     end
