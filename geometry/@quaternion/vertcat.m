dc65e280fa1f0e4ae673525799aa2c37e2e419f4
fl.bachmann <fl.bachmann@localhost>
2009-06-28 14:35:36 +0000

    speedup

--- geometry/@quaternion/vertcat.m
+++ geometry/@quaternion/vertcat.m
@@ -1,8 +1,12 @@
 function q = vertcat(varargin)
 % implements [q1;q2;q3..]
 
-q = quaternion;
-qs = cat(1,varargin{:});
+qs = repmat(struct(quaternion),size(varargin));
+for k=1:numel(varargin)
+  qs(k) = struct(varargin{k});
+end
+
+q = varargin{1};
 q.a = vertcat(qs.a);
 q.b = vertcat(qs.b);
 q.c = vertcat(qs.c);
--- qta/@grain/misorientation.m
+++ qta/@grain/misorientation.m
@@ -20,13 +20,18 @@ function ebsd = misorientation(grains,varargin)
 phase = get(grains,'phase');
 phu = unique(phase);
 
+ebsd = repmat(EBSD,size(phu));
+
+if check_option(varargin,'weighted'), doweights = true; else doweights = false; end
+
 % for each phase
 for k=1:length(phu)
   indp = phase == phu(k);
   gr = grains(indp); 
-  asr = grainsize(gr);
-  tot = sum(asr);
-  
+  if doweights,
+    asr = grainsize(gr);  tot = sum(asr); 
+  end
+   
   mean = get(gr,'mean');
   
   pCS = get(gr(1),'CS');
@@ -37,8 +42,14 @@ for k=1:length(phu)
   
   grain_ids = [gr.id];
   cod(grain_ids) = 1:length(grain_ids);  
+ 
   
-  ix = rep(grain_ids, cellfun('length',{gr.neighbour}));
+  s = [ 0 cumsum(cellfun('length',{gr.neighbour}))];
+	ix = zeros(1,s(end));
+  for l = 1:length(grain_ids)
+      ix(s(l)+1:s(l+1)) = grain_ids(l);
+  end  
+%   ix = rep(grain_ids, cellfun('length',{gr.neighbour}));
   iy = vertcat(gr.neighbour);
 
   msz = max(max(ix),max(iy));
@@ -46,26 +57,30 @@ for k=1:length(phu)
   
   qall = cell(1,length(gr));
   weight = cell(1,length(gr));
+  
+  qsym = symmetriceQuat(pCS,pSS, mean).';
+    
+  s1 = size(qsym,1);  
+  cndx = [0 cumsum(repmat(s1,1,max(sum(T1,2))))];
+  
   for l=1:length(gr)
     sel = cod(T1(:,grain_ids(l)));
         
     if ~isempty(sel)
-      sym_q = symmetriceQuat(pCS,pSS, mean(sel));
-      [omega ndx] = min(2*acos(abs(dot(sym_q,mean(l)))),[],2);
-      sz = size(sym_q);
-      qfinal = sym_q( full(  ...
-        sparse(1:length(ndx), ndx' ,true, sz(1), sz(2))) );
-            
-      qfinal =  mean(l).*inverse(qfinal); 
-      %? qfinal =  qfinal .*inverse( mean(l) ); 
-      qall{l} = qfinal;        
+%       sym_q = symmetriceQuat(pCS,pSS, mean(sel));      
+      sym_q = qsym(:,sel);
+      cen_q = mean(l);
+      [omega ndx] = min(2*acos(abs(dot(sym_q,cen_q))),[],1);
+      s2 = numel(sel);
+           
+      qall{l} = cen_q .* sym_q(ndx + cndx(1:s2))'; 
       
-      weight{l} = (asr(l)+asr(sel))./tot;
+      if doweights, weight{l} = (asr(l)+asr(sel))./tot; end
     end  
   end  
-  g = vertcat(qall{:});
-  
-  if check_option(varargin,'weighted')
+  g = [qall{~cellfun('isempty',qall)}];
+
+  if doweights
     p.weight = [weight{:}]';
   else
     p = struct;
@@ -73,7 +88,5 @@ for k=1:length(phu)
   
   if ~isempty(g)
     ebsd(k) = EBSD(g,pCS,pSS,'options',p,'comment',['grain misorientation ' grains(1).comment]);
-  else
-    ebsd(k) = EBSD;
   end
 end
