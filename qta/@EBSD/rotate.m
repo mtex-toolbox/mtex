9e1a4f497d5abc185451486d1696ef24ab34bb80
Hielscher <ralfHielscher@users.noreply.github.com>
2009-06-29 06:09:22 +0000

    work on rotate ebsd, updated TODO list

--- qta/@EBSD/rotate.m
+++ qta/@EBSD/rotate.m
@@ -14,4 +14,13 @@ end
 
 for i = 1:length(ebsd) 
   ebsd(i).orientations = q * ebsd(i).orientations;
+  
+  if rotaxis(q) == zvector    
+    ebsd(i).xy = (rotz(rotangle(q)) * ebsd(i).xy.').';
+  end
+  
 end
+
+function A = rotz(omega)
+
+A = [[cos(omega) sin(-omega)];[sin(omega) cos(omega)]];
