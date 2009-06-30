ca9a946018c45bb9d5cb83c0c95566ac13e40759
fl.bachmann <famb@users.noreply.github.com>
2009-06-30 16:30:36 +0000

    delete S2grid fix

--- geometry/@S2Grid/delete.m
+++ geometry/@S2Grid/delete.m
@@ -2,26 +2,28 @@ function S2G = delete(S2G,points)
 % elilinates points from grid
 %% Input
 %  S2G    - @S2Grid
-%  points - double | @vector3d
+%  points - logical | double | @vector3d
 %
 %% Output
 %  S2G    - @S2Grid
 
-if isa(points,'logical'), points = find(points);end
-if isa(points,'S2Grid'), points = vector3d(points);end
-if isa(points,'vector3d'), points = find(S2G,points);end
+if isa(points,'S2Grid'), points = vector3d(points); end
+if isa(points,'vector3d'), points = find(S2G,points); end
+if isnumeric(points), 
+  inds = false(sum(GridLength(S2G)),1);
+  inds(points) = true;
+  points = inds; 
+end
 
 S2G.Grid(points) = [];
 
 if check_option(S2G.options,'INDEXED')
 
+  cs = [0 cumsum(GridLength(S2G.rho))];
   % update rho indexing
   for ith = 1:GridLength(S2G.theta)
-    
-    irh = points>0 & points <=GridLength(S2G.rho(ith));
-    S2G.rho(ith) = delete(S2G.rho(ith),points(irh));    
-    
-    points = points - GridLength(S2G.rho(ith));
+    irh = points(cs(ith)+1:cs(ith+1));
+    S2G.rho(ith) = delete(S2G.rho(ith),irh);    
   end
   
   % update theta indexing
