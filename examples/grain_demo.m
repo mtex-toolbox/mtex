c78be03d84bb25e6e61d0656832dcc1138f04812
fl.bachmann <fl.bachmann@localhost>
2009-08-17 10:01:12 +0000

    grain demo update

--- examples/grain_demo.m
+++ examples/grain_demo.m
@@ -8,10 +8,9 @@
 %% Specify Crystal and Specimen Symmetry
 
 % specify crystal and specimen symmetry
-CS = [...
-  symmetry('m-3m'),...
+CS = {...
   symmetry('m-3m'),... % crystal symmetry phase 1
-  symmetry('m-3m')];   % crystal symmetry phase 2
+  symmetry('m-3m')};   % crystal symmetry phase 2
 SS = symmetry('-1');   % specimen symmetry
 
 %% Import ebsd data
@@ -19,84 +18,77 @@ SS = symmetry('-1');   % specimen symmetry
 fname = [mtexDataPath '/aachen_ebsd/85_829grad_07_09_06.txt'];
 
 ebsd = loadEBSD(fname,CS,SS,'interface','generic',...
-  'ColumnNames', { 'Phase' 'x' 'y' 'Euler 1' 'Euler 2' 'Euler 3'},...
-  'Columns', [2 3 4 5 6 7],...
-  'Bunge','ignorePhase',[]);
+  'ColumnNames', { 'Phase' 'x' 'y' 'Euler 1' 'Euler 2' 'Euler 3' 'MAD' 'BC'},...
+  'Columns', [2 3 4 5 6 7 8 9],'Bunge');
 
 plotx2east
 
 %% Plot Spatial Data
 
-figure('color','white'), hold on, plot(ebsd,'phase',1,'colorcoding','ihs')
+plot(ebsd,'phase',1,'colorcoding','ihs')
               
 %% Segmentation
 
-[grains ebsd] = segment2d(ebsd,'angle',12.5*degree,'augmentation','cubei')
+[grains ebsd] = segment2d(ebsd)
 
+%%
 % we can see which segmentations are stored in the ebsd object indicated by
 % grain_idXXXXXXXX as option, so it is possible to treat several
 % different segmentations with one ebsd object
 
 [grains5 ebsd] = segment2d(ebsd,'angle',5*degree,'augmentation','cube')
 
+%% Plot high/low-angle Grainboundary
 
-%% Plot Grains and Subfractions
-
-figure('color','white'), hold on, plot(grains,'color',[0.25 0.1 0.5])
+plot(grains,'color',[0.25 0.1 0.5])
 hold on, plotsubfractions(grains,'color','red','linewidth',2)
 
 %% Select grains after their EBSD phase
-% The Segmentation is connected with the EBSD object, 
+% The Segmentation is connected with the EBSD object, we can select grains
+% after specified ebsd measurements
 
-grains_ph0 = get(grains,ebsd(1));
-grains_ph1 = get(grains,ebsd(2));
-grains_ph2 = get(grains,ebsd(3));
+grains_ph1 = get(grains,ebsd(1));
+grains_ph2 = get(grains,ebsd(2));
 
 %%
-% Some histogram for grainsizes
+% on application of this would be to take a look on the grainsize
+% distribution
 
 % make a expotential bin size
 x = fix(exp(1:0.5:log(max(grainsize(grains)))));
   %also try area(grains), hullarea(grains)
 
 % the histograms
+y1 = hist(grainsize(grains_ph1),x)';
+y2 = hist(grainsize(grains_ph2),x)';
 
-y1 = hist(grainsize(grains_ph0),x)';
-y2 = hist(grainsize(grains_ph1),x)';
-y3 = hist(grainsize(grains_ph2),x)';
-
-%%
 % and the histogram for the segmentation of 5 degrees
-
 y5_1 = hist(grainsize( get(grains5,ebsd(1)) ),x)';
 y5_2 = hist(grainsize( get(grains5,ebsd(2)) ),x)';
-y5_3 = hist(grainsize( get(grains5,ebsd(3)) ),x)';
 
 %%
 % print a table
 
 disp(' ')
-head  = ' <= x <   phase0       | phase1      | phase2        ';
-head2 = '           12.5°    5° |  12.5°   5° |  12.5°   5°   ';
-disp(head); disp(head2);
+disp(' <= x <   phase1       | phase2      ');
+disp('           12.5°    5° |  12.5°   5° ');
+disp('-------------------------------------')
 
-disp(repmat('-',1,length(head)));
-disp(num2str([ x' y1,y5_1, y2, y5_2, y3, y5_3 ],' %10d %11d %6d|%6d %6d|%6d %6d'))
+disp(num2str([ x' y1,y5_1, y2, y5_2 ],' %10d %11d %6d|%6d %6d'))
 
 %%
-% and as barplot
+% and more visual as barplot
 
-figure('color','white'), bar(1:length(x),[y1, y2, y3]);
+figure, bar(1:length(x),[y1,y5_1, y2,y5_2]);
 set(gca,'YScale','log')
 set(gca,'XTickLabel',mat2cell(x',ones(size(x))))
-legend('phase 0','phase 1','phase 2')
+legend('phase 1 : 15°','phase 1 : 5°','phase 2 : 15°','phase 2 : 5°')
 
 %% Compare phases in scatter plots
 
 % plot grain against shapefactor
 
-figure('color','white','Position',[15 50 1200 350]), subplot(1,3,1)
-semilogx(area(grains_ph0),shapefactor(grains_ph0),'r.');
+figure('Position',[15 50 1200 350]), subplot(1,3,1)
 hold on,semilogx(area(grains_ph1),shapefactor(grains_ph1),'b.');
 hold on,semilogx(area(grains_ph2),shapefactor(grains_ph2),'g.');
 xlabel('area'); ylabel('shapefactor');
@@ -106,7 +98,6 @@ axis tight
 % plot perimeter area against aspect ratio
 
 subplot(1,3,2)
-semilogx(perimeter(grains_ph0),aspectratio(grains_ph0),'r.');
 hold on, semilogx(perimeter(grains_ph1),aspectratio(grains_ph1),'b.');
 hold on, semilogx(perimeter(grains_ph2),aspectratio(grains_ph2),'g.');
 xlabel('perimeter'); ylabel('aspect ratio');
@@ -116,7 +107,6 @@ axis tight
 % plot paris area against deltaarea
 
 subplot(1,3,3)
-semilogx(paris(grains_ph0),deltaarea(grains_ph0),'r.');
 hold on, semilogx(paris(grains_ph1),deltaarea(grains_ph1),'b.');
 hold on, semilogx(paris(grains_ph2),deltaarea(grains_ph2),'g.');
 xlabel('paris'); ylabel('deltaarea');
@@ -124,71 +114,66 @@ grid on
 axis tight
 
 %%
-% phase 0 as red, phase 1 blue, phase 2 green
+% phase 1 blue, phase 2 green
 
 %% Select Grains and its EBSD data by other properties
+% select grains with low-angle boundaries
 
-  %select grains with subfractions
-grains_fractions = grains( hassubfraction(grains) );
-  %and its corresponding ebsd data
-ebsd_fractions = get(ebsd, grains_fractions)
+grains_fractions = grains( hassubfraction(grains) )
 
 %%
-figure('color','white'), hold all
+% and its corresponding ebsd data
+
+ebsd_fractions = get(ebsd, grains_fractions)
+
+%% Plotting of grains
+% there are many ways to plot grains
+
+figure, hold all
 plot(ebsd_fractions,'colorcoding','ihs')
 plot(grains_fractions,'color','black','linewidth',1)
   % however the holes of a grain are plotted by default in an other color
 plot(grains_fractions,'b','noholes','linewidth',1.5)
-  % and now the fractions 
+  % and now the low-angle-boundary 
 plotsubfractions(grains_fractions,'r','linewidth',2)
   % and its convex hull
 plot(grains_fractions,'hull','b','linewidth',1.5)
   % and also ellipses of principal components
 plotellipse(grains_fractions,'hull','scale',0.25,'b','linewidth',1.5)
 
-%%
-subfractionratio(grains_fractions)
+%% Assign an orientation to a grain
+% this is a quite important function, hence we need it for further
+% procedings, e.g. ODF estimation, misorientation
 
-%% Calculation of additional Properties: Mean
-% sometimes a property of a grain might be used repeatedly, hence we can
-% store it into the grain object.
+grains = mean(grains, ebsd)
 
-[q grains] = mean(grains, ebsd);
+%% 
+% as we see, the mean is stored as new property 'orientation'. we can plot
+% it
 
-%%
-% We want also visualize a property
+figure, plot(grains,'property','orientation','colorcoding','ihs')
 
-grains_ph1 = get(grains,ebsd(2));
-figure('color','white'), hold on, plot(grains_ph1,'property','mean', 'colorcoding','ihs')
+%% Properties of a grain
+% We also can copy known properties from the ebsd object to our grains
 
-%%
-% and compare it with the original data set
-
-figure('color','white'), hold on, plot(ebsd(2),'colorcoding','ihs')
+grains = copyproperty(grains,ebsd)
 
 %%
-% or combine this, e.g. plot the orientation needed to map ebsd-grain data
-% onto its grain-mean, to inspect visually whether a grain has a trends
+%
+figure, plot(grains,'property','bc')
 
-figure('color','white'), hold on, plot(grains_ph1,ebsd(2),'diffmean', 'colorcoding','ihs')
-hold on, plot(grains,'color','red')
+%%
+% its also possible to plot an abitrary property
 
+figure, plot(grains,'property',shapefactor(grains))
 
 %% Spatial Relation - Join Counts
 % find out which phases depends on other phases and how
 % therefor we can iterate over all grains access its ebsd data
 
-coloring = grainfun('phase',grains,ebsd);
+coloring = get(grains,'phase');
 
-[J T q p] = joincount(grains,coloring)
-
-%%
-% obviolsy phase 1 and phase 2 are independent but not to phase 0,
-% moreover, phase 2 and phase 0 are highly dependend. So let us visual
-% inspect this behavior
-
-figure('color','white'), hold on, set(gca,'colororder',[ 0.5 0.8 0; 1 0.2 0]);
-plotspatial(ebsd([1 3]),'colorcoding','phase')
+[J T q p] = joincount(grains,coloring);
 
 %%
 % we can use also other coloring, for instance binary
@@ -209,98 +194,87 @@ ebsd_fractions
 grainfun(@(ebsd) ebsd, grains_fractions,ebsd_fractions,'uniformoutput',true)
 
 %% Multiple Access and ODF Estimation
-% perform analysis of phase 1 and calculate all ODFs of selected grains 
+% calculate all ODFs of grains, since this takes quite long we restrict the
+% selected grains to a region of interest
 
-dirichlet = kernel('dirichlet',32);
+plxy = [90 90;150 90;150 150; 90 150; 90 90];
+pgrains = inpolygon( grains, plxy,'intersect')
 
-odfs_ph1 = grainfun(@(ebsd) calcODF(ebsd,'kernel',dirichlet,'SILENT') , grains_ph1, ebsd);
-odfs_ph2 = grainfun(@(ebsd) calcODF(ebsd,'kernel',dirichlet,'SILENT') , grains_ph2, ebsd);
-
-%%
-% and an overall ODF for all selected EBSD data
-
-odf_ph1 = calcODF(ebsd(2),'kernel',dirichlet,'SILENT')
-odf_ph2 = calcODF(ebsd(3),'kernel',dirichlet,'SILENT')
-
-%% 
-% the ODFs are returned as cells, since ODFs can be compositions, 
-% we can proceed as the following 
-
-odfs_ph1_textureindex = cellfun(@(odf) textureindex(odf),odfs_ph1,'uniformoutput',true);
-odfs_ph2_textureindex = cellfun(@(odf) textureindex(odf),odfs_ph2,'uniformoutput',true);
+figure, plot(plxy(:,1),plxy(:,2),'r')
+hold on, plot(pgrains)
 
 %%
-% and calculate an overall textureindex for both phases
+% and now the ODF with respect to its origial ebsd-data
 
-odf_ph1_textureindex = textureindex(odf_ph1)
-odf_ph2_textureindex = textureindex(odf_ph2)
-
-%% 
-% the ODFs are returned as cells, we can proceed as the following 
-
-odfs_ph1_entropy = cellfun(@(odf) entropy(odf),odfs_ph1,'uniformoutput',true);
-odfs_ph2_entropy = cellfun(@(odf) entropy(odf),odfs_ph2,'uniformoutput',true);
+dirichlet = kernel('dirichlet',16);
+pgrains = calcODF(pgrains,ebsd,'kernel',dirichlet)
 
 %%
-% and calculate an overall entropy for both phases
-
-odf_ph1_entropy = entropy(odf_ph1)
-odf_ph2_entropy = entropy(odf_ph2)
- 
-%%
-% for plotting
-
-areas_ph1 = area(grains_ph1);
-areas_ph2 = area(grains_ph2);
-area_ph1 = sum(areas_ph1);
-area_ph2 = sum(areas_ph2);
+% the ODF of individual grains are stored as a property, alternativ we can
+% specify the property name, if we want to treat several ODFs
 
 %%
-% now we are ready to make scatter plots of grain properties
+% let us calculate the misorentation to mean followed by an ODF estimation
 
-figure('color','white','Position',[15 50 1200 350]), subplot(1,3,1), hold on
-plot(odfs_ph1_textureindex,odfs_ph1_entropy,'b.')
-plot(odfs_ph2_textureindex,odfs_ph2_entropy,'r.')
-plot(odf_ph1_textureindex,odf_ph1_entropy,'bx','MarkerSize',10,'LineWidth',2)
-plot(odf_ph2_textureindex,odf_ph2_entropy,'rx','MarkerSize',10,'LineWidth',2)
-xlabel('textureindex'); ylabel('entropy');
-axis tight
-grid on
-
-subplot(1,3,2), hold on
-plot(odfs_ph1_textureindex,areas_ph1,'b.')
-plot(odfs_ph2_textureindex,areas_ph2,'r.')
-plot(odf_ph1_textureindex,area_ph1,'bx','MarkerSize',10,'LineWidth',2)
-plot(odf_ph2_textureindex,area_ph2,'rx','MarkerSize',10,'LineWidth',2)
-xlabel('textureindex'); ylabel('area'); 
-set(gca,'YScale','log')
-axis tight
-grid on
-
-subplot(1,3,3), hold on
-plot(odfs_ph1_entropy,areas_ph1,'b.')
-plot(odfs_ph2_entropy,areas_ph2,'r.')
-plot(odf_ph1_entropy,area_ph1,'bx','MarkerSize',10,'LineWidth',2)
-plot(odf_ph1_entropy,area_ph2,'rx','MarkerSize',10,'LineWidth',2)
-xlabel('entropy'); ylabel('area');
-set(gca,'YScale','log','XDir','reverse')
-axis tight
-grid on
+ebsd_mis = misorientation(grains,ebsd);
+pgrains = calcODF(pgrains,ebsd_mis,'kernel',dirichlet,'property','ODF_mis')
 
 %%
-% and 3d
+% now we can work with grainfun by specifiying the property-field 'ODF' and
+% applying an function on it, e.g we want to calculate the textureindex of 
+% each grain
 
-figure('color','white'),  hold on
-plot3(odfs_ph1_textureindex,odfs_ph1_entropy,areas_ph1,'b.')
-plot3(odfs_ph2_textureindex,odfs_ph2_entropy,areas_ph2,'r.')
-xlabel('textureindex'); ylabel('entropy'); zlabel('area');
-set(gca,'ZScale','log','XScale','log')
-view(120.0,22.0)
-grid on
+tindex = grainfun(@textureindex, pgrains,'ODF');
+tindex_mis = grainfun(@textureindex, pgrains,'ODF_mis');
 
-%% 
-% clean up
+%%
 
-% close all
-% clear variables
+figure, plot(pgrains,'property',tindex);
 
+%%
+
+figure, plot(pgrains,'property',tindex_mis);
+
+%%
+% and finally, let us plot all grains with a texture index higher than
+% their misorientation texture index in red
+
+plot(pgrains,'property',tindex>tindex_mis)
+
+%%
+% some scatter plots, are grainsizes somehow correlated with their
+% textureindicies?
+
+figure, loglog(tindex,area(pgrains),'b.')
+hold on, loglog(tindex_mis,area(pgrains),'r.')
+xlabel('texture index')
+ylabel('area')
+ axis tight
+ grid on
+
+%% Calculate ODFs
+% if our grain-data has an orientation, of course we can model an odf
+
+ph = get(grains,'phase');
+
+odf_grains1 = calcODF(grains( ph==1 ),'kernel',dirichlet,'resolution',1*degree)
+
+%%
+% and the odf of the corresponding ebsd data
+
+odf_ebsd1 = calcODF(ebsd(1),'kernel',dirichlet,'resolution',1*degree)
+
+%%
+% let us compare those two, original ebsd with the odf of grains
+
+figure, plotpdf(odf_ebsd1,[Miller(1,1,0) Miller(1,1,1)])
+figure, plotpdf(odf_grains1,[Miller(1,1,0) Miller(1,1,1)])
+
+%% Misorientation to Neighbours
+
+ebsd_nmis = misorientation(grains,'weighted');
+odf_nmis1 = calcODF(ebsd_nmis(1),'kernel',dirichlet,'resolution',1*degree);
+
+figure, plotpdf(odf_nmis1,[Miller(1,1,0) Miller(1,1,1)])
+
+%%
--- qta/@grain/calcODF.m
+++ qta/@grain/calcODF.m
@@ -3,14 +3,19 @@ function grains = calcODF(grains,ebsd,varargin)
 %
 %% Input
 %  grains - @grain
-%  ebsd   - @EBSD
+%  ebsd   - @EBSD, specifiy to perform an ODF estimation based on original data
 %
 %% Output
 %  grains  - @grain with odf as property
+%  or odf  - @ODF if based on orientation of grains
 %
 %% See also
 % ebsd/calcODF
 
-[o grains] = grainfun(@(e) calcODF(e,varargin{:},'silent'), grains, ebsd,'property','ODF');
-
+if nargin>1 && isa(ebsd,'ebsd')
+  [o grains] = grainfun(@(e) calcODF(e,varargin{:},'silent'), grains, ebsd,'property',get_option(varargin,'property','ODF'));
+else
+  if nargin>1, varargin = [{ebsd} varargin]; end
+  grains = calcODF(toebsd(grains),varargin{:});
+end
 
