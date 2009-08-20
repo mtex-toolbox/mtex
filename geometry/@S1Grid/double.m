6d16e2d605c5ce0dc53ed3f493d5c380b9dcd000
fl.bachmann <fl.bachmann@localhost>
2009-08-20 13:36:36 +0000

    speedup

--- geometry/@S1Grid/double.m
+++ geometry/@S1Grid/double.m
@@ -1,8 +1,13 @@
 function d = double(G)
 % convert to double
  
-for i = 1:length(G)
-  G(i).points = reshape(G(i).points,1,[]);
-end
+f = {G.points};
+dim = 1 + (sum(cellfun('size', f, 2)) > length(f));
+d = cat(dim,f{:});
+if dim == 1,  d = reshape(d,1,[]); end
 
-d = [G.points];
+% for i = 1:length(G)
+%   G(i).points = reshape(G(i).points,1,[]);
+% end
+% 
+% d = [G.points];
