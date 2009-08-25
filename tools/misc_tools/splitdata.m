7f52f7deaafc55be5bb054c63c29c6ddc36a440c
fl.bachmann <fl.bachmann@localhost>
2009-08-25 14:15:54 +0000

    minimal change

--- tools/misc_tools/splitdata.m
+++ tools/misc_tools/splitdata.m
@@ -1,6 +1,13 @@
 function ind = splitdata(pl,n)
-% make n - partitions by its mean, returns its indices
+% make n - partitions of a sorted list by its mean, returns its indices
 
+if issorted(pl)
+  r = 0;
+elseif issorted(flipud(pl));
+  r = 1;
+else
+  error('requires sorted input')
+end
 
 n = 1:n;
 n = sum(2.^n)+1;
@@ -9,16 +16,22 @@ pk{1} = pl;
 ind{1} = 1:length(pl);
 ps{1} = 0;
 for k=1:n %pseudo recursion
-  [pk{end+1} pk{end+2} ind{end+1} ind{end+2} ps{end+1} ps{end+2}] = split(pk{k},ps{k});
+  [pk{end+1} pk{end+2} ind{end+1} ind{end+2} ps{end+1} ps{end+2}] = split(pk{k},ps{k},r);
 end
 ind = ind(end-n:end);
 ind(cellfun('isempty',ind)) = [];
 
-function [s1 s2 ind1 ind2 low up] = split(s,ps)
+function [s1 s2 ind1 ind2 low up] = split(s,ps,r)
 
 m = mean(s);
-ind1 = s >  m;
-ind2 = s <= m;
+
+if r
+  ind1 = s >  m;
+  ind2 = s <= m;
+else
+  ind1 = s <  m;
+  ind2 = s >= m;
+end
 s1 = s( ind1 );
 s2 = s( ind2 );
 
