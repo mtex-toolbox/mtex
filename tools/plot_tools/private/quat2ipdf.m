3e515bd63927cea2e3a2ba970523429cd8242e8c
ralf.hielscher <ralf.hielscher@localhost>
2009-08-27 10:05:59 +0000

    added optio: "auto" to automaticaly chose the specimen directions such that the identic orientation is centered to the white point

--- tools/plot_tools/private/quat2ipdf.m
+++ tools/plot_tools/private/quat2ipdf.m
@@ -1,7 +1,24 @@
 function h = quat2ipdf(S3G,varargin)
 
 % get specimen direction
-r = get_option(varargin,'r',xvector,'vector3d');
+if isa(S3G,'SO3Grid') && strcmpi(get_option(varargin,'r',''),'auto')
+
+  cs = get(S3G,'CS');
+  if any(strcmpi(laue(cs),{'m-3m','m-3'}))
+    r = sph2vec(pi/6,pi/8);
+  else
+        
+    [maxtheta,maxrho,minrho] = getFundamentalRegionPF(cs,varargin{:});
+    maxrho = maxrho - minrho;
+    r = sph2vec(pi/4,maxrho/4);
+  end
+  
+else
+  
+  r = get_option(varargin,'r',xvector,'vector3d');
+  
+end
+
 
 % compute crystal directions
 h = inverse(quaternion(S3G)) .* r;
