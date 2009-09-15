4f07369080448d5057fc5fb22b3e9afdb81ab59c
Hielscher <ralfHielscher@users.noreply.github.com>
2009-09-15 04:07:01 +0000

    bug fix: SO3Grid center was multiplied to the wrong side

--- geometry/@SO3Grid/quaternion.m
+++ geometry/@SO3Grid/quaternion.m
@@ -14,7 +14,7 @@ if nargin == 1
   if length(SO3G) > 1  
     for i = 1:length(SO3G) % this can be done faster
       if ~isempty(SO3G(i).center)
-        SO3G(i).Grid = reshape(SO3G(i).Grid * SO3G(i).center,1,[]);
+        SO3G(i).Grid = reshape(SO3G(i).center*SO3G(i).Grid,1,[]);
       else
         SO3G(i).Grid = reshape(SO3G(i).Grid,1,[]);
       end
@@ -24,18 +24,18 @@ if nargin == 1
     if isempty(SO3G.center)
       N = reshape(SO3G.Grid,1,[]);
     else
-      N = reshape(SO3G.Grid * SO3G.center,1,[]);      
+      N = reshape(SO3G.center * SO3G.Grid,1,[]);      
     end    
   end
 elseif nargin == 2
   if ~isempty(SO3G.center)
-    N = reshape(SO3G.Grid(i)*SO3G.center,size(i));
+    N = reshape(SO3G.center*SO3G.Grid(i),size(i));
   else
     N = SO3G.Grid(i);
   end
 else
   if ~isempty(SO3G(i).center)
-    N = SO3G.Grid(i,j)*SO3G.center;
+    N = SO3G.center*SO3G.Grid(i,j);
   else
     N = SO3G.Grid(i,j);
   end
