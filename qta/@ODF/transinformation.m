ec10987947aea6e11858c4b50327937da2a189eb
fl.bachmann <famb@users.noreply.github.com>
2009-09-18 20:53:57 +0000

    forgotten to make it grainfun ready

--- qta/@ODF/transinformation.m
+++ qta/@ODF/transinformation.m
@@ -15,12 +15,17 @@ function [I odf] = transinformation(odf1,odf2,varargin)
 %  resolution - resolution of the discretization
 %
 %% See also
-% ODF/entropy ODF/textureindex ODF/calcFourier
+% ODF/entropy ODF/textureindex ODF/calcerror
 
 
 S3G = extract_SO3grid(odf1,varargin{:});
 e1 = eval(odf1,S3G,varargin{:});
-e2 = eval(odf2,S3G,varargin{:});
+
+if ~check_option([{odf2} varargin],'evaluated'); %second ODF allready evaluated
+  e2 = eval(odf2,S3G,varargin{:});
+else
+  e2 = get_option([{odf2} varargin],'evaluated',[],'double');
+end
 
 e12 = (e1+e2)./sum(e1+e2);
 ed12 =  e1.*e2./(sum(e1.*e2)^2);
