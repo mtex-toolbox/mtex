2ee0d65cacf7d148c8d7b1d4fd680f2595e47555
fl.bachmann <fl.bachmann@localhost>
2009-09-26 10:53:09 +0000

    bugfix

--- qta/interfaces/loadPoleFigure_beartex.m
+++ qta/interfaces/loadPoleFigure_beartex.m
@@ -19,47 +19,56 @@ ipf = 1;
 r = S2Grid('regular','points',[72, 19],'antipodal'); 
 spacegroup =  {'C1','C2','D2','C4','D4','T','O','C3','D3','C6','D6'};
 
+try
+  while ~feof(fid)
 
-while ~feof(fid)
-  
- try % next polefigure
-   c = textscan(fid,'%s',7,'delimiter','\n','whitespace','');
-   comment = deblank(c{1}{1}(1:50));
- catch
-   break
- end
+   try % next polefigure
+     c = textscan(fid,'%s',7,'delimiter','\n','whitespace','');
+     comment = deblank(c{1}{1}(1:50));
+   catch
+     if ~exist('pf','var')
+      error('format BearTex does not match file %s',fname);
+     else
+       break
+     end
+   end
 
- crystal = cell2mat(textscan(c{1}{6},'%n'))';
- cs = symmetry(spacegroup{crystal(7)}, crystal(4:6)*degree,crystal(1:3));
- ss = symmetry(spacegroup{crystal(8)});
- 
- gridinfo = c{1}{7};
- hkl = cell2mat(textscan(gridinfo,'%n',3));
- h = Miller(hkl(1),hkl(2),hkl(3),cs);
+   crystal = cell2mat(textscan(c{1}{6},'%n'))';
+   cs = symmetry(spacegroup{crystal(7)}, crystal(4:6)*degree,crystal(1:3));
+   ss = symmetry(spacegroup{crystal(8)});
 
- 
- k = 11:5:40;
- info = zeros(size(k));
- for l = 1:numel(k), info(l) = str2num(gridinfo( k(l):k(l)+4)); end
-% 
-%  theta = 0:info(3):90-info(3);
-%  rho = 0:info(6):360-info(6);
- 
- data = zeros(18,72);
- for k = 1:76
-  l = fgetl(fid);
-  data(:,k) = str2num( reshape(l(2:end),4,[]).' );
- end
- 
- fgetl(fid);
- 
- pf(ipf) = PoleFigure(h,r,data,cs,ss,'comment',comment,varargin{:});
- 
- mintheta = str2num(gridinfo(12:15));
- maxtheta = str2num(gridinfo(16:20));
+   gridinfo = c{1}{7};
+   hkl = cell2mat(textscan(gridinfo,'%n',3));
+   h = Miller(hkl(1),hkl(2),hkl(3),cs);
 
- pf(ipf) = delete(pf(ipf), getTheta(getr(pf(ipf)))  < info(1)*degree-eps | ...
-   getTheta(getr(pf(ipf)))  > info(2)*degree+eps);
- 
- ipf = ipf+1;
+
+   k = 11:5:40;
+   info = zeros(size(k));
+   for l = 1:numel(k), info(l) = str2num(gridinfo( k(l):k(l)+4)); end
+  % 
+  %  theta = 0:info(3):90-info(3);
+  %  rho = 0:info(6):360-info(6);
+
+   data = zeros(18,72);
+   for k = 1:76
+    l = fgetl(fid);
+    data(:,k) = str2num( reshape(l(2:end),4,[]).' );
+   end
+
+   fgetl(fid);
+
+   pf(ipf) = PoleFigure(h,r,data,cs,ss,'comment',comment,varargin{:});
+
+   mintheta = str2num(gridinfo(12:15));
+   maxtheta = str2num(gridinfo(16:20));
+
+   pf(ipf) = delete(pf(ipf), getTheta(getr(pf(ipf)))  < info(1)*degree-eps | ...
+     getTheta(getr(pf(ipf)))  > info(2)*degree+eps);
+
+   ipf = ipf+1;
+  end
+catch
+  if ~exist('pf','var')
+    error('format BearTex does not match file %s',fname);
+  end
 end
