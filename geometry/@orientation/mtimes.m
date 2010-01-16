1a9250461b3336432a52731d46a65b65f365db88
ralf.hielscher <ralf.hielscher@localhost>
2010-01-16 05:33:05 +0000

    bug fix

--- geometry/@orientation/mtimes.m
+++ geometry/@orientation/mtimes.m
@@ -8,10 +8,51 @@ if isa(a,'orientation') && isa(b,'vector3d')
   
   r = diag(a.i) * (a.quaternion * vector3d(b));
    
-elseif isa(a,'quaternion') && isa(b,'orientation')
-    
+elseif isa(a,'symmetry')
+  
+  if a ~= b.ss
+    warning('MTEX:Orientation','Symmetry mismatch!');
+  end
   r = b;
-  r.quaternion = a * b.quaternion;
+  r.quaternion = quaternion(a) * b.quaternion;
+  r.ss = symmetry;
+  
+elseif isa(b,'symmetry')
+  
+  if a.cs ~= b
+    warning('MTEX:Orientation','Symmetry mismatch!');
+  end
+  r = a;
+  r.quaternion = a.quaternion * quaternion(b);
+  r.cs = symmetry;
+  
+elseif isa(a,'quaternion') && isa(b,'quaternion')
+    
+  if isa(a,'orientation')
+    r = a;
+    if isa(b,'orientation')
+      
+      r.i = a.i * b.i;
+      % check that symmetries are ok
+      if a.cs ~= b.ss
+        warning('MTEX:Orientation','Symmetry mismatch!');
+      end
+      r.cs = b.cs;
+    else
+      if length(r.cs) > 1
+        warning('MTEX:Orientation','Symmetry mismatch!');
+        r.cs = symmetry;
+      end
+    end
+  else
+    r = b;
+    if length(r.ss) > 1
+      warning('MTEX:Orientation','Symmetry mismatch!');
+      r.ss = symmetry;
+    end
+  end
+  
+  r.quaternion = quaternion(a) * quaternion(b);
     
 else
   
--- geometry/@orientation/times.m
+++ geometry/@orientation/times.m
@@ -1,27 +1,42 @@
 function r = times(a,b)
-% implements quaternion .* orientation and quaternion .* Miller 
-%% Input
-%  q1 - @quaternion
-%  q2 - @quaternion | @vector3d
-%
-%% Output
-%  @quaternion | @vector3d
-
 % orientation times Miller and quaternion times orientation
 
-if isa(a,'orientation') && isa(b,'Miller')
-    
-  b = set(b,'CS',a.cs);
-  r = a.i .* (a.quaternion .* vector3d(b));
+
+if isa(a,'orientation') && isa(b,'vector3d')
   
-elseif isa(a,'quaternion') && isa(b,'orientation')
+  if isa(b,'Miller'), b = set(b,'CS',a.cs);end
+  
+  r = a.i .* (a.quaternion .* vector3d(b));
+   
+elseif isa(a,'quaternion') && isa(b,'quaternion')
     
-  r = b;
-  r.quaternion = a .* b.quaternion;
+  if isa(a,'orientation')
+    r = a;
+    if isa(b,'orientation')
+      r.i = a.i .* b.i;
+      % check that symmetries are ok
+      if a.cs ~= b.ss
+        warning('MTEX:Orientation','Symmetry mismatch!');
+      end
+      r.cs = b.cs;      
+    else
+      if length(r.cs) > 1
+        warning('MTEX:Orientation','Symmetry mismatch!');
+        r.cs = symmetry;
+      end
+    end
+  else
+    r = b;
+    if length(r.ss) > 1
+      warning('MTEX:Orientation','Symmetry mismatch!');
+      r.ss = symmetry;
+    end
+  end
+  
+  r.quaternion = quaternion(a) .* quaternion(b);  
     
 else
   
   error([class(a) ' * ' class(b) ' is not defined!'])
     
 end
-
