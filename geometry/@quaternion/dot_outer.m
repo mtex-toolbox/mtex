ef3e2382b9f85b6e4bbbfb7e452eae95b15d71f5
fl.bachmann <fl.bachmann@localhost>
2010-02-27 12:37:44 +0000

    small speed improvement

--- geometry/@quaternion/dot_outer.m
+++ geometry/@quaternion/dot_outer.m
@@ -10,8 +10,15 @@ function d = dot_outer(g1,g2)
 
 if ~isempty(g1) && ~isempty(g2)
 
-	d = g1.a(:) * g2.a(:).' + g1.b(:) * g2.b(:).' + ...
-		g1.c(:) * g2.c(:).' + g1.d(:) * g2.d(:).';
+  a = g1.a(:) * g2.a(:).';
+  b = g1.b(:) * g2.b(:).';
+  c = g1.c(:) * g2.c(:).';
+  d = g1.d(:) * g2.d(:).';
+
+  d = a + b + c + d;
+
+% 	d = g1.a(:) * g2.a(:).' + g1.b(:) * g2.b(:).' + ...
+% 		g1.c(:) * g2.c(:).' + g1.d(:) * g2.d(:).';
 
   %d = quaternion_dot_outer(g1.a,g1.b,g1.c,g1.d,g2.a,g2.b,g2.c,g2.d);
 	%d = abs(d);
