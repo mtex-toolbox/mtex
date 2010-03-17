84b3703e74e23182f346cd8d5255b584e3604eef
fl.bachmann <fl.bachmann@localhost>
2010-03-17 11:00:26 +0000

    fix

--- geometry/@orientation/dot_outer.m
+++ geometry/@orientation/dot_outer.m
@@ -24,51 +24,40 @@ lCS = length(cs);
 lSS = length(ss);
 
 if (l1 < l2) && (l1>0)
-  
-  g1rot = symmetrise(g1,cs,ss,varargin{:}); % -> CS x SS x g1
-  
-  d = zeros(lCS*lSS,l1,l2);
-  
-  parts = part(numel(g2),numel(g1rot));
-  for k=1:numel(parts)-1
-    ndx = parts(k)+1:parts(k+1);
-    t = reshape(abs(dot_outer(g1rot(ndx,:),g2)),[],l1,l2);  %-> CS * SS x g1 x g2
-    d(ndx,:,:) = t;
-  end  
-  
-  if check_option(varargin,'all')
-    d = permute(d,[2 3 1]); % g1 x g2 x CS * SS
-  else
-    d = reshape(max(d,[],1),l1,l2);
-  end
-		
-elseif l2>0
-  
-	g2rot = symmetrise(g2,cs,ss,varargin{:}).'; % g2 x CS x SS
-  
-  d = zeros(l1,l2,lCS*lSS);
  
-  parts = part(numel(g1),numel(g2rot));
-  for k=1:numel(parts)-1
-    ndx = parts(k)+1:parts(k+1);
-    t = reshape(abs(dot_outer(g1,g2rot(:,ndx))),l1,l2,[]); % g1 x g2 x CS * SS
-    d(:,:,ndx) = t;
-  end
+  g1rot = symmetrise(g1,cs,ss,varargin{:}); % -> CS x SS x g1
 
   if ~check_option(varargin,'all')
-    d = max(d,[],3);
+    d = abs(dot_outer(g1rot(1,:),g2));
+    for k=2:(lCS*lSS)
+      d = max(d,abs(dot_outer(g1rot(k,:),g2))); % CS * SS x g1 x g2
+    end
+  else    
+    d = zeros(l1,l2,lCS*lSS);
+    for k=1:(lCS*lSS)
+      d(:,:,k) =abs(dot_outer(g1rot(k,:),g2));
+    end
   end
-	  
+  
+elseif l2>0
+ 
+  g2rot = symmetrise(g2,cs,ss,varargin{:}).'; % g2 x CS x SS
+
+  if ~check_option(varargin,'all')
+    d = abs(dot_outer(g1,g2rot(:,1)));
+    for k=2:(lCS*lSS)
+      d = max(d,abs(dot_outer(g1,g2rot(:,k)))); % g1 x g2 x CS * SS
+    end
+  else    
+    d = zeros(l1,l2,lCS*lSS);
+    for k=1:(lCS*lSS)
+      d(:,:,k) = abs(dot_outer(g1,g2rot(:,k)));
+    end
+  end
+  
 else
-	d = [];
+  d = [];
 end
 
 d = max(min(d,1),-1);
-
-
-function parts = part(n1,n2)
-
-dx =fix(get_mtex_option('memory')./n1);
-parts = [0:dx:n2-1 n2];
-  
 
