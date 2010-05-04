b870f73d5decbcfd2e4486b062b3d29cd4efedf4
fl.bachmann <fl.bachmann@localhost>
2010-05-04 15:09:30 +0000

    area detector frame format (bruker axs)

---
+++ qta/interfaces/loadPoleFigure_frame.m
@@ -0,0 +1,44 @@
+function pf = loadPoleFigure_frame(fname,varargin)
+% load bruker axs frame data format
+%
+%% Input
+%  fname - file name
+%
+%% Output
+%  pf    - @PoleFigure
+%
+%% See also
+% interfacesPoleFigure_index loadPoleFigure
+
+
+try
+  fid = fopen(fname);
+  A = fread(fid);
+  fclose(fid);
+
+  in = find(A == 10);
+  A = sscanf(char(A(in(96):end)),'%f');
+
+  A = reshape(A,sqrt(numel(A)),[]);  % 512^2 or 1024^2
+  % imagesc(A);
+
+  % frame to polar grid
+  [ix iy data] = find(A);
+  ix = (ix-size(A,2)/2);
+  iy = (iy-size(A,2)/2);
+
+  rho = atan2(iy,ix); % beta
+
+  theta = sqrt((ix).^2+(iy).^2); %dist to center %check projection!
+  theta = theta./(size(A,2)/pi); %alpha   
+
+  h = string2Miller(fname);
+  r = S2Grid(vector3d('polar',theta,rho));
+
+  pf = PoleFigure(h,r,data,symmetry('cubic'),symmetry,'antipodal');
+catch
+   if ~exist('pf','var')
+    error('format frame does not match file %s',fname);
+  end
+end
+
