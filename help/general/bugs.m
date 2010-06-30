65251070726d21bbbeedfc3c7be021cbf7c6d027
ralf.hielscher <ralf.hielscher@localhost>
2010-06-30 13:27:03 +0000

    doku

--- help/general/bugs.m
+++ help/general/bugs.m
@@ -1,6 +1,4 @@
 %% List of Known Bugs
 %
 % * ODF/hist and PoleFigure/hist do not show negative values
-% * fourier takes to much memory
-% * ODF/modalorientation does not return the right value
-% * ODF/plotodf(odf,'omega') does not work
+
