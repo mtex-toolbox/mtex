3fabe96d669c7c0bd1e7e01d7d983ddcdec7d499
fl.bachmann <fl.bachmann@localhost>
2010-07-02 08:12:03 +0000

    templates page header

--- tools/template_wizard/howto.m
+++ tools/template_wizard/howto.m
@@ -1,15 +1,14 @@
 %% MTEX Templates 
 %
-%% Abstract
-% <http://code.google.com/p/mtex/source/browse/#svn/trunk/templates Templates> 
-% allow an easy way to apply predefined scripts to data loaded by the
-% <matlab:doc('import_wizard') import_wizard> 
-% 
+%% 
+% Templates allow an easy way to apply predefined scripts to data loaded
+% by the <matlab:doc('import_wizard') import_wizard>, they are located at
+% <matlab:filebrowser,cd(fullfile(mtex_path,'templates')) mtex/templates>.
 %
 % <mailto:fl.bachmann@googlemail.com?subject=Template%20Submission Share> a
 % Template
 %
-% look for a Template <matlab:mtex_templates('online')  online> 
+% look for a Template <matlab:mtex_templates('online')  *online*> 
 %
 %% Contents
 %
--- tools/template_wizard/howto_online.m
+++ tools/template_wizard/howto_online.m
@@ -1,9 +1,9 @@
 %% MTEX Templates (online)
 %
-%% Abstract
-% <http://code.google.com/p/mtex/source/browse/#svn/trunk/templates Templates> 
-% allow an easy way to apply predefined scripts to data loaded by the
-% <matlab:doc('import_wizard') import_wizard> 
+%%
+% Templates allow an easy way to apply predefined scripts to data loaded
+% by the <matlab:doc('import_wizard') import_wizard>, they are located at
+% <matlab:filebrowser,cd(fullfile(mtex_path,'templates')) mtex/templates>.
 % 
 %
 % <mailto:fl.bachmann@googlemail.com?subject=Template%20Submission share a
