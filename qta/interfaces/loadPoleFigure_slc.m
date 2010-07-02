5362144f9c1661abcc61dd270f58c2d82e5847ce
ralf.hielscher <ralf.hielscher@localhost>
2010-07-02 08:33:24 +0000

    pole figure interfaces bugfixes

---
+++ qta/interfaces/loadPoleFigure_slc.m
@@ -0,0 +1,41 @@
+function pf = loadPoleFigure_slc(fname,varargin)
+% load *.txt file with regular grid
+%
+%% Input
+% fname - file name
+%
+%% Output
+% pf    - @PoleFigure
+%
+%% See also
+% interfacesPoleFigure_index loadPoleFigure
+
+% ensure right extension
+try
+  [fdir,fn,ext] = fileparts(fname); %#ok<ASGLU>
+  assert(any(strcmpi(ext,{'.slc'})));
+catch %#ok<CTCH>
+  error('Format slc does not match file %s',fname);
+end
+
+try
+  
+  %read data
+  [A,ffn,nh,SR,hl,fpos] = txt2mat(fname,'InfoLevel',0,...
+    'ReplaceRegExpr',{{'FRAME0.',''}});
+  
+  % set up specimen directions
+  rho = A(:,1)*10*degree;  
+  theta = linspace(90*degree,-90*degree,size(A,2)-1);
+  r = S2Grid('theta',theta,'rho',rho,'antipodal');
+  
+  A = A(:,2:end);
+   
+  h = string2Miller(fname);    
+  c = ones(1,length(h));
+  
+  
+  pf = PoleFigure(h,r,A,symmetry('cubic'),symmetry,'superposition',c,varargin{:}); 
+catch
+  error('format slc does not match file %s',fname);
+end
--- qta/interfaces/tools/string2Miller.m
+++ qta/interfaces/tools/string2Miller.m
@@ -3,42 +3,46 @@ function [m,r] = string2Miller(s)
 
 r = 1;
 
-% extract filename
-s = s(max([1,1+strfind(s,'/')]):end);
+try
+  % extract filename
+  s = s(max([1,1+strfind(s,'/')]):end);
 
-% first try to extract 4 Miller indice
-e = regexp(fliplr(s),'([0123456]-? ?){4}','match');
-m = exp2Miller(e);
-if ~isempty(m), return;end
-
-% if this fails try to extract only 3 Miller indice without minus
-e = regexp(fliplr(s),'([0123456] ?){3}','match');
-m = exp2Miller(e);
-if ~isempty(m), return;end
+  % first try to extract 4 Miller indice
+  e = regexp(fliplr(s),'([0123456]-? ?){4}','match');
+  m = exp2Miller(e);
+  if ~isempty(m), return;end
   
-% if this fails as well try to extract 3 Miller indice with minus
-e = regexp(fliplr(s),'([0123456]-? ?){3}','match');
-m = exp2Miller(e);
-if ~isempty(m), return;end
-
+  % if this fails try to extract only 3 Miller indice without minus
+  e = regexp(fliplr(s),'([0123456] ?){3}','match');
+  m = exp2Miller(e);
+  if ~isempty(m), return;end
+  
+  % if this fails as well try to extract 3 Miller indice with minus
+  e = regexp(fliplr(s),'([0123456]-? ?){3}','match');
+  m = exp2Miller(e);
+  if ~isempty(m), return;end
+  
 % if this fails set to default value and report
+catch  
+
+end
+
 m = Miller(1,0,0);
 r = 0;
 
-
   function m = exp2Miller(e)
        
+    m = Miller;
     for i = 1:length(e)
-      
-      ss = regexp(fliplr(char(e{i})),'-?\d','match');
-      ss = cellfun(@(x) str2double(x),ss,'UniformOutput',0);
-      [m(i),er] = Miller(ss{:}); %#ok<AGROW>
-      if er
-        m = [];
-        break;
-      end      
+      try
+        ss = regexp(fliplr(char(e{i})),'-?\d','match');
+        ss = cellfun(@(x) str2double(x),ss,'UniformOutput',0);
+        [mm,er] = Miller(ss{:});
+        if ~er, m(end+1) = mm; end       %#ok<AGROW>
+      catch
+      end
     end
-    if ~isempty(e)
+    if numel(m)>0
       m = fliplr(m);
     else
       m = [];
