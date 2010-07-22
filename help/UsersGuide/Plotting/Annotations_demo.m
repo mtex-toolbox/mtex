4e9e254a30b65e2d406a7d5d509160bfcf9b43d8
ralf.hielscher <ralf.hielscher@localhost>
2010-07-22 10:06:42 +0000

    renamed plotting

---
+++ help/UsersGuide/Plotting/Annotations_demo.m
@@ -0,0 +1,115 @@
+%% Annotations
+% Explains how to add annotations to plots. This
+% includes colorbars, legends, specimen directions and crystal directions.
+%
+%% Open in Editor
+%
+%% Contents
+%
+
+%% Some sample ODFs
+%
+% Let us first define some model ODFs to be plotted later on.
+
+cs = symmetry('-3m'); ss = symmetry('-1');
+mod1 = orientation('Euler',30*degree,40*degree,10*degree);
+mod2 = orientation('Euler',10*degree,80*degree,70*degree);
+odf = 0.7*unimodalODF(mod1,cs,ss) + 0.3*unimodalODF(mod2,cs,ss);
+
+
+%% Adding a Colorbar
+%
+% Adding a colorbar is simply done by clicking the corresponding button in
+% the figure toolbar or using the command <colorbar.html colorbar>. Note
+% that the colorrange is automaticaly set to *equal* when adding a colorbar
+% to a figure with  more then one plot (see. <ColorCoding_demo.html Color Coding>).
+
+plotpdf(odf,[Miller(1,0,0),Miller(1,1,1)],'antipodal','gray')
+colorbar
+
+
+%%
+% Executing the command <colorbar.html colorbar> twice deletes the colorbar.
+% You can also have a horzontal colorbar at the bottom of the figur using
+% the option *south*.
+
+colorbar           % delete vertical colorbar
+colorbar('south')  % add horizontal colorbar
+
+%% Adding Specimen and Crystal Directions
+%
+% Pole figure and inverse pole figures are much more readable if there are
+% included specimen or crystal directions. Using the MTEX command 
+% <annotate.html annotate> one can easily add <vector3d_index.html specimen
+% coordinate axes> to a pole figure plot.
+
+annotate([xvector,yvector,zvector],'label',{'x','y','z'},...
+  'BackgroundColor','w');
+
+%%
+% The command <annotate.html annotate> allows also to plot
+% <Miller_index.html crystal directions> to inverse pole figures.
+
+plotipdf(odf,[xvector,zvector],'gray','antipodal','marginx',10,'minmax','off')
+annotate([Miller(1,0,0),Miller(1,1,0),Miller(0,0,1),Miller(2,-1,0)],'all','labeled')
+set(gcf,'position',[139 258 672 266])
+
+
+%% Adding Preferred Orientations
+%
+% One can also mark specifc orientations in pole figure, inverse pole
+% figures
+
+plotipdf(odf,[xvector,zvector],'gray','antipodal','marginx',10,'minmax','off')
+annotate(mod1,...
+    'marker','s','MarkerSize',6,'MarkerFaceColor','r',...
+    'label','A','color','w')
+
+annotate(mod2,...
+    'marker','s','MarkerSize',6,'MarkerFaceColor','g',...
+    'label','B','color','w')
+
+%%
+% or ODF plots
+
+plot(odf,'sections',12,'gray','position',[100,100,500,380])
+annotate(mod1,...
+    'MarkerSize',15,'MarkerEdgeColor','r','MarkerFaceColor','none')
+
+annotate(mod2,...
+  'MarkerSize',15,'MarkerEdgeColor','g','MarkerFaceColor','none')
+  
+%%
+% or EBSD scatter plots
+
+ebsd = simulateEBSD(odf,200);
+scatter(ebsd,'center',mod1);
+annotate(mod1,...
+  'MarkerSize',10,'MarkerEdgeColor','r','MarkerFaceColor','r')
+annotate(mod2,...
+  'MarkerSize',10,'MarkerEdgeColor','g','MarkerFaceColor','g')
+
+
+%% Adding a Legend
+%
+% If you have multiple data in one plot then it makes sence to add a legend
+% saying which color / symbol correspond to which data set. 
+%
+% The following example compares the Fourier coefficients of the fibre ODF
+% with the Fourier co,'margin'}efficients of an unimodal ODF.
+
+plotFourier(odf)
+hold all
+plotFourier(fibreODF(Miller(1,0,0),zvector,cs,ss))
+
+legend({'Fibre ODF','Unimodal ODF'})
+
+%% Adding a Spherical Grid
+%
+% Sometimes it is usefull to have a spherical grid in your plot to make the
+% projection easier to understand. For this reason there are the option
+% *grid*, which enables the grid and the option *grid_res*, which allows to
+% specifiy the spacing of the grid lines.
+
+plotpdf(odf,[Miller(1,0,0),Miller(0,0,1)],'grid','grid_res',15*degree,...
+  'gray','antipodal');
---
+++ help/UsersGuide/Plotting/CombinedPlots_demo.m
@@ -0,0 +1,88 @@
+%% Combinded Plots
+% Explains how to combine several plots, e.g. plotting on the
+% top of an inverse pole figure some important crystall directions. 
+%
+%% Open in Editor
+%
+%% Contents
+%
+
+%% General Principle
+% In order to tell MATLAB to plot one plot right on the top of an older
+% plot one has to use the commands [[matlab:doc hold,hold all]] and
+% [[matlab:doc hold,hold off]]. Let demostrate this at a simple example.
+
+plot([2 2])
+
+hold all
+
+plot([1 3])
+
+hold off
+
+%% Combine Different EBSD Data
+% First we want to show up two different EBSD data sets in one plot
+
+%%
+% lets simualted some EBSD data
+cs = symmetry('-3m'); ss = symmetry('triclinic');
+odf = unimodalODF(orientation('euler',0,0,0,cs,ss));
+ebsd = simulateEBSD(odf,100);
+ebsd_rotated = simulateEBSD(rotate(odf,rotation('Euler',60*degree,60*degree,0*degree)),100);
+
+%%
+% plot them as a scatter plot in axis / angle parametrized orientation
+% space
+
+scatter(ebsd)
+hold all
+scatter(ebsd_rotated);
+hold off
+
+
+%%
+% a second way would be to superpose the pole figures of both EBSD data sets.
+
+h = [Miller(0,0,0,1,cs),Miller(1,0,-1,0,cs)];
+plotpdf(ebsd,h,'antipodal','MarkerSize',4)
+hold all % keep plot
+plotpdf(ebsd_rotated,h,'MarkerSize',4);
+hold off % next plot command deletes all plots
+
+
+%% Combine Smooth ODF Plots with EBSD Data Scatter Plots
+% You can also combine a contour plot of a model ODF with a scatter plot of single
+% orientations.
+
+plotpdf(odf,h,'antipodal','contourf','gray','grid')
+hold all
+plotpdf(ebsd,h,'antipodal','MarkerSize',5,'MarkerColor','b','MarkerEdgeColor','w')
+hold all
+plotpdf(ebsd_rotated,h,'MarkerSize',5,'MarkerColor','r','MarkerEdgeColor','k');
+hold off
+hh = legend({'EBSD 1','EBSD 2'},'units','normalized','position',[0.022 0.9 0.1 0.08]);
+
+%%
+% and, of course, you can do the same with ODF plots:
+
+plotodf(odf,'gray','sections',8)
+hold all
+plotodf(ebsd,'MarkerSize',8,'MarkerColor','b','MarkerEdgeColor','w')
+hold all
+plotodf(ebsd_rotated,'MarkerSize',8,'MarkerColor','r','MarkerEdgeColor','k');
+hold off
+
+
+%% Add Miller Indece to a Inverse Pole Figure Plot
+% Next we are going to add some Miller indece to an inverse pole figure
+% plot.
+
+close all;
+plotipdf(odf,xvector,'gray');
+
+hold all % keep plot
+plot(Miller(1,1,-2,0,cs),'all')
+plot(Miller(0,1,-1,0,cs),'all')
+plot(Miller(0,1,-1,1,cs),'all')
+hold off % next plot command deletes all plots
+set(gcf,'position',[100,100,300,220]);
