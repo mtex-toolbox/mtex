86767a2c36dbb4ee8dbfbfa0a2902c94cb9be5f3
fl.bachmann <fl.bachmann@localhost>
2010-08-13 13:20:05 +0000

    minor

--- geometry/@vector3d/polar.m
+++ geometry/@vector3d/polar.m
@@ -3,24 +3,17 @@ function [theta,rho,r] = polar(v)
 %% Input
 %  v - @vector3d
 %% Output
-%  theta, rho, radius
+%  theta  - polar angle
+%  rho    - azimuthal angle
+%  radius
 
-if isa(v,'vector3d')
-    if nargout == 3
-        r = sqrt(v.x.^2 + v.y.^2 + v.z.^2);
-        rho = atan2(v.y,v.x);
-        theta = acos(v.z./r);
-    elseif nargout >= 1
-        r = sqrt(v.x.^2 + v.y.^2 + v.z.^2);
-        rho =  atan2(v.y,v.x);
-        theta = acos(v.z./r);
-    else
-        hr = sqrt(v.x.^2 + v.y.^2 + v.z.^2);
-        hrho =  atan2(v.y,v.x);
-        htheta = acos(v.z./hr);
-        disp(['polar angle = ',xnum2str(reshape(htheta,1,[])/degree),mtexdegchar]);
-        disp(['azimuth     = ',xnum2str(reshape(hrho,1,[])/degree),mtexdegchar]);
-    end
+if nargout == 0
+  hr = sqrt(v.x.^2 + v.y.^2 + v.z.^2);
+  hrho =  reshape(atan2(v.y,v.x),1,[])/degree;
+  htheta = reshape(acos(v.z./hr),1,[])/degree;
+  cprintf([htheta;hrho],'-n',' %3.1f','-Lr',{'polar angle ','azimuthal angle '},'-dt',mtexdegchar);
 else
-    error('argument should be of type vector3d');
+  r = sqrt(v.x.^2 + v.y.^2 + v.z.^2);
+  rho = atan2(v.y,v.x);
+  theta = acos(v.z./r);
 end
