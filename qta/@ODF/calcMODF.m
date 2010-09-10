68e64cfec8018ca847b654224155afd564207c1f
ralf.hielscher <ralf.hielscher@localhost>
2010-09-10 07:26:28 +0000

    bugfix bandwidth + calcMODF works also for two ODFs

--- qta/@ODF/calcMODF.m
+++ qta/@ODF/calcMODF.m
@@ -1,8 +1,9 @@
-function modf = calcMODF(odf,varargin)
-% calculate the uncorrelated MODF from an ODF
+function modf = calcMODF(odf1,varargin)
+% calculate the uncorrelated MODF from one or two ODF
 %
 %% Syntax  
 % modf = calcMODF(odf,'bandwidth',32)
+% modf = calcMODF(odf1,odf2,'bandwidth',32)
 %
 %% Input
 %  odf  - @ODF
@@ -16,23 +17,40 @@ function modf = calcMODF(odf,varargin)
 %% See also
 % EBSD/calcODF
 
+%% first ODF
 % compute Fourier coefficients 
-odf = calcFourier(odf,get_option(varargin,'bandwidth',32));
-L = bandwidth(odf);
+L = get_option(varargin,'bandwidth',32);
+odf1 = calcFourier(odf1,L);
 
-% sum up Fourier coefficients
-odf_hat = zeros(deg2dim(L+1),1);
+% extract Fourier coefficients
+odf1_hat = Fourier(odf1,'bandwidth',L);
+L = min(bandwidth(odf1),L);
 
-for i = 1:length(odf)
-  l = min(length(odf(i).c_hat),length(odf_hat));
-  odf_hat(1:l) = odf_hat(1:l) + reshape(odf(i).c_hat(1:l),[],1);  
+% % sum up Fourier coefficients
+% odf_hat = zeros(deg2dim(L+1),1);
+% 
+% for i = 1:length(odf)
+%   l = min(length(odf(i).c_hat),length(odf_hat));
+%   odf_hat(1:l) = odf_hat(1:l) + reshape(odf(i).c_hat(1:l),[],1);  
+% end
+% 
+
+%% second ODF
+if nargin > 1 && isa(varargin{1},'ODF')
+  odf2 = calcFourier(varargin{1},L);
+  odf2_hat = Fourier(odf2,'bandwidth',L);
+  L = min(bandwidth(odf2),L);
+else  
+  odf2 = odf1;
+  odf2_hat = odf1_hat;
 end
 
 % compute Fourier coefficients of MODF
+odf_hat = [odf1_hat(1)*odf2_hat(1);zeros(deg2dim(L+1)-1,1)];
 for l = 1:L
   ind = deg2dim(l)+1:deg2dim(l+1);
-  odf_hat(ind) = reshape(odf_hat(ind),2*l+1,2*l+1)' * reshape(odf_hat(ind),2*l+1,2*l+1) ./ (2*l+1);
+  odf_hat(ind) = reshape(odf1_hat(ind),2*l+1,2*l+1)' * reshape(odf2_hat(ind),2*l+1,2*l+1) ./ (2*l+1);
 end
 
 % construct MODF
-modf = FourierODF(odf_hat,get(odf,'CS'),get(odf,'CS'));
+modf = FourierODF(odf_hat,get(odf1,'CS'),get(odf2,'CS'));
