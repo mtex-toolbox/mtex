deac8f1e4286c15f272c92beba825788cc812c49
Hielscher <ralfHielscher@users.noreply.github.com>
2010-09-14 06:02:01 +0000

    starting kernel method for MODF calculation

--- qta/@ODF/calcMODF.m
+++ qta/@ODF/calcMODF.m
@@ -18,31 +18,97 @@ function modf = calcMODF(odf1,varargin)
 %% See also
 % EBSD/calcODF
 
-%% first ODF
-% compute Fourier coefficients 
-L = get_option(varargin,'bandwidth',32);
-odf1 = calcFourier(odf1,L);
+%% check input
 
-% extract Fourier coefficients
-odf1_hat = Fourier(odf1,'bandwidth',L);
-L = min(bandwidth(odf1),L);
-
-%% second ODF
 if nargin > 1 && isa(varargin{1},'ODF')
-  odf2 = calcFourier(varargin{1},L);
-  odf2_hat = Fourier(odf2,'bandwidth',L);
-  L = min(bandwidth(odf2),L);
+  odf2 = varargin{1};
 else  
   odf2 = odf1;
-  odf2_hat = odf1_hat;
 end
+cs1 = get(odf1,'CS');
+cs2 = get(odf2,'CS');
 
-% compute Fourier coefficients of MODF
-odf_hat = [odf1_hat(1)*odf2_hat(1);zeros(deg2dim(L+1)-1,1)];
-for l = 1:L
-  ind = deg2dim(l)+1:deg2dim(l+1);
-  odf_hat(ind) = reshape(odf1_hat(ind),2*l+1,2*l+1)' * reshape(odf2_hat(ind),2*l+1,2*l+1) ./ (2*l+1);
+%% Kernel method
+if check_option(varargin,'kernelMethod')
+
+
+  center1 = get(odf1,'center');
+  center2 = get(odf2,'center');
+  psi1 = get(odf1,'psi');
+  psi2 = get(odf2,'psi');
+  c1 = get(odf1,'c');
+  c2 = get(odf2,'c');
+  
+  center = inverse(center1) * center2.';
+  c = c1 * c2.';
+  psi = psi1 * psi2;
+  
+  % approximation
+  if numel(c1)*numel(c2) > 10000 
+  
+    warning('not yet implemented');
+    res = get_option(varargin,'resolution');
+    S3G = SO3Grid(res,cs2,cs1);    
+    
+    % init variables
+    d = zeros(1,numel(S3G));
+
+    % iterate due to memory restrictions?
+    maxiter = ceil(length(cs1) * length(cs2) * numel(center) /...
+      get_mtex_option('memory',300 * 1024));
+    if maxiter > 1, progress(0,maxiter);end
+    
+    for iter = 1:maxiter
+   
+      if maxiter > 1, progress(iter,maxiter); end
+   
+      dind = ceil(numel(center) / maxiter);
+      sind = 1+(iter-1)*dind:min(numel(center),iter*dind);
+      
+      ind = find(S3G,center(sind));
+      for i = 1:length(ind) % TODO -> make it faster
+        d(ind(i)) = d(ind(i)) + c(sind(i));
+      end
+
+    end
+    d = d ./ sum(d(:));
+
+    %% eliminate spare rotations in grid
+    del = d ~= 0;
+    center = subGrid(S3G,del);
+    c = d(del);
+
+    
+  end
+  
+  modf = ODF(center,c,psi,get(odf2,'CS'),get(odf1,'CS'));
+  
+  %% Fourier method
+else 
+  % first ODF
+  L = get_option(varargin,'bandwidth',32);
+  odf1 = calcFourier(odf1,L);
+
+  % extract Fourier coefficients
+  odf1_hat = Fourier(odf1,'bandwidth',L);
+  L = min(bandwidth(odf1),L);
+
+  if nargin > 1 && isa(varargin{1},'ODF')
+    odf2 = calcFourier(odf2,L);
+    odf2_hat = Fourier(odf2,'bandwidth',L);
+    L = min(bandwidth(odf2),L);
+  else
+    odf2_hat = odf1_hat;
+  end
+
+  % compute Fourier coefficients of MODF
+  odf_hat = [odf1_hat(1)*odf2_hat(1);zeros(deg2dim(L+1)-1,1)];
+  for l = 1:L
+    ind = deg2dim(l)+1:deg2dim(l+1);
+    odf_hat(ind) = reshape(odf1_hat(ind),2*l+1,2*l+1)' * reshape(odf2_hat(ind),2*l+1,2*l+1) ./ (2*l+1);
+  end
+
+  % construct MODF
+  modf = FourierODF(odf_hat,get(odf2,'CS'),get(odf1,'CS'));
+    
 end
-
-% construct MODF
-modf = FourierODF(odf_hat,get(odf2,'CS'),get(odf1,'CS'));
