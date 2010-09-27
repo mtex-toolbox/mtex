dc5690ff488b0e745e07ef0ce17a6df8ceb1a2e5
Hielscher <ralfHielscher@users.noreply.github.com>
2010-09-27 07:52:06 +0000

    bug fix, make get(m,'h') working again

--- geometry/@Miller/get.m
+++ geometry/@Miller/get.m
@@ -3,14 +3,40 @@ function value = get(obj,vname)
 
 switch lower(vname)
 
-  case 'hkl'
-    
+  case {'hkl','h','k','l','i'}
+ 
     value = v2m(obj);
+   
+    switch lower(vname)
+        
+      case 'h', value = value(:,1);
+      case 'k', value = value(:,2);
+      case 'l', value = value(:,end);
+      case 'i'
+        if size(value,2) == 4
+          value = value(:,3);
+        else
+          value = [];
+        end
+    end
   
-  case 'uvw'
+  case {'uvw','u','v','w','t'}
     
-    value = v2d(obj);    
+    value = v2d(obj);
     
+    switch lower(vname)
+    
+      case 'u', value = value(:,1);
+      case 'v', value = value(:,2);
+      case 'w', value = value(:,3);
+      case 't'
+        if size(value,2) == 4
+          value = value(:,3);
+        else
+          value = [];
+        end
+        
+    end
   otherwise
     
     value = obj.(vname);
