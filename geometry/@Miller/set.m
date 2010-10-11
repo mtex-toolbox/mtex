e4f1bb769c8d0ef99a7644289bfe681f43f453b6
ralf.hielscher <ralf.hielscher@localhost>
2010-10-11 12:01:57 +0000

    Miller uvw fix

--- geometry/@Miller/set.m
+++ geometry/@Miller/set.m
@@ -5,9 +5,15 @@ if strcmp(vname,'CS')
   
   if value ~= obj.CS
     % recompute representation in cartesian coordinates
-    hkl = v2m(obj);
-    obj.CS = value;
-    obj.vector3d = reshape(m2v(hkl(:,1),hkl(:,2),hkl(:,3),value),size(obj));
+    if check_option(obj,'uvw')
+      uvw = v2d(obj);
+      obj.CS = value;
+      obj.vector3d = reshape(m2v(uvw(:,1),uvw(:,2),uvw(:,3),value),size(obj));
+    else
+      hkl = v2m(obj);
+      obj.CS = value;
+      obj.vector3d = reshape(m2v(hkl(:,1),hkl(:,2),hkl(:,3),value),size(obj));
+    end
   end
   
 else
