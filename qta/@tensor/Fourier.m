b0615b3811923872ecfd4d3e914f11904d5d25de
ralf.hielscher <ralf.hielscher@localhost>
2010-11-05 10:03:03 +0000

    continue working on mean tensor from Fourier coefficients

--- qta/@tensor/Fourier.m
+++ qta/@tensor/Fourier.m
@@ -1,4 +1,4 @@
-function [F T] = Fourier(T)
+function [F T] = Fourier(T,varargin)
 % compute the Fourier coefficients of the tensor
 %
 %% Description
@@ -13,19 +13,26 @@ function [F T] = Fourier(T)
 %% step one - multiply tensor with U 
 U = tensorU;
 
-
+l = get_option(varargin,'order',1);
 
 %% step two - compute tensor representations
 
 %T = EinsteinSum()
 
 if rank(T) == 1
-  T = rotate(T,U.');
-  T = EinsteinSum(T,2,eye(3),[1 3]);
+  
+  if l == 1
+    T = rotate(T,U.');
+    T = EinsteinSum(T,2,eye(3),[1 3]);
+  else
+    T.M(:) = 0;
+  end
+  
 elseif rank(T) == 2
+  
   C = tensorClebschGordan;
   
-  T = EinsteinSum(T,[-1 -2],U,[],U[])
+  T = EinsteinSum(T,[-1 -2],U,[],U,[1])
   
   %T = EinsteinSum(T,[-1 -2],C,[-1 -2 3],C,[1 2 4]);
   
