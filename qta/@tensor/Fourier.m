3835bb3f80216e9b1806fb3da39d650a0984d46a
Hielscher <ralfHielscher@users.noreply.github.com>
2010-11-09 08:09:12 +0000

    continue work on mean tensor from Fourier coefficients

--- qta/@tensor/Fourier.m
+++ qta/@tensor/Fourier.m
@@ -26,21 +26,44 @@ switch rank(T)
   
   case 2
      
+    CG = ClebschGordanTensor(1,1,l);
+    
     if l == 0
   
-      CGU = EinsteinSum(ClebschGordanTensor(1,1,l),[-1 -2],U,[1 -1],U,[2 -2]);
-      CGUc = EinsteinSum(ClebschGordanTensor(1,1,l),[-1 -2],conj(U),[1 -1],conj(U),[2 -2]);
+      CGU = EinsteinSum(CG,[-1 -2],U,[1 -1],U,[2 -2]);
+      CGUc = EinsteinSum(CG,[-1 -2],conj(U),[1 -1],conj(U),[2 -2]);
     
       THat = EinsteinSum(T,[-1 -2],CGUc,[-1 -2],CGU,[1 2]);
     
     else
       % correct tensor CG
-      CGU = EinsteinSum(ClebschGordanTensor(1,1,l),[-1 -2 3],U,[1 -1],U,[2 -2]);
-      CGUc = EinsteinSum(ClebschGordanTensor(1,1,l),[-1 -2 3],conj(U),[1 -1],conj(U),[2 -2]);
+      CG = ClebschGordanTensor(1,1,l);
+      CGU = EinsteinSum(CG,[-1 -2 3],U,[1 -1],U,[2 -2]);
+      CGUc = EinsteinSum(CG,[-1 -2 3],conj(U),[1 -1],conj(U),[2 -2]);
     
       % sum everythink up
       THat = EinsteinSum(T,[-1 -2],CGUc,[-1 -2 4],CGU,[1 2 3]);
     end
     
+  case 3
+    
+    % multiply with conj(U)
+    T = EinsteinSum(T,[-1 -2 -3],conj(U),[-1 1],conj(U),[-2 2],conj(U),[-3 3]);
+
+    J2 = l;
+    % compute decomposition 
+    THat = tensor(zeros([3 3 3 2*l+1 2*l+1]));
+    for J1 = max(l-1,0):2
+      
+      CG1J1  = ClebschGordanTensor(1,1,J1);
+      CGJ1J2 = ClebschGordanTensor(J1,1,J2);
+    
+      THat = THat + EinsteinSum(T,[-1 -2 -3],CG1J1,[-1 -2 -4],...
+        CGJ1J2,[-4 -3 5],CG1J1,[1 2 -5],CGJ1J2,[-5 3 4]);
+    
+    end
+    
+    % multiply with U
+    THat = EinsteinSum(THat,[-1 -2 -3],U,[-1 1],U,[-2 2],U,[-3 3]);
 end
 
