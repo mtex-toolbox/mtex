2a62f07779ef80bbf944ceabf5759fd651d17206
ralf.hielscher <ralf.hielscher@localhost>
2010-11-22 07:59:58 +0000

    store properties as fields in tensor object,

--- qta/@tensor/get.m
+++ qta/@tensor/get.m
@@ -1,3 +1,27 @@
-function value = get(T,vname)
+function value = get(T,pName)
+% extract data from a tensor variable
+%
+%% Syntax
+%  d = get(T,'name')  % individuel orientations
+% 
+%% Input
+%  T     - @tensor
+%  pName - property name
+%
+%% Output
+%
+%% See also
+% EBSD/set
 
-value = T.(vname);
+
+switch pName
+  
+  case fields(T)
+    
+    value = T.(pName);
+    
+  case fields(T.properties)
+  
+    value = T.properties.(pName);
+    
+end
