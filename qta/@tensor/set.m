c0638c15074585ab60f86f2a4513a1da507944ff
Hielscher <ralfHielscher@users.noreply.github.com>
2010-11-25 08:39:47 +0000

    tensor displays all properties, set and get reworked

--- qta/@tensor/set.m
+++ qta/@tensor/set.m
@@ -1,3 +1,28 @@
-function T = set(T,vname,value)
+function T = set(T,pName,pValue)
+% set properties of a tensor variable
+%
+%% Syntax
+%  T = set(T,'mineral','mineral name')  
+% 
+%% Input
+%  T      - @tensor
+%  pName  - property name
+%  pValue - property value
+%
+%% Output
+%
+%% See also
+% tensor/get
 
-T.(vname) = value;
+
+switch pName
+
+  case fields(T)
+    
+    T.(pName) = pValue;
+    
+  case fields(T.properties)
+  
+    T.properties.(pName) = pValue;
+    
+end
