f70d57a0ad8053de327da1fe862937f69a184849
ralf.hielscher <ralf.hielscher@localhost>
2010-11-28 12:55:07 +0000

    bug fix

--- qta/@tensor/inv.m
+++ qta/@tensor/inv.m
@@ -3,38 +3,38 @@ function T = inv(T)
 %
 %% Input
 %  T - @tensor
-% 
+%
 %% Output
 %  T - @tensor
 %
 
 switch T.rank
-  
+
   case 1
-    
+
   case 2
-    
+
     T.M = inv(T.M);
-    
+
   case 3
-    
+
   case 4
 
     % check for symmetry
 
-    % convert to a matrix    
+    % convert to a matrix
     M = tensor42(T.M);
-    
+
     % invert the matrix
     M = inv(M);
-    
+
     % make some corrections
     w = 1./(1+((1:6)>3));
     w = w.' * w;
     M = M .* w;
-    
+
     % convert to back a 4 rank tensor
     T.M = tensor24(M);
 end
 
-T.name = ['inverse ' T.name];
+T = set(T,'name',['inverse ' get(T,'name')]);
