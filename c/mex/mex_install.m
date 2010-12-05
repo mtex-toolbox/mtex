b9f5ee78374f931d80246e4332bd0363e0efb6cc
ralf.hielscher <ralf.hielscher@localhost>
2010-12-05 20:51:32 +0000

    moved mtimesx to the central place for all mex files

--- c/mex/mex_install.m
+++ c/mex/mex_install.m
@@ -1,37 +1,43 @@
-function mex_install(mtexpath,mexoptions)
+function mex_install(mtexpath,blas,varargin)
 % compiles all mex files for use with MTEX
 
 if nargin == 0, mtexpath = mtex_path;end
+if nargin <= 1, blas = false;end
 mexpath = fullfile(mtexpath,'c','mex');
 mexfile = @(file)fullfile(mexpath,file);
 
-if nargin < 2
+%% check whether to set largeArrayDims option
+if nargin < 2 && newer_version(7.3)
   if strfind(computer,'64')
-    mexoptions = '-largeArrayDims';
+    varargin = {'-largeArrayDims'};
   else
-    mexoptions = '-compatibleArrayDims';
+    varargin = {'-compatibleArrayDims'};
   end
 end
 
-places = strcat({'S1Grid','S2Grid','SO3Grid','quaternion'}, '_*.c');
+%% define the places
+if blas
+  places = {'mtimesx.c'};  
+else
+  places = strcat({'S1Grid','S2Grid','SO3Grid','quaternion'}, '_*.c');
+end
 
+%% compile all the files
 for p = 1:length(places)
   files = dir(mexfile(places{p}));
   files = {files.name};
   for f = 1:length(files)
     if exist(mexfile(files{f}),'file')      
-      disp(['>   compile ',files{f}]);
-      if newer_version(7.3)
-        mex(mexoptions,'-outdir',mexfile(get_mtex_option('architecture')),mexfile(files{f}));
-      else
-        mex('-outdir',mexfile(get_mtex_option('architecture')),mexfile(files{f}));
+      disp(['... compile ',files{f}]);
+      try
+        if blas
+          mex_blas(mexfile(files{f}),varargin{:},'-outdir',mexfile(get_mtex_option('architecture')));
+        else
+          mex(varargin{:},'-outdir',mexfile(get_mtex_option('architecture')),mexfile(files{f}));
+        end
+      catch %#ok<CTCH>
+        disp(['Compiling ' mexfile(files{f}) ' failed!']);
       end
     end
   end
-%  try
-%    movefile([mexpath,places{p},'*.mex*'],...
-%      [mtexpath,'/geometry/@',places{p},'/private']);
-%  catch
-%    disp('There was an error while moving the mex files! Please move the files manualy')
-%  end
 end
