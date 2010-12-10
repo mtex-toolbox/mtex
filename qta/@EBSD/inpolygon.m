ba36619f39154dc3de6f01b339cadfb9fce02fd7
fl.bachmann <famb@users.noreply.github.com>
2010-12-10 10:18:40 +0000

    todo: lookout for decrepated geometry commands

--- qta/@EBSD/inpolygon.m
+++ qta/@EBSD/inpolygon.m
@@ -7,7 +7,7 @@ if ~isempty(XY)
   p = polygon(varargin{:});
   ind = false(size(XY,1),1);
   for k=1:numel(p)
-    xy = get(p(k),'xy');
+    xy = get(p(k),'vertices');
     ind = ind | inpolygon(XY(:,1),XY(:,2),xy(:,1),xy(:,2));
   end
 
