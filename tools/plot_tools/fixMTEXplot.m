e8e36f955e4bfdebbf58128b71fc28e30124f5e4
ralf.hielscher <ralf.hielscher@localhost>
2010-12-23 13:56:54 +0000

    better zooming

--- tools/plot_tools/fixMTEXplot.m
+++ tools/plot_tools/fixMTEXplot.m
@@ -43,41 +43,51 @@ set(ax,'units','normalized');
 
 
 setappdata(gcf,'extend',[xlim ylim])
-% try to extend zoom to hole figure % axis fill
+% try to extend zoom to hole figure 
+% axis fill
 try
   h = zoom;
   set(h,'ActionPreCallback',{@(e,v) setappdata(gcf,'previousZoom',[xlim ylim])});
   set(h,'ActionPostCallback',@resizeCanvas);
-catch
+catch %#ok<CTCH>
 end
 
+% function for zooming - may be xlim and ylim can be changed for a better
+% view
+function resizeCanvas(obj,eventdata) %#ok<INUSD>
 
-function resizeCanvas(obj,eventdata)
-
+% do everything for pixels
 set(gcf,'units','pixel');
 set(gca,'units','pixel');
 
+% get the available space
 ax = get(gca,'position');               
-%ax_r = diff(ax([1 3]))/ diff(ax([2 4])); 
+
+% x/y ratios of available space
 ax_r = ax(4)/ ax(3);
-ay_r = ax(3)/ ax(4);                    % axis ratio of gca
-%
-ex = getappdata(gcf,'extend');          % extend of current data
-ex_r = diff(ex(3:4))/diff(ex(1:2));
+ay_r = ax(3)/ ax(4);                    
+
+% maximum xlim and ylim of the data
+ex = getappdata(gcf,'extend');
+
+% x/y rations of of maximum xlim  / ylim
 ey_r = diff(ex(1:2))/diff(ex(3:4));
-%
-cx = [xlim ylim];                       % current axis
+
+% current xlim  / ylim
+cx = [xlim ylim];
 dx = diff(cx(1:2));
 dy = diff(cx(3:4));
-cy_r = dx/dy;
-cx_r = dy/dx;
+
 
 %% resize ylim
 if ay_r < ey_r              
-  dy = dy.*(cy_r - ay_r)/ey_r;
-
-  y = cx(3:4)+ [-dy dy]; %limit to extend
+    
+  % new ylim = xlim * are_ratio
+  dy = dx * ax_r - dy;
   
+  % extend xlim to both sides
+  y = cx(3:4) + [-1 1] * dy./2;
+    
   % may be a shift is necessary 
   if y(1) < ex(3)
     y(2) = min(ex(4),y(2)+ex(3)-y(1));
@@ -86,14 +96,18 @@ if ay_r < ey_r
     y(1) = max(ex(3),y(1)+ex(4)-y(2));
     y(2) = ex(4);
   end
-    
+  
+  % set the new limit
   ylim(y)
   
 %% resize xlim  
-else                         
-  dx = dx.*(cx_r - ax_r)/ex_r;
-%   
-  x = cx(1:2) + [-dx dx]; %limit to extend
+else  
+  
+  % new xlim = ylim * are_ratio
+  dx = dy * ay_r - dx;
+  
+  % extend xlim to both sides
+  x = cx(1:2) + [-1 1] * dx./2;
   
   % may be a shift is necessary 
   if x(1) < ex(1)
@@ -104,5 +118,6 @@ else
     x(2) = ex(2);
   end
     
+  % set the new limit
   xlim(x);
 end
