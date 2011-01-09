ef7a24a8cc54c54689d9b0eb9a40e1545c76d4b5
Hielscher <ralfHielscher@users.noreply.github.com>
2011-01-09 21:07:15 +0000

    grain/ebsd "copy" allows for phase filtering

--- qta/@EBSD/copy.m
+++ qta/@EBSD/copy.m
@@ -1,4 +1,4 @@
-function ebsd = copy(ebsd,condition)
+function ebsd = copy(ebsd,varargin)
 % copy selected points from EBSD data
 %
 %% Syntax  
@@ -15,31 +15,70 @@ function ebsd = copy(ebsd,condition)
 %% See also
 % EBSD/get EBSD/copy EBSD_index
 
+if nargin == 1, return;end
+
 smpsz = sampleSize(ebsd);
 cs = cumsum([0,smpsz]);
-if isa(condition,'double'),  
+
+%% if integer indexing convert to logical indexing
+if isa(varargin{1},'double'),  
   x = zeros(1,cs(end));
-  x(condition) = 1;
-  condition = logical(x);
+  x(varargin{1}) = 1;
+  varargin{1} = logical(x);
 end
 
+%% logical indexing
+if isa(varargin{1},'logical')
+  
+  % for each phase
+  for i=1:length(ebsd)
+    idi = condition(cs(i)+1:cs(i+1));
+    
+    % filter spatial coordinates
+    if ~isempty(ebsd(i).X)
+      ebsd(i).X = ebsd(i).X(idi,:);
+    end
 
-for i=1:length(ebsd)
-  idi = condition(cs(i)+1:cs(i+1));
- 
- 	if ~isempty(ebsd(i).X)
-    ebsd(i).X = ebsd(i).X(idi,:);
+    % filter properties
+    ebsd_fields = fields(ebsd(i).options);
+    for f = 1:length(ebsd_fields)
+      if numel(ebsd(i).options.(ebsd_fields{f})) == smpsz(i)
+        ebsd(i).options.(ebsd_fields{f}) = ebsd(i).options.(ebsd_fields{f})(idi,:);
+      end
+    end
+  
+    % filter orientations
+    ebsd(i).orientations = ebsd(i).orientations(idi);
   end
+end
 
-  ebsd_fields = fields(ebsd(i).options);  
-  for f = 1:length(ebsd_fields)
-     if numel(ebsd(i).options.(ebsd_fields{f})) == smpsz(i)
-       ebsd(i).options.(ebsd_fields{f}) = ebsd(i).options.(ebsd_fields{f})(idi,:);
-     end
+%% filter by phase
+
+varargin = delete_option(varargin,'property',1);
+
+if check_option(varargin,'phase')
+  
+  phase = get_option(varargin,'phase');
+
+  % filter by phase number
+  if isa(phase,'double')
+    
+    % find matching phases
+    match = ismember([ebsd.phase],phase);
+
+  % filter by mineral name
+  elseif isa(phase,'char') || isa(phase,'cell')
+    
+    % extract mineral names
+    [CS{1:numel(ebsd)}] = get(ebsd,'CS');
+    phases = cellfun(@(cs) get(cs,'mineral'),CS,'uniformOutput',false);
+    
+    % compare to given phase
+    match = cellfun(@(str) any(strcmpi(phase,str)),phases);
+
   end
   
-	ebsd(i).orientations = ebsd(i).orientations(idi);  
+  % restrict to found phases
+  ebsd = ebsd(match);
+  
 end
-
-% ebsd = ebsd(sampleSize(ebsd)>0); %causes empty ebsd
-
---
+++ qta/@grain/copy.m
@@ -0,0 +1,61 @@
+function grains = copy(grains,varargin)
+% copy selected grains from a list of grains
+%
+%% Syntax  
+% grains = copy(grains,condition)
+% grains = copy(grains,get(grains,'phase')~=1)
+%
+%% Input
+%  grains    - @grain
+%  condition - index set 
+%
+%% Output
+%  grains - @grain
+%
+%% See also
+% grain/get grain_index
+
+
+%% if indexing use direct indexing
+if isa(varargin{1},'double') || isa(varargin{1},'logical'),   
+  grains = grains(varargin{1});
+end
+
+%% filter by phase
+
+% remove this duplicated usage of property
+varargin = delete_option(varargin,'property',1);
+
+if check_option(varargin,'phase')
+  
+  phase = get_option(varargin,'phase');
+
+  % filter by phase number
+  if isa(phase,'double')
+    
+    % find matching phases
+    match = ismember([grains.phase],phase);
+
+  % filter by mineral name
+  elseif isa(phase,'char') || isa(phase,'cell')
+    
+    % find all phases
+    grainPhases = [grains.phase];
+    [phases ind] = unique(grainPhases);
+    
+    % extract mineral names
+    CS = get(grains(ind),'CS');
+    minerals = cellfun(@(cs) get(cs,'mineral'),CS,'uniformOutput',false);
+    
+    % compare to given phase
+    matchingPhases = cellfun(@(str) any(strcmpi(phase,str)),minerals);
+
+    % find matching phases
+    match = ismember(grainPhases,phases(matchingPhases));
+    
+  end
+  
+  % restrict to found phases
+  grains = grains(match);
+  
+end
