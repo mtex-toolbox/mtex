993b1aa93d0bc170f0fe1385c2d7d7f9e4a561f2
ralf.hielscher <ralf.hielscher@localhost>
2011-01-10 08:07:27 +0000

    better grain display

--- qta/@grain/display.m
+++ qta/@grain/display.m
@@ -15,17 +15,44 @@ if ~isempty(grains)
      h = [h, ' (' grains(1).comment ')'];
   end
 end
-  disp(h)
+disp(h)
 
 if ~isempty(grains) 
-  checksums = dec2hex(unique([grains.checksum]));  
-  checksums = strcat('  grain_id', cellstr(checksums) ,',');
-  disp([ checksums{:}]);
+  %checksums = dec2hex(unique([grains.checksum]));  
+  %checksums = strcat('  grain_id', cellstr(checksums) ,',');
+  %disp([ checksums{:}]);
   if ~isempty(fields(grains(1).properties))
     disp(['  properties: ',option2str(fields(grains(1).properties))]);
   end
 end
 
+[grainPhases,phases,ind] = get(grains,'phase');
 
-disp(['  size: ' num2str(size(grains,1)) ' x ' num2str(size(grains,2))])
+matrix = cell(length(phases),5);
+for i = 1:length(phases)
+  
+  % phase
+  matrix{i,1} = num2str(phases(i));
+  
+  % symmetry
+  CS = get(grains(ind(i)).orientation,'CS');
+  matrix{i,4} = get(CS,'name'); 
+  
+  % reference frame
+  matrix{i,5} = option2str(get(CS,'alignment')); 
+  
+  % mineral
+  if ~isempty(get(CS,'mineral'))
+    matrix{i,3} = char(get(CS,'mineral')); 
+  end    
+  
+  % orientations
+  matrix{i,2} = nnz(grainPhases == phases(i));
+  
+end
+
+cprintf(matrix,'-L','  ','-Lc',...
+  {'phase' 'grains' 'mineral'  'symmetry' 'crystal reference frame'},...
+  '-ic','F');
+
 disp(' ');
