40eacace9dc2b137db0ca1b263015bd446ffa7d8
Hielscher <ralfHielscher@users.noreply.github.com>
2011-01-17 07:14:23 +0000

    merged bugfix to branch

--- qta/@ODF/rotate.m
+++ qta/@ODF/rotate.m
@@ -21,7 +21,7 @@ for i = 1:length(odf)
     for l = 0:L
       odf(i).c_hat(deg2dim(l)+1:deg2dim(l+1)) = ...
         reshape(D(deg2dim(l)+1:deg2dim(l+1)),2*l+1,2*l+1) * ...
-        reshape(nodf.c_hat(deg2dim(l)+1:deg2dim(l+1)),2*l+1,2*l+1);
+        reshape(odf.c_hat(deg2dim(l)+1:deg2dim(l+1)),2*l+1,2*l+1);
     end
     
   elseif ~check_option(odf(i),'UNIFORM')
