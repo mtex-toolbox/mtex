66f655c34463201cd24019831a3796aa3823f99e
fl.bachmann <famb@users.noreply.github.com>
2011-03-13 08:04:49 +0000

    omit slow concatenation (speed up)

--- qta/@grain/get.m
+++ qta/@grain/get.m
@@ -1,5 +1,5 @@
 function varargout = get(grains, vname, varargin)
-% get object property or intersection with ebsd 
+% get object property or intersection with ebsd
 %
 %% Input
 %  grains - @grain
@@ -19,6 +19,7 @@ if nargin > 1
   %assert_property(grains,vname);
   switch vname
     case {'CS','SS'}
+      
       [phase,uphase,id] = get(grains,'phase');
       
       sym = cell(size(id));
@@ -26,35 +27,38 @@ if nargin > 1
         sym{l} = get(grains(id(l)).orientation,vname);
       end
       varargout{1} = sym;
+      
     case 'mineral'
       
-      varargout = cellfun(@(x) get(x,'mineral') ,{grains.orientation},'uniformoutput',false);    
-            
+      CS = get(grains,'CS');
+      for k=1:numel(CS)
+        varargout{1}{k} = get(CS{k},'mineral');
+      end
+      
     case 'phase'
-      varargout{1} = [grains.phase];
-      %varargout{1} = arrayfun(@(x) x.phase,struct(grains));
+      
+      varargout{1} = populate(grains,vname);
       if nargout > 1
         [varargout{2}, varargout{3}] = unique(varargout{1},'first');
       end
-    case {'neighbour' 'cells'}
-      varargout{1} = {grains.(vname)};
+      
     case {'orientations','orientation'}
       
       if check_option(varargin,'phase')
         
         [phase uphase] = get(grains,'phase');
-      	ind = phase == get_option(varargin,'phase',uphase(1));
-      
+        ind = phase == get_option(varargin,'phase',uphase(1));
+        
       elseif check_option(varargin,'CheckPhase')
-
+        
         [phase uphase] = get(grains,'phase');
         warning('MTEX:MultiplePhases','This operatorion is only permitted for a single phase! I''m going to process only the first phase.');
         ind = phase == uphase(1);
-
+        
       else
-
+        
         ind = true(size(grains));
-
+        
       end
       
       varargout{1} = [grains(ind).orientation];
@@ -64,23 +68,56 @@ if nargin > 1
       end
       
     case fields(grains)
-      varargout{1} = [grains.(vname)];
-    case fields(grains(1).properties)
-      opt = cell(size(grains));
-      for k=1:length(opt)
-        opt{k} = grains(k).properties.(vname);
-      end
       
-      kind = grains(1).properties.(vname);
-      if (isnumeric(kind) || isa(kind,'quaternion')) && ...
-          sum(cellfun('prodofsize',opt)) == length(grains)
-        opt = [opt{:}];
-      end
-      varargout{1} = opt;
+      varargout{1} = populate(grains,vname);
+      
+    case fields(grains(1).properties)
+      
+      p = populate(grains,'properties');
+      varargout{1} =  populate(p,vname);
+      
     otherwise
-     error(['There is no ''' vname ''' property in the ''grain'' object'])
+      error(['There is no ''' vname ''' property in the ''grain'' object'])
   end
 else
   vnames = get_obj_fields(grains(1),'properties');
   if nargout, varargout{1} = vnames; else disp(vnames), end
 end
+
+
+
+function out = populate(struc, field)
+
+p = strcmpi(field, fieldnames(struc));
+
+struc = struct2cell(struc);
+struc = squeeze(struc(p,:,:));
+
+if all(cellfun('prodofsize',struc) == 1)
+  
+  if isnumeric(struc{end})    % the whole thing should be numeric, otherwise error
+    
+    out = zeros(size(struc));   
+    
+  elseif isstruct(struc{end})
+    
+    out = repmat(struc{1},size(struc));
+    
+  else
+    
+    out = horzcat(struc{:});
+    return
+    
+  end
+  
+  for k=1:numel(out) % copy
+    out(k) = struc{k};
+  end
+  
+else
+  out = struc;
+end
+
+
+
+
