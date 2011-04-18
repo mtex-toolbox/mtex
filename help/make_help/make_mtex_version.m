b988e7a22d416fed0a557a7dcbeceef023f80d27
Hielscher <ralfHielscher@users.noreply.github.com>
2011-04-18 07:21:16 +0000

    mtex depends now completely on dochelp

--- help/make_help/make_mtex_version.m
+++
@@ -1,7 +0,0 @@
-function make_mtex_version
-
-dom = com.mathworks.xml.XMLUtils.createDocument('mtex');
-
-struct2DOMnode(dom, dom.getDocumentElement,get_mtex_option('version'),'version')
-
-xmlwrite('mtex_version.xml',dom)
--- help/make_help/private/nodePosition.m
+++
@@ -1,9 +0,0 @@
-function pos = nodePosition(toc,node)
-
-pos = -1;
-items = toc.getElementsByTagName('item');
-for k=0:items.getLength-1
-  if node.isSameNode(items.item(k)),
-    pos = k+1;
-  end
-end
