e75747ac69db8df854b279dac51e8603a98dd37e
fl.bachmann <fl.bachmann@localhost>
2011-05-26 08:01:44 +0000

    speed up quaternion mtimes, TODO: check slow-down cases

--- geometry/@quaternion/mtimes.m
+++ geometry/@quaternion/mtimes.m
@@ -4,52 +4,63 @@ function q = mtimes(q1,q2)
 if isa(q1,'quaternion') && isa(q2,'quaternion')
   
   q = q1;
-  
   a1 = q1.a(:);   b1 = q1.b(:);   c1 = q1.c(:);   d1 = q1.d(:);
-  a2 = q2.a(:).'; b2 = q2.b(:).'; c2 = q2.c(:).'; d2 = q2.d(:).';
+  %   a2 = q2.a(:).'; b2 = q2.b(:).'; c2 = q2.c(:).'; d2 = q2.d(:).';
+  
+  La = [a1 -b1 -c1 -d1];
+  Lb = [b1  a1 -d1  c1];
+  Lc = [c1  d1  a1 -b1];
+  Ld = [d1 -c1  b1  a1];
+  
+  qr = [q2.a(:),q2.b(:),q2.c(:),q2.d(:)]';
+  q.a = La * qr;
+  q.b = Lb * qr;
+  q.c = Lc * qr;
+  q.d = Ld * qr;
   
   % stadard algorithm
-%    	a = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2;
-%    	b = a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2;
-%    	c = a1 * c2 + c1 * a2 + d1 * b2 - b1 * d2;
-%    	d = a1 * d2 + d1 * a2 + b1 * c2 - c1 * b2;
+  %    	a = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2;
+  %    	b = a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2;
+  %    	c = a1 * c2 + c1 * a2 + d1 * b2 - b1 * d2;
+  %    	d = a1 * d2 + d1 * a2 + b1 * c2 - c1 * b2;
   
   % fast algorithm
-  aa = times_outer(d1 + b1,b2 + c2);
-  cc = times_outer(a1 - c1,a2 + d2);
-  dd = times_outer(a1 + c1,a2 - d2);
-  bb = aa + cc + dd;
-  qq = 0.5 * (bb + times_outer(d1 - b1,b2 - c2));
-
-  a = qq - aa + times_outer(d1 - c1,c2 - d2);
-  b = qq - bb + times_outer(b1 + a1,b2 + a2);
-  c = qq - cc + times_outer(a1 - b1,c2 + d2);
-  d = qq - dd + times_outer(d1 + c1,a2 - b2);
-
-  q.a = a; q.b = b; q.c = c; q.d = d;
+  %   aa = times_outer(d1 + b1,b2 + c2);
+  %   cc = times_outer(a1 - c1,a2 + d2);
+  %   dd = times_outer(a1 + c1,a2 - d2);
+  %   bb = aa + cc + dd;
+  %   qq = 0.5 * (bb + times_outer(d1 - b1,b2 - c2));
+  %
+  %   a = qq - aa + times_outer(d1 - c1,c2 - d2);
+  %   b = qq - bb + times_outer(b1 + a1,b2 + a2);
+  %   c = qq - cc + times_outer(a1 - b1,c2 + d2);
+  %   d = qq - dd + times_outer(d1 + c1,a2 - b2);
+  %
+  %
+  %   q.a = a; q.b = b; q.c = c; q.d = d;
   
   %[a,b,c,d] = quaternion_mtimes_qq(q1.a,q1.b,q1.c,q1.d,q2.a,q2.b,q2.c,q2.d);
   
 elseif isa(q1,'quaternion') && (isa(q2,'vector3d') || isa(q2,'Miller'))
-	
-  q = rotate(q2,q1);  
-		
-elseif isa(q1,'quaternion') && isa(q2,'double') % einfache Zahl   
-	
+  
+  q = rotate(q2,q1);
+  
+elseif isa(q1,'quaternion') && isa(q2,'double') % einfache Zahl
+  
   q.a = q1.a * q2;
   q.b = q1.b * q2;
   q.c = q1.c * q2;
   q.d = q1.d * q2;
   q = class(q,'quaternion');
-		
-elseif isa(q2,'quaternion') && isa(q1,'double') % einfache Zahl   
-	  
+  
+elseif isa(q2,'quaternion') && isa(q1,'double') % einfache Zahl
+  
   q.a = q1 * q2.a;
   q.b = q1 * q2.b;
   q.c = q1 * q2.c;
   q.d = q1 * q2.d;
   q = class(q,'quaternion');
   
-else 
+else
   error('wrong type of arguments');
 end
