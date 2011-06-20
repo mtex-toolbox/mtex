0acf653b159494ff7c919c3fbdcff94f4020383f
ralf.hielscher <ralf.hielscher@localhost>
2011-06-20 17:12:20 +0000

    orientation may also be missorientation

--- geometry/@orientation/ldivide.m
+++ geometry/@orientation/ldivide.m
@@ -1,26 +1,17 @@
 function r = ldivide(a,b)
-% o \ v 
+% o .\ v 
+%
+%% Syntax
+%  h = o .\ r
+%
+%% Input
+%  o - @orientation
+%  r - @vector3d
+%
+%% Output
+%  h - @Miller indice
+%
+%% See also
+% 
 
-
-% if isa(b,'vector3d')
-
-%   r = Miller(a.rotation .\ b,a.CS);
-
-if isa(a,'orientation') && isa(b,'orientation')
-  % solve (a*q = b) modulo symmetry
-  
-  r = a' .* b;
-  
-elseif isa(a,'orientation') && ~isa(b,'orientation') && isa(b,'quaternion')
- 
-  r = a .\ orientation(b,a.CS,a.SS);
-  
-elseif isa(b,'orientation') && ~isa(a,'orientation') && isa(a,'quaternion')
- 
-  r = orientation(a,b.CS,b.SS) .\ b;
-  
-else
-  
-  error([class(a) ' .\ ' class(b) ' is not defined!'])
-  
-end
+r = inverse(a) .* b;
--- geometry/@vector3d/rotate.m
+++ geometry/@vector3d/rotate.m
@@ -1,16 +1,28 @@
 function v = rotate(v,q)
 % rotate vector3d by quaternion
+%
+%% Syntax
+%  r = rotate(v,q)
+%
 %% Input
 %  v - @vector3d
 %  q - @quaternion
+%
 %% Output
-%  r = q v;
+%  r = q * v;
 
+%extract coefficients
 [a,b,c,d] = double(q);
+
+% rotate
 [v.x,v.y,v.z] = quaternion_mtimes(a(:),b(:),c(:),d(:),v.x(:).',v.y(:).',v.z(:).');
 		
+% resize
 if numel(v) == 1  
   v = reshape(v,size(q));
 elseif numel(q) == 1
   v = reshape(v,size(v));
 end
+
+% convert to vector3d
+v = vector3d(v);
