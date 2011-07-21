a68b1254506455a56ccc72d5f4a351d2d10d6e19
ralf.hielscher <ralf.hielscher@localhost>
2011-07-21 18:53:45 +0000

    better interface check

--- tests/checkInterfaces.m
+++ tests/checkInterfaces.m
@@ -1,19 +1,35 @@
 function  checkInterfaces
 
+% find all interfaces
+interfaces = dir([mtex_path '/qta/interfaces/loadPoleFigure_*.m']);
+interfaces = {interfaces.name};
+% do not use interfaces generic
+ind = cellfun('isempty',strfind(interfaces,'generic'));
+interfaces = interfaces(ind);
+
+
 files = dir([mtexDataPath filesep 'PoleFigure']);
+
+int = {};
 
 for i = 4:length(files)
   
-  close all
-  disp(files(i).name);
-  try
-    pf = loadPoleFigure(fullfile(mtexDataPath,'PoleFigure',files(i).name));
+  try    
+    [~,interface] = loadPoleFigure(fullfile(mtexDataPath,'PoleFigure',files(i).name));    
+    disp([files(i).name, repmat(' ',1,20-length(files(i).name)), ' - ', interface]);
+    interfaces = interfaces(cellfun('isempty',strfind(interfaces,interface)));
   catch
-    disp(lasterr);
-  end
-  plot(pf,'silent');
-  
+    disp([files(i).name, repmat(' ',1,20-length(files(i).name)), ' - FAILED']);
+    %disp(lasterr);
+  end  
+end
+
+if ~isempty(interfaces)  
+  disp(' ');
+  disp('There are unused interfaces:');  
+  cprintf(interfaces.')
 end
 
 end
+
 
