3d7f956cceaa586c0e8f8e624d433df01e0f78f9
ralf.hielscher <ralf.hielscher@localhost>
2011-07-26 18:26:52 +0000

    bug fix

--- qta/@grain/plotodf.m
+++ qta/@grain/plotodf.m
@@ -3,8 +3,7 @@ function plotodf(grains,varargin)
 
 o = get(grains,'orientations');
 
-varargin = set_option_property(grains(ind),varargin{:});
+varargin = set_option_property(grains,varargin{:});
 
 plotodf(o,...
   'FigureTitle',[inputname(1) ' (' get(grains(1),'comment') ')'],varargin{:});
-
