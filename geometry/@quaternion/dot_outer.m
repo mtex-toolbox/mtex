8e669dec100d631b424f7285882f7d2c77c9f867
fl.bachmann <fl.bachmann@localhost>
2011-08-01 15:39:25 +0000

    speedup

--- geometry/@quaternion/dot_outer.m
+++ geometry/@quaternion/dot_outer.m
@@ -10,18 +10,10 @@ function d = dot_outer(g1,g2)
 
 if ~isempty(g1) && ~isempty(g2)
 
-  a = times_outer(g1.a,g2.a);
-  b = times_outer(g1.b,g2.b);
-  c = times_outer(g1.c,g2.c);
-  d = times_outer(g1.d,g2.d);
-
-  d = a + b + c + d;
-
-% 	d = g1.a(:) * g2.a(:).' + g1.b(:) * g2.b(:).' + ...
-% 		g1.c(:) * g2.c(:).' + g1.d(:) * g2.d(:).';
-
-  %d = quaternion_dot_outer(g1.a,g1.b,g1.c,g1.d,g2.a,g2.b,g2.c,g2.d);
-	%d = abs(d);
+  q1 = [g1.a(:) g1.b(:) g1.c(:) g1.d(:)];
+  q2 = [g2.a(:) g2.b(:) g2.c(:) g2.d(:)];
+  
+  d = q1*q2';
 		
 else
 		d = [];
