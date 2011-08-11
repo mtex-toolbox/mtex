4834c7821966f4eec6d8b18a58c0d75058060931
fl.bachmann <fl.bachmann@localhost>
2011-08-11 09:55:31 +0000

    revert bug  introduced by rev 1724

--- geometry/@quaternion/mtimes.m
+++ geometry/@quaternion/mtimes.m
@@ -7,7 +7,6 @@ if isa(q1,'quaternion') && isa(q2,'quaternion')
   a1 = q1.a(:); b1 = q1.b(:); c1 = q1.c(:); d1 = q1.d(:);
   a2 = q2.a(:); b2 = q2.b(:); c2 = q2.c(:); d2 = q2.d(:);
   
-  if numel(a1) < numel(a2) % assume faster concatenation for its smaller array
 
     % left side matrix Q_l(q1)
     qr = [a2,b2,c2,d2]';
@@ -16,16 +15,6 @@ if isa(q1,'quaternion') && isa(q2,'quaternion')
     q.c = [c1  d1  a1 -b1] * qr;
     q.d = [d1 -c1  b1  a1] * qr;
 
-  else
-    
-    % right side matrix Q_r(q2)
-    ql = [a1,b1,c1,d1]';
-    q.a = [a2 -b2 -c2 -d2] * ql;
-    q.b = [b2  a2  d2 -c2] * ql;
-    q.c = [c2 -d2  a2  b2] * ql;
-    q.d = [d2  c2 -b2  a2] * ql;
-
-  end
 
   % stadard algorithm
   %    	a = a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2;
