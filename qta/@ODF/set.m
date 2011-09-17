19e512fe3a5408e29439db3319e08d61c8c42f7d
Hielscher <ralfHielscher@users.noreply.github.com>
2011-09-17 12:19:41 +0000

    bug fix, to be checked again

--- qta/@ODF/set.m
+++ qta/@ODF/set.m
@@ -14,8 +14,8 @@ for i = 1:numel(obj)
   if check_option(obj(i),'fibre')
     if strcmp(vname,'CS'), obj(i).center{1} = set(obj(i).center{1},'CS',ivalue,varargin{:});end
   elseif isa(obj(i).center,'SO3Grid')
-    if strcmp(vname,'CS'), obj(i).center = set(obj(i).center,'CS',{ivalue},varargin{:});end
-    if strcmp(vname,'SS'), obj(i).center = set(obj(i).center,'SS',{ivalue},varargin{:});end
+    if strcmp(vname,'CS'), obj(i).center = set(obj(i).center,'CS',ivalue,varargin{:});end
+    if strcmp(vname,'SS'), obj(i).center = set(obj(i).center,'SS',ivalue,varargin{:});end
   end
 end
 
