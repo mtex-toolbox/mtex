d7970958a7f77d33f5ebdc828f3e07d9b1edabf8
ralf.hielscher <ralf.hielscher@localhost>
2011-09-24 12:23:19 +0000

    combine exact version and integration

--- qta/@ODF/calcAngleDistribution.m
+++ qta/@ODF/calcAngleDistribution.m
@@ -15,59 +15,52 @@ function [density,omega] = calcAngleDistribution(odf,varargin)
 %% See also
 
 
-if check_option(varargin,'integration')
-  
-  
-  CS = get(odf,'CS');
-  
-  % the rotational angles and angle distribution of the uniformODF
-  [density,omega] = angleDistribution(CS);
-
-  S2G = S2Grid('equispaced','points',72*19*16); % create a grid
-  S2G = rotate(S2G,axis2quat(yvector,90*degree));
-
-  S2G = delete_option(S2G,'indexed');
-      
-  % for all angles
-  for k=1:numel(omega)  
-    
-    % create a orientation ball
-    o = axis2quat(S2G,omega(k)); 
-    
-    [d,zone] = max(abs(dot_outer(o,CS)),[],2); % and select those
-        
-    % the angel frequency
-    if any(zone==1)
-      density(k) = density(k) * mean(eval(odf,o(zone==1)));  %#ok<EVLC>
-    end
-  end
-  
-end
-
-  
-else
-  
-  
-
 % get resolution
 points = get_option(varargin,'points',100000);
 res = get_option(varargin,'resolution',2.5*degree);
 
-%% simluate EBSD data
-ebsd = calcEBSD(odf,points,'resolution',res);
+if check_option(varargin,'integration')
+  
+  % the angle distribution of the uniformODF
+  [density,omega] = angleDistribution(get(odf,'CS'));
 
-% compute angles
-angles = angle(get(ebsd,'orientations'));
+  % for all angles
+  for k=1:numel(omega)  
+    
+    S2G = S2Grid('equispaced','points',max(1,round(4/3*sin(omega(k)/2).^2/res^2))); % create a grid
+        
+    % create orientations
+    o = axis2quat(S2G,omega(k));
+    
+    % and select those
+    angle = abs(dot_outer(o,get(odf,'CS')));
+    maxAngle = max(angle,[],2); 
+        
+    % the angel frequency
+    %sum(angle(:,1)>maxAngle-0.0001)
+    if any(angle(:,1)>maxAngle-0.0001)
+      density(k) = density(k) * mean(eval(odf,o(angle(:,1)>maxAngle-0.0001)));  %#ok<EVLC>
+    end
+  end
+  
+else
+      
 
-maxangle = max(angles);
+  %% simluate EBSD data
+  ebsd = calcEBSD(odf,points,'resolution',res);
+
+  % compute angles
+  angles = angle(get(ebsd,'orientations'));
+
+  maxangle = max(angles);
 
 
-%% perform kernel density estimation
+  %% perform kernel density estimation
 
-[bandwidth,density,omega,cdf] = kde(angles,2^8,0,maxangle);
+  [bandwidth,density,omega,cdf] = kde(angles,2^8,0,maxangle);
 
-density = density ./ mean(density) * pi ./ maxangle;
-
+  density = density ./ mean(density) * pi ./ maxangle;
+  
 end
 
 % where to evaluate
