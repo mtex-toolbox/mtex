f7f68ae47d5820fce9ea0e732b1c8df5340feec5
ralf.hielscher <ralf.hielscher@localhost>
2011-10-22 20:19:16 +0000

    definition of the tensobr - matrix conversion was different from that of David Mainprice

--- qta/@tensor/private/tensor32.m
+++ qta/@tensor/private/tensor32.m
@@ -1,6 +1,22 @@
-function M = tensor32(M)
+function M = tensor32(A)
 
-b = [1 5 9 6 3 2];
-A = bsxfun(@plus,(0:2)'*9,b);
-M = M(A);
-M(:,4:end) = M(:,4:end)*2;
+%b = [1 5 9 6 3 2];
+%A = bsxfun(@plus,(0:2)'*9,b);
+%M = M(A);
+%M(:,4:end) = M(:,4:end)*2;
+
+M = zeros(3,6);
+for i=1:3
+  for j=1:3
+    for k=1:3
+      
+      if j == k
+        n = j;
+      else
+        n = 9-k-j;
+      end
+      
+      M(i,n) = M(i,n) + A(i,j,k);
+    end
+  end
+end
