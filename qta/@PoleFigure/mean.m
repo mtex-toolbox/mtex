58c0898266c123610345f324daad0b1a1919c70b
ralf.hielscher <ralf.hielscher@localhost>
2011-10-22 21:57:28 +0000

    make use of the voronoi decomposition to cmpute weight for calcODF and mean

--- qta/@PoleFigure/mean.m
+++ qta/@PoleFigure/mean.m
@@ -14,26 +14,8 @@ function m = mean(pf,varargin)
 m = zeros(size(pf));
 for i = 1:length(pf)
   
-  % for regular grids 
-  if size(pf(i).data,1) ~= 1 && size(pf(i).data,2) ~= 1
-  
-    % analyze spherical grid
-    theta = get(pf(i).r,'theta');
-    utheta = unique(theta);
-    dtheta = utheta(2) - utheta(1);
+  w = calcQuadratureWeights(pf(i).r);
     
-    % compute quadrature weights
-    N = pi / dtheta;
-    w = fclencurt(N,-1,1);
-    w = w(1:length(utheta));
-    w = w ./ sum(w) ./ numel(theta) .* numel(utheta);
-        
-  else
-    
-    w = 1./numel(pf(i).data);
-    
-  end
-    
-  m(i) = sum(reshape(pf(i).data,size(pf(i).r)) * w);
+  m(i) = sum(sum(reshape(pf(i).data,size(w)) .* w));
   
 end
