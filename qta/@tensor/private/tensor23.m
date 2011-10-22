a1f57ac1ea7ad9c4b82719f2c3f78cdd019c93bd
Hielscher <ralfHielscher@users.noreply.github.com>
2011-10-22 20:19:16 +0000

    definition of the tensobr - matrix conversion was different from that of David Mainprice

--- qta/@tensor/private/tensor23.m
+++ qta/@tensor/private/tensor23.m
@@ -1,11 +1,29 @@
-function M = tensor23(M)
+function A = tensor23(M)
 
-if all(size(M) == [3 6])
-  M(:,4:end) = M(:,4:end)/2;
-else
-  error('Tensor doesn''t have the right shape');
+% if all(size(M) == [3 6])
+%   M(:,4:end) = M(:,4:end)/2;
+% else
+%   error('Tensor doesn''t have the right shape');
+% end
+% 
+% b = [1 6 5 6 2 4 5 4 3];
+% A = reshape(bsxfun(@minus,b(:)*3,2:-1:0),3,3,3);
+% M = M(A);
+% 
+
+A = zeros(3,3,3);
+for i = 1:3 
+  for j = 1:3
+    for k = 1:3
+      
+      if j == k
+        n = j;
+      else
+        n = 9-k-j;
+      end
+      
+      A(i,j,k) = M(i,n);
+      if j ~= k, A(i,j,k) = A(i,j,k)./2;end
+    end
+  end
 end
-
-b = [1 6 5 6 2 4 5 4 3];
-A = reshape(bsxfun(@minus,b(:)*3,2:-1:0),3,3,3);
-M = M(A);
