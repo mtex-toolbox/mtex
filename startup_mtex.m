4e400688aebfc4be6c924dd7058912c759a407b6
Hielscher <ralfHielscher@users.noreply.github.com>
2011-10-22 20:43:37 +0000

    minor

--- startup_mtex.m
+++ startup_mtex.m
@@ -66,15 +66,8 @@ if isempty(lasterr) % everything fine
 end
 
 disp(' done!')
-disp(' ');
-if isempty(javachk('desktop'))
-  disp('Basic tasks:')
-  disp('- <a href="matlab:doc mtex">Show MTEX documentation</a>')
-  disp('- <a href="matlab:import_wizard(''PoleFigure'')">Import pole figure data</a>')
-  disp('- <a href="matlab:import_wizard(''EBSD'')">Import EBSD data</a>')
-  disp('- <a href="matlab:import_wizard(''ODF'')">Import ODF data</a>')
-  disp(' ');
-end
+
+MTEXmenu;
 
 
 end
