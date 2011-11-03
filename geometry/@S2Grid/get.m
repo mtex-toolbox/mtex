95e6f4b79075d79f95f23e12f6b84ac1aad11505
Hielscher <ralfHielscher@users.noreply.github.com>
2011-11-03 21:51:16 +0000

    bug fix in get resolution

--- geometry/@S2Grid/get.m
+++ geometry/@S2Grid/get.m
@@ -4,7 +4,7 @@ function value = get(obj,vname)
 switch vname
   case {'resolution','res'}
     
-    if obj.res >= 2*pi-0.001
+    if obj.res >= 2*pi-0.001 && numel(obj)>1
       a = calcVoronoiArea(obj);
       value = sqrt(mean(a));
     else
