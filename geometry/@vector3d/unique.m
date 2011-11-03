45ecae5fd96b89eb6be0a9596508793db09c36cc
ralf.hielscher <ralf.hielscher@localhost>
2011-11-03 21:27:16 +0000

    much faster unique for vector3d

--- geometry/@vector3d/unique.m
+++ geometry/@vector3d/unique.m
@@ -1,4 +1,4 @@
-function [v,ind] = unique(v,varargin)
+function [v,m,n] = unique(v,varargin)
 % disjoint list of vectors
 %
 %% Syntax
@@ -10,6 +10,24 @@ function [v,ind] = unique(v,varargin)
 %% Output
 %  v - @vector3d
 
-[v,ind] = cunion(v,@(a,b) eq(a,b,varargin{:}));
+x = v.x(:);
+y = v.y(:);
+z = v.z(:);
+
+if check_option(varargin,'antipodal') || check_option(v,'antipodal')
+  xyz = [x.^2,y.^2,z.^2,x.*y,x.*z,y.*z];  
+else
+  xyz = [x,y,z];  
+end
+
+% find duplicates points
+[ignore,m,n] = unique(round(xyz*1e7),'rows'); %#ok<ASGLU>
+
+% remove duplicated points
+v.x = v.x(m);
+v.y = v.y(m);
+v.z = v.z(m);
+
+%[v,ind] = cunion(v,@(a,b) eq(a,b,varargin{:}));
 
 v = delete_option(v,'INDEXED');
