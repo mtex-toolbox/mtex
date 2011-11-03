d4505304926405ae353e68682abef93923208210
ralf.hielscher <ralf.hielscher@localhost>
2011-11-03 21:28:51 +0000

    S2Grid/subsref remains a S2Grid

--- geometry/@S2Grid/subsref.m
+++ geometry/@S2Grid/subsref.m
@@ -1,4 +1,5 @@
-function v = subsref(S2G,varargin)
+function S2G = subsref(S2G,varargin)
 %overloads subsref
 
-v = subsref(S2G.vector3d,varargin{:});
+S2G.vector3d = subsref(S2G.vector3d,varargin{:});
+S2G = delete_option(S2G,'indexed');
