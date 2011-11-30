dee79eaf9e2d3ab345b9bb66c636cba680746d74
fl.bachmann <famb@users.noreply.github.com>
2011-11-30 15:36:03 +0100

    concatenation of grains

--- qta/@EBSD/subsref.m
+++ qta/@EBSD/subsref.m
@@ -8,16 +8,23 @@ function ebsd = subsref(ebsd,s)
 %
 
 if isa(s,'double') || isa(s,'logical')
-
+  
   ebsd.options = structfun(@(x) x(s),ebsd.options,'UniformOutput',false);
   ebsd.rotations = ebsd.rotations(s);
   ebsd.phase = ebsd.phase(s);
-
+  
 elseif strcmp(s.type,'()')
-
-  ind = subsind(ebsd,s.subs);
-  ebsd = subsref(ebsd,ind);
-
+  
+  if check_option(s.subs,'sort')
+    
+    ebsd = subsref(ebsd,get_option(s.subs,'sort'));
+    
+  else
+    
+    ind = subsind(ebsd,s.subs);
+    ebsd = subsref(ebsd,ind);
+    
+  end
 end
 
 
