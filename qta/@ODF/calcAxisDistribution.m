2f00056fc681ab35dcba3a25e868aa32bb3fb7cb
Hielscher <ralfHielscher@users.noreply.github.com>
2011-12-16 21:26:04 +0100

    calcAxisDistribution now support: 1. all axes of an orientation 2. only axes with the largest angle 3. only axis with the smalles angle (default)

--- qta/@ODF/calcAxisDistribution.m
+++ qta/@ODF/calcAxisDistribution.m
@@ -7,18 +7,19 @@ function x = calcAxisDistribution(odf,h,varargin)
 %  h   - @vector3d
 %
 %% Flags
-%  EVEN       - calculate even portion only
-%  FOURIER    - use NFSOFT based algorithm (not yet implemented)
+%  smallesAngle - use axis corresponding to the smalles angle
+%  largestAngle - use axis corresponding to the largest angle
+%  allAngles    - use all axes
 %
 %% Output
 %  x   - values of the axis distribution
 %
 %% See also
 
-res = get_option(varargin,'resolution',2.5*degree);
+res = get_option(varargin,'resolution',2.5*degree)/8;
 
 % angle discretisation
-omega = -pi:res:pi;
+omega = -pi:res:pi-res;
 weight = sin(omega./2).^2 ./ length(omega);
 
 % define a grid for quadrature
@@ -26,6 +27,25 @@ h = repmat(h(:),1,length(omega));
 omega = repmat(omega,size(h,1),1);
 S3G = orientation('axis',h,'angle',omega,odf(1).CS,odf(1).SS);
 
-f = eval(odf,S3G,varargin{:}); %#ok<EVLC>
+if check_option(varargin,'allAngles')
+  
+  f = eval(odf,S3G,varargin{:}); %#ok<EVLC>
+  
+else
+  
+  omega2 = angle(S3G);
+  if check_option(varargin,'largestAngle')
+    ind = abs(omega) >= omega2 - 0.01;
+  else % smalles angle - default
+    ind = abs(omega) <= omega2 + 0.01;
+  end
+  
+  [i,j] = find(ind);
+
+  f = eval(odf,S3G(ind),varargin{:}); %#ok<EVLC>
+  f = sparse(i,j,f,size(S3G,1),size(S3G,2));
+  
+end
 
 x = 2 * f * weight(:);
+x = x./mean(x);
