3f7d571649b56911223120f35f49518fcb514194
fl.bachmann <fl.bachmann@localhost>
2011-12-19 15:50:04 +0100

    misorientations for grains

--- qta/@GrainSet/calcBoundaryMisorientation.m
+++ qta/@GrainSet/calcBoundaryMisorientation.m
@@ -1,6 +1,8 @@
 function [m,weights] = calcBoundaryMisorientation(grains,varargin)
 
 
+checkSinglePhase(grains);
+
 if check_option(varargin,{'sub','subboundary','internal','intern'})
   I_FD = grains.I_FDsub;
 elseif  check_option(varargin,{'external','ext','extern'})
@@ -10,43 +12,20 @@ else % otherwise select all boundaries
 end
 
 [d,i] = find(I_FD(sum(I_FD,2) == 2,any(grains.I_DG,2))');
-pairs = reshape(d,2,[]);
 
-phase = get(grains.EBSD,'phase');
-phaseMap = get(grains,'phaseMap');
-
-r       = get(grains.EBSD,'rotations');
-phase   = get(grains.EBSD,'phase');
-notIndexed = ~isNotIndexed(grains.EBSD);
-
-phase = phase(pairs);
-notIndexed = notIndexed(pairs);
-
-del = diff(phase)~=0 | any(~notIndexed);
-phase(:,del) = []; pairs(:,del) = [];
-
-% check only a single phase is involved
-if numel(unique(phase)) > 1
+if numel(d) >0
   
-  error('MTEX:MultiplePhases',['This operatorion is only permitted for a single phase! ' ...
-    'See ' doclink('xx','xx')  ...
-    ' for how to restrict GrainSet to a single phase.']);
+  Dl = d(1:2:end);
+  Dr = d(2:2:end);
+  
+  o = get(grains.EBSD,'orientations');
+  m = o(Dl).\o(Dr);
   
 else
   
-  sel = phase == phaseMap(grains.phase(1));
-  
-  if any(sel(:))
-    CS = get(grains,'CSCell');
-    SS = get(grains,'SS');
-    
-    
-    phasepair = reshape(pairs(sel),2,[]);
-    o   = orientation(r(phasepair),CS{grains.phase(1)},SS);
-    m  = o(1,:).\o(2,:); % misorientation
-  else
-    warning('selected phase does not contain any boundaries')
-    m = orientation;
-  end
+  warning('selected phase does not contain any boundaries')
+  m = orientation;
   
 end
+
+
