60004a986ffc63047dc603efe686eb53c3cc81e2
fl.bachmann <famb@users.noreply.github.com>
2011-12-19 15:13:54 +0100

    small fix

--- tools/misc_tools/doclink.m
+++ tools/misc_tools/doclink.m
@@ -1,13 +1,17 @@
 function linkText = doclink(fname,lname)
 
+
+
+
 % check whether script is published 
 stack = dbstack;
 if isempty(strmatch('publish',{stack.name},'exact'))
 
   % generate link text
   linkText = ['<a href="matlab:web(''jar:file:///'  ...
-    fullfile(mtex_path,'','help','mtex','help.jar!',fname) ...
+    regexprep(fullfile(mtex_path,'','help','mtex','help.jar!',fname),'\','/') ...
     '.html'',''-helpbrowser'')">' lname '</a>'];
+  
 else
   linkText = lname;
 end
