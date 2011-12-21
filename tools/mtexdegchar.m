108c4e6629302f0ef91bbb95805b3d605596bd81
ralf.hielscher <ralf.hielscher@localhost>
2011-12-21 20:06:21 +0100

    temporary fix for missing native2unicode function in Octave

--- tools/mtexdegchar.m
+++ tools/mtexdegchar.m
@@ -1,4 +1,8 @@
 function c = mtexdegchar
 % returns the degree character defined by the global varaibel mtex_degree
 
+if isOctave()
+  c = get_mtex_option ('degree_char', '°', 'char');
+else
 c = get_mtex_option('degree_char',native2unicode([194 176],'UTF-8'),'char');
+end
