c21aab74d8301d1ea1171030bf38305761a2da8f
ralf.hielscher <ralf.hielscher@localhost>
2012-02-22 12:28:28 +0100

    compute and display uncorrelated angle distribution analogously like grain boundary misorientations

--- qta/@GrainSet/calcMisorientation.m
+++ qta/@GrainSet/calcMisorientation.m
@@ -1,11 +1,13 @@
-function [mo,weights] = calcMisorientation(grains,varargin)
+function mori = calcMisorientation(grains,varargin)
 % calculate misorientation for any two neighbored measurments of the same
 % phase
 %
 %% Input 
 % grains - @GrainSet
+%
 %% Flags
-% withoutBoundary - do not consider neighborhoods over grain boundaries.
+% uncorrelated - the uncorrelated misorientation, i.e., independent of
+% grain boundaries
 % mis2mean - instead of neighbored measurments take the misorientation to
 %   mean orientation of a grain.
 %
@@ -21,26 +23,17 @@ function [mo,weights] = calcMisorientation(grains,varargin)
 % GrainSet/calcBoundaryMisorientation GrainSet/plotAngleDistribution
 
 
-checkSinglePhase(grains);
+if check_option(varargin,'mis2mean')
 
-
-mapping = any(grains.I_DG,2);
-A_D = grains.A_D(mapping,mapping);
-
-if check_option(varargin,{'withoutBoundary'})
-  I_FD = grains.I_FDext | grains.I_FDsub;
+  mori = get(grains.EBSD,'mis2mean');
   
-  [d,i] = find(I_FD(sum(I_FD,2) == 2,any(grains.I_DG,2))');  
-  A_D(sub2ind(size(A_D),d(1:2:end),d(2:2:end))) = 0;  
-elseif check_option(varargin,'mis2mean')  
-  mo = get(grains.EBSD,'mis2mean');
-  return
+elseif check_option(varargin,'uncorrelated')
+  
+  mori = calcMisorientation(get(grains,'ebsd'),varargin{:});
+  
+else
+  
+  mori = calcBoundaryMisorientation(grains,varargin{:});
+  
 end
-
-o = get(grains.EBSD,'orientations');
-
-[Dl,Dr] = find(A_D);
-
-mo = o(Dl).\o(Dr);
-
 
