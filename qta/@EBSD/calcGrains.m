4b2c627400f861db07c3312e59dc7b458b38656e
Hielscher <ralfHielscher@users.noreply.github.com>
2012-02-23 17:03:12 +0100

    remove not indexed data for grain reconstruction by default

--- qta/@EBSD/calcGrains.m
+++ qta/@EBSD/calcGrains.m
@@ -26,6 +26,18 @@ function grains = calcGrains(ebsd,varargin)
 
 thresholds = get_option(varargin,{'angle','threshold'},15*degree,'double');
 
+%% remove not indexed phases
+
+if ~check_option(varargin,'keepNotIndexed')
+  disp('  I''m removing all not indexed phases. The option "keepNotIndexed" keeps them.');
+  
+  CSCell = get(ebsd,'CSCell');
+  ommit = find(cellfun('isclass',CSCell,'char'));
+  
+  ebsd = subsref(ebsd,~ismember(ebsd.phase,ommit));
+  
+end
+
 %% verify inputs
 
 if numel(thresholds) == 1 && numel(ebsd.CS) > 1
@@ -235,9 +247,10 @@ end
 
 %%
 
-grainSet.comment  = [ebsd.comment ', thresholds: ' ...
-  sprintf(['%3.1f' mtexdegchar ', '],thresholds/degree)];
-grainSet.comment(end-1:end) = [];
+grainSet.comment  = ebsd.comment;
+%', thresholds: ' ...
+%  sprintf(['%3.1f' mtexdegchar ', '],thresholds/degree)];
+%grainSet.comment(end-1:end) = [];
 
 grainSet.A_D      = logical(A_D);
 grainSet.I_DG     = logical(I_DG);
