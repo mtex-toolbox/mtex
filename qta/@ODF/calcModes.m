3779ab1588ac3e8cf9f5087e9b7803f3b0003fca
ralf.hielscher <ralf.hielscher@localhost>
2012-02-24 08:39:31 +0100

    calcModes(odf) and max(odf) supports again multiple modes

--- qta/@ODF/calcModes.m
+++ qta/@ODF/calcModes.m
@@ -3,7 +3,7 @@ function [modes, values] = calcModes(odf,varargin)
 %
 %% Input
 %  odf - @ODF 
-%  n   - number of modes (not yet supported)
+%  n   - number of modes
 %
 %% Output
 %  modes - modal @orientation
@@ -23,6 +23,12 @@ function [modes, values] = calcModes(odf,varargin)
 %
 %% See also
 % ODF/max
+
+% find multiple modes
+if isnumeric(varargin{1})
+  [modes, values] = findMultipleModes(odf,varargin{:});
+  return
+end
 
 % initial resolution
 res = 5*degree;
@@ -86,3 +92,72 @@ end
 
 [values,ind] = max(f(:));
 modes = S3G(ind);
+
+%% -------------------------------------------------------
+
+function [modes,values] = findMultipleModes(odf,varargin)
+
+res = get_option(varargin,'resolution',5*degree);
+
+S3G = extract_SO3grid(odf,'resolution',res,varargin{:});
+f = eval(odf,S3G,varargin{:}); %#ok<EVLC>
+
+
+% eliminate zeros
+del = f>0;
+qa = S3G(del);
+f = f(del);
+
+% the search grid
+S3Gs = subGrid(S3G,del);
+T = find(S3Gs,qa,res*1.5);
+
+[f ndx] = sort(f,'descend');
+qa = qa(ndx);
+
+T = T(ndx,:);
+T = T(:,ndx);
+T = T -speye(size(T));
+
+
+% look for maxima
+nn = numel(f);
+ls = false(1,nn);
+ids = false(1,nn);
+
+[ix iy] = find(T);
+cx = [0 ; find(diff(iy))];
+
+num = get_option(varargin,'num',1);
+if nargin > 1 && isa(varargin{1},'double')
+  num = varargin{1};
+end
+
+for k=1:length(cx)-1
+  id = ix(cx(k)+1:cx(k+1));
+  ls(id) = true;  
+  ids(k) = ls(:,k)==0;
+  
+  if nnz(ids)>=num, break,end;
+end
+
+% the retrived maximas
+q = qa(ids);
+values = f(ids);
+
+accuracy = get_option(varargin,'accuracy',0.25*degree);
+%centering of local max
+for k=1:numel(q)
+  res2 = res/2;
+  while res2 > accuracy
+    res2 = res2/2;
+    S3G = SO3Grid(res2,odf(1).CS,odf(1).SS,'center',q(k),'max_angle',res2*4);
+    f = eval(odf,S3G,varargin{:}); %#ok<EVLC>
+    
+    [mo ndx] = max(f);
+    q(k) = S3G(ndx);
+    values(k) = mo;
+  end
+end
+
+modes = orientation(q,odf(1).CS,odf(1).SS);
