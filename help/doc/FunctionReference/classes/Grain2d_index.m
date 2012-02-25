c29b11e0a0c899ddfa5c70413d36d48e4c4c3478
fl.bachmann <famb@users.noreply.github.com>
2012-02-25 10:12:36 +0100

    function reference GrainSet

---
+++ help/doc/FunctionReference/classes/Grain2d_index.m
@@ -0,0 +1,17 @@
+%% 2d Grains (The Class [[Grain2d_index.html,Grain2d]])
+%
+% class representing *2d Grains*. inherits methods of <GrainSet_index.html
+% GrainSet>
+%
+
+mtexdata aachen
+grains = calcGrains(ebsd)
+
+%%
+%
+
+plot(grains)
+
+
+
+
