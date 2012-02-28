f9ea4c9b298687f547a2b8a05ab259fa6f91c5d0
Hielscher <ralfHielscher@users.noreply.github.com>
2012-02-28 15:44:35 +0100

    bug fix, take care of different crystal reference frames

--- geometry/@symmetry/disjoint.m
+++ geometry/@symmetry/disjoint.m
@@ -1,22 +1,40 @@
 function s = disjoint(s1,s2)
 % returns the disjoint of two symmetry groups
 
-% compare symmetry elements
-is2 = any(isappr(dot_outer(s1,s2),1));
+% both symmetries are equal -> nothing is to do
+if s1 == s2
+  s = s1;
+  return;
+end
+
+% check for equal rotations
+[is1,is2] = find(isappr(dot_outer(s1,s2),1));
+
+% no common rotation -> take triclinic
+if numel(is1) == 1, s = symmetry; return; end
 
 % take the equal ones
-s2 = s2.rotation(is2);
+s = s1;
+s.rotation = s1.rotation(is1);
+s.mineral = '';
+s.name = 'common symmetry';
 
-% find a symmetry that exactly contains these
-
+% find a symmetry that exactly contains s
 for i=1:11 % check all Laue groups
   
-  s = symmetry(i);
+  ss = symmetry(i);
   
-  if numel(s2) == numel(s) && all(any(isappr(dot_outer(s,s2),1)))
+  if numel(ss) == numel(s) && all(any(isappr(dot_outer(s,ss),1)))
+    s = ss;
     return
   end
   
 end
 
-s = symmetry;
+if numel(s) == 6
+  s.laue = '-3m';
+elseif numel(s) == 12
+  s.laue = '6/mmm';
+else
+  s.laue = 'unknown';
+end
