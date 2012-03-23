02b72b519858a4de2a7605bf866c4c7415745409
fl.bachmann <fl.bachmann@localhost>
2012-03-23 13:40:52 +0100

    bugfix

--- qta/@GrainSet/horzcat.m
+++ qta/@GrainSet/horzcat.m
@@ -20,12 +20,13 @@ for k = 2:numel(varargin)
   dOld = find(any(grains.I_DG,2));
   dNew = find(any(g.I_DG,2));
   
-  [a,gndx] = sort([gOld(:);gNew(:)]);
-  [a,dndx] = sort([dOld(:);dNew(:)]);
+  % intersection of sets and indexes
+  [a,gndx] = unique([gOld(:);gNew(:)]);
+  [a,dndx] = unique([dOld(:);dNew(:)]);
   
-  grains.A_D = max(grains.A_D,g.A_D);
+  grains.A_D  = max(grains.A_D,g.A_D);
   grains.I_DG = max(grains.I_DG,g.I_DG);
-  grains.A_G = max(grains.A_G,g.A_G);
+  grains.A_G  = max(grains.A_G,g.A_G);
   
   grains.meanRotation = [grains.meanRotation;g.meanRotation];
   grains.meanRotation = grains.meanRotation(gndx);
@@ -34,10 +35,16 @@ for k = 2:numel(varargin)
   grains.phase = grains.phase(gndx);
   
   grains.I_FDext = max(grains.I_FDext,g.I_FDext);
-  grains.I_FDsub = max(grains.I_FDsub,g.I_FDsub);  
+  grains.I_FDsub = max(grains.I_FDsub,g.I_FDsub);
   
   grains.F = max(grains.F,g.F);
   grains.V = max(grains.V,g.V);
+  
+  for fn = fieldnames(grains.options)'
+    cfn = char(fn);
+    newoption = vertcat(grains.options.(cfn),g.options.(cfn));
+    grains.options.(cfn) = newoption(gndx);
+  end
   
   grains.EBSD = [grains.EBSD g.EBSD];
   grains.EBSD = grains.EBSD('sort',dndx);
