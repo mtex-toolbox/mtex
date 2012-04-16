71679db0a13a3869ab456fce9459b76a709e009b
fl.bachmann <fl.bachmann@localhost>
2012-04-16 18:28:42 +0200

    small fix

--- geometry/@Miller/dspacing.m
+++ geometry/@Miller/dspacing.m
@@ -10,11 +10,10 @@ bb = cross(a(3),a(1)) ./ V;
 cc = cross(a(1),a(2)) ./ V;
 
 hkl = v2m(h);
-h = hkl(:,1) * aa + hkl(:,2) * bb + hkl(:,3) * cc;
+h = hkl(:,1) * aa + hkl(:,2) * bb + hkl(:,end) * cc;
 
 dspace = 1./norm(h);
 
-floor(dspace/2)+1
 
 
 
