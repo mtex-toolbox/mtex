fc7947179455b871d1943bd3ffe6f5f75ea6ec78
Hielscher <ralfHielscher@users.noreply.github.com>
2012-06-06 18:53:19 +0200

    double convention also for rank 4 tensor, since this is needed for the compliance tensor
    fixes a bug reported by Karsten Kunze

--- qta/@tensor/subsasgn.m
+++ qta/@tensor/subsasgn.m
@@ -2,13 +2,13 @@ function T = subsasgn(T,s,b)
 % overloads subsasgn
 
 if T.rank == 4
-  M = tensor42(T.M);
+  M = tensor42(T.M,T.doubleConvention);
   M = subsasgn(M,s,b);
-  T.M = tensor24(M);
+  T.M = tensor24(M,T.doubleConvention);
 elseif T.rank == 3
-  M = tensor32(T.M,isfield(T,'doubleconvention'));
+  M = tensor32(T.M,T.doubleConvention);
   M = subsasgn(M,s,b);
-  T.M = tensor23(M,isfield(T,'doubleconvention'));
+  T.M = tensor23(M,T.doubleConvention);
 else
   T.M = subsasgn(T.M,s,b);
 end
--- qta/@tensor/subsref.m
+++ qta/@tensor/subsref.m
@@ -2,10 +2,10 @@ function d = subsref(T,s)
 %overloads subsref
 
 if T.rank == 4 && numel(s.subs)==2
-  M = tensor42(T.M);
+  M = tensor42(T.M,T.doubleConvention);
   d = subsref(M,s);
 elseif T.rank==3 && numel(s.subs)==2
-  M = tensor32(T.M,isfield(T,'doubleconvention'));
+  M = tensor32(T.M,T.doubleConvention);
   d = subsref(M,s);
 else
   d = subsref(T.M,s);
