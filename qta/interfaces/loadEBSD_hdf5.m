8c11128fa0d6a8c9660399c4b897d9ad2afe7e3b
Philippe Pinard <philippe.pinard@gmail.com>
2012-07-19 17:25:58 +0200

    Add loader and saver to HDF5 format. This allows to load very quickly large EBSD datasets.

---
+++ qta/interfaces/loadEBSD_hdf5.m
@@ -0,0 +1,54 @@
+function ebsd = loadEBSD_hdf5(fname,varargin)
+
+%% Read rotations
+as = h5read(fname, '/ebsd/rotations/a');
+bs = h5read(fname, '/ebsd/rotations/b');
+cs = h5read(fname, '/ebsd/rotations/c');
+ds = h5read(fname, '/ebsd/rotations/d');
+
+rotations = rotation(quaternion(as, bs, cs, ds));
+
+%% Read phases
+% Array 
+phases = h5read(fname, '/ebsd/phases');
+
+% Symmetry
+info = h5info(fname, '/ebsd/cs');
+cs = cell(numel(info.Datasets), 1);
+for i = 1:numel(info.Datasets)
+  location = ['/ebsd/cs/' num2str(i)];
+  cs{i} = loadSymmetry(fname, location);
+end
+
+ss = loadSymmetry(fname, '/ebsd/ss');
+
+%% Read unit cell
+unitCell = h5read(fname, '/ebsd/unitCell');
+
+%% Read options
+info = h5info(fname, '/ebsd/options/');
+options = struct();
+for i = 1:numel(info.Datasets)
+  name = info.Datasets(i).Name;
+  location = ['/ebsd/options/', name];
+  data = h5read(fname, location);
+  options.(name) = data;
+end
+
+%% Read comment
+comment = h5readatt(fname, '/ebsd', 'comment');
+
+%% Construct EBSD
+ebsd = EBSD(rotations, 'CS', cs, 'SS', ss, 'phase', phases, ...
+            'unitCell', unitCell, 'options', options, 'comment', comment);
+
+end
+
+function sym = loadSymmetry(fname, location)
+  name = h5readatt(fname, location, 'name');
+  axesLength = h5readatt(fname, location, 'axes');
+  axesAngle = h5readatt(fname, location, 'angles');
+  mineral = h5readatt(fname, location, 'mineral');
+  
+  sym = symmetry(name, axesLength, axesAngle, 'mineral', mineral);
+end
