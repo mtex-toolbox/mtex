bce826d410057a1400cb973ebe0706195ef4d4d1
hielscher <hielscher@ptiza>
2012-09-24 23:56:26 +0200

    minor fixes

--- tools/import_wizard/private/path2filename.m
+++ tools/import_wizard/private/path2filename.m
@@ -1,14 +1,18 @@
 function fnames = path2filename(arg)
 
+fnames = arg;
+
 if ~isempty(arg)
   if iscell(arg)
     for i=1:length(arg)
       n = regexp(arg{i},filesep);
-      fnames{i} = arg{i}((n(end)+1):end);
+      if ~isempty(n), fnames{i} = arg{i}((n(end)+1):end); end
     end  
   else
     n = regexp(arg,filesep);
-    fnames = arg((n(end)+1):end);
+    
+    if ~isempty(n), fnames = arg((n(end)+1):end);end
   end
-else fnames=blanks(0);
+else
+  fnames=blanks(0);
 end
