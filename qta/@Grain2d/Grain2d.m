e9d0f7a5e4adf8637eded39bc0ec5275c1ba4590
hielscher <hielscher@ptiza>
2012-09-30 22:40:44 +0200

    fixes bug: http://code.google.com/p/mtex/issues/detail?id=70 invalid empty constructor for Grain3d and Grain2d

--- qta/@Grain2d/Grain2d.m
+++ qta/@Grain2d/Grain2d.m
@@ -1,9 +1,12 @@
-function grains = Grain2d(grainSet,ebsd)
+function grains = Grain2d(varargin)
 % constructor for a 2d-GrainSet
 %
 % *Grain2d* represents 2d grains. a *Grain2d* represents grains and grain
 % boundaries spatially and topologically. It uses formally the class
 % [[GrainSet.GrainSet.html,GrainSet]].
+%
+%% Syntax
+%   grains = Grain2d(grainSet,ebsd)
 %
 %% Input
 % grainSet - @GrainSet
@@ -12,4 +15,5 @@ function grains = Grain2d(grainSet,ebsd)
 %% See also
 % EBSD/calcGrains GrainSet/GrainSet Grain3d/Grain3d
 
-grains = class(struct,'Grain2d',GrainSet(grainSet,ebsd));
+
+grains = class(struct,'Grain2d',GrainSet(varargin{:}));
