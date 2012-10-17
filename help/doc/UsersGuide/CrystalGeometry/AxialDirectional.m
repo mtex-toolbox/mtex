c5acef3ebe7823c92892e1c1ed996fee9fe3ef52
hielscher <hielscher@ptiza>
2012-10-17 22:14:37 +0200

    doku

--- help/doc/UsersGuide/CrystalGeometry/AxialDirectional.m
+++ help/doc/UsersGuide/CrystalGeometry/AxialDirectional.m
@@ -20,7 +20,7 @@ v2 = vector3d(1,1,-2);
 %%
 % when plotted at the sphere
 
-close all; figure('position',[100 100 400 300])
+close all; figure('position',[100 100 500 300])
 plot([v1,v2],'label',{'v_1','v_2'})
 
 %%
@@ -77,7 +77,7 @@ o = orientation('Euler',20*degree,30*degree,0,'ZYZ',CS);
 odf = unimodalODF(o);
 
 % plot pole figures
-plotpdf(odf,Miller(1,2,2),'position',[100 100 400 200])
+plotpdf(odf,[Miller(1,2,2),-Miller(1,2,2)],'position',[100 100 500 300])
 
 %%
 % Hence, if one wants to compare calculated pole figures with experimental
@@ -91,7 +91,7 @@ plotpdf(odf,Miller(1,2,2),'antipodal')
 % look at complete, inverse pole figures they do not posses antipodal symmetry
 % in general
 
-plotipdf(odf,yvector,'position',[100 100 400 200],'complete')
+plotipdf(odf,[yvector,-yvector],'position',[100 100 500 300],'complete')
 
 %%
 % However, if we add the keyword antipodal, antipodal symmetry is enforced.
@@ -104,11 +104,11 @@ plotipdf(odf,yvector,'antipodal','complete')
 
 close all
 figure(1)
-plotipdf(odf,yvector,'position',[100 100 400 200])
+plotipdf(odf,yvector,'position',[100 100 400 300])
 
 
 figure(2)
-plotipdf(odf,yvector,'antipodal','position',[100 100 400 200])
+plotipdf(odf,yvector,'antipodal','position',[100 100 400 300])
 
 
 %% EBSD Colocoding
