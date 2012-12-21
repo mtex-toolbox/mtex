fa669718afb3bcbe64cc32ae70c00281168c060b
Hielscher <ralfHielscher@users.noreply.github.com>
2012-12-21 12:15:58 +0100

    minor

--- geometry/@orientation/plotpdf.m
+++ geometry/@orientation/plotpdf.m
@@ -49,11 +49,14 @@ data = get_option(varargin,'property',[]);
 
 %% subsample if needed 
 
-if sum(numel(o))*length(cs)*length(ss) > 10000 || check_option(varargin,'points')
+if ~check_option(varargin,'all') && ...
+    (sum(numel(o))*length(cs)*length(ss) > 10000 || check_option(varargin,'points'))
 
   points = fix(get_option(varargin,'points',10000/length(cs)/length(ss)));
   disp(['  plotting ', int2str(points) ,' random orientations out of ', int2str(numel(o)),' given orientations']);
-
+  disp('You can specify the the number points by the option "points".'); 
+  disp('The option "all" ensures that all data are plotted');
+  
   samples = discretesample(ones(1,numel(o)),points);
   o.rotation = o.rotation(samples);
   if ~isempty(data), data = data(samples); end
@@ -71,8 +74,7 @@ data = @(i) repmat(data(:).',[numel(ss) numel(sh(i))]);
 
 [minTheta,maxTheta,minRho,maxRho] = getFundamentalRegionPF(ss,'restrict2Hemisphere',varargin{:});
 
-multiplot(ax{:},numel(h),r,data,...
-  'scatter','TR',@(i) h(i),...
+multiplot(ax{:},numel(h),r,data,'scatter','TR',@(i) h(i),...  
   'minRho',minRho,'maxRho',maxRho,'minTheta',minTheta,'maxTheta',maxTheta,...
   varargin{:});
 
