68890385518ea4f6e696f04827cdb3267daf4b71
Hielscher <ralfHielscher@users.noreply.github.com>
2013-01-19 07:17:52 +0100

    improved installation

--- tools/misc_tools/MTEXmenu.m
+++ tools/misc_tools/MTEXmenu.m
@@ -8,5 +8,13 @@ if isempty(javachk('desktop'))
   disp('- <a href="matlab:import_wizard(''PoleFigure'')">Import pole figure data</a>')
   disp('- <a href="matlab:import_wizard(''EBSD'')">Import EBSD data</a>')
   disp('- <a href="matlab:import_wizard(''ODF'')">Import ODF data</a>')
+  
+  if getpref('mtex','isInstalled')
+    disp('- <a href="matlab:uninstall_mtex">Uninstall MTEX</a>')
+  else
+    disp('- <a href="matlab:install_mtex">Install MTEX for future sessions</a>')    
+  end
+  
   disp(' ');
+  
 end
