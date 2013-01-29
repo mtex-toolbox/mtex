3b0884504dfbc36228a5c78093a747269ba4dad2
hielscher <hielscher@ptiza>
2013-01-29 08:47:13 +0100

    reoved duplication of rhoInside removed obolete files

--- geometry/@vector3d/private/project.m
+++ geometry/@vector3d/private/project.m
@@ -19,7 +19,7 @@ end
 
 % check for azimuth angle
 if extend.maxRho - extend.minRho < 2*pi-1e-6  
-  inRho = inside(rho,extend.minRho,extend.maxRho) | isnull(sin(theta));  
+  inRho = rhoInside(rho,extend.minRho,extend.maxRho) | isnull(sin(theta));  
   rho(~inRho) = NaN;
 end
 
@@ -69,20 +69,3 @@ end
 X = reshape(X,size(v));
 Y = reshape(Y,size(v));
 
-
-function ind = inside(rho,minRho,maxRho)
-
-minr = mod(minRho+1e-6,2*pi)-3e-6; % in [-2e-6,2*pi-3e-6]
-maxr = mod(maxRho-1e-6,2*pi)+3e-6; % in [2e-6,2*pi+3e-6]
-
-if minr < 0
-  rho = mod(rho+1e-6,2*pi);
-else
-  rho = mod(rho-1e-6,2*pi);
-end
-
-if minr < maxr
-  ind = rho > minr & rho < maxr;
-else
-  ind = rho > minr | rho < maxr;
-end
