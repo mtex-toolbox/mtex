b0f64f42c671828e096f27e3bc163c5bbb37c195
hielscher <hielscher@ptiza>
2013-02-20 20:51:55 +0100

    * checkSinglePhase works now also if there are not indexed data present * checkSinglePhase returns only indexed phases and the coresponding symmetry * this may also effect other functions

--- qta/@EBSD/private/checkSinglePhase.m
+++ qta/@EBSD/private/checkSinglePhase.m
@@ -1,30 +1,32 @@
-function varargout = checkSinglePhase(ebsd)
+function [ebsd,cs] = checkSinglePhase(ebsd)
 
 % check only a single phase is involved
 try
   
-  if numel(unique(ebsd.phase)) > 1
-    
+  % restrict to indexed phases
+  phases = find(cellfun(@(cs) isa(cs,'symmetry'),ebsd.CS));
+  
+  phases = intersect(phases,unique(ebsd.phase));
+  
+  if numel(phases) > 1
+  
     error('MTEX:MultiplePhases',['This operatorion is only permitted for a single phase! ' ...
-      'Please see ' doclink('xx','xx')  ...
-      '  for restrict EBSD data to a single phase.']);
+      'Please see ' doclink('EBSDModifyData','modify EBSD data')  ...
+      ' for how to restrict EBSD data to a single phase.']);
+  
+  elseif isempty(phases)
     
-  elseif numel(ebsd.phase) == 0
-    
-    error('MTEX:MultiplePhases',['The EBSD set is Empty! ' ...
-      'Please see ' doclink('xx','xx') ' for restrict a EBSD data to a single phase.']);
-    
-%   elseif any(isNotIndexed(ebsd))
-%     
-%      error('MTEX:MultiplePhases',['The EBSD data contains only non-indexed orientations! ' ...
-%       'Please see ' doclink('xx','xx') ' for restrict EBSD data to a single phase.']);
+    error('MTEX:NoPhase','There are no indexed data in this variable!');
     
   end
   
-  if nargout > 0
-    varargout{1} = true;
-  end
+  ebsd = subsref(ebsd,ebsd.phase == phases);
+  cs = ebsd.CS{phases};
   
+  if numel(ebsd.phase) == 0
+    error('MTEX:MultiplePhases','The EBSD set is Empty!');
+  end
+    
 catch e
   throwAsCaller(e)
 end
