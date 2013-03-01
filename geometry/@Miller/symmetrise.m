81db06b26462f8be68c3ae784b10a88b704091a3
hielscher <hielscher@ptiza>
2013-03-01 07:48:55 +0100

    very bad workaround to be removed soon

--- geometry/@Miller/symmetrise.m
+++ geometry/@Miller/symmetrise.m
@@ -1,4 +1,4 @@
-function [m,l] = symmetrise(m,varargin)
+function varargout = symmetrise(m,varargin)
 % directions symmetrically equivalent to m
 %
 %% Syntax
@@ -13,8 +13,20 @@ function [m,l] = symmetrise(m,varargin)
 %% Options
 %  antipodal - include [[AxialDirectional.html,antipodal symmetry]]
 
+%% !!!!!! very bad workaround !!!!!!!!
+% somtimes this function gets called although first argument is vector3d
+if ~isa(m,'Miller')
+  isMiller = cellfun(@(x) isa(x,'Miller'),varargin);
+  varargin(isMiller) = [];
+  [varargout{1:nargout}] = symmetrise(m,varargin{:});
+  return
+end  
+
+%%
 if nargout==2
   [m.vector3d,l] = symmetrise(m.vector3d,m.CS,varargin{:});
+  varargout = {m,l};
 else
   m.vector3d = symmetrise(m.vector3d,m.CS,varargin{:});
+  varargout = {m};
 end
