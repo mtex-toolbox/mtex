0ff0d14e7d4abcd434543521a2ca8b128299411b
hielscher <hielscher@ptiza>
2013-03-11 08:15:12 +0100

    remove Miller superior to vector3d this hopefuly fixes a long standing bug that optional arguments of type Miller lead to wrong function calls.
    Now the effectd vector3d methods check themselfes whether the Miller routine has
    to be called.
    
    Would be nice to have this for quaternion as well to eliminate all superiorto calls.
    
    This removes also the undocumented option 'all' in Miller/dot_outer.
    I couln't find that it has ever been used.

--- geometry/@Miller/symmetrise.m
+++ geometry/@Miller/symmetrise.m
@@ -13,16 +13,6 @@ function varargout = symmetrise(m,varargin)
 %% Options
 %  antipodal - include [[AxialDirectional.html,antipodal symmetry]]
 
-%% !!!!!! very bad workaround !!!!!!!!
-% somtimes this function gets called although first argument is vector3d
-if ~isa(m,'Miller')
-  isMiller = cellfun(@(x) isa(x,'Miller'),varargin);
-  varargin(isMiller) = [];
-  [varargout{1:nargout}] = symmetrise(m,varargin{:});
-  return
-end  
-
-%%
 if nargout==2
   [m.vector3d,l] = symmetrise(m.vector3d,m.CS,varargin{:});
   varargout = {m,l};
