2a5cc78cee00929c8e0fba71eba385f89301b441
Hielscher <ralfHielscher@users.noreply.github.com>
2013-04-29 09:30:16 +0200

    some speed ups

--- geometry/@vector3d/subsref.m
+++ geometry/@vector3d/subsref.m
@@ -1,12 +1,12 @@
 function v = subsref(v,s)
 %overloads subsref
 
-if isa(s,'double') || isa(s,'logical')
-  v.x = v.x(s);
-  v.y = v.y(s);
-  v.z = v.z(s);
-else
+if isstruct(s)
   v.x = subsref(v.x,s);
   v.y = subsref(v.y,s);
   v.z = subsref(v.z,s);
+else
+  v.x = v.x(s);
+  v.y = v.y(s);
+  v.z = v.z(s);  
 end
