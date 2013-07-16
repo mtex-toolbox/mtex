a13382fda764af43f02162869ba579ea67291f07
flb <famb@users.noreply.github.com>
2013-07-16 18:24:20 +0200

    forgotten to update FMC demo

--- examples/EBSD_FMC_demo.m
+++ examples/EBSD_FMC_demo.m
@@ -10,21 +10,8 @@
 
 
 %% Import EBSD data
-% specify Crystal and Specimen Symmetry
-CS = symmetry('cubic'); % crystal symmetry
-SS = symmetry('cubic');   % specimen symmetry
 
-% file name 
-fname = fullfile(mtexDataPath,'EBSD','single_grain_aluminum.txt');
-
-% fname = 'scanClean35.txt';
-
-% import ebsd data
-ebsd = loadEBSD(fname, CS, SS, 'interface','generic',...
-   'RADIANS','ColumnNames', { 'Euler 1' 'Euler 2' 'Euler 3' 'x' 'y'},...
-  'Columns', [1 2 3 4 5],'ignorePhase', 0, 'Bunge');
-
-% plotting convention
+mtexdata single
 plotx2east
 
 figure('position',[100 100 800 350])
@@ -33,16 +20,13 @@ plot(ebsd)
 %% Segment with thresholding
 % No pixel-to-pixel threshold value captures important boundaries without
 % including many irrelevant or false regions.
-grains_high = calcGrainsFMC(ebsd,'threshold',1*degree, 'keepNotIndexed');
-grains_low = calcGrainsFMC(ebsd,'threshold',0.5*degree, 'keepNotIndexed');
-
+grains_high = calcGrains(ebsd,'angle',1*degree);
+grains_low  = calcGrains(ebsd,'angle',0.5*degree);
 
 figure('position',[100 100 800 350])
 plot(grains_high)
 figure('position',[500 100 800 350])
 plot(grains_low)
-
-
 
 %% Segment with FMC
 % Analogous with the threshold angle, a  single parameter, C_Maha controls
@@ -51,7 +35,7 @@ plot(grains_low)
 % general features, while a value of 4 identifies finer features but is
 % slightly oversegmented.
 
-grains_FMC = calcGrainsFMC(ebsd,'FMC',3.5)
+grains_FMC = calcGrains(ebsd,'FMC',3.5)
 
 %%
 % plot the grains
