cc97aa54cc990a67c4fc5d1762f13f1a73e2f200
flb <famb@users.noreply.github.com>
2013-07-17 16:40:05 +0200

    quaternion unique works now like vector3d unique

--- geometry/@quaternion/unique.m
+++ geometry/@quaternion/unique.m
@@ -10,4 +10,13 @@ function q = unique(q,varargin)
 %% Output
 %  q - @quaternion
 
-q = cunion(q,@(a,b) eq(a,b,varargin{:}));
+% q = cunion(q,@(a,b) eq(a,b,varargin{:}));
+
+% find duplicates points
+[ignore,m,n] = unique(round([q.a(:),q.b(:),q.c(:),q.d(:)]*1e7),'rows'); %#ok<ASGLU>
+
+% remove duplicated points
+q.a = q.a(m);
+q.b = q.b(m);
+q.c = q.c(m);
+q.d = q.d(m);
