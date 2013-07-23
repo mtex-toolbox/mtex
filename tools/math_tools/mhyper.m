87697d8b46ca01c3e4c6e8f1096cb94675273836
flb <flb@localhost>
2013-07-23 08:06:50 +0200

    fix mhyper for larger kappa

--- tools/math_tools/mhyper.m
+++ tools/math_tools/mhyper.m
@@ -13,32 +13,29 @@ function y = mhyper(kappa)
 s = length(kappa);
 p = poly(kappa);          % characteristic polynomial of matrix X  (with roots a_i)
 p = fliplr(p(2:end)); % eliminate leading coefficient and reflect
-d = ones(s,1);
 
 % corresponds to 1/b, where  b is the second argument of 1F1
 beta1 = s/2;
-c = 1/beta1;
+d = ones(s,1)/beta1;
 
 y = 1;
 % the first few steps are a little different
 for i=1:s-1
   d(i+1) = (-((i:2*i-1)./(2*i)).*p(s-i+1:s))*d(1:i);
-  y = y+c*d(i+1);
-  c = c/(beta1+i);
+  y = y+d(i+1);
+  d = d/(beta1+i);
 end
 
 circshift = mod(1:s,s)+1;
 
 kp = (-s:-1)/2.*p;
 % the main iteration, note the cyclic shift of the entries of vector d
-while c*d(end) > eps
+while d(end) > eps
   i = i+1;
   
-  d(1) = (-(kp/i+p))*d;
-  d = d(circshift);
-  
-  y = y + c*d(end);
-  c = c/(beta1+i);
+  d(1) = (-(kp/i+p))*d; 
+  y = y + d(1);
+  d = d(circshift)/(beta1+i);
 end
 
 
