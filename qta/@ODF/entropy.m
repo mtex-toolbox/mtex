18ab1b07b353395a34e3c0075ff650819f6173a9
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2013-09-06 08:01:20 +0200

    bug fix: entropy should be real

--- qta/@ODF/entropy.m
+++ qta/@ODF/entropy.m
@@ -24,4 +24,4 @@ S3G = extract_SO3grid(odf,varargin{:});
 % eval odf
 %disp(' evaluate odf');
 e = eval(odf,S3G,varargin{:});
-e = - nansum(e .* log(e))/numel(S3G);
+e = real(- nansum(e .* log(e))/numel(S3G));
