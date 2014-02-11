e290a03374ed0babef49e6513787587338812faf
Hielscher <ralfHielscher@users.noreply.github.com>
2014-02-11 08:25:59 +0100

    clean up tools directory

--- qta/FMC/clusterBreaker.m
+++ qta/FMC/clusterBreaker.m
@@ -1,4 +1,4 @@
-function [assignmentsN flag contPoints] = clusterBreaker(assignmentsN, point, assignments, flag, NNlist, oldClust, newClust, contPoints, rDepth, rLimit)
+function [assignmentsN, flag, contPoints] = clusterBreaker(assignmentsN, point, assignments, flag, NNlist, oldClust, newClust, contPoints, rDepth, rLimit)
 %Recursively assigns points based on neighboring assignments
 %If flag == 0, the point is fresh, 
 %           1, the point is set
@@ -23,7 +23,7 @@ if ((flag(point) == 0) || (flag(point) > 1))
             if ((assignments(n,1) == oldClust) && (flag(n) == 0))
                 flag(n) = newClust;     %Reserve the similar point
                 if rDepth < rLimit      %Don't break MATLAB...
-                    [assignmentsN flag contPoints] = clusterBreaker(assignmentsN, n, assignments, flag, NNlist, oldClust, newClust, contPoints, rDepth + 1, rLimit);
+                    [assignmentsN, flag, contPoints] = clusterBreaker(assignmentsN, n, assignments, flag, NNlist, oldClust, newClust, contPoints, rDepth + 1, rLimit);
                     assignmentsN(n,1) = newClust;
                     else
                     contPoints = [contPoints n];
