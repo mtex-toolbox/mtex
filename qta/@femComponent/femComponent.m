f777e7d9fa4e3d1b6c3f5c0ce2bce1f223c4051a
Hielscher <ralfHielscher@users.noreply.github.com>
2014-02-18 09:53:28 +0100

    updated FEM component

--- qta/@femComponent/femComponent.m
+++ qta/@femComponent/femComponent.m
@@ -15,27 +15,15 @@ classdef femComponent < ODFComponent
  
   methods
     
-    function odf = femODF(varargin)
+    function odf = femODF(center,weights)
                       
       if nargin == 0, return;end
       
       % get center
-      if nargin > 0 && isa(varargin{1},'quaternion')
-
-        odf.center = varargin{1};
-        
-        if isa(odf.center,'orientation')
-          odf.CS = get(odf.center,'CS');
-          odf.SS = get(odf.center,'SS');
-        else
-          odf.center = orientation(odf.center,odf.CS,odf.SS);
-        end
-      else
-        odf.center = orientation(idquaternion,odf.CS,odf.SS);
-      end
-
+      odf.center = center;
+      
       % get weights
-      odf.weights = get_option(varargin,'weights',ones(size(odf.center)));
+      odf.weights = weights;
       assert(numel(odf.weights) == length(odf.center),...
         'Number of orientations and weights must be equal!');
             
