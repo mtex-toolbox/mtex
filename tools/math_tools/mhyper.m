a44d6372d8dd21baf5e563fec0c979a82ef55c06
flb <flb@localhost>
2014-03-06 08:47:30 +0100

    fix mhyper

--- tools/math_tools/mhyper.m
+++ tools/math_tools/mhyper.m
@@ -2,10 +2,10 @@ function y = mhyper(kappa)
 % multivariat hypergeometric function 1F1(1/2,p/2, kappa)
 %
 % reference:
-% G. HILLIER, R. KAN, and X. WANG: 
-%   Computationally Efficient Recursions for Top-Order Invariant 
+% G. HILLIER, R. KAN, and X. WANG:
+%   Computationally Efficient Recursions for Top-Order Invariant
 %   Polynomials with Applications
-%   in Econometric Theory, 25(1), 211-242, 2009. 
+%   in Econometric Theory, 25(1), 211-242, 2009.
 %   http://www.rotman.utoronto.ca/~kan/papers/zonal5.pdf
 % with many thanks to Raymond Kan
 %
@@ -29,11 +29,12 @@ end
 circshift = mod(1:s,s)+1;
 
 kp = (-s:-1)/2.*p;
+
 % the main iteration, note the cyclic shift of the entries of vector d
-while d(end) > eps
+while norm(d) > eps
   i = i+1;
   
-  d(1) = (-(kp/i+p))*d; 
+  d(1) = (-(kp/i+p))*d;
   y = y + d(1);
   d = d(circshift)/(beta1+i);
 end
