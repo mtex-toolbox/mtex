f180aa982d75b77cf897f4cd5e217310a5046b92
Hielscher <ralfHielscher@users.noreply.github.com>
2014-03-08 22:56:40 +0100

    new class sphericalProjection

---
+++ geometry/sphericaProjections/orthographicProjection.m
@@ -0,0 +1,26 @@
+classdef orthographicProjection < sphericalProjection
+  %equal area projection
+  
+  methods 
+        
+    function [x,y] = project(sP,v,varargin)
+      % compute polar angles
+  
+      [rho,theta] = project@sphericalProjection(sP,v,varargin{:});
+
+      % formula for equal area projection
+      r =  sin(theta);
+            
+      % compute coordinates
+      x = reshape(cos(rho) .* r,size(v));
+      y = reshape(sin(rho) .* r,size(v));
+      
+    end
+    
+    function v = iproject(sP,x,y)
+      v = vector3d('theta',x*degree,'rho',y*degree);
+    end
+    
+  end
+  
+end
---
+++ geometry/sphericaProjections/plainProjection.m
@@ -0,0 +1,26 @@
+classdef plainProjection < sphericalProjection
+  %sphericalProjection
+  
+  methods 
+    
+    function proj = plainProjection(varargin)
+      proj = proj@sphericalProjection(varargin{:});
+    end
+    
+    function [rho,theta] = project(sP,v,varargin)
+      % compute polar angles
+  
+      [rho,theta] = project@sphericalProjection(sP,v,varargin{:});
+      
+      rho = reshape(rho,size(v))./ degree;
+      theta = reshape(theta,size(v))./ degree;
+      
+    end
+    
+    function v = iproject(sP,x,y)
+      v = vector3d('theta',x*degree,'rho',y*degree);
+    end
+    
+  end
+  
+end
