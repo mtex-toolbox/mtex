381f22ebb8779fdb349953bc11404372c061b58b
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-03-28 06:58:18 +0100

    reflections by normals

---
+++ geometry/geometry_tools/reflection.m
@@ -0,0 +1,14 @@
+function r = reflection(n)
+% defines a reflection at plane with normal n
+%
+% Syntax
+%   r = reflection(n)
+%
+% Input
+%  n - plane normals of the reflection planes
+%
+% Output
+%  r - improper @rotation
+%
+
+r = -rotation('axis',n,'angle',pi);
