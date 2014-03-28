bf7b0c2dadc3bf971d3ce905f86de752db477e5f
Hielscher <ralfHielscher@users.noreply.github.com>
2014-03-28 07:03:41 +0100

    adapt surf to mtexFigure

--- geometry/@vector3d/surf.m
+++ geometry/@vector3d/surf.m
@@ -1,4 +1,4 @@
-function varargout = surf(v,varargin)
+function surf(v,cdata,varargin)
 %
 % Syntax
 %
@@ -11,57 +11,35 @@ function varargout = surf(v,varargin)
 % See also
 %
 
-% get input
 
-% where to plot
-[ax,v,varargin] = splitNorthSouth(v,varargin{:},'surf');
-if isempty(ax), return;end
+% initialize spherical plot
+sP = newSphericalPlot(v,varargin{:});
 
-% extract plot options
-[projection,extend] = getProjection(ax,v,varargin{:});
+for j = 1:numel(sP)
 
-% why should I do this?
-hfig = get(ax,'parent');
-set(hfig,'color',[1 1 1]);
+  %set(gcf,'renderer','zBuffer');
+  %shading interp
 
-% extract colors 
-cdata = varargin{1};
-
-set(gcf,'renderer','zBuffer');
-shading interp
-
-% draw surface
-
-hold(ax,'on')
+  % draw surface
+      
+  % project data
+  [x,y] = project(sP(j).proj,v,'removeAntipodal');
     
-% project data
-[x,y] = project(v,projection,extend,'removeAntipodal');
+  % extract non nan data
+  ind = ~isnan(x);
+  x = submatrix(x,ind);
+  y = submatrix(y,ind);
+  data = reshape(submatrix(cdata,ind),[size(x) 3]);
+  
+  % otherwise surf would change current axes settings
+  hold(sP(j).ax,'on')
+  
+  % plot surface  
+  h = surf(x,y,zeros(size(x)),real(data),'parent',sP(j).ax);
     
-% extract non nan data
-ind = ~isnan(x);
-x = submatrix(x,ind);
-y = submatrix(y,ind);
-data = reshape(submatrix(cdata,ind),[size(x) 3]);
-     
-% plot surface
-h = surf(ax,x,y,zeros(size(x)),real(data));
-    
-hold(ax,'off')
+  hold(sP(j).ax,'off')
+  
+  % set styles
+  optiondraw(h,'LineStyle','none','Fill','on',varargin{:});
 
-% set styles
-optiondraw(h,'LineStyle','none','Fill','on',varargin{:});
-
-
-%-------- finalize the plot ---------------------------
-
-% plot polar grid
-plotGrid(ax,projection,extend,varargin{:});
-
-% plot annotations
-plotAnnotate(ax,varargin{:})
-
-% output
-if nargout > 0
-  varargout{1} = h;
 end
-
