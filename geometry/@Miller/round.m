529841176cd7c5cb9875aa2a79c3d6371d3432ce
Hielscher <ralfHielscher@users.noreply.github.com>
2014-03-28 06:54:49 +0100

    round now return Miller

--- geometry/@Miller/round.m
+++ geometry/@Miller/round.m
@@ -2,20 +2,24 @@ function h = round(h,varargin)
 % tries to round miller indizes to greatest common divisor
 
 
-if check_option(h,'uvw')
-  mv = v2d(h);  
-else
-  mv = v2m(h);
+switch h.dispStyle
+  
+  case 'uvw'
+    mv = v2d(h);  
+  case 'hkl'
+    mv = v2m(h);
+  otherwise
+    return;
 end
 
 mv = mv(:,[1 2 end])';
 
 mbr = reshape(selectMaxbyColumn(abs(mv)),size(h));
 
-tol = get_option(varargin,{'tol','tolerance'},1*degree);
 maxHKL = get_option(varargin,'maxHKL',12);
 
-for im = 1:length(h)
+fak = ones(size(h));
+for im = 1:size(mv,2)
 %   
   mm = mv(:,im)/mbr(im) * (1:maxHKL);  
   rm = round(mm);
@@ -23,12 +27,10 @@ for im = 1:length(h)
   e = sum(mm ./ repmat(sqrt(sum(mm.^2,1)),3,1) .* rm ./ repmat(sqrt(sum(rm.^2,1)),3,1));
   
   e = round(e*1e7);
-  [e,j] = sort(e,'descend');
+  [~,j] = min(e);
   
-  h(im) = h(im)*(j(1)/mbr(im)); 
+  fak(im) = j/mbr(im);
+  
 end
 
-
-
-
-
+h = h .* fak;
