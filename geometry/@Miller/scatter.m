e35e552bbb691c9b046ca89e11e75bb1224cc2cf
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-04-01 23:27:09 +0200

    bug fix

--- geometry/@Miller/scatter.m
+++ geometry/@Miller/scatter.m
@@ -15,17 +15,27 @@ function scatter(m,varargin)
 % vector3d/scatter
 
 % symmetrise if needed
-if check_option(varargin,'symmetrised') && ~check_option(varargin,'skipSymmetrise')
-  
-  % first dimension cs - second dimension m
-  m = symmetrise(m,varargin{:});
+if check_option(varargin,'symmetrised') && ~check_option(varargin,'skipSymmetrise')  
   
   % restrict to fundamental region
   varargin = [varargin,{'removeAntipodal','skipSymmetrise'}];
-
-  % symmetrise data
-  if numel(varargin) > 0 && isnumeric(varargin{1}) && ~isempty(varargin{1})
-    varargin{1} = repmat(varargin{1}(:)',size(m,1),1);
+  
+  % symmetrise data with repetition
+  if numel(varargin) > 0 && isnumeric(varargin{1}) && ~isempty(varargin{1});
+  
+    % first dimension cs - second dimension m
+    m = symmetrise(m,varargin{:});
+    
+    varargin{1} = repmat(varargin{1}(:)',size(m,1),1);    
+      
+  elseif numel(m) < 100 || check_option(varargin,{'labeled','label'}) 
+  
+      [m,l] = symmetrise(m); % symmetrise without repetition
+        
+  else 
+    
+    m = symmetrise(m,varargin{:}); % symmetrise with repetition
+    
   end
   
 end
