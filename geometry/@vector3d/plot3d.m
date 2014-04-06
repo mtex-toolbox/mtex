a6b16ddac7fe953013f4fed2ed75db5710bfe4a2
Hielscher <ralfHielscher@users.noreply.github.com>
2014-04-06 14:19:17 +0200

    minor imrovements on 3d

--- geometry/@vector3d/plot3d.m
+++ geometry/@vector3d/plot3d.m
@@ -23,15 +23,6 @@ surf(v.x,v.y,v.z,reshape(data,size(v,1),size(v,2),[]),'parent',ax);
 
 shading(ax,'interp');
 axis(ax,'equal','vis3d','off');
-axis on
-c = caxis(ax);
-arrow3d(zeros(3),eye(3)*1.5,15,'cylinder',[0.15,0.15]);
-caxis(ax,c);
-
-text(1.6,0,0,'x','FontSize',15,'parent',ax)
-text(0,1.6,0,'y','FontSize',15,'parent',ax)
-text(0,0,1.6,'z','FontSize',15,'parent',ax)
-%set(gca,'position',[-0.3,-0.3,1.6,1.6]);
 
 set(ax,'XDir','rev','YDir','rev',...
-'XLim',[-1.5,1.5],'YLim',[-1.5,1.5],'ZLim',[-1.5,1.5]);
+'XLim',[-1,1],'YLim',[-1,1],'ZLim',[-1,1]);
--- geometry/@vector3d/scatter3d.m
+++ geometry/@vector3d/scatter3d.m
@@ -26,17 +26,8 @@ h = scatter3(v.x(:),v.y(:),v.z(:),30,...
   [reshape(data,length(v)/2,[]);reshape(data,length(v)/2,[])],'filled');
 
 axis(ax,'equal','vis3d','off');
-axis on
-c = caxis(ax);
-arrow3d(zeros(3),eye(3)*1.5,15,'cylinder',[0.15,0.15]);
-caxis(ax,c);
-
-text(1.6,0,0,'x','FontSize',15,'parent',ax)
-text(0,1.6,0,'y','FontSize',15,'parent',ax)
-text(0,0,1.6,'z','FontSize',15,'parent',ax)
-%set(gca,'position',[-0.3,-0.3,1.6,1.6]);
 
 set(ax,'XDir','rev','YDir','rev',...
-'XLim',[-1.5,1.5],'YLim',[-1.5,1.5],'ZLim',[-1.5,1.5]);
+'XLim',[-1.02,1.02],'YLim',[-1.02,1.02],'ZLim',[-1.02,1.02]);
 
 if nargout == 0, clear h;end
