880d249958196b6700ca3125aaeb795c389f905c
Hielscher <ralfHielscher@users.noreply.github.com>
2014-06-25 07:21:39 +0200

    some new symmetries: mm2, m2m, 2mm

--- geometry/@symmetry/private/calcQuat.m
+++ geometry/@symmetry/private/calcQuat.m
@@ -4,7 +4,14 @@ function rot = calcQuat(Laue,axis,inv,varargin)
 ll0axis = vector3d(1,1,0);
 %Tl0axis = axis(1)+axis(2);
 lllaxis = vector3d(1,1,1);
-maxis = axis(1);
+
+if check_option(varargin,{'2||a','m||a'})
+  twoFoldAxis = axis(1);
+elseif check_option(varargin,{'2||c','m||c'})
+  twoFoldAxis = axis(3);
+else
+  twoFoldAxis = axis(2);
+end
 
 % compute rotations
 switch Laue
@@ -14,16 +21,9 @@ switch Laue
   
     % determine rotation axis
     % this should be orthogonal to the other two axes
-    orth = sum(abs(dot_outer(axis,axis)));
-    
+    orth = sum(abs(dot_outer(axis,axis)));    
     if all(round(orth)==1)
-      if check_option(varargin,'2||a')
-        rot = {Axis(axis(1),2)};
-      elseif check_option(varargin,'2||c')
-        rot = {Axis(axis(3),2)};
-      else
-        rot = {Axis(axis(2),2)};
-      end
+      rot = {Axis(twoFoldAxis,2)};    
     else
       rot = {Axis(axis(round(orth)==1),2)};
     end
@@ -32,7 +32,7 @@ switch Laue
   case '-3'
     rot = {Axis(axis(3),3)};
   case '-3m'
-    rot = {Axis(maxis,2),Axis(axis(3),3)};
+    rot = {Axis(twoFoldAxis,2),Axis(axis(3),3)};
   case '4/m'
     rot = {Axis(axis(3),4)};
   case '4/mmm' 
@@ -40,7 +40,7 @@ switch Laue
   case '6/m'
     rot = {Axis(axis(3),6)};
   case '6/mmm'
-    rot = {Axis(maxis,2),Axis(axis(3),6)};
+    rot = {Axis(twoFoldAxis,2),Axis(axis(3),6)};
   case 'm-3'
     rot = {Axis(lllaxis,3),Axis(axis(1),2),Axis(axis(3),2)};
   case 'm-3m'
