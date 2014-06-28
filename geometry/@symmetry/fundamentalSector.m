566c4fd43f5d0a9aedf8b549d3fee90a10269f37
Hielscher <ralfHielscher@users.noreply.github.com>
2014-06-29 01:18:27 +0200

    support for even more symmetries, i.e. -4m2 and -42m

--- geometry/@symmetry/fundamentalSector.m
+++ geometry/@symmetry/fundamentalSector.m
@@ -31,59 +31,55 @@ if length(cs) > 1+length(N)
   N = [N,vector3d('theta',90*degree,'rho',[90*degree,drho-90*degree])];
 end
 
-% TODO correct for different alignments in 32
-
 aAxis = Miller(1,0,0,cs);
 
 % some special cases
 switch cs.id
   
-  case 1 % 1
-       
-  case 2 % -1
-    
-    N = zvector;
-    
-  case 3 % 2
-    
+  case 1 % 1       
+  case 2 % -1    
+    N = zvector;    
+  case {3,6,9} % 2    
     if isnull(dot(getMinAxes(cs),zvector))
       N = zvector;
     else
       ind = find(isnull(dot(getMinAxes(cs),cs.axes)),1);
       N = cs.axes(ind);
     end
-  case 4 % m
+  case {4,7,10} % m
     N = getMinAxes(cs);
-  case 5 % 2/m
-    
-  case 6 % 222
-  case {7,33,34} % 2mm, m2m, mm2
+  case 5 % 2/m11
+    N = rotate(N,-90*degree);
+  case {8,11} % 12/m1 112/m  
+  case 12 % 222    
+  case {13,14,15} % 2mm, m2m, mm2
     N = cs.subSet(cs.isImproper).axis; % take mirror planes
-  case 8 % mmm
-  case 9 % 3
-  case 10 % -3
-  case 11 % 32
-    N = rotate(N,rotation('axis',zvector,'angle',aAxis.rho-120*degree));
-  case 12 % 3m
-    N = rotate(N,rotation('axis',zvector,'angle',aAxis.rho-120*degree));
-  case 13
-    N = rotate(N,rotation('axis',zvector,'angle',aAxis.rho-120*degree));
-  case {21,24} % 6, 622    
-    N = rotate(N,-mod(round(aAxis.rho./degree),60)*degree);
-  case 19 %-42m
+  case 16 % mmm
+  case 17 % 3
+  case 18 % 3, -3
+    N = rotate(N,-mod(round(aAxis.rho./degree),120)*degree);   
+  case {19,20,21} % 32, 3m, -3m
+    N = rotate(N,-mod(round(aAxis.rho./degree),120)*degree);   
+  case {22,23,24}
+    N = rotate(N,-mod(round(aAxis.rho./degree-30),60)*degree);   
+  case 30 %-42m
     N = rotate(N,-45*degree);
-  case 26 % 6m2
+  case {33,34,35,36} % 6, 622    
+    N = rotate(N,-mod(round(aAxis.rho./degree),60)*degree);  
+  case 38 % 62m
     N = rotate(N,-mod(round(aAxis.rho./degree-30),60)*degree);
-  case 28 % 23
+  case 39 % 6m2
+    N = rotate(N,-mod(round(aAxis.rho./degree),60)*degree);
+  case 41 % 23
     %N = [vector3d(0,-1,1),vector3d(-1,0,1),vector3d(1,0,1),yvector,zvector];
     N = vector3d([1 1 0 0],[1 -1 1 -1],[0 0 1 1]);
-  case 29 % m-3
+  case 42 % m-3
     N = [vector3d(0,-1,1),vector3d(-1,0,1),xvector,yvector,zvector];
-  case 30 % 432
+  case 43 % 432
     N = [vector3d(1,-1,0),vector3d(0,-1,1),yvector];
-  case 31 % -43m
+  case 44 % -43m
     N = [vector3d(1,-1,0),vector3d(1,1,0),vector3d(-1,0,1)];
-  case 32 % m-3m    
+  case 45 % m-3m    
     N = [vector3d(1,-1,0),vector3d(-1,0,1),yvector];
 end
 
@@ -93,146 +89,3 @@ else
   sR = sphericalRegion(N);
 end
 
-return
-
-% default values from the symmetry
-minTheta = 0;
-maxRho = cs.rotangle_max_z;
-minRho = 0;
-if check_option(varargin,'antipodal') && cs.rotangle_max_y/2 < pi
-  maxRho = maxRho / 2;
-end
-maxTheta = cs.rotangle_max_y(varargin{:})/2;
-v = [Miller(1,0,0),Miller(1,1,0),Miller(0,1,0),Miller(-1,2,0),Miller(0,0,1)];
-
-fak = 2;
-switch cs.LaueName
-  case '-3m'
-    minRho = mod(cs.axes(1).rho,120*degree);
-    if check_option(varargin,'antipodal')
-      minRho = minRho-30*degree;
-      fak = 1;
-    end
-  case 'm-3m'
-    if check_option(varargin,'antipodal')
-      maxRho = pi/4;
-      maxTheta = @(rho) pi- atan2(cos(pi/4),sin(pi/4)*cos(mod(rho,2*maxRho)+pi));
-    else
-      maxRho = pi/4;
-      maxTheta = @(rho) pi- atan2(cos(pi/4),sin(pi/4)*cos(mod(rho,2*maxRho)+pi/2));
-    end
-    v = [Miller(0,0,1),Miller(1,1,0),Miller(1,1,1)];
-  case 'm-3'
-    if check_option(varargin,'antipodal')
-      %maxRho = pi/4;
-      %maxTheta = @(rho) pi- atan2(cos(pi/4),sin(pi/4)*cos(mod(rho,2*maxRho)+pi/2));
-      maxRho = pi/2;
-      maxTheta = @(rho) maxThetam3(mod(rho,maxRho));
-    else
-      maxRho = pi;
-      maxTheta = @(rho) maxThetam3(mod(rho,maxRho));
-    end
-    v = [Miller(0,0,1),Miller(1,1,0),Miller(1,1,1),Miller(1,-1,1)];
-  otherwise
-end
-
-%
-
-if check_option(varargin,{'complete','3d'})
-  minRho = 0;
-  maxRho = 2*pi;
-  minTheta = 0;
-  maxTheta = pi;
-  v = [];
-else
-  
-  % try to make minRho close to true x Axis  
-  drho = minRho - (1-NWSE(getMTEXpref('xAxisDirection')))*90*degree;  
-  drho = ceil(round(100*drho./maxRho*fak)./100)*maxRho/fak;
-  minRho = minRho - drho;
-  
-  v.CS = cs;
-  opt = extract_option(varargin,'antipodal');
-  v = unique(v,opt{:});
-end
-
-% get values from direct options
-minTheta = get_option(varargin,'minTheta',minTheta);
-maxTheta = get_option(varargin,'maxTheta',maxTheta);
-minRho   = get_option(varargin,'minRho',minRho);
-maxRho = maxRho + minRho;
-maxRho   = get_option(varargin,'maxRho',maxRho);
-
-% restrict using meta options upper, lower
-
-
-
-if check_option(varargin,'upper') && isnumeric(maxTheta) && maxTheta > pi/2
-  maxTheta = pi/2;
-end
-
-if check_option(varargin,'lower') && isnumeric(maxTheta) && maxTheta > pi/2+0.001
-  
-  minTheta = pi/2;
-
-elseif check_option(varargin,'restrict2Hemisphere') ...
-    && isnumeric(maxTheta) && maxTheta>pi/2
-  
-  maxTheta = pi/2;
-
-end
-
-
-% TODO
-% find a position in the first quadrant, i.e. minRho + rotate should be
-%rotate = get_option(varargin,'rotate',0);
-
-
-% describe Fundamental region by normal to planes
-switch cs.LaueName
-
-  case 'm-3m' %ok
-
-    if check_option(varargin,'antipodal')
-      N = [vector3d(1,-1,0),vector3d(-1,0,1),yvector,zvector];
-    else
-      N = [vector3d(1,-1,0),vector3d(0,-1,1),yvector,zvector];
-    end
-
-  case 'm-3' %ok
-
-    if check_option(varargin,'antipodal')
-      N = [vector3d(0,-1,1),vector3d(-1,0,1),xvector,yvector,zvector];
-    else
-      N = [vector3d(0,-1,1),vector3d(-1,0,1),vector3d(1,0,1),yvector,zvector];
-    end
-
-  otherwise
-
-    N = vector3d;
-    if maxRho-minRho < 2*pi - 0.001
-      N = axis2quat(zvector,[minRho,maxRho]) .* [yvector,-yvector];
-    end
-
-    if maxTheta < pi
-      N = [N,zvector];
-    end
-
-end
-
-sR = sphericalRegion(N);
-
-end
-
-% --------------- private functions -----------------------
-function maxTheta = maxThetam3(rho)
-
-maxTheta = pi/2 * ones(size(rho));
-ind = rho <= pi/4;
-maxTheta(ind) = pi- atan2(cos(pi/4),sin(pi/4)*cos(rho(ind)+pi));
-ind = rho>pi/4 & rho <= 3/4*pi;
-maxTheta(ind) = pi- atan2(cos(pi/4),sin(pi/4)*cos(rho(ind)+pi/2));
-ind = rho>pi*3/4;
-maxTheta(ind) = pi- atan2(cos(pi/4),sin(pi/4)*cos(rho(ind)));
-
-end
--- geometry/@symmetry/private/Axis.m
+++
@@ -1,4 +0,0 @@
-function q = Axis(a,n)
-% quaternions for n fold symmetry axis a
-
-q = rotation('axis',a,'angle',2*pi/n*(0:n-1));
