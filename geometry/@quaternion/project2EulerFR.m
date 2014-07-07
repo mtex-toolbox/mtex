86db36861c94f07e4f69735febc2e9db76ea6bd8
Hielscher <ralfHielscher@users.noreply.github.com>
2014-07-07 08:05:55 +0200

    bug fix: correctly project Phi to the range [0,\pi/2] if needed

--- geometry/@quaternion/project2EulerFR.m
+++ geometry/@quaternion/project2EulerFR.m
@@ -1,10 +1,11 @@
-function [phi1,Phi,phi2] = project2EulerFR(q,CS1,CS2)
+function [phi1,Phi,phi2] = project2EulerFR(q,CS1,CS2,varargin)
 % projects quaternions to a fundamental region
 %
 % Syntax
-%   project2EulerFR(q,CS)       % to FR around idquaternion
-%   project2EulerFR(q,CS,q_ref) % to FR around reference rotation
-%   project2EulerFR(q,CS1,CS2)  % misorientation to FR around id
+%   [phi1,Phi,phi2] = project2EulerFR(q,CS)       % to FR around idquaternion
+%   [phi1,Phi,phi2] = project2EulerFR(q,CS,'Bunge')  % to FR around idquaternion
+%   [phi1,Phi,phi2] = project2EulerFR(q,CS,q_ref) % to FR around reference rotation
+%   [phi1,Phi,phi2] = project2EulerFR(q,CS1,CS2)  % misorientation to FR around id
 %
 % Input
 %  q        - @quaternion
@@ -12,17 +13,26 @@ function [phi1,Phi,phi2] = project2EulerFR(q,CS1,CS2)
 %  q_ref    - reference @quaternion single or size(q) == size(q_ref)
 %
 % Output
-%  q     - @quaternion
-%  omega - rotational angle to reference quaternion
+%  phi1,Phi,phi2 - Euler angles
 %
 
-[max_phi1,max_Phi,max_phi2] = getFundamentalRegion(CS1,CS2);
-%ori = ori.project2FundamentalRegion;
-   
-if max_Phi<pi
-  [axes,~] = DSO3.CS.getMinAxes;
-  ia = find(isnull(dot(axes,zvector)),1,'first');
-  [~,Phi,~] = Euler(ori);
-  ori(Phi > pi/2) = ori(Phi > pi/2) * orientation('axis',axes(ia),'angle',pi,DSO3.CS,DSO3.CS);
+[~,Phi,~] = Euler(q);
+
+isPerpZ1 = isnull(dot(CS1.axis,zvector)) & ~isnull(angle(CS1));
+isPerpZ2 = isnull(dot(CS2.axis,zvector)) & ~isnull(angle(CS2));
+ind = Phi > pi;
+
+s = substruct('()',{ind});
+if any(isPerpZ1)
+  q = subsasgn(q,s,q.subSet(ind) * CS1.subSet(find(isPerpZ1,1)));
+elseif any(isPerpZ2)
+  q = subsasgn(q,s,CS2.subSet(find(isPerpZ2,1)) * q.subSet(ind));
 end
-   
+
+[maxphi1,~,maxphi2] = getFundamentalRegion(CS1,CS2);
+
+% convert to euler angles angles
+[phi1,Phi,phi2] = Euler(q,varargin{:});
+
+phi1 = mod(phi1,maxphi1);
+phi2 = mod(phi2,maxphi2);
