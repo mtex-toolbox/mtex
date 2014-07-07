6ab6f41147881d1f02cb03a5fe0e9f9262a2f431
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-07-07 08:05:55 +0200

    bug fix: correctly project Phi to the range [0,\pi/2] if needed

--- tools/orientationMappings/BungeRGBOrientationMapping.m
+++ tools/orientationMappings/BungeRGBOrientationMapping.m
@@ -26,25 +26,11 @@ classdef BungeRGBOrientationMapping < orientationMapping
   
   methods
     function rgb = orientation2color(oM,ori)
-      
-      % get reference orientation
-      if oM.center ~= idquaternion
-
-        % restrict to fundamental region
-        ori = project2FundamentalRegion(ori,oM.center);
-      end
-
-      [maxphi1,maxPhi,maxphi2] = getFundamentalRegion(oM.CS1,oM.CS2);
             
       % convert to euler angles angles
-      [phi1,Phi,phi2] = Euler(ori,'Bunge');
-      phi1 = mod(phi1,maxphi1);
-      phi2 = mod(phi2,maxphi2);
-      % this is not realy correct
-      % it should change phi1 or phi2 as well
-      ind = Phi>maxPhi;
-      Phi(ind) = pi - Phi(ind);
-      
+      [phi1,Phi,phi2] = project2EulerFR(quaternion(ori),oM.CS1,oM.CS2,'Bunge');
+                       
+      % colorize Euler angles with red green blue
       r = (phi1 - oM.phi1Range(1))./(oM.phi1Range(2)-oM.phi1Range(1));
       g = (Phi - oM.PhiRange(1))./(oM.PhiRange(2)-oM.PhiRange(1));
       b = (phi2 - oM.phi2Range(1))./(oM.phi2Range(2)-oM.phi2Range(1));
