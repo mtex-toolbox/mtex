71cbe191437abf501ee87514cec33df66938dba1
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-07-11 10:45:38 +0200

    crc files also have misaligned reference frame for Euler and spatial data

--- qta/interfaces/loadEBSD_crc.m
+++ qta/interfaces/loadEBSD_crc.m
@@ -40,6 +40,18 @@ catch %#ok<CTCH>
   interfaceError(fname);
 end
 
+% change reference frame
+if check_option(varargin,'convertSpatial2EulerReferenceFrame')
+  ebsd = rotate(ebsd,rotation('axis',xvector,'angle',180*degree),'keepEuler');
+elseif check_option(varargin,{'convertEuler2SpatialReferenceFrame','wizard'}) 
+  ebsd = rotate(ebsd,rotation('axis',xvector,'angle',180*degree),'keepXY');
+else
+  warning(['.crc and .cpr files have usualy inconsistent conventions for spatial ' ...
+    'coordinates and Euler angles. You may want to use one of the options ' ...
+    '''convertSpatial2EulerReferenceFrame'' or ''convertEuler2SpatialReferenceFrame'' to correct for this']);  
+end
+
+
 
   function CS = getCS(cpr)
     
