5c95e1a5d5e98509a8782bcb3819bbfbcb1e752d
Hielscher <ralfHielscher@users.noreply.github.com>
2014-07-14 13:05:37 +0200

    bug fix: fibre portions should respect antipodal symmetry

--- ODFAnalysis/@fibreComponent/calcPDF.m
+++ ODFAnalysis/@fibreComponent/calcPDF.m
@@ -3,11 +3,11 @@ function Z = calcPDF(component,h,r,varargin)
 
 Z = zeros(length(h),length(r));
 
-sh = symmetrise(component.h,varargin{:});
-sr = symmetrise(component.r,component.SS);
+sh = vector3d(symmetrise(component.h,varargin{:}));
+sr = vector3d(symmetrise(component.r,component.SS));
 
 for i = 1:length(sh)
-  dh = dot_outer(sh(i).normalize,h.normalize);
+  dh = dot_outer(sh(i).normalize,vector3d(h.normalize));
   for j = 1:length(sr)
     dr = dot_outer(sr(j).normalize,r.normalize);
     Z = Z + component.weights * component.psi.RRK(dh.',dr) / length(sh);
