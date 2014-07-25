e8f17b2ba182988a7e3593d48645a1d76b9dc130
Hielscher <ralfHielscher@users.noreply.github.com>
2014-07-25 10:59:01 +0200

    calcOrientations to compute random sample from ODF

--- ODFAnalysis/@ODF/calcEBSD.m
+++ ODFAnalysis/@ODF/calcEBSD.m
@@ -1,4 +1,4 @@
-function ori = calcEBSD(odf,points,varargin)
+function ebsd = calcEBSD(odf,varargin)
 % simulate EBSD data from ODF
 %
 % Syntax
@@ -12,23 +12,8 @@ function ori = calcEBSD(odf,points,varargin)
 %  ebsd   - @EBSD
 %
 % See Also
-% ODF_calcPoleFigure
+% ODF_calcPoleFigure, ODF_calcOrientations
 
-% distribute samples over the parts of the ODF
-if length(odf.components) == 1
-  iodf = ones(points,1);
-else
-  iodf = discretesample([odf.weights], points);
-end
+ori = calcOrientations(odf,varargin{:});
 
-% preallocate orientations
-ori = repmat(orientation(idquaternion,odf.CS,odf.SS),points,1);
-
-% draw random sample from each component
-for i = 1:numel(odf.components)
-
-  ori(iodf==i) = discreteSample(odf.components{i},nnz(iodf==i),varargin{:});
-    
-end
-
-ori = EBSD(ori);
+ebsd = EBSD(ori);
