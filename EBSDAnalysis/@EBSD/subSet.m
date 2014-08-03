ce4ec2bb50668dd1f4cbcfa61718ca8d86bfe9a3
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-08-03 15:05:20 +0200

    grainId should be an additional property

--- EBSDAnalysis/@EBSD/subSet.m
+++ EBSDAnalysis/@EBSD/subSet.m
@@ -9,5 +9,5 @@ ebsd = subSet@dynProp(ebsd,ind);
 ebsd.rotations = ebsd.rotations(ind);
 ebsd.phaseId = ebsd.phaseId(ind);
 ebsd.id = ebsd.id(ind);
-if ~isempty(ebsd.grainId), ebsd.grainId = ebsd.grainId(ind); end
+%if ~isempty(ebsd.grainId), ebsd.grainId = ebsd.grainId(ind); end
 if ~isempty(ebsd.A_D), ebsd.A_D = ebsd.A_D(ind,ind); end
