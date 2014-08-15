79bf567d8a606b4a1b980409b1748e04de1bc05b
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-08-15 09:14:47 +0200

    bug fix

--- tools/orientationMappings/ipdfOrientationMapping.m
+++ tools/orientationMappings/ipdfOrientationMapping.m
@@ -3,7 +3,7 @@ classdef ipdfOrientationMapping < orientationMapping
   %   Detailed explanation goes here
   
   properties
-    inversePoleFigureDirection = zvector 
+    inversePoleFigureDirection = zvector  
   end
   
   methods
@@ -31,27 +31,41 @@ classdef ipdfOrientationMapping < orientationMapping
       else
         defaultPlotCMD = 'pcolor';
       end
-      h = plot(h,d,'TR','',defaultPlotCMD,varargin{:});
+      h = plot(h,d,defaultPlotCMD,varargin{:});
 
       
       if isempty(oM.CS1.mineral)
-        name = oM.CS1.pointGroup;
+        name = ['"' oM.CS1.pointGroup '"'];
       else
         name = oM.CS1.mineral;
       end
       
       ax = get(h(1),'parent'); 
       fig = get(ax,'parent');
-      title(ax,[name ' (' char(oM(1).inversePoleFigureDirection) ') inverse pole figure coloring'])
-   
+      set(fig,'name',['Inverse pole figure coloring for ' name])
+      title(ax,char(oM(1).inversePoleFigureDirection));
       set(fig,'tag','ipdf')
       setappdata(fig,'CS',oM.CS1);
       setappdata(fig,'inversePoleFigureDirection',oM.inversePoleFigureDirection);
       
       % annotate crystal directions
-      h = Miller(unique(sR.vertices),oM.CS1);
-      h.dispStyle = 'uvw';
-      annotate(unique(round(h)),'MarkerFaceColor','k','labeled','symmetrised');
+      if check_option(varargin,'3d')
+        hold on
+        gray = [0.4 0.4 0.4];
+        arrow3d(oM.CS1.axes(1),'facecolor',gray)
+        text3(Miller(1,0,0,'uvw',oM.CS1),'a_1','horizontalAlignment','right')
+
+        arrow3d(oM.CS1.axes(2),'facecolor',gray)
+        text3(Miller(0,1,0,'uvw',oM.CS1),'a_2','verticalAlignment','cap','horizontalAlignment','left')
+
+        arrow3d(oM.CS1.axes(3),'facecolor',gray)
+        text3(Miller(0,0,1,'uvw',oM.CS1),'c','verticalAlignment','bottom')
+        hold off
+      else
+        h = Miller(unique(sR.vertices),oM.CS1);
+        h.dispStyle = 'uvw';
+        annotate(unique(round(h)),'MarkerFaceColor','k','labeled','symmetrised');
+      end
 
     end
         
