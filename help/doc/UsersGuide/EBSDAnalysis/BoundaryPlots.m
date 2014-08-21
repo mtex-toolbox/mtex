b7b2e19b2acb024e754bedfb5815eaace3014836
Hielscher <ralfHielscher@users.noreply.github.com>
2014-08-21 08:06:22 +0200

    docu

--- help/doc/UsersGuide/EBSDAnalysis/BoundaryPlots.m
+++ help/doc/UsersGuide/EBSDAnalysis/BoundaryPlots.m
@@ -23,40 +23,36 @@ gB = grains.boundary
 plot(gB)
 
 %%
-% A grain boundaries plot can be easily combined with further plots by *hold
-% on command, so we can plot various information together
+% Accordingly we can acces the grain boundary of a specific grain by
 
-hold on
-plot(ebsd,ebsd.bc)
-mtexColorMap white2black
+grains(931).boundary
+
+plot(grains(931).boundary)
 
 %%
-% e.g. we want also to see all one pixel grains
+% lets combine it with the orientation measurements inside
 
-grains_selection = grains( grains.grainSize == 1) ;
+oM = ipdfHSVOrientationMapping(grains(931));
+oM.inversePoleFigureDirection = grains(931).meanOrientation * oM.whiteCenter;
+oM.colorStretching = 50;
 
 hold on
-plot(grains_selection.boundary,'linecolor','r','linewidth',2)
+plot(ebsd(grains(931)),oM.orientation2color(ebsd(grains(931)).orientations))
+hold off
+
 
 %% Visualizing special grain boundaries
-% Most interesting is a closer look at special grain boundaries. This can
-% be done by specifing a *property* which should be marked. 
+% Most interesting is a closer look at special grain boundaries.
 %
 %% SUB: Phase boundaries
-% For multi-phase system, the location of phase boundaries may be of
-% interest.
+% For multi-phase system, the location of specific phase transboundaries may be of
+% interest. The following plot highlights all Forsterite to Enstatite phase
+% transitions
 
 close all
 plot(grains,'translucent',.3)
 hold on
-plot(grains.boundary,'linecolor','r','linewidth',1.5)
-
-
-%%
-% Also, one can encode the combination of phases at the grain boundary. Here, for
-% instance we have three types occuring: Fe-Fe, Fe-Mg and Mg-Mg
-
-close, plot(grains.boundary)
+plot(grains.boundary('Fo','En'),'linecolor','r','linewidth',1.5)
 
 %% SUB: Subboundaries
 % Another special type of boundaries, are boundaries that are located
@@ -64,40 +60,48 @@ close, plot(grains.boundary)
 % are somehow conneted by a lattice rotation within a grain.
 % TODO:
 
-close all
-plot(grains.boundary,'external')
-hold on
-plot(grains.boundary,'internal','linecolor','r','linewidth',2)
+%close all
+%plot(grains.boundary,'external')
+%hold on
+%plot(grains.boundary,'internal','linecolor','r','linewidth',2)
 
-%%
-% We also want to see the rotation within the grain.
-
-hold on, plot(ebsd(grains('fo')),ebsd(grains('fo')).mis2mean.angle./degree)
 
 %% SUB: Misorientation
-% Basicly there are two ways to visualize misorientation along a grain
+% Basicly there are two ways to visualize misorientations along grain
 % boundary. The most simplest way is to colorize the grain boundaries
 % with respect to the misorientation angle.
 
-
 close all
-plot(grains.boundary({'Fo','Fo'}),grains.boundary({'Fo','Fo'}).misorientation.angle./degree,'linewidth',1.5)
-colorbar
+gB_Fo = grains.boundary({'Fo','Fo'});
+plot(grains,'translucent',.3)
+legend off
+hold on
+plot(gB_Fo,gB_Fo.misorientation.angle./degree,'linewidth',1.5)
+hold off
+colorbar(gcm)
 
 %%
 % The more sophisticated way is to colorize the misorientation space and
-% apply the color to the respectibe grain boundaries. More details [TODO]
+% apply the color to the respectibe grain boundaries. 
+% TODO: apply here patala colorcoding!!!
 
 close all
+plot(grains,'translucent',.3)
+legend off
+hold on
+
+oM = axisAngleOrientationMapping(gB_Fo);
+oM.maxAngle = 180*degree;
+
 plot(grains.boundary)
 hold on
 
-% TODO
-%plotBoundary(grains('Fo'),'property','misorientation',...
-%  'colorcoding','patala','linewidth',1.5)
+plot(gB_Fo,oM.orientation2color(gB_Fo.misorientation),'linewidth',1.5)
 
-% plot the colorbar
-% colorbar
+hold off
+
+% plot the colorcoding
+% plot(oM)
 
 %% SUB: Classifing special boundaries
 % Actually, it might be more informative, if we classify the grain
@@ -110,8 +114,6 @@ hold on
 
 close all
 
-
-gB_Fo = gB({'Fo','Fo'});
 mAngle = gB_Fo.misorientation.angle./ degree;
 
 %%
@@ -156,15 +158,15 @@ legend('>5^\circ','[100]')
 % difference to the given rotation.
 
 rot = rotation('axis',vector3d(1,1,1),'angle',60*degree);
+ind = angle(gB_Fo.misorientation,rot)<10*degree;
 
 close all
 plot(gB)
 hold on
-plotBoundary(grains,'property',rot,'linewidth',1.5)
-% hold on, plotBoundary(grains,'property',rot,'linecolor','b','linewidth',1)
+plot(gB_Fo(ind),'linewidth',1.5,'linecolor','r')
 
 legend('>2^\circ','60^\circ/[001]')
-colorbar
+
 
 %%
 % In the same manner, we can classify after predefined special rotations,
@@ -172,26 +174,27 @@ colorbar
 % specify a searching radius with the option |'delta'|, in this way, we
 % control how far the misorientation of the boundary segment is actuall
 % away from the specified rotation.
+% TODO
 
-close all
-plot(gB,'color','k')
-
-
-hold on
-plot(gB_Fo(angle(gB_Fo.misorientation,CSL(3))<2*degree),...
-  'linecolor','b','linewidth',2)
-plot(gB_Fo(angle(gB_Fo.misorientation,CSL(5))<4*degree),...
-  'linecolor','m','linewidth',2)
-plot(gB_Fo(angle(gB_Fo.misorientation,CSL(7))<4*degree),...
-  'linecolor','g','linewidth',2)
-plot(gB_Fo(angle(gB_Fo.misorientation,CSL(11))<4*degree),...
-  'linecolor','r','linewidth',2)
-
-legend('>2^\circ',...
-  '\Sigma 3',...
-  '\Sigma 5',...
-  '\Sigma 7',...
-  '\Sigma 11')
+% close all
+% plot(gB)
+% 
+% 
+% hold on
+% plot(gB_Fo(angle(gB_Fo.misorientation,CSL(3))<2*degree),...
+%   'linecolor','b','linewidth',2)
+% plot(gB_Fo(angle(gB_Fo.misorientation,CSL(5))<4*degree),...
+%   'linecolor','m','linewidth',2)
+% plot(gB_Fo(angle(gB_Fo.misorientation,CSL(7))<4*degree),...
+%   'linecolor','g','linewidth',2)
+% plot(gB_Fo(angle(gB_Fo.misorientation,CSL(11))<4*degree),...
+%   'linecolor','r','linewidth',2)
+% 
+% legend('>2^\circ',...
+%   '\Sigma 3',...
+%   '\Sigma 5',...
+%   '\Sigma 7',...
+%   '\Sigma 11')
 
 %%
 % Another kind of special boundaries are tilt and twist boundaries. We can
@@ -202,16 +205,17 @@ legend('>2^\circ',...
 % $$ \left( g_1^{-1} * g_2 \right) * h = h, $$
 % 
 % where $g_1, g_2$ are neighbored orientations.
+% TODO
 
-close all
-plot(grains.boundary)
-hold on
-plot(grains.boundary,'property',Miller(1,1,1),'delta',2*degree,...
-  'linecolor','r','linewidth',1.5)
-plot(grains.boundary,'property',Miller(0,0,1),'delta',2*degree,...
-  'linecolor','b','linewidth',1.5)
-
-legend('>2^\circ',...
-  '\{111\}',...
-  '\{001\}')
+%close all
+%plot(grains.boundary)
+%hold on
+%plot(grains.boundary,'property',Miller(1,1,1),'delta',2*degree,...
+%  'linecolor','r','linewidth',1.5)
+%plot(grains.boundary,'property',Miller(0,0,1),'delta',2*degree,...
+%  'linecolor','b','linewidth',1.5)
+%
+%legend('>2^\circ',...
+%  '\{111\}',...
+%  '\{001\}')
 
