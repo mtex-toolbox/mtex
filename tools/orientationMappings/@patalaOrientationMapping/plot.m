4d891d126681d2721dc33826b7b9ad3eb468bcd4
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-08-28 23:22:12 +0200

    bug fix: call fundamental sector with angle as an option

--- tools/orientationMappings/@patalaOrientationMapping/plot.m
+++ tools/orientationMappings/@patalaOrientationMapping/plot.m
@@ -18,18 +18,27 @@ for i = 1:length(omega)
   
   if i>1, mtexFig.nextAxis; end
   
-  S2G = plotS2Grid(oM.CS1.Laue.fundamentalSector(omega(i)),varargin{:});
+  S2G = plotS2Grid(oM.CS1.Laue.fundamentalSector('angle',omega(i)),varargin{:});
   
   mori = orientation('axis',S2G,'angle',omega(i));
   
   rgb = oM.orientation2color(mori);
-  
+ 
   % plot boundary
-  plot(sR,'parent',mtexFig.gca,'TR',[int2str(omega(i)./degree),'^\circ'],'color',[0.8 0.8 0.8]);
-  
+  plot(sR,'parent',mtexFig.gca,'TR',[int2str(omega(i)./degree),'^\circ'],...
+    'color',[0.8 0.8 0.8],'doNotDraw');
+ 
+  % rescale the figures according to actual volume
+  x = get(mtexFig.gca,'xLim');
+  y = get(mtexFig.gca,'yLim');
+  x = x .* sin(oM.CS1.maxAngle/2) / sin(omega(i)/2);
+  y = y .* sin(oM.CS1.maxAngle/2) / sin(omega(i)/2);
+  xlim(mtexFig.gca,x);
+  ylim(mtexFig.gca,y);
+    
   hold on
   %plot(oM.CS1.fundamentalSector(omega(i)),'parent',mtexFig.gca,'color','k');  
-  S2G.surf(rgb,'parent',mtexFig.gca,varargin{:});
+  S2G.surf(rgb,'parent',mtexFig.gca,varargin{:},'doNotDraw');
   hold off
 end
 
