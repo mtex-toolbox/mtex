4748c3f30605bb09fb9e73789cbe4c3eef5fe37c
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-09-21 20:32:48 +0200

    as there are no holes anymore - findByLocation becomes more simple

--- EBSDAnalysis/@grain2d/findByLocation.m
+++ EBSDAnalysis/@grain2d/findByLocation.m
@@ -18,22 +18,16 @@ function id = findByLocation( grains, pos )
 % See also
 % EBSD/findByLocation GrainSet/findByOrientation
 
-% extract polygons with holes
-isCell = cellfun('isclass',grains.poly,'cell');
 poly = grains.poly;
-poly(isCell) = cellfun(@(x) [x{:}] ,grains.poly(isCell),'UniformOutput',false);
 
 % restrict vertices to available grains
 iV = unique([poly{:}]);
 % and search for the closest vertice
 closestVertex = iV(bucketSearch(grains.V(iV,:),pos));
 
+% list of candidates
 id = find(cellfun(@(x) any(x==closestVertex), poly));
-
-% list of candidates - now without holes
-poly = grains.poly(id);
-isCell = cellfun('isclass',poly,'cell');  
-poly(isCell) = cellfun(@(x) x{1} ,poly(isCell),'UniformOutput',false);
+poly = poly(id);
 
 % check whether pos is inside the canditats
 inside = false(size(id));
@@ -42,7 +36,3 @@ for k=1:numel(poly)
   inside(k) = inpolygon(pos(1),pos(2),V_k(:,1),V_k(:,2));
 end
 id = id(inside);
-
-% maybe its inside of more then one grain - then take the smallest one
-[~,ind] = min(area(grains.subSet(id)));
-id = id(ind);
