5e64e1a08c92fbc0d16002da13e55b7221d50902
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-09-24 08:35:42 +0200

    minor

--- tools/graph_tools/connectedComponents.m
+++ tools/graph_tools/connectedComponents.m
@@ -6,7 +6,7 @@ parent = etree(A);
 
 isleaf = parent ~= 0;
 parent(~isleaf) = 1:nnz(~isleaf);
-i      = find(isleaf);
+i = find(isleaf);
 for i = i(end:-1:1)
-    parent(i) = parent(parent(i));
+  parent(i) = parent(parent(i));
 end
