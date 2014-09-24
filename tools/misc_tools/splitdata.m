17d60d59aced6c5869a271e3204110df80e10004
Hielscher <ralfHielscher@users.noreply.github.com>
2014-09-24 08:35:42 +0200

    minor

--- tools/misc_tools/splitdata.m
+++ tools/misc_tools/splitdata.m
@@ -1,7 +1,7 @@
 function ind = splitdata(x,n,varargin)
 % make n - partitions of a list, returns its indices
 
-[x ndx] = sort(x,varargin{:});
+[x,ndx] = sort(x,varargin{:});
 
 k = 1;
 xf = cell(1,n);
@@ -11,7 +11,7 @@ while k < n
   xf{k+1} = cell(1,2^k);
   
   for l = 1:(2^(k-1))
-    [m xf{k+1}{l*2-1} xf{k+1}{l*2}] = split(xf{k}{l});
+    [m, xf{k+1}{l*2-1}, xf{k+1}{l*2}] = split(xf{k}{l});
   end
   k = k+1;
 end
@@ -26,7 +26,7 @@ for k=1:length(ind)
   ind{k} = ndx(csz(k)+1:csz(k+1));  
 end
 
-function [m xl xr] = split(x)
+function [m, xl, xr] = split(x)
 
 m = mean(x);
 xl = x(x <= m);
