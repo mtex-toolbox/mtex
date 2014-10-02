7b7ce92805319887e7b9eda74ec382f5c362b0eb
Ralf.Hielscher <Ralf.Hielscher@gmail.com>
2014-10-02 09:48:02 +0200

    minor fix

--- plotting/@mtexFigure/getDataCursorPos.m
+++ plotting/@mtexFigure/getDataCursorPos.m
@@ -11,27 +11,12 @@ catch
  pos = dcm_obj.CurrentDataCursor.getCursorInfo.Position;
 end
 
-% get value
+% get graphical object
 try
   target = dcm_obj.getCursorInfo.Target;
 catch
   target = dcm_obj.CurrentDataCursor.getCursorInfo.Target;
 end
-xd = get(target,'xdata');
-yd = get(target,'ydata');
-zd = get(target,'zdata');
-if isempty(zd)
-  zd = get(target,'CData');
-end
-
-if size(xd,1) ~= 1 && size(xd,2) == numel(zd)   
-  xd = mean(xd);
-  yd = mean(yd);  
-end
-
-[~,value] = min((xd-pos(1)).^2 + (yd-pos(2)).^2);
-
-if numel(zd) == numel(xd), value = zd(value); end
 
 % convert pos to vector3d for spherical plots
 ax = get(target,'Parent');
@@ -41,6 +26,23 @@ sP = getappdata(ax,'sphericalPlot');
 if ~isempty(sP)
   pos = sP.proj.iproject(pos(1),pos(2));
 end
+
+% get value
+value = [];
+zd = get(target,'zdata');
+if isempty(zd), return; end
+  
+xd = get(target,'xdata');
+yd = get(target,'ydata');
+
+if size(xd,1) ~= 1 && size(xd,2) == numel(zd)   
+  xd = mean(xd);
+  yd = mean(yd);  
+end
+
+[~,value] = min((xd-pos(1)).^2 + (yd-pos(2)).^2);
+
+if numel(zd) == numel(xd), value = zd(value); end
  
 end
 
