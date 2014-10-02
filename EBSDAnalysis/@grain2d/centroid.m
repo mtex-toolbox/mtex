463802fefe87292a3cd4fe7a0e6fa1f77b8fb8a2
Hielscher <ralfHielscher@users.noreply.github.com>
2014-10-02 08:12:24 +0200

    update centroid to new syntax in grain2d.poly

--- EBSDAnalysis/@grain2d/centroid.m
+++ EBSDAnalysis/@grain2d/centroid.m
@@ -1,66 +1,36 @@
 function [x,y] = centroid(grains)
-% calculates the barycenter of the grain-polygon, with respect to its holes
+% calculates the barycenters of the grain polygons
 %
 
-c = zeros(numel(grains),2);
+x = zeros(size(grains)); y = x;
 
-isHole = hasHole(grains);
+faceOrder = [grains.poly{:}];
 
-if any(isHole)
-  
-  [cH,a] = cellCentroid(grains.V,[grains.poly{isHole}]);
+Vx = grains.V(faceOrder,1);
+Vy = grains.V(faceOrder,2);
 
-  cs = [0; cumsum(cellfun('prodofsize',grains.poly(isHole)))];
+dF = (Vx(1:end-1).*Vy(2:end)-Vx(2:end).*Vy(1:end-1));
+cx = (Vx(1:end-1) +Vx(2:end)).*dF;
+cy = (Vy(1:end-1) +Vy(2:end)).*dF;
 
-  % for each cell
-  for k=1:nnz(isHole)
-    ndx = cs(k)+1:cs(k+1);
-    % sort after area 
-    at = a(ndx);
-    ct =  cH(ndx,:);
+cs = [0; cumsum(cellfun('prodofsize',grains.poly))];
 
-    % the centroid is sum(C.*A)/sum(A) where, A = [a1 -a2 -a3];
-    cH(ndx(1),:) = (ct(1,:).*at(1) - sum(bsxfun(@times,ct(2,:),at(2:end)),1))./(at(1)-sum(at(2:end)));
-  end
-
-  c(isHole,:) = cH(cs(1:end-1)+1,:);
-end
-c(~isHole,:) = cellCentroid(grains.V,grains.poly(~isHole));
-
-if nargout == 2
-  x = c(:,1);
-  y = c(:,2);
-else
-  x = c;
-end
-  
-function [v,a] = cellCentroid(V,D)
-
-D = D(:);
-a = zeros(size(D));
-cHx = zeros(size(D));
-cHy = zeros(size(D));
-
-faceOrder = [D{:}];
-
-x = V(faceOrder,1);
-y = V(faceOrder,2);
-
-dF = (x(1:end-1).*y(2:end)-x(2:end).*y(1:end-1));
-cx = (x(1:end-1)+x(2:end)).*dF;
-cy = (y(1:end-1)+y(2:end)).*dF;
-
-cs = [0; cumsum(cellfun('prodofsize',D))];
-
-for k=1:numel(D)
+for k=1:numel(x)
   ndx = cs(k)+1:cs(k+1)-1;
   
-  a(k) =   sum(dF(ndx));
-  cHx(k) = sum(cx(ndx));
-  cHy(k) = sum(cy(ndx));
+  a = sum(dF(ndx));
+  x(k) = sum(cx(ndx)) / 3 / a;
+  y(k) = sum(cy(ndx)) / 3 / a;
 end
 
-v =  [cHx./(3*a) cHy./(3*a)] ;
-% v = bsxfun(@rdivide,[cHx cHy],3*a);
-a = abs(a*.5);
+if nargout == 1, x = [x,y]; end
 
+end
+
+% some test code
+% mtexdata fo
+% plot(ebsd)
+% grains = calcGrains(ebsd)
+% plot(grains(1806))
+% [x,y] = centroid(grains(1806));
+% hold on, plot(x,y,'o','color','b'); hold off
