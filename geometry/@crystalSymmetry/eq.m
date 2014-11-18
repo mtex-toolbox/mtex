885de443aae1b508ee2e77cbb66f421b59b49755
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2014-11-18 06:56:15 +0100

    crystalSymmetry==crystalSymmetry compares also mineral name when present. This is important when using crystalSymmetry to filter EBSD, grains, grainBoundary

--- geometry/@crystalSymmetry/eq.m
+++ geometry/@crystalSymmetry/eq.m
@@ -2,4 +2,6 @@ function b = eq(S1,S2)
 % check S1 == S2
 
 b = S1.Laue.id == S2.Laue.id && ...
-  all(norm(S1.axes - S2.axes)./norm(S1.axes)<10^-2);
+  all(norm(S1.axes - S2.axes)./norm(S1.axes)<10^-2) && ...
+  (isempty(S1.mineral) || isempty(S2.mineral) || strcmpi(S1.mineral,S2.mineral));
+
