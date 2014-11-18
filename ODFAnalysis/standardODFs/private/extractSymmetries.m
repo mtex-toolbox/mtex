2e8bacbcdfa1336670bccbf666d6f79c3f32a89d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2014-11-18 07:21:42 +0100

    bug fix: allow syntax unimdoalODF(CS1,CS2) for a MDF

--- ODFAnalysis/standardODFs/private/extractSymmetries.m
+++ ODFAnalysis/standardODFs/private/extractSymmetries.m
@@ -1,5 +1,21 @@
 function [CS,SS] = extractSymmetries(varargin)
 % extract crystal and specimen symmetry from input arguments
 
-CS = getClass(varargin,'crystalSymmetry',crystalSymmetry);
-SS = getClass(varargin,'specimenSymmetry',specimenSymmetry);
+%CS = getClass(varargin,'crystalSymmetry',crystalSymmetry);
+%SS = getClass(varargin,'specimenSymmetry',specimenSymmetry);
+
+ind = find(cellfun(@(cs) isa(cs,'symmetry'),varargin));
+
+if any(ind)
+  CS = varargin{ind(1)};
+else
+  CS = crystalSymmetry;
+end
+
+if numel(ind)==2
+  SS = varargin{ind(2)};
+elseif numel(ind)>2
+  error('To many symmetries specified');
+else
+  SS = specimenSymmetry;
+end
