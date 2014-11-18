3ea921671334e90053e183d7a8ae5ad40732a554
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2014-11-18 11:29:56 +0100

    direkt formula for Dirichlet kernel compute correct halfwidth for Dirichlet kernel

--- tools/kernelFunctions/@kernel/kernel.m
+++ tools/kernelFunctions/@kernel/kernel.m
@@ -130,7 +130,7 @@ classdef kernel
     end
     
     function hw = halfwidth(psi)
-      hw = fminbnd(@(omega) (psi.K(1)-2*psi.K(cos(omega/2))).^2,0,pi);
+      hw = fminbnd(@(omega) (psi.K(1)-2*psi.K(cos(omega/2))).^2,0,3*pi/4);
     end
 
   end
