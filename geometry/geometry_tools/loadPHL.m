e7560d262df2a5cd720af4e1ad44b0122cf45bbf
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-01-30 19:08:51 +0100

    interface to read Bruker crystal phl files

---
+++ geometry/geometry_tools/loadPHL.m
@@ -0,0 +1,28 @@
+function cs = loadPHL(fname)
+% 
+
+cs = {};
+doc = xmlread(fname);
+root = doc.getDocumentElement();
+phaseList = root.getElementsByTagName('ClassInstance');
+
+for i = 1:phaseList.getLength
+ 
+  thisPhase = phaseList.item(i-1);
+  if ~strcmp(thisPhase.getAttribute('Type'),'TEBSDExtPhaseEntry'), continue; end
+   
+  mineral = char(thisPhase.getAttribute('Name'));
+  phaseEntry = thisPhase.getElementsByTagName('TEBSDPhaseEntry').item(0);
+   
+  %SG = phaseEntry.getElementsByTagName('SG').item(0).getFirstChild.getNodeValue;
+  IT = str2num(phaseEntry.getElementsByTagName('IT').item(0).getFirstChild.getNodeValue);
+  density = str2num(phaseEntry.getElementsByTagName('DENSITY').item(0).getFirstChild.getNodeValue);
+  abc = str2num(phaseEntry.getElementsByTagName('Dim').item(0).getFirstChild.getNodeValue);
+  angles = str2num(phaseEntry.getElementsByTagName('Angles').item(0).getFirstChild.getNodeValue);  
+  
+  cs{end+1} = crystalSymmetry('spaceId',IT,abc,angles,'density',density,'mineral',mineral);  
+  
+end
+
+% fname = '/home/hielscher/mtex/master/data/cif/crystal.phl';
+% 
