7c1941f0e5572aed93c692a8796e18fad583e25e
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-03-30 15:32:27 +0200

    use option 'percent' to plot angle distribution in percent

--- geometry/@symmetry/plotAngleDistribution.m
+++ geometry/@symmetry/plotAngleDistribution.m
@@ -8,7 +8,7 @@ function h = plotAngleDistribution(CS,varargin)
 %  resolution - resolution of the plots
 %
 
-mtexFig = newMtexFigure(varargin{:}); 
+[mtexFig,isNew] = newMtexFigure(varargin{:}); 
 
 % compute angle distribution
 [f,omega] = angleDistribution(CS);
@@ -17,17 +17,24 @@ mtexFig = newMtexFigure(varargin{:});
 pPatch = findobj(mtexFig.gca,'Type','patch');
 pBar = findobj(mtexFig.gca,'type','bar');
 
+unit = '%';
 if ~isempty(pPatch)
   faktor = 100 / mean(f) / size(get(pPatch(1),'faces'),1);
 elseif ~isempty(pBar)
   faktor = 100 / mean(f) / size(get(pBar(1),'XData'),2);
+elseif check_option(varargin,'percent')
+  faktor = 100;
 else
   faktor = 1;
+  unit = 'mrd';
 end
 
 h = optiondraw(plot(omega/degree,faktor * max(0,f),'parent',mtexFig.gca),...
   'LineWidth',2,varargin{:});
 
-xlabel(mtexFig.gca,'orientation angle in degree')
+if isNew
+  xlabel(mtexFig.gca,'Misorientation angle (degrees)')
+  ylabel(mtexFig.gca,['Frequency (' unit ')'])
+end
 
 if nargout == 0, clear h; end
