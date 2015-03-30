90114e1ae3ee9fcfbf9892a128dad74f64f04b74
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-03-30 09:31:34 +0200

    fixes mtex-toolbox/mtex#41, consider also calling ODFComponent/discreteSample

--- ODFAnalysis/@ODF/discreteSample.m
+++ ODFAnalysis/@ODF/discreteSample.m
@@ -1,11 +1,16 @@
 function ori = discreteSample(odf,npoints,varargin)
 % draw a random sample
 %
+% TODO: this might also call ODFComponent/discreteSample
 
 res = get_option(varargin,'resolution',5*degree);
 
 % some local grid
+% TODO: maybe crystal and specimen symmetry is not needed here
 S3G_local = localOrientationGrid(odf.CS,odf.SS,res,'resolution',res/5);
+
+% take local random samples
+q2 = discreteSample(quaternion(S3G_local),npoints);
 
 % the global grid
 if check_option(varargin,'precompute_d')
@@ -16,9 +21,9 @@ else
   d = eval(odf,S3G_global); %#ok<EVLC>
 end
 
-d(d<0) = 0;
-    
-r1 = discretesample(d,npoints);
-r2 = discretesample(length(S3G_local),npoints,'XX');
+% take global random samples
+d(d<0) = 0;   
+q1 = quaternion(S3G_global,discretesample(d,npoints));
 
-ori = orientation(quaternion(S3G_global,r1) .* quaternion(S3G_local,r2),odf.CS,odf.SS);
+% combine local and global
+ori = orientation(q1(:) .* q2(:),odf.CS,odf.SS);
