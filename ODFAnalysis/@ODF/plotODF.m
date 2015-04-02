2ab88ce9573bff94b24ac9a0c8f6b1a8c2f89560
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-04-02 22:46:46 +0200

    makes use of the new section plot

--- ODFAnalysis/@ODF/plotODF.m
+++ ODFAnalysis/@ODF/plotODF.m
@@ -1,126 +1,31 @@
 function plotODF(odf,varargin)
-% plot odf
-%
-% Plots the ODF as various sections which can be controled by options.
-%
-% Input
-%  odf - @ODF
+% plot ODF sections
 %
 % Options
-%  sections   - number of sections
+%  sections - number of sections
+%  points   - number of orientations to be plotted
+%  all      - plot all orientations
 %  resolution - resolution of each plot
 %
 % Flags
-%  phi2  - sections along phi2 Bunge angle (default)
-%  phi1  - sections along phi1 Bunge angle
-%  sigma - sections along phi1 - phi2
-%  alpha - sections along alpha Matthies angle
-%  gamma - sections along gamma Matthies angle
-%  omega - sections along crystal directions @Miller
-%
+%  phi2      - phi2 sections (default)
+%  phi1      - phi1 sections
+%  sigma     - sigma = phi1 - phi2 sections
+%  axisAngle - rotational angle sections
+%  smooth
+%  countourf
+%  contour
 %  contour3, surf3, slice3 - 3d volume plot
 %
 % See also
-% S2Grid/plot savefigure Plotting Annotations_demo ColorCoding_demo PlotTypes_demo
-% SphericalProjection_demo
-
-% generate grids
-[S3G,S2G,sec] = plotSO3Grid(odf.CS,odf.SS,'resolution',2.5*degree,varargin{:});
-
-Z = eval(odf,orientation(S3G),varargin{:});
-clear S3G;
-
-% ------------------------- plot -----------------------------------------
-sectype = get_flag(varargin,{'alpha','phi1','gamma','phi2','sigma','omega','axisangle'},'phi2');
-[symbol,labelx,labely] = sectionLabels(sectype);
-
-if ~any(strcmpi(sectype,{'sigma','omega','axisangle'}))
-  varargin = [{'projection','plain',...
-    'xAxisDirection','east','zAxisDirection','intoPlane'},varargin];
-end
-
-fprintf(['\nPlotting ODF as ',sectype,' sections, range: ',...
-  xnum2str(min(sec)/degree),mtexdegchar,' - ',xnum2str(max(sec)/degree),mtexdegchar,'\n']);
-
-% make new plot
-[mtexFig,isNew] = newMtexFigure('datacursormode',@tooltip,varargin{:});
-
-for i = 1:length(sec)
-    
-  if i>1, mtexFig.nextAxis; end
-    
-  S2G.plot(Z(:,:,i),'TR',[int2str(sec(i)*180/pi),'^\circ'],...
-    'xlabel',labelx,'ylabel',labely,...
-    'colorRange',[min(Z(:)),max(Z(:))],'smooth',...
-    'parent',mtexFig.gca,'doNotDraw',varargin{:});
-end
-  
-if isNew % finalize plot
-
-  set(gcf,'Name',['ODF ' sectype '-sections "',inputname(1),'"']);
-  setappdata(gcf,'sections',sec);
-  setappdata(gcf,'SectionType',sectype);
-  setappdata(gcf,'CS',odf.CS);
-  setappdata(gcf,'SS',odf.SS);
-  set(gcf,'tag','odf')
-
-  if strcmpi(sectype,'omega') && ~isempty(find_type(varargin,'Miller'))
-    h = varargin{find_type(varargin,'Miller')};
-    setappdata(gcf,'h',h);
-  end
-
-  dcm = mtexFig.dataCursorMenu;
-  uimenu(dcm, 'Label', 'Mark equivalent orientations', 'Callback', @markEquivalent);  
-  
-  mtexFig.drawNow('figSize',getMTEXpref('figSize'),varargin{:});
-  
-end
-
-% --------------- Tooltip function -------------------------------
-function txt = tooltip(varargin)
-  [ori,v] = currentOrientation;
-  txt = [xnum2str(v) ' at ' char(ori,'nodegree')];
-end
-
-function markEquivalent(varargin)
-  annotate(currentOrientation);
-end
+% saveFigure Plotting
 
 
-function [ori,value] = currentOrientation
+oS = newODFSectionPlot(odf.CS,odf.SS,varargin{:});
 
-  [pos,value,ax] = getDataCursorPos(mtexFig);
+S3G = oS.makeGrid('resolution',2.5*degree,varargin{:});
 
-  iax = mtexFig.children == ax;
+Z = odf.eval(S3G);
+clear S3G
 
-  switch getappdata(gcf,'SectionType')
-    case 'phi1'
-      euler1 = sec(iax);
-      euler2 = pos.theta;
-      euler3 = pos.rho;
-      convention = 'Bunge';
-    case 'phi2'
-      euler3 = sec(iax);
-      euler2 = pos.theta;
-      euler1 = pos.rho;
-      convention = 'Bunge';
-    case 'alpha'
-      euler3 = sec(iax);
-      euler2 = pos.theta;
-      euler1 = pos.rho;
-      convention = 'Matthies';
-    case 'sigma'
-      euler1 = pos.rho;
-      euler2 = pos.theta;
-      euler3 = sec(iax) - pos.rho;
-      convention = 'Matthies';
-    otherwise
-      error('unknown sectioning!')
-  end
-
-  ori = orientation('Euler',euler1,euler2,euler3,convention,odf.CS,odf.SS);
-
-end
-
-end
-
+oS.plot(Z,'smooth','colorRange',[min(Z(:)),max(Z(:))],varargin{:});
--- plotting/@ODFSections/plot.m
+++ plotting/@ODFSections/plot.m
@@ -1,5 +1,9 @@
 function plot(oS,varargin)
-      
+% plot data into ODF sections
+%
+% Syntax
+%
+
 [mtexFig,isNew] = newMtexFigure('ensureAppdata',{{'ODFSections',oS}},varargin{:});
 
 % extract orientations
@@ -27,9 +31,21 @@ if ~isempty(data), data = reshape(data,numData,[]); end
 
 %
 if exist('ori','var') || isempty(oS.plotGrid)
-    
+
+  % subsample to reduce size
+  if ~check_option(varargin,'all') && length(ori) > 2000 || check_option(varargin,'points')
+    points = fix(get_option(varargin,'points',2000));
+    disp(['  plotting ', int2str(points) ,' random orientations out of ', ...
+      int2str(length(ori)),' given orientations']);
+
+    samples = discretesample(length(ori),points);
+    ori = ori(samples);
+    if ~isempty(data), data = data(samples,:); end
+  end
+  
   [v,secAngle] = project(oS,ori);
-        
+  %TODO: v.resolution = ori.resolution;      
+  
   for s = 1:oS.numSections
     
     if s>1, mtexFig.nextAxis; end
@@ -64,7 +80,78 @@ else
     
 end
 
-if isNew, mtexFig.drawNow(varargin{:}); end
+if isNew || check_option(varargin,'figSize')
+  mtexFig.drawNow('figSize',getMTEXpref('figSize'),varargin{:}); 
+end
 
 end
   
+% --------------- Tooltip function -------------------------------
+% ,'datacursormode',@tooltip
+
+
+function txt = tooltip(varargin)
+
+% 
+dcm_obj = datacursormode(mtexFig.parent);
+
+hcmenu = dcm_obj.createContextMenu;
+%hcmenu = dcm_obj.CurrentDataCursor.uiContextMenu;
+if numel(get(hcmenu,'children'))<10
+  uimenu(hcmenu, 'Label', 'Mark equivalent orientations', 'Callback', @markEquivalent);
+  mcolor = uimenu(hcmenu, 'Label', 'Marker color', 'Callback', @display);
+  msize = uimenu(hcmenu, 'Label', 'Marker size', 'Callback', @display);
+  mshape = uimenu(hcmenu, 'Label', 'Marker shape', 'Callback', @display);
+end
+
+%
+[ori,v] = currentOrientation;
+
+txt = [xnum2str(v) ' at ' char(ori,'nodegree')];
+
+end
+
+%
+function markEquivalent(varargin)
+
+annotate(currentOrientation);
+
+end
+
+
+function [ori,value] = currentOrientation
+
+[pos,value,ax] = getDataCursorPos(mtexFig);
+
+iax = mtexFig.children == ax;
+
+switch getappdata(gcf,'SectionType')
+  case 'phi1'
+    euler1 = sec(iax);
+    euler2 = pos.theta;
+    euler3 = pos.rho;
+    convention = 'Bunge';
+  case 'phi2'
+    euler3 = sec(iax);
+    euler2 = pos.theta;
+    euler1 = pos.rho;
+    convention = 'Bunge';
+  case 'alpha'
+    euler3 = sec(iax);
+    euler2 = pos.theta;
+    euler1 = pos.rho;
+    convention = 'Matthies';
+  case 'sigma'
+    euler1 = pos.rho;
+    euler2 = pos.theta;
+    euler3 = sec(iax) - pos.rho;
+    convention = 'Matthies';
+  otherwise
+    error('unknown sectioning!')
+end
+
+ori = orientation('Euler',euler1,euler2,euler3,convention,odf.CS,odf.SS);
+
+end
+
+
