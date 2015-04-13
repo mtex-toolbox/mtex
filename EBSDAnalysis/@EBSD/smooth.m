18f01e2a33af0b06303490b35b7d799f76c8a4e5
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-04-13 06:54:51 +0200

    not working anymore

--- EBSDAnalysis/@EBSD/smooth.m
+++ EBSDAnalysis/@EBSD/smooth.m
@@ -31,37 +31,103 @@ function [ebsd,alpha] = smooth(ebsd,alpha)
 
 % generate regular grid
 ext = ebsd.extend;
-dx = ebsd.unitCell(1,1)-ebsd.unitCell(4,1);
-dy = ebsd.unitCell(1,2)-ebsd.unitCell(2,2);
+dx = max(ebsd.unitCell(:,1))-min(ebsd.unitCell(:,1));
+dy = max(ebsd.unitCell(:,2))-min(ebsd.unitCell(:,2));
 [xgrid,ygrid] = meshgrid(ext(1):dx:ext(2),ext(3):dy:ext(4)); % ygrid runs first
+sGrid = size(xgrid);
 
 % detect position within grid
-xpos = 1 + round((ebsd.prop.x - ext(1))/dx);
-ypos = 1 + round((ebsd.prop.y - ext(3))/dy);
+ind = sub2ind(sGrid, 1 + round((ebsd.prop.y - ext(3))/dy), ...
+  1 + round((ebsd.prop.x - ext(1))/dx));
 
-% compute components in Lie algebra
-[qmean,~,~,~,q] = mean(ebsd.orientations);
-q = inv(qmean)*q; %#ok<MINV>
-tq = log(q);
-
-% fill interpolation matrix
-Tq = nan(numel(xgrid),3);
-Tq(sub2ind(size(xgrid),ypos,xpos),:) = tq;
-Tq = reshape(Tq,[size(xgrid),3]);
-
-% perform smoothing
-if nargin < 2, alpha = []; end
-[T,alpha] = smoothn({Tq(:,:,1),Tq(:,:,2),Tq(:,:,3)},alpha,'robust');
-
-% fill ebsd variable
 ebsd.prop.x = xgrid(:);
 ebsd.prop.y = ygrid(:);
 ebsd.id = (1:numel(xgrid)).';
-ebsd.rotations = reshape(rotation(quaternion(qmean)*expquat([T{:}])),[],1);
-ebsd.phaseId = repmat(ebsd.phaseId(1),numel(xgrid),1);
+clear xgrid ygrid
+
+% the rotations
+a = nan(sGrid); b = a; c = a; d = a;
+a(ind) = ebsd.rotations.a;
+b(ind) = ebsd.rotations.b;
+c(ind) = ebsd.rotations.c;
+d(ind) = ebsd.rotations.d;
+ebsd.rotations = reshape(rotation(quaternion(a,b,c,d)),[],1);
+clear a b c d
+
+% the grainIds
+grainId = zeros(sGrid);
+if isprop(ebsd,'grainId'), grainId(ind) = ebsd.grainId; end
+ebsd.prop.grainId = grainId(:);
+
+% the phaseId
+phaseId = zeros(numel(ebsd.id),1);
+phaseId(ind) = ebsd.phaseId;
+ebsd.phaseId = phaseId;
+clear phaseId
 
 % delete all other properties
 for fn = fieldnames(ebsd.prop).'
-  if any(strcmp(char(fn),{'x','y','z'})), continue;end
+  if any(strcmp(char(fn),{'x','y','z','grainId'})), continue;end
   ebsd.prop = rmfield(ebsd.prop,fn);  
 end
+
+% ---------------------------------
+
+if nargin < 2, alpha = []; end
+
+% generate a regular grid for the ebsd data
+% set grainId correctly
+% set orientation to NaN
+%ebsd = fill(ebsd);
+
+% extract data for speed reasons
+rot = reshape(ebsd.rotations,sGrid);
+
+CSList = ebsd.CSList;
+
+% loop through all grains
+grainIds = unique(grainId).';
+progress(0,length(grainIds));
+for id = grainIds
+
+  progress(id,length(grainIds));
+  
+  % the values to be smoothed
+  ind = grainId == id;
+        
+  % local coordinates in a local rectangular grid
+  [irow,icol] = find(ind);
+  minCol = min(icol); nCol = 1 + max(icol) - minCol;
+  minRow = min(irow); nRow = 1 + max(irow) - minRow;
+  indLocal = sub2ind([nRow,nCol],irow - minRow + 1,icol - minCol + 1);
+    
+  % skip small grains
+  if nRow < 3 || nCol < 3, continue, end
+    
+  % remove NaN values 
+  notNaN = ~isnan(rot.a(ind));
+  indLocal = indLocal(notNaN);
+  ori = orientation(rot(ind),CSList{2});   
+  
+  % compute components in the Lie algebra
+  [qmean,~,~,~,q] = mean(ori(notNaN));
+  q = inv(qmean)*q; %#ok<MINV>  
+  tq1 = NaN(nCol,nRow); tq2 = tq1; tq3 = tq1;
+  tq = log(q);
+  tq1(indLocal) = tq(:,1);
+  tq2(indLocal) = tq(:,2);
+  tq3(indLocal) = tq(:,3);
+    
+  % perform smoothing
+  [T,alpha] = smoothn({tq1,tq2,tq3},alpha,'robust');
+
+  rot(minRow + (0:nRow-1),minCol + (0:nCol-1)) = ...
+    reshape(rotation(quaternion(qmean)*expquat([T{:}])),nRow,nCol);
+  
+end
+
+% store to EBSD variable
+ebsd.rotations = rot;
+    
+end
+  
