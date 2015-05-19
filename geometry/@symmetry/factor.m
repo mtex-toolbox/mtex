af47ba1a82b3258eb739aa7564a799d6af146c72
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-05-19 22:48:20 +0200

    orientation/axis should return axis in specimen coordinates if called with two orientations as discussed in mtex-toolbox/mtex#52

---
+++ geometry/@symmetry/factor.m
@@ -0,0 +1,34 @@
+function [l,d,r] = factor(s1,s2)
+% factorizes s1 and s2 into l, d, r such that s1 = l * d and s2 = d * r
+%
+% Syntax
+%   [l,d,r] = factor(s1,s2)
+%
+% Input
+%  s1, s2  - @symmetry
+%  l, r, d - @quaternion
+%
+
+qs1 = unique(quaternion(s1),'antipodal');
+qs2 = unique(quaternion(s2),'antipodal');
+
+% step 1: find common quaterions d
+[is1,~] = find(isappr(abs(dot_outer(qs1,qs2)),1));
+
+d = subSet(qs1,is1);
+
+% step 2: compute l
+l = idquaternion;
+c = any(isappr(abs(dot_outer(l*d,qs1)),1));
+while ~all(c)
+  l = [l;subSet(qs1,find(~c,1))]; %#ok<AGROW>
+  c = any(isappr(abs(dot_outer(l*d,qs1)),1));
+end
+
+% step 3: compute r
+r = idquaternion;
+c = any(isappr(abs(dot_outer(d*r,qs2)),1));
+while ~all(c)
+  r = [r;subSet(qs2,find(~c,1))]; %#ok<AGROW>
+  c = any(isappr(abs(dot_outer(d*r,qs2)),1));
+end
