2e8f90dd87b35175fc47468026e2ae6168a25e32
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-05-28 07:29:36 +0200

    allow to plot orientationRegion also in axis/angle parametrisation

--- geometry/@orientationRegion/plot.m
+++ geometry/@orientationRegion/plot.m
@@ -5,15 +5,40 @@ function h = plot(oR,varargin)
 % boundary and adjust the axes limits properly.
 %
 
-% embedd into NaN matrix
-FF = nan(length(oR.F),max(cellfun(@length,oR.F)));
-for i = 1:length(oR.F)
-  FF(i,1:length(oR.F{i})) = oR.F{i};
+if check_option(varargin,'rodriguez')
+
+  % embedd into NaN matrix
+  FF = nan(length(oR.F),max(cellfun(@length,oR.F)));
+  for i = 1:length(oR.F)
+    FF(i,1:length(oR.F{i})) = oR.F{i};
+  end
+  
+  VR = reshape(double(oR.V.Rodrigues),[],3);
+  clf;
+  h = patch('faces',FF,'vertices',VR,'faceAlpha',0.1);
+  
+else
+
+  r = plotS2Grid;
+  r = oR.maxAngle(r) .* r;
+
+  surf(r.x,r.y,r.z,'faceColor','k','facealpha',0.1,'edgecolor','none')
+  hold on
+  t = linspace(0,1);
+  for i = 1:length(oR.F)
+    
+    for j = 1:length(oR.F{i})
+      
+      r = t .* oR.V(oR.F{i}(j)).axis + (1-t) .* oR.V(oR.F{i}(mod(j,length(oR.F{i}))+1)).axis;
+      r = normalize(r) .* oR.maxAngle(r);
+      line(r.x,r.y,r.z,'Color','k');
+
+    end
+  end
+  hold off
 end
 
-VR = reshape(double(oR.V.Rodrigues),[],3);
-clf;
-h = patch('faces',FF,'vertices',VR,'faceAlpha',0.1);
 axis equal off
 
 if nargout == 0, clear h; end
+
