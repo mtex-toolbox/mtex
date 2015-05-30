020fd873cd79711d7907e6a9dcaf13692913c4b2
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-05-30 09:30:04 +0200

    bug fix

--- geometry/@symmetry/disjoint.m
+++ geometry/@symmetry/disjoint.m
@@ -24,7 +24,7 @@ for i=1:45 % check all Laue groups
   
   ss = crystalSymmetry('pointId',i);
   
-  if length(ss) == length(s) && all(any(isappr(dot_outer(s,ss),1)))
+  if length(ss) == length(s) && all(any(isappr(abs(dot_outer(s,ss)),1)))
     s = ss;
     return
   end
--- geometry/@symmetry/fundamentalRegion.m
+++ geometry/@symmetry/fundamentalRegion.m
@@ -15,9 +15,9 @@ function  oR = fundamentalRegion(cs,varargin)
 %  invSymmetry - wheter mori == inv(mori)
 %
 
-q = quaternion(cs);
+q = unique(quaternion(cs),'antipodal');
 if nargin == 2 && isa(varargin{1},'symmetry')
-  q = q * quaternion(varargin{1});
+  q = q * unique(quaternion(varargin{1}),'antipodal');
   dcs = disjoint(cs,varargin{1});
   N0 = rotation('axis',dcs.fundamentalSector.N,'angle',pi-1e-5);  
 else
@@ -35,8 +35,8 @@ for i = 1:length(axes)
   angles(i) = min(q(c==i).angle);
 end
 
-N = [axes,-axes];
-Nq = axis2quat(N,pi-[angles,angles]/2);
+N = [axes;-axes];
+Nq = axis2quat(N,pi-[angles;angles]/2);
 
 
-oR = orientationRegion([Nq,N0(:).']);
+oR = orientationRegion([Nq(:).',N0(:).']);
