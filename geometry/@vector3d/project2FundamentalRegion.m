bec766b9386ab9e797c408ad66ff05c242c15c9c
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-06-10 14:31:46 +0200

    much faster implementation for projection into fundametalSector - this needs to be checked

--- geometry/@vector3d/project2FundamentalRegion.m
+++ geometry/@vector3d/project2FundamentalRegion.m
@@ -1,4 +1,4 @@
-function v = project2FundamentalRegion(v,sym,varargin)
+function v = project2FundamentalRegion(v,cs,varargin)
 % projects vectors to the fundamental sector of the inverse pole figure
 %
 % Input
@@ -11,8 +11,25 @@ function v = project2FundamentalRegion(v,sym,varargin)
 % Output
 %  v - @vector3d
 
+% antipodal symmetry is nothing else then adding inversion to the symmetry
+% group
+if check_option(varargin,'antipodal'), cs = cs.Laue; end
+
 %get fundamental region
-sR = sym.fundamentalSector(varargin{:});
+sR = cs.fundamentalSector(varargin{:});
+
+% maybe there is nothing to do
+if isempty(sR.N) || length(cs)==1, return; end
+
+symCenter = symmetrise(cs,sR.center);
+
+dist = dot_outer(vector3d(v),symCenter);
+
+[~,col] = max(dist,[],2);
+
+v = reshape(inv(subSet(rotation(cs),col)),size(v)) .* v;
+
+return
 
 % symmetrise
 v = symmetrise(sym,v);
@@ -24,3 +41,10 @@ inside = sR.checkInside(v);
 [~,col] = max(inside,[],2);
 v = v.subSet(sub2ind(size(v),(1:size(v,1)).',col));
 
+% some testing code
+% v = randv(100)
+% cs = crystalSymmetry('m-3m')
+% tic; v_proj = project2FundamentalRegion(v,cs); toc
+% sR = cs.fundamentalSector
+% plot(v_proj), hold on, plot(sR), hold off
+% all(sR.checkInside(v_proj))
