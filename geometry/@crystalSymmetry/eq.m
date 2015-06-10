3c4f735f237e8c4b78a7e84f01db67d0b96c6216
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-06-10 14:25:05 +0200

    for specimen symmetry eq should check only LaueId

--- geometry/@crystalSymmetry/eq.m
+++ geometry/@crystalSymmetry/eq.m
@@ -1,7 +1,13 @@
 function b = eq(S1,S2)
 % check S1 == S2
 
-b = S1.Laue.id == S2.Laue.id && ...
-  all(norm(S1.axes - S2.axes)./norm(S1.axes)<10^-2) && ...
-  (isempty(S1.mineral) || isempty(S2.mineral) || strcmpi(S1.mineral,S2.mineral));
+if ~isa(S1,'crystalSymmetry') || ~isa(S2,'crystalSymmetry')
+  
+  b = S1.Laue.id == S2.Laue.id;
+  
+else
 
+  b = S1.Laue.id == S2.Laue.id && ...
+    all(norm(S1.axes - S2.axes)./norm(S1.axes)<10^-2) && ...
+    (isempty(S1.mineral) || isempty(S2.mineral) || strcmpi(S1.mineral,S2.mineral));
+end
