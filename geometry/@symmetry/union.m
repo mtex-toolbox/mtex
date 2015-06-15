1ac8a80772e2cfb7fccfb02e9938d609c7abf42a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-06-16 00:57:36 +0200

    symmetry union may try to build up a symmetry

--- geometry/@symmetry/union.m
+++ geometry/@symmetry/union.m
@@ -5,3 +5,18 @@ u  = s1 * s2;
 
 u = unique(u(:));
 
+% take the equal ones
+s = quaternion(u);
+s = unique(s,'antipodal');
+
+% find a symmetry that exactly contains s
+for i=1:45 % check all Laue groups
+  
+  ss = crystalSymmetry('pointId',i);
+  
+  if length(ss) == length(s) && all(any(isappr(abs(dot_outer(rotation(s),ss)),1)))
+    u = ss;
+    return
+  end
+  
+end
