198251aab43cb407b19453089753f332bc0f49b4
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-06-17 23:33:54 +0200

    bug fix, log did not work for quaternions q with negative q.a

--- geometry/@quaternion/log.m
+++ geometry/@quaternion/log.m
@@ -14,5 +14,12 @@ function tq = log(q)
 % See also
 % mat2quat Euler axis2quat hr2quat
 
+ind = q.a < 0;
+q.a(ind) = -q.a(ind);
+q.b(ind) = -q.b(ind);
+q.c(ind) = -q.c(ind);
+q.d(ind) = -q.d(ind);
+
+
 omega = 2 * acos(q.a) ./ sqrt(1-q.a.^2);
 tq = [omega(:) .* q.b(:),omega(:) .* q.c(:),omega(:) .* q.d(:)];
