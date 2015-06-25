c9bf594354e673ade5d3127cd815a3d7e35fcbac
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-06-25 08:01:16 +0200

    halfquadratic is working now

--- EBSDAnalysis/EBSDSmoothing/halfQuadraticFilter.m
+++ EBSDAnalysis/EBSDSmoothing/halfQuadraticFilter.m
@@ -2,13 +2,12 @@ classdef halfQuadraticFilter < EBSDFilter
   
   properties
     alpha = [0.1,0.1];                      % regularization in x, y direction
-    weight = @(t,eps) 1./(t.^2+e^2).^(1/2); % weight function handle b^*(t,epsilon)
+    weight = @(t,eps) 1./(t.^2+eps^2).^(1/2); % weight function handle b^*(t,epsilon)
     eps = 1e-1;                             % parameter for the weight function
     tol = 1e-5                              % tolerance for gradient descent  
   end
 
   methods
-    
     
     function q = smooth(F,q)           
       % Johannes Persch 09.06.2015
@@ -97,10 +96,10 @@ classdef halfQuadraticFilter < EBSDFilter
         u_old = u;
         entrance = false;
         i = i+1;
-        wx = F.weight(M.dist(u,ux),epsilon);
-        wy = F.weight(M.dist(u,uy),epsilon);
-        wxd =F.weight(M.dist(u,uxd),epsilon);
-        wyd =F.weight(M.dist(u,uyd),epsilon);
+        wx = F.weight(M.dist(u,ux),F.eps);
+        wy = F.weight(M.dist(u,uy),F.eps);
+        wxd =F.weight(M.dist(u,uxd),F.eps);
+        wyd =F.weight(M.dist(u,uyd),F.eps);
         %Nullen streichen
         wx(maskx_zero)=0;
         wy(masky_zero)=0;
@@ -119,9 +118,9 @@ classdef halfQuadraticFilter < EBSDFilter
         wyd = reshape(wyd,n_1,n_2);
         wyd = repmat(wyd,[1,1,mani_dims]);
         wyd = permute(wyd,[3:2+length(mani_dims),1,2]);
-        f(mask)=u(mask);
+        q(mask)=u(mask);
         %berechne gradient
-        grad_u = M.log(u,f) + F.alpha(1)*wx.*M.log(u,ux) + ...
+        grad_u = M.log(u,q) + F.alpha(1)*wx.*M.log(u,ux) + ...
           F.alpha(2)*wy.*M.log(u,uy)+F.alpha(1)*wxd.*M.log(u,uxd) + ...
           F.alpha(2)*wyd.*M.log(u,uyd);
         grad_u(static_mask) = vector3d([0,0,0]);
@@ -149,27 +148,28 @@ classdef halfQuadraticFilter < EBSDFilter
         uxd = reshape(uxd,dims);
         uyd = reshape(uyd,dims);
       end
-      disp(['Stopped after ', num2str(i) ,' iterations with last step length ',num2str(max(max(M.dist(u_old,u))))])
-      U=u;
-    end
       
-    
-    function u = setzen_mit_karcher(U)
-      %Find the initial values for the first layer of U as means of the
-      %surrounding values
-      [p_m,nr_points,~] = size(U);
-      u = reshape(U(:,:,1),p_m,nr_points);
-      for j= 1:nr_points
-        if sum(sum(~squeeze(any(isnan(U(:,j,:).a),1)),2)) == 1
-          u(:,j)= U(:,j,~isnan(U(1,j,:).a));
-        else
-          points = squeeze(U(:,j,~isnan(U(1,j,:).a)));
-          points = points(~(points.a==1 & points.b==0));
-          if size(points,1) ~=0
-            u(:,j) = points(ceil(rand*length(points)));% could be fixed to eg 1
+      disp(['Stopped after ', num2str(i) ,' iterations with last step length ',num2str(max(max(M.dist(u_old,u))))])
+      q=u;
+      
+      function u = setzen_mit_karcher(U)
+        %Find the initial values for the first layer of U as means of the
+        %surrounding values
+        [p_m,nr_points,~] = size(U);
+        u = reshape(U(:,:,1),p_m,nr_points);
+        for j= 1:nr_points
+          if sum(sum(~squeeze(any(isnan(U(:,j,:).a),1)),2)) == 1
+            u(:,j)= U(:,j,~isnan(U(1,j,:).a));
+          else
+            points = squeeze(U(:,j,~isnan(U(1,j,:).a)));
+            points = points(~(points.a==1 & points.b==0));
+            if size(points,1) ~=0
+              u(:,j) = points(ceil(rand*length(points)));% could be fixed to eg 1
+            end
           end
         end
-      end
+      end      
     end
   end
 end
+  
