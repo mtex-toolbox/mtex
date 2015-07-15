6da6567685ebdc3b9988ed93b8a9a78d3989efe3
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-07-15 13:28:02 +0200

    allow colored orientation and vector3d scatter plots in 3d

--- geometry/@orientation/scatter.m
+++ geometry/@orientation/scatter.m
@@ -16,18 +16,28 @@ function varargout = scatter(o,varargin)
 % See also
 % EBSD/plotPDF savefigure
 
+if nargin > 1 && isnumeric(varargin{1})
+  data = varargin{1};
+  varargin(1) = [];
+else
+  data = [];
+end
+
 % subsample to reduce size
 if length(o) > 2000 || check_option(varargin,'points')
   points = fix(get_option(varargin,'points',2000));
   disp(['plot ', int2str(points) ,' random orientations out of ', ...
     int2str(length(o)),' given orientations']);
-  o = o.discreteSample(fix(points));
+  [o,ind] = discreteSample(o,fix(points),'withoutReplacement');
+  if ~isempty(data), data = data(ind); end
+else
+  
 end
 
 [mtexFig,isNew] = newMtexFigure(varargin{:});
 
 % plot
-[varargout{1:nargout}]= scatter@rotation(rotation(o),'parent',mtexFig.gca,varargin{:});
+[varargout{1:nargout}]= scatter@rotation(rotation(o),data,'parent',mtexFig.gca,varargin{:});
 
 if isNew
   set(mtexFig.parent,'Name',['Scatter plot of "',get_option(varargin,'FigureTitle',inputname(1)),'"']);
