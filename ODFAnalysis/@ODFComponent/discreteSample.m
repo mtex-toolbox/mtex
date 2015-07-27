9340751c8ae13150ae2f8f199632d1ca3818b335
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-07-27 16:47:12 +0200

    ODF/discreteSample makes use of ODFComponent/discreteSample

--- ODFAnalysis/@ODFComponent/discreteSample.m
+++ ODFAnalysis/@ODFComponent/discreteSample.m
@@ -7,18 +7,16 @@ res = get_option(varargin,'resolution',5*degree);
 % some local grid
 S3G_local = localOrientationGrid(component.CS,component.SS,res,'resolution',res/5);
 
+% take local random samples
+q2 = discreteSample(quaternion(S3G_local),npoints);
+
 % the global grid
-if check_option(varargin,'precompute_d')
-  S3G_global = get_option(varargin,'S3G');
-  d = get_option(varargin,'precompute_d',[]);
-else
-  S3G_global = equispacedSO3Grid(component.CS,component.SS,'resolution',res);
-  d = eval(component,S3G_global); %#ok<EVLC>
-end
+S3G_global = equispacedSO3Grid(component.CS,component.SS,'resolution',res);
+d = eval(conmponent,S3G_global); %#ok<EVLC>
 
-d(d<0) = 0;
-r1 = discretesample(d,npoints);
+% take global random samples
+d(d<0) = 0;   
+q1 = quaternion(S3G_global,discretesample(d,npoints));
 
-ori = orientation(reshape(quaternion(S3G_global(r1)),[],1) ...
-  .* reshape(quaternion(discreteSample(S3G_local,npoints)),[],1),...
-  component.CS,component.SS);
+% combine local and global
+ori = orientation(q1(:) .* q2(:),component.CS,component.SS);
