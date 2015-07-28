4134318adfa9809f91d40a2684d1a7b875ea0149
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-07-28 13:53:29 +0200

    bug fix: as reported by David Mainprice

--- geometry/@orientation/angle.m
+++ geometry/@orientation/angle.m
@@ -14,9 +14,22 @@ function omega = angle(o1,o2,varargin)
 if nargin == 1
   
   % do not care about inversion
-  o1.i = o1.i & ~o1.i;
+  q = quaternion(o1);
   
-  omega = real(2*acos(dot(o1,idquaternion,varargin{:})));
+  % for misorientations we do not have to consider all symmetries
+  [l,d,r] = factor(o1.CS,o1.SS);
+  dr= d * r;
+  qs = l * dr;
+  
+  % may be we can skip something
+  minAngle = reshape(abs(qs.angle),[],1);
+  minAngle = min([inf;minAngle(minAngle > 1e-3)]);
+  omega = 2 * real(acos(q.a));
+  notInside = omega > minAngle/2;
+  
+  % compute all distances to the symmetric equivalent orientations
+  % and take the minimum
+  omega(notInside) = 2 * real(acos(max(abs(dot_outer(q.subSet(notInside),qs)),[],2)));
   
 else
   
