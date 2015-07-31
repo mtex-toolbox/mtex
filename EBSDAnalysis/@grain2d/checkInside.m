82a0a0f6331c2c7855f9d61e7c0782c7bbc543ab
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-07-31 22:49:39 +0200

    new functions to detect inclusions grains inside a host grain

---
+++ EBSDAnalysis/@grain2d/checkInside.m
@@ -0,0 +1,43 @@
+function isInside = checkInside( grains, xy )
+% check for points or grains to be inside a big grain
+%
+% Syntax
+%   isInside = checkInside(hostGrains, [x,y])
+%   isInside = checkInside(hostGrains, inclusionGrains)
+%
+% Input
+%  hostGrains      - @grain2d
+%  inclusionGrains - @grain2d
+%  [x,y] - list of [x(:) y(:)] coordinates
+%
+% Output
+%  isInside - numInclusionGrains x numHostGrains matrix
+%
+% Example
+%  mtexdata small
+%  grains = calcGrains(ebsd('indexed'))
+%  plot(grains(grains(75).checkInside(grains)))
+%
+% See also
+% EBSD/findByLocation grain2d/findByOrientation
+
+
+if isa(xy,'grain2d'), xy = xy.centroid; end
+
+isInside = false(size(xy,1),length(grains));
+
+poly = grains.poly;
+V = grains.V;
+
+for i = 1:length(poly)
+
+  p = poly{i};
+  
+  firstloop = find(p(2:end)==p(1),1);
+  
+  p = p(1:firstloop);
+  Vx = V(p,1); Vy = V(p,2);
+  
+  isInside(:,i) = inpolygon(xy(:,1),xy(:,2),Vx,Vy);
+  
+end
