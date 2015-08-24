2dbc123da89850e9728401dca38164a7fafbad6a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-08-24 22:14:35 +0200

    role back saveFigure a bit as export_figure is not so relyable wth pdf

--- plotting/saveFigure.m
+++ plotting/saveFigure.m
@@ -22,18 +22,116 @@ if nargin == 0,
   fname = [pathstr,name];
 end
 
-try
-  oldColor = get(gcf,'color');
-  set(gcf,'color','w');
-  export_fig(gcf,fname,'-m1.5');
-  %export_fig(gcf,fname);
-  if exist(fname,'file'),
-    set(gcf,'color',oldColor);
-    return;
+% seperate extension
+[~, ~, ext] = fileparts(fname);
+
+if any(strcmpi(ext,{'.pdf','.eps','.ps'}))
+  
+  setPaperSize;
+
+  % for recent Matlab versions saveas should be fine
+  %if ~verLessThan('matlab','8.4')
+  %  saveas(gcf,fname); 
+  %  return; 
+  %end
+
+  % try to switch to painters mode for vector formats
+  % by converting RGB graphics to indexed graphics
+  if ~strcmpi(get(gcf,'renderer'),'painters') && isRGB
+  
+    try
+      convertFigureRGB2ind;
+      set(gcf,'renderer','painters');
+    catch
+      warning('MTEX:export','Unable to switch to painter''s mode. You may need to export to png or jpg');
+    end  
   end
-catch
+
+  % determine flags
+  switch lower(ext) 
+    case {'.eps','.ps'}
+      flags = {'-depsc'};
+    case {'.pdf'}
+      flags = {'-dpdf'};
+  end
+
+  printOptions = delete_option(varargin,{'crop','pdf'});
+  print(fname,flags{:},printOptions{:});
+
+  if check_option(varargin,'pdf')
+    unix(['epstopdf' ' ' fname]);
+    fname = strrep(fname,'eps','pdf');
+    unix(['pdfcrop' ' ' fname ' ' fname]);
+  end
+
+  if check_option(varargin,'crop')
+    unix(['pdfcrop' ' ' fname ' ' fname]); 
+  end
+  
+else % use export_fig with 50% magnification
+
+  try
+    oldColor = get(gcf,'color');
+    set(gcf,'color','w');
+    export_fig(gcf,fname,'-m1.5');
+    if exist(fname,'file'),
+      set(gcf,'color',oldColor);
+      return;
+    end
+  catch
+  end
+
+  set(gcf,'color',oldColor);
+  
+end
 end
 
-set(gcf,'color',oldColor);
+
+function out = isRGB
+
+out = false;
+
+ax = findall(gcf,'type','axes');
+childs = findobj(ax,'-property','CData');
+    
+if isempty(childs), return;end
+    
+CData = ensurecell(get(childs,'CData'));
+    
+out = any(cellfun(@(x) size(x,3)==3,CData));
+
+end
+
+%
+function convertAxisLabel2text
+
+ax = findall(gcf,'type','axes');
+
+for iax = 1:numel(ax)
+  
+  xLabel = get(ax(iax),'XTickLabel');
+  x = get(ax(iax),'XTick');
+  if ~isempty(xLabel) && ~isempty(x)
+    y = ylim(ax(iax));
+    text(ax(iax),x,repmat(y(1),size(x)),...
+      'FontName',get(ax(iax),'FontName'),...
+      'FontAngle',get(ax(iax),'FontAngle'),...
+      'FontWeight',get(ax(iax),'FontWeight'),...
+      'interpreter','tex');
+    set(ax(iax),'XTickLabel',[]);
+  end    
+end
+  
+end
+
+function setPaperSize
+% resize figure to look good
+
+ounits = get(gcf,'Units');
+set(gcf,'PaperPositionMode','auto');
+set(gcf,'Units','centimeters');
+pos = get(gcf,'PaperPosition');
+set(gcf,'PaperUnits','centimeters','PaperSize',[pos(3),pos(4)]);
+set(gcf,'Units',ounits);
 
 end
