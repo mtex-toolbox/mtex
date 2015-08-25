e86c19eaea12b790465dd8d7345fe61c3b8d8876
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-08-25 09:57:05 +0200

    correctly align axis angle sections

--- plotting/ODFSections/axisAngleSections.m
+++ plotting/ODFSections/axisAngleSections.m
@@ -82,7 +82,7 @@ classdef axisAngleSections < ODFSections
       if isempty(findall(ax,'tag','outerBoundary'))
         plot(fundamentalSector(oS.jointCS,varargin{:}),'parent',ax,...
           'TR',['\omega = ' int2str(oS.angles(sec)./degree),'^\circ'],'color',[0.8 0.8 0.8],...
-          'doNotDraw','tag','outerBoundary','hitTest','off');
+          'doNotDraw','tag','outerBoundary','xAxisDirection','east','zAxisDirection','outOfPlane','hitTest','off');
       end
       
       % rescale the axes according to actual volume      
