d69acfae610f7ec6812c17ea47c77511416dd33a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-09-14 16:18:21 +0200

    small fix in grain2d/perimeter make grainBoundary/perimeter functional
    fixes mtex-toolbox/mtex#85

--- EBSDAnalysis/@grainBoundary/perimeter.m
+++ EBSDAnalysis/@grainBoundary/perimeter.m
@@ -1,30 +1,33 @@
 function  peri = perimeter(gb,varargin)
-% boundary length
+% boundary length per grain including holes
 %
+% Syntax
+%   grains.boundary.perimeter 
+%
+%   % include subgrain boundaries
+%   grains.boundary.perimeter + grains.innerBoundary.perimeter
+%
+%   gB = grains.boundary('Iron','Iron');
+%
+%   % length of low angle boundary per grain
+%   gB(gB.misorientation.angle < 15*degree).perimeter 
+%
+%   % length of special boundaries per grain
+%   gB(gB.isTwinning(CSL(3))).perimeter
+%   
 % Input
 %  gb - @grainBoundary
 %
 % Output
-%  peri    - perimeter
-%
-% Syntax
-%   p = perimeter(grains.boundary) - 
-%
-%   p = perimeter(grains,10*degree) - returns the length of low angle
-%   boundaries per grain
-%
-% p = perimeter(grains,CSL(3)) - returns the length of special boundaries
-%   per grains
-%
-% p = perimeter(grains,property,...,param,val,...) -
+%  peri - list of grain perimeter sorted by grain id
 %
 % See also
-% Grain2d/equivalentPerimeter
-
+% Grain2d/perimeter
 
 F = reshape(nonzeros(gb.F),[],2);
 
 edgeLength = sqrt(sum((gb.V(F(:,1),:) - gb.V(F(:,2),:)).^2,2));
 
-peri = full(sparse(g,1,edgeLength,size(gb,1),1));
-
+peri = sparse(gb.grainId(:,2),1,edgeLength,max(gb.grainId(:)),1);
+ind = gb.grainId(:,1) > 0;
+peri = peri + sparse(gb.grainId(ind,1),1,edgeLength(ind),length(peri),1);
