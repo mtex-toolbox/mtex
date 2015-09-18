9ae70f4720cd0c85f0d0d72d79414ad5ec95638e
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-09-18 15:17:34 +0200

    fixes mtex-toolbox/mtex#89

--- ODFAnalysis/@ODF/discreteSample.m
+++ ODFAnalysis/@ODF/discreteSample.m
@@ -6,7 +6,11 @@ function ori = discreteSample(odf,npoints,varargin)
 ori = orientation.id(npoints,1,odf.CS,odf.SS);
 
 % which component
-icmp = discretesample(odf.weights,npoints);
+if numel(odf.weights) == 1
+  icmp = ones(size(ori));
+else
+  icmp = discretesample(odf.weights,npoints);
+end
 
 % compute discrete sample for each component seperately
 for ic = 1:length(odf.components)
