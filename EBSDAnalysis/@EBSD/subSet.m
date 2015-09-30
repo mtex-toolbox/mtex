68cd753e1e3f56aebab30c690349fb83f24a8025
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-09-30 22:25:02 +0200

    bug fix: EBSD phaseList must always be a column vector

--- EBSDAnalysis/@EBSD/subSet.m
+++ EBSDAnalysis/@EBSD/subSet.m
@@ -7,7 +7,7 @@ function ebsd = subSet(ebsd,ind)
 
 ebsd = subSet@dynProp(ebsd,ind);
 ebsd.rotations = ebsd.rotations(ind);
-ebsd.phaseId = ebsd.phaseId(ind);
+ebsd.phaseId = reshape(ebsd.phaseId(ind),[],1);
 ebsd.id = ebsd.id(ind);
 %if ~isempty(ebsd.grainId), ebsd.grainId = ebsd.grainId(ind); end
 if ~isempty(ebsd.A_D), ebsd.A_D = ebsd.A_D(ind,ind); end
