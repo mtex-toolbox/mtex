5a6108ca3e6f03397828e42e21f84704194ce3af
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-10-28 18:07:59 +0100

    bug fix

--- EBSDAnalysis/@tripelPointList/subSet.m
+++ EBSDAnalysis/@tripelPointList/subSet.m
@@ -10,6 +10,7 @@ function tP = subSet(tP,ind)
 %
 
 tP.V = tP.V(ind,:);
+tP.id = tP.id(ind,:);
 tP.boundaryId = tP.boundaryId(ind,:);
 tP.grainId = tP.grainId(ind,:);
 tP.phaseId = tP.phaseId(ind,:);
