cf4be5daab5ad1f895215af50b8ad4f8bf0cdf13
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2015-10-30 00:03:50 +0100

    phi1 sections as requested in https://groups.google.com/forum/#!topic/mtexmail/azoTq7KnfSY

---
+++ plotting/ODFSections/phi1Sections.m
@@ -0,0 +1,73 @@
+classdef phi1Sections < ODFSections
+  
+  properties
+    phi1
+    sR
+    maxphi1
+  end
+    
+  methods
+    
+    function oS = phi1Sections(CS1,CS2,varargin)
+   
+      oS = oS@ODFSections(CS1,CS2);
+      
+      % get fundamental plotting region
+      [oS.maxphi1,Phi,maxphi2] = fundamentalRegionEuler(CS1,CS2,varargin{:}); %#ok<*PROP>
+      oS.sR = sphericalRegion('maxTheta',Phi,'maxRho',maxphi2);
+            
+      % get sections
+      nsec = get_option(varargin,'sections',6);
+      oS.phi1 = linspace(0,oS.maxphi1,nsec+1);
+      oS.phi1(end) = [];
+      oS.phi1 = get_option(varargin,'phi1',oS.phi1,'double');
+      
+    end
+    
+    function ori = makeGrid(oS,varargin)
+      % generate a grid for plotting smooth functions
+    
+      oS.plotGrid = plotS2Grid(oS.sR,varargin{:});
+      oS.gridSize = (0:numel(oS.phi1)) * length(oS.plotGrid);
+      phi2 = repmat(oS.plotGrid.rho,[1,1,numel(oS.phi1)]);
+      Phi = repmat(oS.plotGrid.theta,[1,1,numel(oS.phi1)]);
+      phi1 = repmat(reshape(oS.phi1,1,1,[]),[size(oS.plotGrid) 1]);
+      
+      ori = orientation('Euler',phi1,Phi,phi2,'Bunge',oS.CS,oS.SS);
+
+    end
+
+    function n = numSections(oS)
+      n = numel(oS.phi1);
+    end
+    
+    function [S2Pos,secPos] = project(oS,ori,varargin)
+    
+      % maybe this can be done more efficiently
+      ori = ori.symmetrise('proper').';
+      [phi1,Phi,phi2] = Euler(ori,'Bunge'); %#ok<*PROPLC>
+
+      secPos = oS.secList(mod(phi1,oS.maxphi1),oS.phi1);
+      
+      S2Pos = vector3d('polar',Phi,phi2);
+
+    end
+        
+    function ori = iproject(oS,phi2,Phi,iphi1)
+      ori = orientation('Euler',oS.phi1(iphi1),Phi,phi2,'Bunge',oS.CS,oS.SS);
+    end
+    
+    function h = plotSection(oS,ax,sec,v,data,varargin)
+      
+      % plot data
+      h = plot(v,data{:},oS.sR,'TR',[int2str(oS.phi1(sec)./degree),'^\circ'],...
+        'parent',ax,'projection','plain','xAxisDirection','east',...
+        'xlabel','$\varphi_2$','ylabel','$\Phi$','dynamicMarkerSize',...
+        'zAxisDirection','intoPlane',varargin{:},'doNotDraw');
+
+    end
+  end
+end
+
+
+  
