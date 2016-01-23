ad96b06caf0e9f4e408e8320d88952e74ba02005
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-01-23 15:54:06 +0100

    subSet of EBSDsquare may not be EBSDsquare anymore

--- EBSDAnalysis/@EBSD/subSet.m
+++ EBSDAnalysis/@EBSD/subSet.m
@@ -10,4 +10,8 @@ ebsd.rotations = ebsd.rotations(ind);
 ebsd.phaseId = reshape(ebsd.phaseId(ind),[],1);
 ebsd.id = ebsd.id(ind);
 %if ~isempty(ebsd.grainId), ebsd.grainId = ebsd.grainId(ind); end
-if ~isempty(ebsd.A_D), ebsd.A_D = ebsd.A_D(ind,ind); end
+if ~isempty(ebsd.A_D), ebsd.A_D = ebsd.A_D(ind(:),ind(:)); end
+
+if islogical(ind) || min(size(ind))==1
+  ebsd = EBSD(ebsd);
+end
