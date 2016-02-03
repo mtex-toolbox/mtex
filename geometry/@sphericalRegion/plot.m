e13e009afaa1fbb52983db04a4f293d38cc97d53
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-02-03 17:54:06 +0100

    bug fix

--- geometry/@sphericalRegion/plot.m
+++ geometry/@sphericalRegion/plot.m
@@ -17,6 +17,7 @@ for j = 1:numel(sP)
  
   %if all(sR.checkInside(sP(j).sphericalRegion.N)) && ~isempty(sP(j).boundary)
   if ~isempty(sP(j).boundary) && sR == sP(j).sphericalRegion
+    varargin = delete_option(varargin,'parent');
     h = optiondraw(sP(j).boundary,varargin{:});
     continue; 
   end
