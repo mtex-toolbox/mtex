4b0d75325f9a5cfaf4eddede0c7c40942333e6ea
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-03-08 15:17:26 +0100

    quiver for EBSD

---
+++ EBSDAnalysis/@EBSD/quiver.m
@@ -0,0 +1,37 @@
+function h = quiver(ebsd,dir,varargin)
+% plot directions at ebsd centers
+%
+% Syntax
+%   quiver(ebsd,dir,'linecolor','r')
+%
+%   mtexdata small
+%   quiver(ebsd,ebsd.rotations.axis)
+%
+% Input
+%  ebsd - @grain2d
+%  dir    - @vector3d
+%
+% Options
+%  antipodal -
+%  maxHeadSize
+%
+
+xy = [ebsd.prop.x(:),ebsd.prop.y(:)];
+
+if check_option(varargin,'antipodal') || dir.antipodal
+
+  varargin = [{'MaxHeadSize',0,'linewidth',2,'autoScaleFactor',0.5},varargin];
+  xy = [xy;xy];
+  dir = [dir(:);-dir(:)];
+  
+else
+  
+  varargin = [{'MaxHeadSize',5,'linewidth',2,'autoScaleFactor',0.5},varargin];
+    
+end
+ 
+h = optiondraw(quiver(xy(:,1),xy(:,2),dir.x,dir.y),varargin{:});
+
+if nargout == 0, clear h; end
+
+end
