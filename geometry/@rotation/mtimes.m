6bf28264af1a23d33f85ab553ec13490506ba64c
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-03-11 21:43:19 +0100

    bug fix

--- geometry/@rotation/mtimes.m
+++ geometry/@rotation/mtimes.m
@@ -4,15 +4,15 @@ function r = mtimes(a,b)
 % multiplication with -1 -> inversion
 if isnumeric(a) 
   assert(all(abs(a(:))==1),'Rotations can be multiplied only by 1 or -1');
-  tmp = rotation(idquaternion(size(a)));
-  tmp(a==-1) = -tmp(a==-1);
+  tmp = rotation(rotation.id(size(a)));
+  tmp.i = (1-a)./2;
   a = tmp;
 end  
 
 if isnumeric(b)
   assert(all(abs(b(:))==1),'Rotations can be multiplied only by 1 or -1');
-  tmp = rotation(idquaternion(size(b)));
-  tmp(b==-1) = -tmp(b==-1);
+  tmp = rotation(rotation.id(size(b)));
+  tmp.i = (1-b)./2;
   b = tmp;
 end  
 
