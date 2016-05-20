8c8622b57b7c4ff5d9626d55faff3d3a822ceabc
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-05-20 08:39:27 +0200

    avoid warning about using repmat with empty argument

--- tools/misc_tools/vdisp.m
+++ tools/misc_tools/vdisp.m
@@ -1,6 +1,7 @@
 function vdisp(s,varargin)
 
 global prevCharCnt;
+if isempty(prevCharCnt), prevCharCnt = 0; end
 
 if ~check_option(varargin,'silent') && ~getMTEXpref('generatingHelpMode')
   fprintf(repmat('\b',1, prevCharCnt));
