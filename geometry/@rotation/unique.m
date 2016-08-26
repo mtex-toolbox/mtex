ae8bcc1b14562cb0d6d7e6d6480e95a8c47e189c
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-08-26 08:04:56 +0200

    minor

--- geometry/@rotation/unique.m
+++ geometry/@rotation/unique.m
@@ -16,14 +16,15 @@ a = r.a(:);
 b = r.b(:);
 c = r.c(:);
 d = r.d(:);
+i = r.i(:);
 
-abcd = [a.^2,b.^2,c.^2,d.^2,a.*b,a.*c,a.*d,b.*c,b.*d,c.*d,r.i(:)];
+abcd = [a.^2,b.^2,c.^2,d.^2,a.*b,a.*c,a.*d,b.*c,b.*d,c.*d,i];
 
 [ignore,m,n] = unique(round(abcd*1e4),'rows'); %#ok<ASGLU>
 
 % remove duplicated points
-r.a = r.a(m);
-r.b = r.b(m);
-r.c = r.c(m);
-r.d = r.d(m);
-r.i = r.i(m);
+r.a = a(m);
+r.b = b(m);
+r.c = c(m);
+r.d = d(m);
+r.i = i(m);
