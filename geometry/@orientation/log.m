2c6d05a90aed51871abdf4ff5f012d4ff824a51a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-11-09 07:13:40 +0100

    when computing the log of an orientation we should keep the reference frame of the reference orientation
    this is particularly important when computing the gradient as we need the same reference frame for gradientX and gradientY

--- geometry/@orientation/log.m
+++ geometry/@orientation/log.m
@@ -19,12 +19,18 @@ if nargin == 2
 
   if isa(ori.CS,'crystalSymmetry')
     ori = inv(ori_ref) .* ori;
-  else
+    % we should not change the reference frame of the reference
+    % orientation
+    ori.SS = specimenSymmetry;
+  else    
     ori = ori .* inv(ori_ref);
+    % we should not change the reference frame of the reference
+    % orientation
+    ori.CS = specimenSymmetry;
   end
     
 end
-  
+
 ori = project2FundamentalRegion(ori);
 
 v = log@quaternion(ori);
