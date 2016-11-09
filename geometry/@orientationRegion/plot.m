8e1f98875fc19310d2912c7a2b2017db444b1604
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2016-11-09 07:10:09 +0100

    fix mtex-toolbox/mtex#220
    draw the edges of the orientation region using fibres. This makes the code much nicer and more robust as it avoids to call orientationRegion/maxAngle

--- geometry/@orientationRegion/plot.m
+++ geometry/@orientationRegion/plot.m
@@ -65,34 +65,32 @@ switch lower(projection)
     % TODO: do not use maxAngle - because this would allow us to rotate the
     % orientation region
     r = oR.maxAngle(r) .* r ./ degree;
-    %surf(r.x,r.y,r.z,'faceColor','k','facealpha',0.1,'edgecolor','none')
     surf(r.x,r.y,r.z,'faceColor',color,'facealpha',0.1,...
       'edgecolor','none')
     
-    %plot the edges
-    hold on
-    t = linspace(0,1);
-    for i = 1:length(oR.F)
-      
-      for j = 1:length(oR.F{i})
-        
-        % can we use axis(..,'noSymmetry') here?
-        r = t .* axis(quaternion(oR.V(oR.F{i}(j)))) + (1-t) .* ...
-          axis(quaternion(oR.V(oR.F{i}(mod(j,length(oR.F{i}))+1))));
-        % TODO: plot the geodesics and do not use maxAngle
-        r = normalize(r) .* oR.maxAngle(r) ./ degree;
-        line(r.x,r.y,r.z,'color',color,'linewidth',1.5);
-        
-      end
-    end
+    % extract the vertices 
+    left = oR.V(vertcat(oR.F{:}));
+    right = cellfun(@(x) circshift(x,1), oR.F,'UniformOutput',false);
+    right = oR.V(vertcat(right{:}));
+    
+    % edges are just fibres connecting the vertices
+    f = fibre(left,right);
+    
+    % some of the edges should not be ploted
+    f = f(angle(f.o1,f.o2,'noSymmetry')>1e-5);
+    f = f(angle(f.o1,'noSymmetry')<pi | angle(f.o2,'noSymmetry')<pi);
+    
+    % plot the fibres
+    plot(f,'color',color,'linewidth',1.5)
+    
     hold off
+    
 end
 
 if isNew
   axis equal off
   fcw;
   view(mtexFig.gca,3);
-  
 end
 
 if nargout == 0, clear h; end
