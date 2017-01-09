256df87d6278e3d6286c6f82fc1dd3066fb915b2
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-01-09 07:31:56 +0100

    some functions return sphFunTri

--- geometry/@tensor/shearModulus.m
+++ geometry/@tensor/shearModulus.m
@@ -16,8 +16,26 @@ function E = shearModulus(C,h,u)
 % See also
 % tensor/YoungsModulus tensor/volumeCompressibility tensor/ChristoffelTensor
 
+% prepare to return a spherical function
+generateFun = 0;
+if nargin == 1 || isempty(h)
+  h = equispacedS2Grid('points',10000);
+  generateFun = 1;
+end
+if nargin <= 2 || isempty(u)
+  u = equispacedS2Grid('points',10000);
+  generateFun = 2;
+end
+
 % compute the compliance tensor
 S = inv(C);
 
 % compute directional magnitude
 E = 0.25./matrix(EinsteinSum(S,[-1 -2 -3 -4],h,-1,u,-2,h,-3,u,-4));
+
+% generate a function if required
+if generateFun == 1
+  E = sphFunTri(h,E);
+else
+  E = sphFunTri(u,E);
+end  
