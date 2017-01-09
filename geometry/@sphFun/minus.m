1c8c6d38ce6d2d4268793bb9ce6821b675f199ee
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-01-09 07:29:47 +0100

    rename sphFun in sphFunTri to allow also for other spherical approximations

--- geometry/@sphFun/minus.m
+++ geometry/@sphFun/minus.m
@@ -1,20 +1,5 @@
 function sF = minus(sF1,sF2)
         
-if isnumeric(sF1) % sF = 1 - sF2
-  
-  sF = sF2;
-  sF.values = sF1 - sF.values;
-  
-elseif isnumeric(sF2) % sF = sF1 - 2
-  
-  sF = sF1;
-  sF.values = sF1.values - sF2;
-  
-else                     % sF = sF1 - sF2
-  
-  sF = sF1;
-  sF.values = sF1.values - sF2.values;
-  
-end
+sF = plus(sF1,-sF2);
         
 end
