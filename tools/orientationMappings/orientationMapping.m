e028103480ee086f2242b3ad98454be72ae62bf0
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-02-25 21:25:50 +0100

    orientationMapping should have the option antipodal

--- tools/orientationMappings/orientationMapping.m
+++ tools/orientationMappings/orientationMapping.m
@@ -4,7 +4,8 @@ classdef orientationMapping < handle
   
   properties
     CS1 = crystalSymmetry % crystal symmetry
-    CS2 = specimenSymmetry % crystal symmetry of a second phase for misorientations    
+    CS2 = specimenSymmetry % crystal symmetry of a second phase for misorientations
+    antipodal = false
   end
    
   methods
@@ -16,10 +17,12 @@ classdef orientationMapping < handle
       elseif isa(ebsd,'orientation')
         oM.CS1 = ebsd.CS;
         oM.CS2 = ebsd.SS;
+        oM.antipodal = ensd.antipodal;
       elseif isa(ebsd,'grainBoundary')
         [oM.CS1,oM.CS2] = deal(ebsd.CS{:});
         oM.CS1 = oM.CS1;
         oM.CS2 = oM.CS2;
+        oM.antipodal = all(diff(ebsd.phaseId,[],2)==0);
       elseif isa(ebsd,'symmetry')
         oM.CS1 = ebsd;
       end
@@ -27,6 +30,8 @@ classdef orientationMapping < handle
       if nargin > 1 && isa(varargin{1},'symmetry')
         oM.CS2 = varargin{1};
       end
+
+      oM.antipodal = oM.antipodal || check_option(varargin,'antipodal');
       
 %       if oM.CS1.id == oM.CS1.Laue.id && ~getMTEXpref('generatingHelpMode')
 % 
@@ -36,12 +41,20 @@ classdef orientationMapping < handle
 %         disp(' ')
 %       end
       
+
+
     end
       
     function plot(oM,varargin)
       % plot an color bar
 
-      oS = newODFSectionPlot(oM(1).CS1,oM(1).CS2,varargin{:});
+      if oM(1).antipodal
+        flag = 'antipodal';
+      else
+        flag = '';
+      end
+      oS = newODFSectionPlot(oM(1).CS1,oM(1).CS2,...
+        flag,varargin{:});
 
       ori = oS.makeGrid(varargin{:});
       rgb = oM.orientation2color(ori);
