b1610905c9b198d0aeb21c3f95a0321c6afba70d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-03-16 11:20:26 +0100

    bug fix, return the gradient in specimen coordinates

--- EBSDAnalysis/@EBSDsquare/EBSDsquare.m
+++ EBSDAnalysis/@EBSDsquare/EBSDsquare.m
@@ -50,20 +50,40 @@ classdef EBSDsquare < EBSD
     % --------------------------------------------------------------
     
     function gX = get.gradientX(ebsd)
+      % gives the gradient in X direction with respect to specimen
+      % coordinate system
+      
       ori = ebsd.orientations;
           
       ori_right = ori(:,[2:end end-1]);
-      gX = log(ori_right,ori) ./ ebsd.dx;
+      gX = ori .* log(ori_right,ori) ./ ebsd.dx;
       gX(:,end) = - gX(:,end);
     end
     
     function gY = get.gradientY(ebsd)
+      % gives the gradient in Y direction with respect to specimen
+      % coordinate system
+      
       ori = ebsd.orientations;
           
       ori_up = ori([2:end end-1],:);
-      gY = log(ori_up,ori) ./ ebsd.dy;
+      gY = ori .* log(ori_up,ori) ./ ebsd.dy;
       gY(end,:) = - gY(end,:);
     end
+    
+    % some testing code - gradient can be either in specimen coordinates or
+    % in crystal coordinates 
+    % 
+    % cs = crystalSymmetry('321')
+    % ori1 = orientation.rand(cs)
+    % ori2 = orientation.rand(cs)
+    %
+    % the following output should be constant
+    % gO = log(ori1,ori2.symmetrise) % but not true for this
+    % gO = log(ori1.symmetrise,ori2) % true for this
+    %
+    % gO = ori2.symmetrise .* log(ori1,ori2.symmetrise) % true for this
+    % gO = ori2 .* log(ori1.symmetrise,ori2) % true for this
     
   end
       
