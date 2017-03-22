39e5e8a3d0be5b8ea3b56c4ce0b743df4c94bc5d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-03-22 22:51:52 +0100

    docu

--- geometry/@orientation/scatter.m
+++ geometry/@orientation/scatter.m
@@ -14,6 +14,11 @@ function varargout = scatter(o,varargin)
 %  points    - number of orientations to be plotted
 %  center    - orientation center
 %
+% Flags
+%  ignoreFundamentalRegion    - plot orientation as they are
+%  project2FundamentalRegion  - project orientations to fundamentalRegion (default)
+%  restrict2FundamentalRegion - ignore all orientations outside the fundamentalRegion
+%
 % See also
 % vector3d/text orientation/plot
 
