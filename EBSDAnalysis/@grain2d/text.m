4a4b7233d59a500d66a185e0051ed9261f58cc91
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-06-22 13:34:44 +0200

    docu

--- EBSDAnalysis/@grain2d/text.m
+++ EBSDAnalysis/@grain2d/text.m
@@ -4,9 +4,13 @@ function h = text(grains,txt,varargin)
 % Syntax
 %   text(grains(29),'A')
 %
+%   % plot grains with grainId
+%   text(grains,arrayfun(@num2str,grains.id,'uniformOutput',false))
+%
 % Input
 %  grains - @grain2d
 %
+
 
 
 xy = grains.centroid;
