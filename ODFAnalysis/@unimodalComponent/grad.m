d4d2e721fe843df0d7f4950bc8068040af5fb4aa
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-09-09 21:59:06 +0200

    faster gradient computation

--- ODFAnalysis/@unimodalComponent/grad.m
+++ ODFAnalysis/@unimodalComponent/grad.m
@@ -20,11 +20,10 @@ function g = grad(component,ori,varargin)
 
 % we need to consider all symmetrically equivalent centers
 q2 = quaternion(ori);
-CS = ori.CS;
 center = component.center;
 qSS = unique(quaternion(component.SS));
 psi = component.psi;
-epsilon = min(pi,get_option(varargin,'epsilon',psi.halfwidth*4));
+epsilon = min(pi,get_option(varargin,'epsilon',psi.halfwidth*3.5));
 
 % initialize output
 g = vector3d.zeros(size(ori));
@@ -33,22 +32,24 @@ g = vector3d.zeros(size(ori));
 for issq = 1:length(qSS)
   d = abs(dot_outer(center,qSS(issq) * q2,'epsilon',epsilon,...
     'nospecimensymmetry'));
-   
-  for iori = 1:length(ori)
-    
-    id = d(:,iori)>0;
-    cc = project2FundamentalRegion(inv(qSS(issq) * q2(iori)) * quaternion(center,id),CS);
-    v = log(cc);
-    nv = norm(v);
-    v(nv>0) = v(nv>0)./nv(nv>0);
-    
-    %c = project2FundamentalRegion(quaternion(center,id),CS,qSS(issq) * q2);
-    %v = log(c,qSS(issq) * q2);
-    g(iori) = g(iori) - sum(v .* component.weights(id) .* psi.DK(full(d(id,iori))));
-  end
+  
+  d(d<=cos(epsilon/2)) = 0;
+  
+  [i,j] = find(d>cos(epsilon/2));
+  
+  % the normalized logarithm
+  v = log(center(i),reshape(qSS(issq) * ori(j),[],1));
+  nv = norm(v);
+  v(nv>0) = v(nv>0)./nv(nv>0);
+  
+  v = sparse(i,j,v,length(center),length(ori)) .* spfun(@psi.DK,d);
+  
+  g = g - v.' * component.weights;
+  
 end
 
 % TODO: consider antipodal
 if component.antipodal
+  
 end
 
