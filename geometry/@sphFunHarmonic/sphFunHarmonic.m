8fc8922fd4fac95eb33a462c53ed880cb1c0b21b
Felix Bartel <felixbartel@protonmail.com>
2017-12-12 14:46:11 +0100

    removed tabs

--- geometry/@sphFunHarmonic/sphFunHarmonic.m
+++ geometry/@sphFunHarmonic/sphFunHarmonic.m
@@ -2,50 +2,50 @@ classdef sphFunHarmonic < sphFun
 % a class representing a function on the sphere
 
 properties
-	fhat = [] % harmonic coefficients
+  fhat = [] % harmonic coefficients
 end
 
 properties (Dependent=true)
-	M % maximum harmonic degree / bandwidth
+  M % maximum harmonic degree / bandwidth
 end
 
 methods
   
-	function sF = sphFunHarmonic(fhat)
+  function sF = sphFunHarmonic(fhat)
         if nargin == 0, return; end
   % initialize a spherical function
   
     fhat = fhat(:);
-		M = ceil(sqrt(length(fhat))-1); % make (M+1)^2 entries
-		fhat = [fhat; zeros((M+1)^2-length(fhat), 1)];
+    M = ceil(sqrt(length(fhat))-1); % make (M+1)^2 entries
+    fhat = [fhat; zeros((M+1)^2-length(fhat), 1)];
 
     % truncate neglectable coefficients
-		cutoff = eps; 
+    cutoff = eps; 
     ii = 0;
-		while sum(abs(fhat((M-ii)^2+1:(M+1)^2))) <= cutoff && M > 0
-			ii = ii+1; 
-		end
-		sF.fhat = fhat(1:(M+1-ii)^2);
+    while sum(abs(fhat((M-ii)^2+1:(M+1)^2))) <= cutoff && M > 0
+      ii = ii+1; 
+    end
+    sF.fhat = fhat(1:(M+1-ii)^2);
     
   end
   
-	function M = get.M(sF)
-		M = sqrt(length(sF.fhat))-1;
+  function M = get.M(sF)
+    M = sqrt(length(sF.fhat))-1;
   end
   
-	function fhat = get_fhat(sF, m, l)
-		if abs(l) <= m && 0 <= m && m <= sF.M
-			fhat = sF.fhat(m*(m+1)+l+1);
-		else
-			fhat = 0;
-		end
+  function fhat = get_fhat(sF, m, l)
+    if abs(l) <= m && 0 <= m && m <= sF.M
+      fhat = sF.fhat(m*(m+1)+l+1);
+    else
+      fhat = 0;
+    end
   end
   
 end
 
 methods (Static = true)
-	sF = approximation(v, y, varargin);
-	sF = quadrature(f, varargin);
+  sF = approximation(v, y, varargin);
+  sF = quadrature(f, varargin);
 end
 
 end
