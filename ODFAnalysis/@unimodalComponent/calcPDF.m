f5977e1503450c3d82f660e300f2b284fe2d16e1
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-12-14 09:12:25 +0100

    use sphFunHarmonic to compute pole figures

--- ODFAnalysis/@unimodalComponent/calcPDF.m
+++ ODFAnalysis/@unimodalComponent/calcPDF.m
@@ -1,7 +1,42 @@
 function pdf = calcPDF(component,h,r,varargin)
+% compute the pole density function for a given unimodal component
 
+if nargin > 2 && min(length(h),length(r)) > 0 && ...
+    (check_option(varargin,'old') || max(length(h),length(r)) < 1000)
+  
+  pdf = component.psi.RK_symmetrised(...
+    component.center,h,r,component.weights,...
+    component.CS,component.SS,varargin{:});
+  return
+  
+end
 
-pdf = component.psi.RK_symmetrised(...
-  component.center,h,r,component.weights,...
-  component.CS,component.SS,varargin{:});
+if length(h) == 1
 
+  sh = symmetrise(h,'unique');
+  pdf = sphFunHarmonic.quadrature(...
+    repmat(component.weights(:),1,length(sh)),component.center*sh);
+  
+  pdf = 4 * pi * conv(pdf,component.psi)./ length(sh);
+
+  % symmetrise with respect to specimen symmetry
+  pdf = pdf.symmetrise(component.SS);
+  
+  % maybe we should evaluate
+  if nargin > 2 && isa(r,'vector3d'), pdf = pdf.eval(r); end
+  
+else
+
+  sr = symmetrise(r,component.SS,'unique');
+  pdf = sphFunHarmonic.quadrature(...
+    repmat(component.weights(:),1,length(sr)),inv(component.center)*sr);
+  
+  pdf = 4 * pi * conv(pdf,component.psi) ./ length(sr);
+
+  % symmetrise with respect to crystal symmetry
+  pdf = pdf.symmetrise(component.CS);
+  
+  % maybe we should evaluate
+  if isa(h,'vector3d'), pdf = pdf.eval(h); end
+  
+end
