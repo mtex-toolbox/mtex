0cd903d4b41b387a6ac7f5297167499a1a19b421
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-12-14 08:23:33 +0100

    rotating a harmonic spherical function keeps the polynomial degree

--- geometry/@sphFunHarmonic/rotate.m
+++ geometry/@sphFunHarmonic/rotate.m
@@ -1,21 +1,15 @@
-function sF = rotate(sF, varargin)
-% rotate a function by rot
+function sF = rotate(sF, rot)
+% rotate a function by a rotation
+%
 % Syntax
 %  sF = sF.rotate(rot)
-%  sF = sF.rotate(rot, 'm', M)
 %
 % Input
-%  sF - spherical function
-%  rot - rotation
+%  sF - @sphFunHarmonic
+%  rot - @rotation
 %
-% Options
-%  M - minimal degree of the spherical harmonic
-%
-
-rot = varargin{1};
-M = get_option(varargin, 'm', min(sF.M, 500));
 
 f = @(v) sF.eval(v.rotate(rot));
-sF = sphFunHarmonic.quadrature(f, 'm', max(M, 100));
+sF = sphFunHarmonic.quadrature(f, 'M', sF.M);
 
 end
