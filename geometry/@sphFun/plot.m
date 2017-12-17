dd196a3c175a500bafe6b7902e8e64da2e64b624
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2017-12-17 18:22:16 +0100

    better plotting

--- geometry/@sphFun/plot.m
+++ geometry/@sphFun/plot.m
@@ -11,6 +11,9 @@ function h = plot(sF,varargin)
 % See also
 %
 
+% create a new figure if needed
+[mtexFig,isNew] = newMtexFigure('datacursormode',@tooltip,varargin{:});
+
 % generate a grid where the function will be plotted
 plotNodes = plotS2Grid(varargin{:});
 
@@ -22,5 +25,14 @@ h = plot(plotNodes,values,'contourf',varargin{:});
 
 % remove output if not required
 if nargout == 0, clear h; end
+
+  function txt = tooltip(varargin)
+    
+    [r_local,v] = getDataCursorPos(mtexFig);
+    %v = sF.eval(r_local);
+    
+    txt = [xnum2str(v) ' at (' int2str(r_local.theta/degree) ',' int2str(r_local.rho/degree) ')'];
+  
+  end
 
 end
--- geometry/@sphFunHarmonicS/plot.m
+++ geometry/@sphFunHarmonicS/plot.m
@@ -1,4 +1,4 @@
-function h = plot(sF,varargin)
+function varargout = plot(sF,varargin)
 %
 % Syntax
 %
@@ -11,7 +11,29 @@ function h = plot(sF,varargin)
 % See also
 %
 
-sR = sF.s.fundamentalSector;
-sF.plot@sphFun(sR);
+% create a new figure if needed
+[mtexFig,isNew] = newMtexFigure('datacursormode',@tooltip,varargin{:});
+
+% get plotting region
+sR = sF.s.fundamentalSector(varargin{:});
+
+% set axes alignment
+if isa(sF.s,'crystalSymmetry')
+  varargin = [sF.s.plotOptions,varargin];
+end
+
+% perform plotting
+[varargout{1:nargout}] = sF.plot@sphFun(sR,'parent',mtexFig.gca,varargin{:});
+
+
+function txt = tooltip(varargin)
+
+[h_local,value] = getDataCursorPos(mtexFig);
+
+h_local = Miller(h_local,sF.s,'uvw');
+h_local = round(h_local,'tolerance',3*degree);
+txt = [xnum2str(value) ' at ' char(h_local)];
+
+end
 
 end
