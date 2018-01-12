f1a7f4436e85bc8b9ddd391b1ced6a01e67590bb
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-12 13:20:10 +0100

    important bug fix

--- ODFAnalysis/@FourierComponent/calcPDF.m
+++ ODFAnalysis/@FourierComponent/calcPDF.m
@@ -9,6 +9,13 @@ even = 1 + (check_option(varargin,'antipodal') ...
 L = get_option(varargin,'bandwidth',component.bandwidth);
 L = min(L,component.bandwidth);
 
+
+if length(h) == 1  % pole figures
+  sym = component.SS;
+else
+  sym = component.CS;
+end
+
 ipdf_hat = cumsum([0,2*(0:L)+1]);
 
 % calculate Fourier coefficients of the pole figure
@@ -17,6 +24,7 @@ for l = 0:even:L
     P_hat(1+ipdf_hat(l+1):ipdf_hat(l+2)) = reshape(...
       component.f_hat(1+deg2dim(l):deg2dim(l+1)),2*l+1,2*l+1) ./(2*l+1) ...
       * 4 * pi * sphericalY(l,h).';
+    
   elseif  length(r) == 1 % inverse pole figures
     P_hat(1+ipdf_hat(l+1):ipdf_hat(l+2)) = reshape(...
       component.f_hat(1+deg2dim(l):deg2dim(l+1)),2*l+1,2*l+1)' ./(2*l+1) ...
@@ -27,7 +35,7 @@ for l = 0:even:L
 end
 
 % setup a spherical harmonic function
-pdf = S2FunHarmonic(conj(P_hat));
+pdf = S2FunHarmonicSym(conj(P_hat(:)),sym);
 
 % evaluate if required
 if length(h) > 1
