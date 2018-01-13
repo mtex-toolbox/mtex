e16d0a1f958c8f15cf0c7da3a5b1afdb66a5c7bd
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-13 06:48:36 +0100

    very minor

--- S2Fun/@S2FunHarmonic/private/drho.m
+++ S2Fun/@S2FunHarmonic/private/drho.m
@@ -1,4 +1,4 @@
-function sF = drho(sF);
+function sF = drho(sF)
 % first derivative in direction rho
 
 s = size(sF);
@@ -6,7 +6,7 @@ sF = reshape(sF, []);
 
 fhat = zeros(size(sF.fhat));
 for m = 0:sF.bandwidth
-  fhat(m*(m+1)+(-m:m)+1, :) = i*(-m:m)'.*sF.fhat(m*(m+1)+(-m:m)+1, :);
+  fhat(m*(m+1)+(-m:m)+1, :) = 1i*(-m:m)'.*sF.fhat(m*(m+1)+(-m:m)+1, :);
 end
 sF = S2FunHarmonic(fhat);
 
