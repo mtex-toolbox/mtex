5be982d1ac3adcf0b37608da8db6d8888445963d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-14 23:08:46 +0100

    subsref for crystalShape This allows for crystalShape plots in orientation sections

---
+++ geometry/@crystalShape/subsref.m
@@ -0,0 +1,25 @@
+function varargout = subsref(cS,s)
+%overloads subsref
+
+switch s(1).type
+  case '()'
+    
+    F = reshape(cS.F,[],size(cS.V,2),size(cS.F,2));
+    
+    ind = subsref((1:size(cS.V,2)).',s(1));
+    
+    cS.V = cS.V(:,ind);
+    shift = repmat(repelem(((1:length(ind)).' - ind)*size(cS.V,1),size(F,1),1),1,size(F,3));
+    cS.F = reshape(F(:,ind,:),[],size(F,3)) + shift;
+    
+    if numel(s)>1
+      [varargout{1:nargout}] = builtin('subsref',cS,s(2:end));
+    else
+      varargout{1} = cS;
+    end  
+  otherwise
+    
+    [varargout{1:nargout}] = builtin('subsref',cS,s);
+      
+end
+end
