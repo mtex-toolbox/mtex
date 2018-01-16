963c2c13360aa80a58ecde5151c6129e5426497d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-16 07:18:24 +0100

    not needed anymore

--- geometry/@tensor/plotS2Grid.m
+++
@@ -1,9 +0,0 @@
-function S2G = plotS2Grid(T,varargin)
-% define a plotting grid suitable for tensor plots
-  
-if iseven(T.rank)
-  varargin = [varargin,'antipodal'];
-end
-  
-sR = fundamentalSector(T.CS,varargin{:});
-S2G = plotS2Grid(sR,varargin{:});
--- geometry/@tensor/quiverGrid.m
+++
@@ -1,6 +0,0 @@
-function S2G = quiverGrid(T,varargin)
-
-if iseven(T.rank), varargin = [varargin,'antipodal']; end 
-sR = fundamentalSector(T.CS,varargin{:});
-S2G = equispacedS2Grid('resolution',10*degree,'no_center','antipodal',varargin{:});
-S2G = S2G(sR.checkInside(S2G));
