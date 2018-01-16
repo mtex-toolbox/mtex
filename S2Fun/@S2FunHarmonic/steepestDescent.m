e2a48e8520b9bc041b5060ba0fabd4ea6118b3ae
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-16 07:06:19 +0100

    + more simple yet effectiy line search + take care about antipodal

--- S2Fun/@S2FunHarmonic/steepestDescent.m
+++ S2Fun/@S2FunHarmonic/steepestDescent.m
@@ -16,64 +16,55 @@ function [f,v] = steepestDescent(sF, varargin)
 %  STARTINGNODES  -  starting nodes of type @vector3d
 %  
 
-% parameters{{{
-tau   = get_option(varargin, 'tau', 1e-8); % tolerance
-kmax  = get_option(varargin, 'kmax', 6); % maximal iterations
-mu    = get_option(varargin, 'mu', 0.4); % in (0, 0.5) for Armijo condition
-tauLS = get_option(varargin, 'tauLS', 0.75); % in (0, 1) alpha(k+1) = tauLS*alpha(k)
-kmaxLS= get_option(varargin, 'kmaxLS', 8); % maximal iterations for line search
-v = get_option(varargin, 'startingnodes', equispacedS2Grid('points', 2^10));
+% parameters
+res = get_option(varargin,'resolution',0.025*degree);
+kmax  = get_option(varargin, 'kmax', 20); % maximal iterations
+isAntipodal = {'','antipodal'};
+v = get_option(varargin, 'startingnodes', ...
+  equispacedS2Grid('points', min(1000000,2*sF.bandwidth^2), isAntipodal{sF.antipodal+1}));
 v = v(:);
 v = v(v.theta > 0.01 & v.theta < pi-0.01);
-%}}}
 
-% actual steepest descent{{{
+% possible steplength
+omega = 1.25.^(-30:1:15) * degree;
+omega(omega<res) = [];
+omega = [0,omega];
+
+% actual steepest descent
 for k = 0:kmax
-% debug{{{
-%  clf; hold on;
-%  plot(sF, 'complete');
-%  scatter(v, 'MarkerSize', 10, 'MarkerColor', 'k');
-%  pause(1);
-%}}}
 
-  d = -sF.grad(v);
-  normd = norm(d);
-  if  sum(normd)/length(v) < tau, break; end
-  alpha = normd/max(normd);
-% step length by linesearch{{{
-  f0 = sF.eval(v);
-  vd = diag(cos(normd))*v+diag(sin(normd)./max(normd, 1e-5))*d;
-  g = dot(sF.grad(vd), d);
-  for kLS = 1:kmaxLS
-    valphad = diag(cos(alpha.*normd))*v+diag(sin(alpha.*normd)./max(normd, 1e-5))*d;
-    f = sF.eval(valphad);
-    allgood = true;
-    for ii = 1:length(v)
-      if f(ii)-f0(ii) > mu*alpha(ii)*g(ii)
-        alpha(ii) = tauLS*alpha(ii);
-        allgood = false;
-      end
-    end
-    if allgood == true, break; end
-  end
-%}}}
-  [v, I] = unique(valphad, 'tolerance', 0.005);
+  d = -normalize(sF.grad(v));
+  
+  % search line
+  line_v = repmat(v,1,length(omega)) + d * omega;
+  
+  % evaluate along lines
+  line_f = reshape(sF.eval(line_v),size(line_v));
+  
+  % take the maximum
+  [f,id] = min(line_f,[],2);
+  
+  % update v
+  v = normalize(line_v(sub2ind(size(line_v),(1:length(v)).',id)));
+  
+  if all(id == 1), break; end
+
+  % maybe we can reduce the number of points a bit
+  [v, I] = unique(v, 'tolerance', 0.005);
+  f = f(I);
+  
 end
-%}}}
 
-% format output{{{
-f = f(I);
+% format output
 [f, I] = sort(f);
 if check_option(varargin, 'numLocal')
   n = get_option(varargin, 'numLocal');
   n = min(length(v), n);
   f = f(1:n);
-  v = v(I(1:n));
 else
   n = sum(f-f(1) < 1e-4);
   f = f(1);
 end
 v = v(I(1:n));
-%}}}
 
 end
