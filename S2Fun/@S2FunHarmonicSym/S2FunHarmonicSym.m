657b4c7e5faee5d3a90fa86b42f76d398eff62e1
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-16 06:56:54 +0100

    add CS and SS for compatibility

--- S2Fun/@S2FunHarmonicSym/S2FunHarmonicSym.m
+++ S2Fun/@S2FunHarmonicSym/S2FunHarmonicSym.m
@@ -5,11 +5,25 @@ properties
   s = []; % symmetry
 end
 
+properties (Dependent = true)
+  CS
+  SS 
+end
+
+
 methods
   function sF = S2FunHarmonicSym(fhat, s)
     if nargin == 0, return; end
     sF.fhat = fhat;
     sF.s = s;
+  end
+  
+  function CS = get.CS(sF)
+    CS = sF.s;
+  end
+  
+  function SS = get.SS(sF)
+    SS = sF.s;
   end
   
 end
