e841fedcbbb03225f946437e5719fd2aed72b8f4
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-16 07:05:23 +0100

    set starting nodes to by of type Miller then unique works nicely within the loop

--- S2Fun/@S2FunHarmonicSym/steepestDescent.m
+++ S2Fun/@S2FunHarmonicSym/steepestDescent.m
@@ -1,15 +1,15 @@
 function [f,v] = steepestDescent(sF, varargin)
 % calculates the minimum of a spherical harminc
 % Syntax
-%   [v,pos] = steepestDescent(sF) % the position where the minimum is atained
+%   [f,pos] = steepestDescent(sF) % the position where the minimum is atained
 %
-%   [v,pos] = steepestDescent(sF,'numLocal',5) % the 5 largest local minima
+%   [f,pos] = steepestDescent(sF,'numLocal',5) % the 5 largest local minima
 %
 %   % with all options
-%   [v,pos] = steepestDescent(sF, 'startingnodes')
+%   [f,pos] = steepestDescent(sF, 'startingnodes')
 %
 % Output
-%  v - double
+%  f - double
 %  pos - @vector3d
 %
 % Options
@@ -19,13 +19,15 @@ function [f,v] = steepestDescent(sF, varargin)
 % parameters
 if ~check_option(varargin, 'startingnodes')
   sR = sF.s.fundamentalSector;
-  v = equispacedS2Grid('points', 2^10/sR.volume);
+  v = equispacedS2Grid('points',min(1000000,2*sF.bandwidth^2));
   v = v(sR.checkInside(v));
+  
+  if isa(sF.s,'crystalSymmetry')
+    v = Miller(v, sF.s);
+  end
   varargin = set_option(varargin, 'startingnodes', v);
 end
 
-[f, v] = steepestDescent@S2FunHarmonic(sF, varargin{:})
-
-v = Miller(v, sF.s);
+[f, v] = steepestDescent@S2FunHarmonic(sF, varargin{:});
 
 end
