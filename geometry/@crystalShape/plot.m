4ded85f22f676cb8027a3c55b0b9402577575210
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-01-17 20:55:44 +0100

    allow syntax
    plot(x,y,crystalShape)
    
    and
    
    plot(xy,crystalShape)

--- geometry/@crystalShape/plot.m
+++ geometry/@crystalShape/plot.m
@@ -3,15 +3,33 @@ function h = plot(cS,varargin)
 %
 % Syntax
 %   plot(cS)  % colorize by phase
-%   plot(cS,property) % colorize by property
+%   plot(x,y,cS)
+%   plot(xy,cS)
+%   plot(cS,'faceColor','red','faceAlpha',0.5,'edgeColor','k') % colorize by property
 %
 % Input
 %  cS  - @crystalShape
+%  x,y - coodinates
+%  xy  - nx2 or nx3 coordinate matrix
 %
 %  PatchProperty - see documentation of patch objects for manipulating the
 %                 apperance, e.g. 'EdgeColor'
 % See also
 % grains/plot
+
+% create a new plot
+[mtexFig,isNew] = newMtexFigure(varargin{:});
+
+% get position of provided
+if isnumeric(cS)
+  xy = cS;
+  if isnumeric(varargin{1})
+    xy = [xy(:),varargin{1}(:)];
+    cS = xy + varargin{2};
+  else
+    cS = xy + varargin{1};
+  end        
+end
 
 % extract color
 fc = get_option(varargin,'FaceColor',cS.CS.color);
@@ -29,7 +47,7 @@ varargin = set_option(varargin,'FaceColor',fc);
 V = reshape(double(cS.V),[],3);
 h = optiondraw(patch('Faces',cS.F,'Vertices',V,'edgeColor','k'),varargin{:});
 
-if ~check_option(varargin,'parent')
+if isNew
   view(45,0)
   axis('equal','vis3d');
   fcw
