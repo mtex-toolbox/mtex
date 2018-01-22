a6087b0794d90291ff005f6b32eab7dbadb7e520
Felix Bartel <felixbartel@protonmail.com>
2018-01-22 14:21:41 +0100

    bug fix

--- S2Fun/@S2FunHarmonic/reshape.m
+++ S2Fun/@S2FunHarmonic/reshape.m
@@ -1,10 +1,6 @@
 function sF = reshape(sF, varargin)
 % overloads reshape
 
-if isempty(varargin{:})
-  sF.fhat = reshape(sF.fhat, (sF.bandwidth+1)^2, varargin{:});
-else
-  sF.fhat = reshape(sF.fhat, [(sF.bandwidth+1)^2 varargin{:}]);
-end
+sF.fhat = reshape(sF.fhat, (sF.bandwidth+1)^2, varargin{:});
 
 end
