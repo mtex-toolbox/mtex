bf65f760c2a1009f38e2f1628d5870f206897b33
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-02-05 17:56:09 +0100

    adapt all functions to the new fhat convention which is identical to the one used in the NFSOFT - difference to the old one: fhat = fhat.'

--- ODFAnalysis/@FourierComponent/calcMDF.m
+++ ODFAnalysis/@FourierComponent/calcMDF.m
@@ -8,11 +8,25 @@ L = min([odf1.bandwidth,odf2.bandwidth]);
 f_hat = [odf1.f_hat(1) * odf2.f_hat(1); zeros(deg2dim(L+1)-1,1)];
 for l = 1:L
   ind = deg2dim(l)+1:deg2dim(l+1);
-  f_hat(ind) = reshape(odf1.f_hat(ind),2*l+1,2*l+1)' * ...
-    reshape(odf2.f_hat(ind),2*l+1,2*l+1) ./ (2*l+1);
+  f_hat(ind) = reshape(odf2.f_hat(ind),2*l+1,2*l+1) * ...
+    reshape(odf1.f_hat(ind),2*l+1,2*l+1)' ./ (2*l+1);
 end
 
 % construct mdf
 mdf = FourierODF(f_hat,odf2.CS,odf1.CS);
+
+end
+
+function testing
+cs1 = crystalSymmetry('321');
+cs2 = crystalSymmetry('432');
+
+odf2 = unimodalODF(orientation.copper(cs1));
+odf1 = unimodalODF(orientation.copper2(cs2));
+mdf1 = calcMDF(odf2,odf1,'kernelMethod')
+
+mdf2 = calcMDF(odf2,odf1)
+figure(1);plotSection(mdf1,'sigma')
+figure(2);plotSection(mdf2,'sigma')
 
 end
--- ODFAnalysis/@FourierComponent/calcPDF.m
+++ ODFAnalysis/@FourierComponent/calcPDF.m
@@ -6,11 +6,9 @@ even = 1 + (check_option(varargin,'antipodal') ...
   || h.antipodal || component.CS.isLaue);
 if nargin > 2, even = even || r.antipodal; end
 
-
 % bandwidth
 L = get_option(varargin,'bandwidth',component.bandwidth);
 L = min(L,component.bandwidth);
-
 
 if length(h) == 1  % pole figures
   sym = component.SS;
@@ -24,12 +22,12 @@ ipdf_hat = cumsum([0,2*(0:L)+1]);
 for l = 0:even:L
   if length(h) == 1  % pole figures
     P_hat(1+ipdf_hat(l+1):ipdf_hat(l+2)) = reshape(...
-      component.f_hat(1+deg2dim(l):deg2dim(l+1)),2*l+1,2*l+1) ./(2*l+1) ...
+      component.f_hat(1+deg2dim(l):deg2dim(l+1)),2*l+1,2*l+1).' ./(2*l+1) ...
       * 4 * pi * sphericalY(l,h).';
     
   elseif  length(r) == 1 % inverse pole figures
     P_hat(1+ipdf_hat(l+1):ipdf_hat(l+2)) = reshape(...
-      component.f_hat(1+deg2dim(l):deg2dim(l+1)),2*l+1,2*l+1)' ./(2*l+1) ...
+      conj(component.f_hat(1+deg2dim(l):deg2dim(l+1))),2*l+1,2*l+1) ./(2*l+1) ...
       * 4 * pi * sphericalY(l,r).';
   else
     error('Either h or r should be a single direction!');
