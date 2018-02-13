40d2a4d124e3c0b2be3e8e030fd3a0b98c7ccf55
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-02-13 20:28:18 +0100

    make pdfOrientstionMapping/plot ware of the new plot system

--- tools/orientationMappings/ipdfOrientationMapping.m
+++ tools/orientationMappings/ipdfOrientationMapping.m
@@ -38,16 +38,16 @@ classdef ipdfOrientationMapping < orientationMapping
       else
         defaultPlotCMD = 'pcolor';
       end
-      plot(h,d,defaultPlotCMD,varargin{:});
       mtexTitle(mtexFig.gca,char(oM.inversePoleFigureDirection,'LaTeX'),varargin{:});
-      
+      [~,caxes] = plot(h,d,defaultPlotCMD,varargin{:});
+            
       name = oM.CS1.pointGroup;
       if ~isempty(oM.CS1.mineral), name = [oM.CS1.mineral ' (' name ')']; end
         
       set(mtexFig.parent,'name',['IPF key for ' name])      
-      set(mtexFig.gca,'tag','ipdf')
-      setappdata(mtexFig.gca,'CS',oM.CS1);
-      setappdata(mtexFig.gca,'inversePoleFigureDirection',oM.inversePoleFigureDirection);
+      set(caxes,'tag','ipdf')
+      setappdata(caxes,'CS',oM.CS1);
+      setappdata(caxes,'inversePoleFigureDirection',oM.inversePoleFigureDirection);
       
       % annotate crystal directions
       if check_option(varargin,'3d')
