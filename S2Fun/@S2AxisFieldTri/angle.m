3683a9d2c359d0d51764d74ed547129edff20265
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-02-22 17:21:16 +0100

    work on

---
+++ S2Fun/@S2AxisFieldTri/angle.m
@@ -0,0 +1,16 @@
+function sF = angle(varargin)
+%
+% Syntax
+%   sVF = angle(sVF,sVF2)
+%   sVF = angle(sVF,v)
+%
+% Input
+%  sVF  - @S2AxisFieldTri
+%  sVF2 - @S2AxisField
+%  v    - @vector3d
+%
+% Output
+%  sF - @S2FunTri
+%
+sF = dot(varargin{:});
+sF.values = acos(sF.values);
