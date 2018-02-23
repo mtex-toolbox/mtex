3bde243532cea2b66b4b2f462437f90a5adf8132
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-02-23 07:07:56 +0100

    minor

--- S2Fun/@S2VectorField/plotSection.m
+++ S2Fun/@S2VectorField/plotSection.m
@@ -24,7 +24,7 @@ S2 = axis2quat(sec,omega)*axis2quat(orth(sec),eta)*sec;
 d = reshape(sVF.eval(S2),length(S2), []);
 delta = getappdata(mtexFig.gca,'delta');
 if isempty(delta)
-  delta = max(norm(d)) / 200 ;
+  delta = 1 / 100 ;
   setappdata(mtexFig.gca,'delta',delta);
 end
 delta = delta * get_option(varargin,'linewidth',1);
@@ -33,9 +33,9 @@ if isa(d,'double') && ~isreal(d), d = real(d);end
 
 if strcmpi(get_option(varargin,'color'),'interp')
   varargin = delete_option(varargin,'color',1);
-  x = [d.x+delta,d.x-delta];
-  y = [d.y+delta,d.y-delta];
-  z = [d.z+delta,d.z-delta];
+  x = [d.x .* (1+delta),d.x .* (1-delta)];
+  y = [d.y .* (1+delta),d.y .* (1-delta)];
+  z = [d.z .* (1+delta),d.z .* (1-delta)];
   
   h = surface(x,y,z,[norm(d),norm(d)],'parent',mtexFig.gca,'edgecolor','none','facecolor','interp');
   
