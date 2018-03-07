d101bece5a008a004bf23ab7354c8943dbb06b1e
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-03-07 08:05:30 +0100

    required as there is no rank 0 tensor anymore

--- TensorAnalysis/@tensor/directionalMagnitude.m
+++ TensorAnalysis/@tensor/directionalMagnitude.m
@@ -1,4 +1,4 @@
-function q = directionalMagnitude(T,v)
+function T = directionalMagnitude(T,v)
 % magnitude of a tensor in direction v
 %
 % Formula
@@ -16,17 +16,16 @@ function q = directionalMagnitude(T,v)
 
 % return a function if required
 if nargin == 1 || isempty(v)
-  q = S2FunHarmonicSym.quadrature(@(x) directionalMagnitude(T,x),'bandwidth',4,T.CS);
+  T = S2FunHarmonicSym.quadrature(@(x) directionalMagnitude(T,x),'bandwidth',4,T.CS);
   
   return
 end
 
 % compute tensor products with directions v with respect to all dimensions
-while T.rank > 0
+while ~isnumeric(T)
   T = EinsteinSum(T,[-1 1:T.rank-1],v.normalize,-1);
 end
 
-q = T.M;
-if length(v)>1
-  q = reshape(q,size(v));
+if length(v)>1, T = reshape(T,size(v));
+  
 end
