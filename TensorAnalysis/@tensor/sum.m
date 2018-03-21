1fc97a7516d281b621309f5b1930c85630dd85ba
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-03-21 22:17:15 +0100

    take first non singleton dimension in tensor/mean and sum

--- TensorAnalysis/@tensor/sum.m
+++ TensorAnalysis/@tensor/sum.m
@@ -1,6 +1,6 @@
 function T = sum(T,dim)
 % sum over all tensors allong dimension 1
 
-if nargin == 1, dim = 1;end
+if nargin == 1, dim = 1 + (size(T,1) == 1); end
 dim = dim + T.rank;
 T.M = sum(T.M,dim);
