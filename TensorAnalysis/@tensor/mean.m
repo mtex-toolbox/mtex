32383d6dee050a8a84259678ce67b3cd57dae5b9
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-03-22 06:58:34 +0100

    bug fix

--- TensorAnalysis/@tensor/mean.m
+++ TensorAnalysis/@tensor/mean.m
@@ -1,5 +1,13 @@
 function T = mean(T,dim)
-% sum over all tensors allong dimension 1
+% mean of a list of tensors
+%
+% Syntax
+%  T = mean(T)     % mean along the first non singleton dimension
+%  T = mean(T,dim) % mean along dimension dim
+%
+% Input
+%  T - @tensor
+%  dim - dimension with respect to which the mean is taken 
 
 if nargin == 1, dim = 1 + (size(T,1) == 1);end
 dim = dim + T.rank;
--- TensorAnalysis/@tensor/sum.m
+++ TensorAnalysis/@tensor/sum.m
@@ -1,5 +1,13 @@
 function T = sum(T,dim)
-% sum over all tensors allong dimension 1
+% sum of a list of tensors
+%
+% Syntax
+%  T = sum(T)     % sum along the first non singleton dimension
+%  T = sum(T,dim) % sum along dimension dim
+%
+% Input
+%  T - @tensor
+%  dim - dimension with respect to which the sum is taken 
 
 if nargin == 1, dim = 1 + (size(T,1) == 1); end
 dim = dim + T.rank;
