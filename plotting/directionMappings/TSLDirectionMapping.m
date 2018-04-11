ccfb017a5b8e7b3000335a64c8296668cecdb0c1
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-04-11 21:13:19 +0200

    introduce directionMappings

---
+++ plotting/directionMappings/TSLDirectionMapping.m
@@ -0,0 +1,48 @@
+classdef TSLDirectionMapping < directionMapping
+  % converts directions to rgb values
+    
+  methods
+    
+    function dM = TSLDirectionMapping(varargin)
+      dM@directionMapping(varargin{:});
+      dM.sym = dM.sym.Laue;
+      dM.sR = dM.sym.fundamentalSector;
+    end
+  
+    function rgb = direction2color(dM,h,varargin)      
+      % in TSL all fundamental sectors are colorized with white in the center
+  
+      % project to fundamental region
+      h_sR = h.project2FundamentalRegion(dM.sym);
+      h = h_sR;
+
+      % this should become white if not stated differently
+      center = dM.sR.center;
+
+      if ismember(dM.sym.id,[2,18,27,35,42])
+        warning('Not a topological correct colormap! Green to blue colorjumps possible');
+      end
+
+      % compute angle of the points "sh" relative to the center point "center"
+      % this should be between 0 and 1
+      [radius,rho] = polarCoordinates(dM.sR,h,center,xvector);
+
+      % white center
+      radius = 0.5+radius./2;
+
+      % compute the color vector on the sphere
+      v = vector3d('rho',rho,'theta',radius.*pi);
+
+      % post processing of the color vector
+      % by default we have white at the z, black at the -z, red
+      % at x and green and blue at 120 and 240 degree accordingly
+      % post rotate the color
+      v = dM.colorPostRotation * v;
+
+      % compute rgb values
+      rgb = ar2rgb(mod(v.rho./ 2 ./ pi,1),v.theta./pi,get_option(varargin,'grayValue',1),'noHueCorrection');      
+      
+    end
+  end
+  
+end
---
+++ plotting/directionMappings/directionMapping.m
@@ -0,0 +1,71 @@
+classdef directionMapping < handle
+  % converts directions to rgb values
+    
+  properties
+    colorPostRotation = rotation.id
+    sR = sphericalRegion
+    sym % 
+  end
+
+  methods
+    
+    function dM = directionMapping(sym,varargin)
+      dM.sym = sym;
+      dM.sR = sym.fundamentalSector;
+    end
+    
+    function plot(dM,varargin)
+      
+      
+      [mtexFig,isNew] = newMtexFigure(varargin{:});
+
+      % init plotting grid
+      v = plotS2Grid(dM.sym.fundamentalSector,'resolution',1*degree,varargin{:});
+      
+      % make it Miller for crystal symmetry
+      if isa(dM.sym,'crystalSymmetry'), v = Miller(v,dM.sym); end
+            
+      % compute colors
+      d = dM.direction2color(v);
+
+      % plot the colored sector
+      if numel(d) == 3*length(v)
+        d = reshape(d,[size(v),3]);
+        defaultPlotCMD = 'surf';
+      else
+        defaultPlotCMD = 'pcolor';
+      end
+      
+      [~,caxes] = plot(v,d,defaultPlotCMD,varargin{:});
+            
+      setappdata(caxes,'CS',dM.sym);
+            
+      % annotate crystal directions
+      if check_option(varargin,'3d')
+        if ~check_option(varargin,'noLabel')
+          hold on
+          gray = [0.4 0.4 0.4];
+          arrow3d(dM.sym.axes(1),'facecolor',gray)
+          text3(Miller(1,0,0,'uvw',dM.sym),'a_1','horizontalAlignment','right')
+          
+          arrow3d(dM.sym.axes(2),'facecolor',gray)
+          text3(Miller(0,1,0,'uvw',dM.sym),'a_2','verticalAlignment','cap','horizontalAlignment','left')
+          
+          arrow3d(dM.sym.axes(3),'facecolor',gray)
+          text3(Miller(0,0,1,'uvw',dM.sym),'c','verticalAlignment','bottom')
+          hold off
+        end
+        if isNew, fcw; end                
+      end
+      
+      try
+        mtexFig.drawNow('figSize',getMTEXpref('figSize'),varargin{:});
+      end
+    end        
+  end
+  
+  methods (Abstract=true)
+    rgb = direction2color(oM,h,varargin)
+  end
+  
+end
