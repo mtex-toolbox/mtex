c52d51b58e33dca4a7b893ba197d9ff6fa1be4f8
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-05-11 12:10:04 +0200

    vectorize eig2 -> much faster

--- tools/math_tools/eig2.m
+++ tools/math_tools/eig2.m
@@ -7,9 +7,6 @@ function [lambda,v] = eig2(M,a12,a22)
 %
 %   lambda = eig2(a11,a12,a22)
 %
-%
-%   [v,lambda] = eig3(a11,a12,a22)
-%
 % Input
 %  M - array of symmetric 2x2 matrix
 %  a11, a12,a22 - vector of matrix elements
@@ -29,45 +26,65 @@ end
 % input should be column vectors
 a11 = a11(:).'; a12 = a12(:).'; a22 = a22(:).';
 
-% 0 = (a11 - l)(a22-l) - a12^2 = a11 a22 - a12^2 - (a11 + a22) l 
+% eigen value compution
+% 0 = (a11 - l)(a22-l) - a12^2 = l^2 - (a11 + a22) l + a11 a22 - a12^2 
 % lambda12 = (a11 + a22)./ 2 +- sqrt((a11 + a22)^2 - 4 a11 a22 + 4a12^2   ) / 2
 
-  for k = 1:length(a11)
-    
-    [v(:,:,k),lambda(:,k)] = eig([a11(k) a12(k); a12(k) a22(k)],'vector');
-    
-  end
+p = (a11 + a22)./2;
+D = real(sqrt(p.^2 - a11 .* a22 + a12.^2));
+lambda(1,:) = p - D;
+lambda(2,:) = p + D;
+
+% a first eigen vector
+v(1,1,:) =  -a12;
+v(2,1,:) = a11 - lambda(1,:);
+
+% its norm
+n = sqrt(v(1,1,:).^2 + v(2,1,:).^2);
+
+% maybe some of them are zero?
+v(1,1,n==0) = 1;
+v(2,1,n==0) = 0;
+n(n==0) = 1;
+
+% the second eigen vector is alway orthogonal
+v(1,2,:) = v(2,1,:);
+v(2,2,:) = -v(1,1,:);
+
+% normalize the eigenvectors
+v = v ./ repmat(reshape(n,1,1,[]),2,2);
+
+%for k = 1:length(a11)   
+%  [v(:,:,k),lambda(:,k)] = eig([a11(k) a12(k); a12(k) a22(k)],'vector');  
+%end
   
-  % for some reason Matlab eig function changes to order outputs if called
-  % with two arguments - so we should do the same
-  [lambda,v] = deal(v,lambda);
+% for some reason Matlab eig function changes to order outputs if called
+% with two arguments - so we should do the same
+[lambda,v] = deal(v,lambda);
   
 end
 
 function test
 
-% generate random symmetric 3x3 matrixes
-N = 10
-a = rand(6,N);
+% generate random symmetric 2x2 matrixes
+N = 1000000;
+a = rand(3,N);
 
 tic
-[V1,lambda1] = eig3(a(1,:),a(2,:),a(3,:),a(4,:),a(5,:),a(6,:));
+[V1,lambda1] = eig2(a(1,:),a(2,:),a(3,:));
 toc
 
 tic
-lambda2 = zeros(3,N);
-V2 = vector3d.zeros(3,N)
+lambda2 = zeros(2,N);
+V2 = zeros(2,2,N);
 for i = 1:N
-  A = [a(1,i),a(2,i),a(3,i);a(2,i),a(4,i),a(5,i);a(3,i),a(5,i),a(6,i)];
-  [V,lambda2(:,i)] = eig(A,'vector');
-  V2(1,i) = vector3d(V(:,1));
-  V2(2,i) = vector3d(V(:,2));
-  V2(3,i) = vector3d(V(:,3));
+  A = [a(1,i),a(2,i);a(2,i),a(3,i)];
+  [V2(:,:,i),lambda2(:,i)] = eig(A,'vector');
 end
 toc
 
-norm(lambda1 - flipud(lambda2)) ./ N
-max(angle(V1(:),V2(:)))
+norm(lambda1 - lambda2) ./ N
+squeeze(sum(sum((V1 - V2).^2,1),2))
 
 end
 
