fa20b966b35b05b4cc48942be0e1ba7747810a01
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-05-13 07:25:20 +0200

    merge circular polarization to spectralTransmissionOrientationMapping
    set polarizationDirection = [] to activate it

--- tools/orientationMappings/spectralTransmissionOrientationMapping.m
+++ tools/orientationMappings/spectralTransmissionOrientationMapping.m
@@ -3,7 +3,7 @@ classdef spectralTransmissionOrientationMapping < orientationMapping
   properties
     rI                      % refractiveIndexTensor
     thickness               % thickness of the sample
-    vprop = vector3d.Z;     % propagation direction in specimen coordinates
+    propagationDirection = vector3d.Z;     % propagation direction in specimen coordinates
     polarizer = vector3d.X; % direction of the polarizer in specimen coordinates
     phi = 90*degree;        % angle between polarizer and analyzer
   end
@@ -20,18 +20,22 @@ classdef spectralTransmissionOrientationMapping < orientationMapping
     
     function rgb = orientation2color(oM,ori,varargin)
       
-      % compute propagation sdirection in crystal coordinates
-      propCS = inv(ori) .* oM.vprop;
-      
-      % compute polarization direction in crystal coordinates
-      pCS = (inv(ori) .* oM.polarizer);
-      
+      % compute propagation direction in crystal coordinates
+      propCS = inv(ori) .* oM.propagationDirection;
+
       % compute spectral transmission
-            % this option should be for circular polarisiton
-          %rgb = spectralTransmission(oM.rI,propCS,oM.thickness,'phi',oM.phi);
+      if isempty(oM.polarizer) %  circular polarisation
+        
+        rgb = spectralTransmission(oM.rI,propCS,oM.thickness,'phi',oM.phi);
       
-          rgb = spectralTransmission(oM.rI,propCS,oM.thickness,'polarizationDirection',pCS,'phi',oM.phi);
+      else
+        
+        % compute polarization direction in crystal coordinates
+        pCS = (inv(ori) .* oM.polarizer);
+        
+        rgb = spectralTransmission(oM.rI,propCS,oM.thickness,'polarizationDirection',pCS,'phi',oM.phi);
       
+      end
     end
     
   end
