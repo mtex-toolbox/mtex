a2cfaf33f0b2a4a4c203a84b3bd53bbcd20165b3
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-08-07 13:23:34 +0200

    minor

---
+++ obsolete/BungeRGBOrientationMapping.m
@@ -0,0 +1,8 @@
+function oM  = BungeRGBOrientationMapping(varargin)
+
+warning('Starting with MTEX 5.1 "BungeRGBOrientationMapping" has been replaced by "BungeColorKey".')
+
+oM = BungeColorKey(varargin{:});
+
+end
+
