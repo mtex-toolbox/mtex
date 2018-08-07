ecb72e2aace58f0a26b0d02d6e3df599a5109d79
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-08-07 07:12:55 +0200

    fix #337 for withInclusion we now use boundary.segLength which is actually faster then the old code

--- EBSDAnalysis/@grain2d/perimeter.m
+++ EBSDAnalysis/@grain2d/perimeter.m
@@ -16,15 +16,32 @@ function  peri = perimeter(grains,varargin)
 % See also
 % grain2d/equivalentPerimeter
 
-poly = grains.poly;
 
-% remove inclusions
-if ~check_option(varargin,'withInclusion')
+if check_option(varargin,'withInclusion')
+  
+  bnd = grains.boundary;
+  grainId = bnd.grainId;
+  segLength = bnd.segLength;
+
+  isGrain1 = grainId(:,1)>0;
+  isGrain2 = grainId(:,2)>0;
+  peri = accumarray(grainId(isGrain1,1),segLength(isGrain1),[max(grainId(:)),1]) ...
+    + accumarray(grainId(isGrain2,2),segLength(isGrain2),[max(grainId(:)),1]);
+
+  peri = peri(grains.id);
+
+else
+  
+  poly = grains.poly;
+
+  % remove inclusions
   incl = grains.inclusionId;
   for i = find(incl>0).'
     poly{i} = poly{i}(1:end-incl(i));
   end
-end
 
-V = grains.V;
-peri =  cellfun(@(ind) sum(sqrt(sum(diff(V(ind,:)).^2,2))),poly);
+  V = grains.V;
+
+  peri =  cellfun(@(ind) sum(sqrt(sum(diff(V(ind,:)).^2,2))),poly);
+  
+end
