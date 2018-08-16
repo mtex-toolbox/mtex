7e19fda19f76f441b1d1a836a793fbb4679de3b5
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-08-16 09:58:51 +0200

    docu

--- plotting/orientationColorKeys/axisAngleColorKey.m
+++ plotting/orientationColorKeys/axisAngleColorKey.m
@@ -1,11 +1,28 @@
 classdef axisAngleColorKey < orientationColorKey
   % 
-  %   Detailed explanation goes here
+  %
+  % Colorizes orientations according to their misorientation axis and
+  % misorientation angle in specimen coordinates with respect to some
+  % refererence orientation. The angle determines the saturation of the
+  % color, i.e., a misorientation angle of 0 corresponds to gray and the
+  % misorientation ange at maximum corresponds to fully saturated colors.
+  % The saturated color is determined by the misorientation axis. In the
+  % X-Y plane the colors circle through the rainbow colors and goes to
+  % white and black for z to +/-1.
+  % 
+  % The reference orientation may be single fixed orientation or a list of
+  % orientations. A typical example is to set the reference orientation as
+  % the mean orientation of the grain the pixel orientation belongs to.
+  %
+  % Reference: Thomsen et al.: Quaternion-based disorientation coloring of
+  % orientation maps, 2017
+  %
+  
   
   properties
     dirMapping % direction mapping
-    oriRef 
-    maxAngle = inf
+    oriRef     % the reference orientation 
+    maxAngle = inf % misorientation angle at which the colors are maximum saturated
   end
   
   methods
