f5e10f511ea069054afe369df0e0c3ca7f0f59c8
kilir <kilir@users.noreply.github.com>
2018-10-09 15:53:42 +0200

    allow @symmetry/calcAxisDistribution to return S2FunHarmonic in case no direction is specified (see https://github.com/mtex-toolbox/mtex/issues/373)

--- geometry/@symmetry/calcAxisDistribution.m
+++ geometry/@symmetry/calcAxisDistribution.m
@@ -1,21 +1,25 @@
-function x = calcAxisDistribution(cs,h,varargin)
+function x = calcAxisDistribution(cs,varargin)
 % compute the axis distribution of an uniform ODF or MDF
 %
 % Input
-%  oR - @orientationRegion
-%  h  - @vector3d
-%
+%  cs - @crystalSymmetry
+%  h  - @vector3d (optional)
+%  
 % Output
 %  x   - values of the axis distribution
-%
+%  or
+%  x   - spherical function
 % See also
 
 varargin = delete_option(varargin,'complete');
 [oR,dcs,nSym] = fundamentalRegion(cs,varargin{:});
 
-
-h = project2FundamentalRegion(h,dcs);
-omega = oR.maxAngle(h);
-
-
-x = nSym * (omega - sin(omega)) ./ pi;
+if nargin > 1 && isa(varargin{1},'vector3d')
+    h = varargin{1}; 
+    h = project2FundamentalRegion(h,dcs);
+    omega = oR.maxAngle(h);
+    x = nSym * (omega - sin(omega)) ./ pi;
+else
+    f = @(h) (oR.maxAngle(h) - sin(oR.maxAngle(h))) ./ pi * nSym ;
+    x = S2FunHarmonicSym.quadrature(f,cs);
+end
