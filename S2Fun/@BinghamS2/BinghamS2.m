56f7c5551bfd1b08dad3cd1cbf6c4a47404b477f
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-10-13 08:46:09 +0200

    make normalization right

--- S2Fun/@BinghamS2/BinghamS2.m
+++ S2Fun/@BinghamS2/BinghamS2.m
@@ -35,7 +35,7 @@ classdef BinghamS2 < S2Fun
             end
             
             % compute normalization constant
-            BS2.N = BS2.normalizationConst;         
+            BS2.N = 4*pi./BS2.normalizationConst;
         end
         
 
@@ -47,7 +47,7 @@ classdef BinghamS2 < S2Fun
         
         function N = normalizationConst(BS2)   % needs esternal mex
           %   calc normalization parameter
-          N = numericalSaddlepointWithDerivatives(sort(-BS2.Z')+1)*exp(1);
+          N = numericalSaddlepointWithDerivatives(sort(-BS2.Z(:))+1)*exp(1);
           N = N(3);
         end
         
@@ -55,63 +55,61 @@ classdef BinghamS2 < S2Fun
     
     methods (Static = true)
         
-        function BS2 = fit(v)
-            % function to fit Bingham parameters
-            %
-            % Syntax
-            %   %   BS2 = BinghamS2.fit(v)
-            %
-            % Input
-            %  v - vector3d
-            %
-            % Output
-            %  BS2 - @BinghamS2
-            %
+      function BS2 = fit(v)
+        % function to fit Bingham parameters
+        %
+        % Syntax
+        %   %   BS2 = BinghamS2.fit(v)
+        %
+        % Input
+        %  v - vector3d
+        %
+        % Output
+        %  BS2 - @BinghamS2
+        %
+        
+        [a,kappa] = eig3(v*v);
+        kappa = kappa./sum(kappa);
+        Z =estimateZ(kappa);
+        BS2 = BinghamS2(Z, a);
+        BS2.N = BS2.normalizationConst;
+        
+        function Z = estimateZ(kappa)
+          % adapted from https://github.com/libDirectional/libDirectional
+          % Igor Gilitschenski, Gerhard Kurz, Simon J. Julier, Uwe D. Hanebeck,
+          % Efficient Bingham Filtering based on Saddlepoint Approximations, 2014
+          % Proceedings of the 2014 IEEE International Conference on Multisensor Fusion and Information Integration (MFI 2014), Beijing, China, September 2014.
+          
+          f = @(z) findZ(z, kappa);
+          Z = fsolve(f, -ones(2,1), optimset('display', 'off', 'algorithm', 'levenberg-marquardt'));
+          Z = [Z; 0];
+          [Z, order] = sort(Z,'ascend');
+          
+          function R = findZ(Z, rhs)  % needs esternal mex
+            Z=[Z;0];
+            d = size(Z,1);
             
-            [a,kappa] = eig3(v*v);
-            kappa = kappa./sum(kappa);
-            Z =estimateZ(kappa);
-            BS2 = BinghamS2(Z, a);
-            BS2.N = BS2.normalizationConst;
+            % normalization constant
+            A = numericalSaddlepointWithDerivatives(sort(-Z)+1)*exp(1);
+            A = A(3);
             
-            function Z = estimateZ(kappa)
-                % adapted from https://github.com/libDirectional/libDirectional
-                % Igor Gilitschenski, Gerhard Kurz, Simon J. Julier, Uwe D. Hanebeck,
-                % Efficient Bingham Filtering based on Saddlepoint Approximations, 2014
-                % Proceedings of the 2014 IEEE International Conference on Multisensor Fusion and Information Integration (MFI 2014), Beijing, China, September 2014.
-
-                f = @(z) findZ(z, kappa);
-                Z = fsolve(f, -ones(2,1), optimset('display', 'off', 'algorithm', 'levenberg-marquardt'));
-                Z = [Z; 0];
-                [Z, order] = sort(Z,'ascend');
-                               
-                function R = findZ(Z, rhs)  % needs esternal mex
-                    Z=[Z;0];
-                    d = size(Z,1);
-                    
-                    % normalization constant
-                    A = numericalSaddlepointWithDerivatives(sort(-Z)+1)*exp(1);
-                    A = A(3);
-                    
-                    % derivative of normalization constant
-                    B = zeros(1,3);
-                    dim = size(Z,1);
-                    for i=1:dim
-                        mZ = Z([1:i i i:dim]);
-                        T = numericalSaddlepointWithDerivatives(sort(-mZ)+1)*exp(1)/(2*pi);
-                        B(i) = T(3);
-                    end
-                    
-                    R = zeros(d-1,1);
-                    for i=1:(d-1)
-                        R(i) = B(i)/A - rhs(i);
-                    end
-                end
+            % derivative of normalization constant
+            B = zeros(1,3);
+            dim = size(Z,1);
+            for i=1:dim
+              mZ = Z([1:i i i:dim]);
+              T = numericalSaddlepointWithDerivatives(sort(-mZ)+1)*exp(1)/(2*pi);
+              B(i) = T(3);
             end
-  
+            
+            R = zeros(d-1,1);
+            for i=1:(d-1)
+              R(i) = B(i)/A - rhs(i);
+            end
+          end
         end
         
+      end
+        
     end
-    
-
 end
