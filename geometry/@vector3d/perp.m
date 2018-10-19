7e139274978d68b7f61113451069626f203757e9
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-10-19 16:21:08 +0200

    more direct perp implementation

--- geometry/@vector3d/perp.m
+++ geometry/@vector3d/perp.m
@@ -1,7 +1,5 @@
 function N = perp(v)
 % conmpute an vector best orthogonal to a list of directions
 
-M = reshape(double(v),[],3);
-[u,~,~] = svd(M'*M);
-
-N = vector3d(u(:,3));
+[N,~] = eig3(v*v);
+N = N.subSet(1);
