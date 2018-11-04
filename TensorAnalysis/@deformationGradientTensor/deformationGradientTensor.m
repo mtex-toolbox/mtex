87284ee0680d3ca26e6491ca05bf93c5a16a76dd
kilir <kilir@users.noreply.github.com>
2018-11-04 19:10:30 +0100

    Add deformationGradientTensor

---
+++ TensorAnalysis/@deformationGradientTensor/deformationGradientTensor.m
@@ -0,0 +1,11 @@
+classdef deformationGradientTensor < tensor
+  
+  methods
+    function F = deformationGradientTensor(varargin)
+      F = F@tensor(varargin{:},'rank',2);
+    end       
+  end
+  
+
+end
+
