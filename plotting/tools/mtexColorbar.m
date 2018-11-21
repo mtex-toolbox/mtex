4660d5f898e54299896d6400627c41eeb1709e12
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-11-21 14:06:02 +0100

    docu

--- plotting/tools/mtexColorbar.m
+++ plotting/tools/mtexColorbar.m
@@ -1,5 +1,20 @@
 function varargout = mtexColorbar(varargin)
 % overide buildin Matlab colorbar function
+%
+% Syntax
+%   % add a colorbar vertical colorbar
+%   mtexColorbar('location','eastoutside')
+%
+%   % add a horizontal colorbar
+%   mtexColorbar('location','southoutside')
+%
+%   % add one colorbar for all sub figures
+%   setColorRange('equal') % set equal color range for all subplots
+%   mtexColorbar % add the color bar
+%  
+%   % add individual colorbars for all sub figures
+%   mtexColorbar('multiple')
+%
 
 if isappdata(gcf,'mtexFig')
   [varargout{1:nargout}] = colorbar(gcm,varargin{:});
