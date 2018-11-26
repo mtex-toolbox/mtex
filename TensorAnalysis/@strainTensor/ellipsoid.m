28dea12c179345e0b52c9b9819e3f8858b841acb
kilir <kilir@users.noreply.github.com>
2018-11-26 23:45:09 +0100

    Minor: Some strain tensor cleanup, work in progress

--- TensorAnalysis/@strainTensor/ellipsoid.m
+++ TensorAnalysis/@strainTensor/ellipsoid.m
@@ -1,13 +1,23 @@
 function [fe,qe] = ellipsoid(E)
-%
+% Length (1+e) and direction of principal axes of strain ellipsoid from
+% finite, lagrangian/eulerian strain tensor E with eigenvalues lambda
+% with
+% (1+e) = sqrt(1+2*lambda) for a Lagranage strain tensor (default)
+% (1+e) = sqrt(1-2*lambda) for a Euler strain tensor
 % Syntax
 %   [fe,qe] = ellipsoid(E)
-% 
+%
 % Input
-%  E - @strainTensor
+%  E -  @strainTensor
 %
 % Output
-%
+%  fe - directions of prinicpal axes of strain ellipsoid @vector3d
+%  qe - length of ellipse axes (1+e)
 
 [fe,qe] = eig(E);
-qe = sqrt(1+2*qe);
+ if strcmp(E.type,'Euler')
+     qe = sqrt(1-2*qe);
+ else
+    qe = sqrt(1+2*qe);
+ end
+end
--- TensorAnalysis/@strainTensor/strainTensor.m
+++ TensorAnalysis/@strainTensor/strainTensor.m
@@ -1,30 +1,23 @@
 classdef strainTensor < tensor
-  
+
+  properties (SetAccess=protected)
+        type = 'Lagrange'   % 'Euler'
+  end  
+    
   methods
     function sT = strainTensor(varargin)
-
       sT = sT@tensor(varargin{:});
-      
     end
   end
-  
-   
+
+
   methods (Static = true)
-    
+
     function eps = load(varargin)
       T = load@tensor(varargin{:});
       eps = strainTensor(T);
     end
-    
-    function eps = simpleShear()
-    end
-    
-    function eps = pureShear()
-    end
-        
-    function eps = planeShear(v1,v2,gamma)
-      
-      eps = strainTensor();
-    end
+
   end
 end
+
