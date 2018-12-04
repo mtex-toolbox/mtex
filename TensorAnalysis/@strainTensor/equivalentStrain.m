6b7889d65be50e47391baa8f5b6ea00c3cad1a76
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2018-12-04 07:31:43 +0100

    updated equivalent stress and strain

--- TensorAnalysis/@strainTensor/equivalentStrain.m
+++ TensorAnalysis/@strainTensor/equivalentStrain.m
@@ -1,6 +1,9 @@
-function r0 = equivalentStrain(epsilon)
+function epsilonEq = equivalentStrain(epsilon)
 % von Mises equivalent strain
 
-[r12,r23] = axisRatios(epsilon);
+e = epsilon.deviatoricStrain;
+epsilonEq = sqrt(2/3 * e:e);
 
-r0 = 2/3 * sqrt(r12.^2 + r12.*r23 + r23.^2);
+% this does work only in specific cases
+%[r12,r23] = axisRatios(epsilon);
+%epsilonEq = 2/3 * sqrt(r12.^2 + r12.*r23 + r23.^2)
--- TensorAnalysis/@stressTensor/equivalentStress.m
+++ TensorAnalysis/@stressTensor/equivalentStress.m
@@ -1,4 +1,4 @@
-function s = equivalentStress(sigma,varargin)
+function sigmaEq = equivalentStress(sigma,varargin)
 % von Mises equivalent stress
 %
 % Input
@@ -8,12 +8,13 @@ function s = equivalentStress(sigma,varargin)
 %  s - double
 %
 
-M = sigma.M;
+s = sigma.deviatoricStress;
+sigmaEq = sqrt(3/2 * s:s);
 
-s = sqrt(M(1,1,:).^2 + M(2,2,:).^2 + M(3,3,:).^2 ...
-  - M(1,1,:).*M(2,2,:) - M(2,2,:).*M(3,3,:)- M(1,1,:).*M(3,3,:) ...
-  + 3*(M(1,2).^2 + M(1,3).^2 + M(2,3).^2 ));
+% the following code gives the same result
+% is more efficient but less readable
 
-% this is acutally the same as
-% s = sigma.deviatoricStress
-% s = sqrt(3/2)*norm(s)
+% M = sigma.M;
+% sigmaEq = sqrt(M(1,1,:).^2 + M(2,2,:).^2 + M(3,3,:).^2 ...
+%   - M(1,1,:).*M(2,2,:) - M(2,2,:).*M(3,3,:)- M(1,1,:).*M(3,3,:) ...
+%   + 3*(M(1,2).^2 + M(1,3).^2 + M(2,3).^2 ));
