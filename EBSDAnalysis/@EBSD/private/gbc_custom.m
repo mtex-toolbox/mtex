815628a577be6fbf8a088464f684ea7bcbe90b67
kilir <ruediger.kilian@unibas.ch>
2019-02-09 19:03:31 +0100

    Allow custom segmentation to work properly for vector3d

--- EBSDAnalysis/@EBSD/private/gbc_custom.m
+++ EBSDAnalysis/@EBSD/private/gbc_custom.m
@@ -4,4 +4,9 @@ function criterion = gbc_custom(ori,CS,Dl,Dr,varargin)
 % check whether the two ebsd measurements are seperated by a grain boundary
 custom = get_option(varargin,'custom');
 delta = get_option(varargin,'delta');
-criterion = abs(custom(Dl)-custom(Dr)) < delta;
+if isa(custom,'vector3d')
+   criterion = angle(custom(Dl),custom(Dr)) < delta;
+else
+   criterion = abs(custom(Dl)-custom(Dr)) < delta;
+end
+
