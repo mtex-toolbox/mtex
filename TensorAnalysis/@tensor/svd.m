fe93fc543aac256de1d6aa9a128e52f152991b6d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-02-24 06:41:12 +0100

    svd for tensors :)

--- TensorAnalysis/@tensor/svd.m
+++ TensorAnalysis/@tensor/svd.m
@@ -1,20 +1,27 @@
-function [V,E] = svd(T)
+function [U,S,V] = svd(T)
 % singular value decomposition of a rank two tensor
 %
 % Syntax
-%   E = svd(T)
-%   [V,E] = svd(T)
+%   S = svd(T)
+%   [U,S,V] = svd(T)
 %
 % Input
 %  T - list of M rank 2 @tensor
 %
 % Output
-%  E - 3xM list of singular values
-%  V - 3xM list left singular vectors @vector3d
+%  S - 3xM sorted singular values, starting with the largest
+%  U - 3xM left singular @vector3d
+%  V - 3xM right singular @vector3d
 %
 
 
-[V,E] = eig(T' * T);
-E = sqrt(E);
+A = matrix(T' * T);
+[V,S] = eig3(A(1,1,:),A(1,2,:),A(1,3,:),A(2,2,:),A(2,3,:),A(3,3,:));
+%[V,S] = eig(T' * T);
+V = flipud(V);
+S = flipud(sqrt(S));
 
-if nargout <= 1, V = E; end
+if nargout <= 1, U = S; end
+if nargout == 3
+  U = normalize(vector3d(EinsteinSum(T,[1 -1],V,-1)));
+end
