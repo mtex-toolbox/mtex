704b75bce4f20096da302ed26538c41cf9445272
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-03-01 08:03:56 +0100

    bug fix

--- TensorAnalysis/@strainRateTensor/strainRateTensor.m
+++ TensorAnalysis/@strainRateTensor/strainRateTensor.m
@@ -13,7 +13,7 @@ classdef strainRateTensor < velocityGradientTensor
       E = E@velocityGradientTensor(varargin{:},'rank',2);
       
       % ensure it is antisymmetric
-      E.M = 0.5*(E.M + E.M');
+      E.M = 0.5*(E.M + permute(E.M,[2 1 3:ndims(E.M)]));
       
     end
     
