231f1faec5dff3aed60a34f9cf15c956a284c10b
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-03-06 20:52:37 +0100

    bug fix

--- TensorAnalysis/@refractiveIndexTensor/opticalAxis.m
+++ TensorAnalysis/@refractiveIndexTensor/opticalAxis.m
@@ -16,9 +16,9 @@ function oA = opticalAxis(rI)
 if n(1) == n(3)
   disp('  No optical axes. Refractive index tensor is isotropic');
 elseif n(1) == n(2)
-  oA = reshape(v(1,:),size(rI));
-elseif n(2) == n(3)
   oA = reshape(v(3,:),size(rI));
+elseif n(2) == n(3)
+  oA = reshape(v(1,:),size(rI));
 else
   oA = reshape(sqrt((n(3)-n(2))./(n(3)-n(1))) .* v(3,:),[],1) * [-1 1] ...
     + repmat(reshape(sqrt((n(2)-n(1))./(n(3)-n(1))) .* v(1,:),[],1),1,2);
