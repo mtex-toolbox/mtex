7441099c7148437e0d9f5eab7ac7affe86773900
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-03-21 09:02:53 +0100

    for antipodal functions we need to start our search only at one hemisphere

--- S2Fun/@S2FunHarmonicSym/steepestDescent.m
+++ S2Fun/@S2FunHarmonicSym/steepestDescent.m
@@ -19,7 +19,9 @@ function [f,v] = steepestDescent(sF, varargin)
 % parameters
 if ~check_option(varargin, 'startingnodes')
   
-  v = equispacedS2Grid(sF.s.fundamentalSector,'points',min(1000000,2*sF.bandwidth^2));
+  antipodalFlag = {'','antipodal'};
+  sR = sF.s.fundamentalSector(antipodalFlag{sF.antipodal+1});
+  v = equispacedS2Grid(sR,'points',min(1000000,2*sF.bandwidth^2));
     
   if isa(sF.s,'crystalSymmetry')
     v = Miller(v, sF.s);
