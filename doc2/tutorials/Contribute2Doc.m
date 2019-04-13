ce80ce042f466061ef3287055c269706d6237acf
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-04-13 16:17:09 +0200

    work on

--- doc2/tutorials/Contribute2Doc.m
+++ doc2/tutorials/Contribute2Doc.m
@@ -2,27 +2,35 @@
 %
 %% Documentation Location
 %
-% The documentation files are stored in the mtex/doc folder which is not added to your path by default.
-% To add this path simply change into the mtex directory and type
+% The documentation files are stored in the mtex/doc folder which is added
+% to your MTEX search path by default. Hence you can open the script file
+% corresponding to any help file by typing into the command window
 
-%  addpath doc
+edit Contribute2Doc
 
-%% Edit some file
+%%
 %
-% To edit a page you find in the documentation you can simply click on "Open in Editor" for the offline Matlab documentation.
-% If you are on the online documentation you will find the filename on the bottom of each page and you can simply type 'edit filename' into the Matlab commandline where you replace filename with the actual filename.
+% The correct file name of the help script file you find by clicking on "Open in
+% Editor" in the offline Matlab documentation. If you are on the online
+% documentation you will find the filename on the bottom of each page.
 %
 %% Publish command
 %
-% The documentation files are processed via the publish command which takes the m-file and creates an html-file.
-% The basic syntax can be found here: <https://de.mathworks.com/help/matlab/matlab_prog/publishing-matlab-code.html>.
+% The documentation files are processed via the publish command which takes
+% the m-file and creates an html-file. The basic syntax can be found here:
+% <https://de.mathworks.com/help/matlab/matlab_prog/publishing-matlab-code.html>.
 % To verify that your modified documentation page works you can type
 
-%  publish filename
+publish filename
 
 %%
-% which creates an folder, named 'hmtl', in your current directory where you can find your modified documentation page.
+% which creates an folder, named 'hmtl', in your current directory where
+% you can find your modified documentation page.
 
-%% Further processing
+%% Contributing to MTEX
 %
-% If you made some contributions to the documentation which you think should be included into the official documentation you can make a pull request to the mtex-GitHub repository or simply send the documentation file and we will make sure you can admire you changes in the official documentation.
+% If you made some contributions to the documentation which you think
+% should be included into the official documentation you can open a pull
+% request to the mtex-GitHub repository or simply send the documentation
+% file and we will make sure you can admire you changes in the official
+% documentation.
--- doc2/tutorials/GrainTutorial.m
+++ doc2/tutorials/GrainTutorial.m
@@ -1,3 +1,117 @@
 %% Grain Tutorial
 %
+%% Data import
+% We start by importing some EBSD data 
+
+% import a demo data set
+mtexdata small
+plotx2east
+
+% visualize the orientations according by the default ipf color key
+plot(ebsd('Fo'),ebsd('Fo').orientations)
+hold on
+plot(ebsd('En'),ebsd('En').orientations)
+plot(ebsd('Di'),ebsd('Di').orientations)
+hold off
+
+%% Grain reconstruction
+% 
+% Grains are reconstructed by the command <EBSD.calcGrains.html calcGrains>
+% which uses as main parameter a threshold angle to identify grain
+% boundaries.
+
+% perform grain segmentation
+[grains, ebsd.grainId] = calcGrains(ebsd('indexed'),'threshold',10*degree);
+grains
+
+%% Visualize the grain boundary
+% We may plot the grain boundaries on top of our orientation map
+
+hold on
+plot(grains.boundary,'linewidth',2)
+hold off
+
+
+%% The grain boudary
+%
+% The grain boundary segments are stored within the grain variable an can
+% be extracted by the command
+
+gB = grains.boundary
+
 %%
+% How to analyse grain boundaries is described in the
+% <GrainBoundaryTutorial.html grain boundary tutorial>.
+
+%% Visualize the grain orientations
+%
+% We may visualize the grain mean orientations by plotting an accordingly
+% rotated <CrystalShapeTutorial.html crystal shape>.
+
+% define the unrotated crystal shape for the Forsterite phase
+cS = crystalShape.olivine(ebsd('Fo').CS)
+
+% plot the crystal shapes on top of the previous plot
+hold on
+plot(grains('Fo'),0.8*cS,'FaceColor',[1 1 0],'FaceAlpha',0.5)
+hold off
+
+%% Grain properties and shape parameters
+% 
+% Beside the boundary grains have many other properties. A complete list of
+% grain properties and shape parameters can be found in the section
+% <ShapeParameters.html shape parameters>. Lets consider as an example the
+% area of the grains
+
+% extract the grain area
+area = grains.area;
+
+% and colorize the grains according to their area
+plot(grains,area)
+
+%% Extracting specific grains
+%
+% Grain properties can be used to extract specific grains. As an example we
+% ask for the largest grain
+
+% detect the largest grain
+[~,id] = max(grains.area);
+
+grains(id)
+
+%%
+% Lets highlight this grain.
+
+hold on
+plot(grains(id).boundary,'LineWidth',2,'lineColor','red')
+hold off
+
+%% Accessing the orientation within a grain
+%
+% During grain segmentation a grainId has been stored for each pixel in our
+% EBSD map. This grainId may used to access the ebsd data within a specific
+% grain
+
+% select EBSD data inside the maximum grain
+ebsd_maxGrain = ebsd(ebsd.grainId == id)
+
+% the previous command is equivalent to the more simple
+ebsd_maxGrain = ebsd(grains(id));
+
+%%
+% Lets visualize the misorientation angle between the orientations within
+% the grain and the grain meanorientation
+
+% compute the misorientation angle
+moriAngle = angle(grains(id).meanOrientation, ebsd_maxGrain.orientations);
+
+% plot the misorientation angle in degree
+plot(ebsd_maxGrain,moriAngle./degree)
+
+% plot the surrounding grain boundary
+hold on
+plot(grains(id).boundary,'lineWidth',2)
+hold off
+
+% add a colorbar
+mtexColorbar('title','misorientation angle')
