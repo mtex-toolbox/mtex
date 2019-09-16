e26af6110650ec6e3263077339ba2b8a443dc059
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-09-16 13:48:18 +0200

    bugfix

--- TensorAnalysis/@complianceTensor/VRHBulkModulus.m
+++ TensorAnalysis/@complianceTensor/VRHBulkModulus.m
@@ -18,10 +18,10 @@ C = matrix(inv(S),'voigt');
 S = matrix(S,'voigt');
 
 % KVoigt = ((C(1,1)+C(2,2)+C(3,3))+(2*(C(1,2)+C(2,3)+C(3,1))))/9
-KV = mean(C,[1,2]);
+KV = mean(C(1:3,1:3,:),[1,2]);
 
 % KReuss = 1/((S(1,1)+S(2,2)+S(3,3))+2*(S(1,2)+S(2,3)+S(3,1))) 
-KR = 1./sum(S,[1,2]);
+KR = 1./sum(S(1:3,1:3,:),[1,2]);
 
 % Voigt Reuss Hill average
 KVRH = 0.5.*(KV + KR);
