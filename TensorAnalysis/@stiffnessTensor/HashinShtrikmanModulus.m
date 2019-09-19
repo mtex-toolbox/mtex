71d0307363b04b957eb21f5f8dcbfceeef7b0f38
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-09-19 10:49:53 +0200

    rewrite HSModulus entirely in tensor notation

--- TensorAnalysis/@stiffnessTensor/HashinShtrikmanModulus.m
+++ TensorAnalysis/@stiffnessTensor/HashinShtrikmanModulus.m
@@ -1,4 +1,4 @@
-function [khs, ghs, minmax] = HashinShtrikmanModulus(C,K0,G0)
+function [khs, ghs, def] = HashinShtrikmanModulus(C,Ko,Go)
 % Hashin Shtrikman moduli 
 %
 % The equations are from Peselnick and Meister 1965 through Watt and
@@ -6,68 +6,57 @@ function [khs, ghs, minmax] = HashinShtrikmanModulus(C,K0,G0)
 %
 % Syntax:
 %
-%   [khs, ghs, minmax] = HashinShtrikmanModulus(C,K0,G0)
+%   [khs, ghs, def] = HashinShtrikmanModulus(C,Ko,Go)
 %
 % Input
 %  C  - @stiffnessTensor
-%  K0 - bulk modulus of the reference isotropic material
-%  G0 - shear modulus of the reference isotropic material
+%  Ko - bulk modulus of the reference isotropic material
+%  Go - shear modulus of the reference isotropic material
 %
 % Output
 %  khs - Hashin Shtrikman bulk modulus
 %  ghs - Hashin Shtrikman shear modulus
-%  minmax - +1 positive definite, -1 for negative definite R
+%  def - +1 positive definite, -1 for negative definite R
 %
 
-warning('off','MATLAB:singularMatrix');
-warning('off','MATLAB:illConditionedMatrix');
-
 % ensure K0 and G0 have same size
-if numel(G0) == 1, G0 = repmat(G0,size(K0)); end
+if numel(Go) == 1, Go = repmat(Go,size(Ko)); end
 
-% the isotropic compliances elements  (eq. 10-11 Watt Peselnick 1980)
-alpha = -3 ./ (3*K0 + 4*G0);
-beta = -3 * (K0 + 2*G0) ./ ( 5*G0 .* (3*K0 + 4*G0) );
+% the isotropic compliances elements (eq. 10-11 Watt Peselnick 1980)
+alpha = -3 ./ (3*Ko + 4*Go);
+beta = -3 * (Ko + 2*Go) ./ ( 5*Go .* (3*Ko + 4*Go) );
 gamma = (alpha - 3 * beta) ./ 9;
 
-% set up isotropic elastic matrix 
-C0 = matrix(2 * G0 .* stiffnessTensor.eye,'Voigt');
-
-% C0 = C0 + K0 - 2/3 * G0
-C0(1:3,1:3,:,:) = C0(1:3,1:3,:,:) + shiftdim(K0 - 2/3 * G0,-2);
+% define the isotropic tensor corresponding to Ko and Go
+Co = 2 * Go .* stiffnessTensor.eye + (Ko - 2/3 * Go) .* dyad(tensor.eye,tensor.eye);
 
 % take difference between anisotropic and isotropic matrices and take
 % inverse to get the residual compliance matrix (eq 4 & 8 Watt Peselnick 1980)
-R = C - C0;
+R = C - Co;
 H = inv(R);
-
 
 % Subtract the isotropic compliances from the residual compliance matrix
 % and invert back to moduli space.  The matrix B is the moduli matrix that
 % when multiplied by the strain difference between the reference isotropic
 % response and the isotropic response of the actual material gives the
-% average stres <pij> (eq 15 - 16 Watt Peselnick 1980) note identity matrix
-% is Iinv
-A = matrix(H - beta .* complianceTensor.eye,'Voigt');
-A(1:3,1:3,:,:) = A(1:3,1:3,:,:) - shiftdim(gamma,-2);
-B = A;
-for k = 1:numel(A)/36
-  B(:,:,k) = inv(A(:,:,k));
-end
+% average stres <pij> (eq 15 - 16 Watt Peselnick 1980).
+A = H - beta .* complianceTensor.eye - gamma .* dyad(tensor.eye,tensor.eye);
+B = inv(A);
+
 
 % now perform the averaging of the B matrix to get two numbers (B1 and B2)
 % that are related to the two isotropic moduli - perturbations of the
 % reference values (eq 21 and 22 Watt Peselnick 1980)
-sB1 = sum(B(1:3,1:3,:,:),[1,2]);
+sB1 = EinsteinSum(B,[-1 -1 -2 -2]);
+sB2 = EinsteinSum(B,[-1 -2 -1 -2]);
 
-sB2 = B(1,1,:,:) + B(2,2,:,:) + B(3,3,:,:) + 2 * (B(4,4,:,:) + B(5,5,:,:) + B(6,6,:,:));
-B1 = reshape(2*sB1 - sB2,size(alpha)) / 15;
-B2 = reshape(3*sB2 - sB1,size(alpha)) / 30;
+B1 = (2*sB1 - sB2) / 15;
+B2 = (3*sB2 - sB1) / 30;
 
 % The Hashin-Shtrikman moduli are then calculated as changes from the
 % reference isotropic body. (eq 25 & 27 Watt and Peselnick 1980)
-khs = K0 + ( 3 * B1 + 2 * B2 ) ./ ( 3 + alpha .* (3 * B1 + 2 * B2) );
-ghs = G0 + B2 ./ ( 1 + 2 * beta .* B2 );
+khs = Ko + ( 3 * B1 + 2 * B2 ) ./ ( 3 + alpha .* (3 * B1 + 2 * B2) );
+ghs = Go + B2 ./ ( 1 + 2 * beta .* B2 );
 
 % The moduli are valid in two limits - minimizing or maximizing the
 % anisotropic difference elastic energy. Think of it as the maximum
@@ -77,10 +66,11 @@ ghs = G0 + B2 ./ ( 1 + 2 * beta .* B2 );
 % the properties of R.  A +1 is returned if positive definite, a -1 is
 % retunred if negative definite.  0 returned otherwise.
 
-warning('on','MATLAB:singularMatrix');
-warning('on','MATLAB:illConditionedMatrix');
-
-minmax = zeros(size(R));
+def = zeros(size(R));
 D = eig(R);
-minmax(all(D>0,1)) = 1;
-minmax(all(D<0,1)) = -1;
+def(all(D>0,1)) = 1;
+def(all(D<0,1)) = -1;
+
+% isf minmax == 0 this means ...
+khs(def==0) = NaN;
+ghs(def==0) = NaN;
