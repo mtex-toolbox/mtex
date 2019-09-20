072f622fe04a1bbde12e801eeb6d9762b3ae49b8
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-09-20 10:32:51 +0200

    do not store the color name in CS directly but only the RGB values

--- plotting/tools/str2rgb.m
+++ plotting/tools/str2rgb.m
@@ -1,8 +1,16 @@
 function rgb = str2rgb(str)
 % convert str to rgb values
 
+oldNames = {'light blue','light green','light red',...
+  'dark blue','dark green','dark red','Amethyst'};
+
+oldColors = {[0.5 0.5 1],[0.5 1 0.5],[1 0.5 0.5],...
+  [0 0 0.3],[0 0.3 0],[0.3 0 0],[0.2344 0.6992 0.4414]};
+
 if isnumeric(str)
+  
   rgb = str;
+  
 elseif any(strcmpi(str,{'blue','red','black','white','green','yellow',...
     'cyan','magenta','b','r','k','w','g','y','c','m','none','flat'}))
 
@@ -10,6 +18,10 @@ elseif any(strcmpi(str,{'blue','red','black','white','green','yellow',...
 
 else
   
-  [~,rgb] = colornames(getMTEXpref('colorPalette','CSS'),str);
+  try % for compatibility
+    rgb = oldColors{strcmpi(str,oldNames)};  
+  catch  
+    [~,rgb] = colornames(getMTEXpref('colorPalette','CSS'),str);
+  end
 
 end
