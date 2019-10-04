f9aad8ce89b7bf597474004912364351dffb4967
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-10-04 22:22:35 +0200

    bug fix

--- TensorAnalysis/@tensor/logm.m
+++ TensorAnalysis/@tensor/logm.m
@@ -21,22 +21,18 @@ switch T.rank
   case 3
 
   case 4
-
-    % check for symmetry
-
+    
     % convert to a matrix
-    M = tensor42(T.M,T.doubleConvention);
+    M = tensor42(T.M,2);
 
     % log the matrix
     for l = 1:length(T)
       M(:,:,l) = logm(M(:,:,l));
-    end
-
-    %
-    T.doubleConvention = ~T.doubleConvention;
-    
+    end    
+        
     % convert to back a 4 rank tensor
-    T.M = tensor24(M,T.doubleConvention);
+    T.M = tensor24(M,2);
+  
 end
 
 % change the name
