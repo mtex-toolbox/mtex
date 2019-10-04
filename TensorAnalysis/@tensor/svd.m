4979471c123b1a7b9d86396d1afa9ccbd9d857db
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-10-04 22:23:55 +0200

    svd for rank 4 tensors

--- TensorAnalysis/@tensor/svd.m
+++ TensorAnalysis/@tensor/svd.m
@@ -14,14 +14,31 @@ function [U,S,V] = svd(T)
 %  V - 3xM right singular @vector3d
 %
 
+switch T.rank
+  
+  case 2
+    A = matrix(T' * T);
+    [V,S] = eig3(A(1,1,:),A(1,2,:),A(1,3,:),A(2,2,:),A(2,3,:),A(3,3,:));
+    %[V,S] = eig(T' * T);
+    V = flipud(V);
+    S = flipud(sqrt(S));
 
-A = matrix(T' * T);
-[V,S] = eig3(A(1,1,:),A(1,2,:),A(1,3,:),A(2,2,:),A(2,3,:),A(3,3,:));
-%[V,S] = eig(T' * T);
-V = flipud(V);
-S = flipud(sqrt(S));
-
-if nargout <= 1, U = S; end
-if nargout == 3
-  U = normalize(vector3d(EinsteinSum(T,[1 -1],V,-1)));
+    if nargout <= 1, U = S; end
+    if nargout == 3
+      U = normalize(vector3d(EinsteinSum(T,[1 -1],V,-1)));
+    end
+    
+  case 4
+    
+    S = T;
+    
+    for i=1:length(T)
+      [u,s] = mlsvd(T.M(:,:,:,:,i));
+      S.M(:,:,:,:,i) = s;
+      U(i) = rotation.byMatrix(u{1});
+    end
+    
+  otherwise 
+    
+    error('SVD is only supported for rank 2 and rank 4')
 end
