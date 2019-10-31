0714d52d81e0b4e2c57a509f7422690f07ced1b0
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-10-31 13:26:42 +0100

    put the overview page to matlab as the markdown version made troubles

--- doc/Documentation.m
+++ doc/Documentation.m
@@ -1,2 +1,48 @@
-%% MTEX Toolbox
+%% Documentation
+%
+% Use the left sidebar to navigate through the different topics. Note that all
+% documentation pages are essentially MTEX scripts. This means you can open them
+% in Matlab by typing
+%
+%   edit fileName
+%
+% This allows you to execute the sections by step by step and investiage the
+% effect of each single MTEX command. It also should motivate you to play around
+% with different parameters, different input files or further analysis
+% steps. Eventually, these documentation scripts may serve as a good starting
+% point for your own analysis scripts.
+%
+%% Contributing to the Documentation
+%
+% Documenting a project like MTEX is a challenging and ongoing task. Therefore
+% we are extremely happy about any spelling fixes, examples, theoretical
+% explainations, special use cases, etc. As a bonus everybody who contributed to
+% MTEX will automatically appear at the <https://github.com/mtex-toolbox/mtex/graphs/contributors contributors page>.
+%
+% The easiest way to suggest changes to the MTEX documentation is online via
+% Github. This is done as follows:
+%
+% # sign into <https://github.com GitHub>
+% # open the help page you want to change, e.g., this one
+% # click
+% <https://github.com/mtex-toolbox/mtex-toolbox.github.io/blob/master/pages/documentation/documentation.md
+% edit page> which leads you to the file on <https://github.com GitHub>
+% # click the little pencil in the top right corner which allows you to
+% edit the script online
+% # enter of copy and paste your changes into the site
+% # click at the bottom *suggest changes*
+% # click create *pull request*
+%
+% Alternatively, you can send us you changes simply by <mailto:ralf.hielscher@mathematik.tu-chemnitz.de email>.
+%
+%% The Publish Command
+%
+% You may get a visual preview of your altered documentation page by using the
+% matlab function
+% <https://de.mathworks.com/help/matlab/matlab_prog/publishing-matlab-code.html publish>
+%
+%   publish filename
+%
+% which creates an folder, named |html|, in your current directory where you can
+% find the generated html page.
 %
