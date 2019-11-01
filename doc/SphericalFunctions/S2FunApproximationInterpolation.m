4fe1d8ff7a33e9bff59a276b675b44f214a6eb01
felixbartel <felixbartel@protonmail.com>
2019-11-01 16:01:56 +0100

    work on S2Fun docu

--- doc/SphericalFunctions/S2FunApproximationInterpolation.m
+++ doc/SphericalFunctions/S2FunApproximationInterpolation.m
@@ -1,4 +1,39 @@
 %% Spherical Approximation and Interpolation
 %
+
 %%
+% Given data nodes
+
+load(fullfile(mtexDataPath, 'vector3d', 'smiley.mat'));
+
+scatter(nodes, f, 'upper');
+
+%% Interpolation!
 %
+
+sFTri = interp(nodes, f, 'linear');
+
+%%
+% Very wow!
+
+norm(eval(sFTri, nodes)-f)
+
+%%
+% Plotting it is meh
+
+contourf(sFTri, 'upper');
+
+%% Approximation?
+
+sF = interp(nodes, f, 'harmonicApproximation');
+
+%%
+% Error meh
+
+norm(eval(sF, nodes)-f)
+
+%%
+% Plot wow!
+
+contourf(sF, 'upper');
+
