09da254b568d15770937f3a2ab0d4bc4e5b46c5b
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-11-22 15:34:09 +0100

    make use of S2VectorField in pfSections

--- plotting/ODFSections/pfSections.m
+++ plotting/ODFSections/pfSections.m
@@ -29,7 +29,7 @@ classdef pfSections < ODFSections
       oS.omega(end) = [];
       oS.omega = get_option(varargin,'omega',oS.omega,'double');
 
-      oS.referenceField = @(r) pfSections.oneSingularityField(r);
+      oS.referenceField = @(r) S2VectorField.oneSingularity;
       %oS.referenceField = @(r) pfSections.polarField(r);
 
     end
@@ -64,7 +64,7 @@ classdef pfSections < ODFSections
       % determine omega angle
       rF = ori * oS.h2;
       vF = vectorField(oS,r);
-      omega = angle(rF,vF,r);
+      omega = angle(vF,rF,r);
 
       % this builds a list
       bounds = sort(unique([oS.omega - oS.tol,oS.omega + oS.tol]));
@@ -87,18 +87,27 @@ classdef pfSections < ODFSections
       h = plot(v,data{:},oS.sR,'TR',[int2str(oS.omega(sec)./degree),'^\circ'],...
         'parent',ax,varargin{:},'doNotDraw');
 
-      hold on
+      wasHold = ishold(ax);
+      hold(ax,'on');
       r = equispacedS2Grid(oS.sR,'resolution',15*degree);
       vF = oS.vectorField(r,oS.omega(sec));
       quiver(r,vF,'parent',ax,'doNotDraw','arrowSize',0.1,'color',0.7*[1 1 1],'HitTest','off');
-      hold off
+      if ~wasHold, hold(ax,'off'); end
+
+    end
+    
+    function h = quiverSection(oS,ax,sec,v,data,varargin)
+
+      % plot data
+      h = quiver(v,data{:},oS.sR,'TR',[int2str(oS.omega(sec)./degree),'^\circ'],...
+        'parent',ax,varargin{:},'doNotDraw');
 
     end
 
     function vF = vectorField(oS,r,omega)
 
 
-      vF = oS.referenceField(r);
+      vF = oS.referenceField.eval(r);
 
       if nargin == 3
         vF = rotation.byAxisAngle(r,omega) .* vF;
@@ -107,47 +116,7 @@ classdef pfSections < ODFSections
     end
 
 
-  end
-
-  methods (Static = true)
-    function vF = polarField(r,rRef)
-      % compute and reference vector field on the pole figure
-
-      if nargin == 1, rRef = xvector; end
-
-      vF = normalize(rRef - r);
-      vF = normalize(vF - dot(vF,r) .* r);
-
-    end
-
-    function vF = sigmaField(r)
-
-      [theta,rho] = polar(r);
-      rot = rotation.byEuler(rho,theta,-rho,'ZYZ');
-      vF = rot * xvector;
-
-    end
-
-    function vF = oneSingularityField(r1)
-
-      r2 = r1; r2.x = -r2.x;
-
-      N = normalize(cross(r1+zvector,r2+zvector));
-
-      ind = r1.x == 0;
-      N(ind) = normalize(r1(ind) - zvector);
-
-      ind = r1 == zvector;
-      N(ind) = yvector;
-
-      vF = normalize(r1 - dot(r1,N) .* N);
-      vF = rotation.byAxisAngle(N,sign(N.y)*90*degree) .* vF;
-
-      vF = normalize(vF - dot(vF,r1) .* r1);
-
-    end
-
-  end
+  end     
 
 end
 
--- plotting/ODFSections/sigmaSections.m
+++ plotting/ODFSections/sigmaSections.m
@@ -1,90 +1,21 @@
-classdef sigmaSections < ODFSections
-
-  properties
-    sigma
-    sR
-    grid
-  end
-
-  properties (Hidden=true)
-    maxSigma
-  end
-
+classdef sigmaSections < pfSections
+     
   methods
-
-    function oS = sigmaSections(varargin)
-
-      oS = oS@ODFSections(varargin{:});
-
-      % get fundamental plotting region
-      [~,~,phi2] = fundamentalRegionEuler(oS.CS1,oS.CS2,varargin{:});
-      oS.maxSigma = phi2;
-      oS.sR = oS.CS2.fundamentalSector(varargin{:},'upper');
-
+    
+    function oS = sigmaSections(CS1,CS2,varargin)
+            
+      oS = oS@pfSections(CS1,CS2);
+                
+      oS.maxOmega = 2*pi / CS1.nfold(oS.h1);
+      
       % get sections
-      oS.sigma = linspace(0,phi2,1+get_option(varargin,'sections',6));
-      oS.sigma(end) = [];
-      oS.sigma = get_option(varargin,'sigma',oS.sigma,'double');
-
-    end
-
-    function ori = makeGrid(oS,varargin)
-
-      oS.plotGrid = plotS2Grid(oS.sR,varargin{:});
-      oS.gridSize = (0:numel(oS.sigma)) * length(oS.plotGrid);
-      phi1 = repmat(oS.plotGrid.rho,[1,1,numel(oS.sigma)]);
-      Phi = repmat(oS.plotGrid.theta,[1,1,numel(oS.sigma)]);
-      sigmaLarge = repmat(reshape(oS.sigma,1,1,[]),[size(oS.plotGrid) 1]);
-
-      ori = orientation.byEuler(phi1,Phi,sigmaLarge - phi1,'ZYZ',oS.CS,oS.SS);
-
-    end
-
-    function n = numSections(oS)
-      n = numel(oS.sigma);
-    end
-
-    function [S2Pos,secPos] = project(oS,ori,varargin)
-
-      % maybe this can be done more efficiently
-      ori = ori.symmetrise('proper').';
-
-      [e1,e2,e3] = Euler(ori,'ZYZ');
-
-      sigma = mod(e1 + e3,oS.maxSigma); %#ok<*PROPLC,*PROP>
-      secPos = oS.secList(sigma,oS.sigma);
-
-      S2Pos = vector3d.byPolar(e2,e1);
-
-    end
-
-    function ori = iproject(oS,phi1,Phi,isigma)
-      ori = orientation.byEuler(phi1,Phi,oS.sigma(isigma)-phi1,'ZYZ',oS.CS,oS.SS);
-    end
-
-    function h = plotSection(oS,ax,sec,v,data,varargin)
-
-      % plot data
-      if isa(oS.SS,'crystalSymmetry')
-        varargin = [oS.SS.plotOptions,varargin];
-      end
-      h = plot(v,data{:},oS.sR,'TR',[int2str(oS.sigma(sec)./degree),'^\circ'],...
-        'parent',ax,varargin{:},'doNotDraw');
-
-      if (isempty(oS.grid) || length(oS.grid) < sec) && ~check_option(varargin,'noGrid')
-
-        fak = sqrt(length(oS.SS));
-        r = equispacedS2Grid(oS.sR,'resolution',15*degree / fak);
-
-        [theta,rho] = polar(r);
-        rot = rotation.byEuler(rho,theta,-rho,'ZYZ');
-
-        vF = rotation.byAxisAngle(r,oS.sigma(sec)) .* rot * xvector;
-
-        hold on
-        oS.grid(sec) = quiver(r,vF,'parent',ax,'doNotDraw','arrowSize',0.1/fak,'color',0.7*[1 1 1],'HitTest','off');
-        hold off
-      end
-    end
-  end
+      oS.omega = linspace(0,oS.maxOmega,1+get_option(varargin,'sections',6));
+      oS.omega(end) = [];
+      oS.omega = get_option(varargin,'sigma',oS.omega,'double');
+      
+      
+      oS.referenceField = S2VectorField.sigma;
+            
+    end            
+  end  
 end
