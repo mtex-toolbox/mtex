51b278fb13db4f1e40dda56e62a68ef770fe0e9d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-12-11 05:58:07 +0100

    angle computation with respect to a normal direction should be symmetry free

--- geometry/@vector3d/angle.m
+++ geometry/@vector3d/angle.m
@@ -19,12 +19,12 @@ function a = angle(v1,v2,varargin)
 if nargin == 3 && isa(varargin{1},'vector3d')
 
   N = normalize(varargin{1});
-  v1 = normalize(v1 - dot(v1,N) .* N);
-  v2 = normalize(v2 - dot(v2,N) .* N);
+  v1 = normalize(v1 - dot(v1,N,'noSymmetry') .* N);
+  v2 = normalize(v2 - dot(v2,N,'noSymmetry') .* N);
 
   a  = angle(v1,v2);
 
-  ind = dot(N,cross(v1,v2))<0;
+  ind = dot(N,cross(v1,v2),'noSymmetry')<0;
   a(ind) = 2*pi - a(ind);
 
 else
