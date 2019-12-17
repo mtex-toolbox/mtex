713068e6f2d1228413c134ca5dce5aad6c0a93b7
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2019-12-17 20:52:57 +0100

    pass threshold angle to custom grain reconstruction

--- EBSDAnalysis/@EBSD/private/gbc_custom.m
+++ EBSDAnalysis/@EBSD/private/gbc_custom.m
@@ -1,9 +1,9 @@
-function criterion = gbc_custom(ori,CS,Dl,Dr,varargin)
+function criterion = gbc_custom(ori,CS,Dl,Dr,delta,varargin)
 % blue print for a self defined grain boundary criterion
 
 % check whether the two ebsd measurements are seperated by a grain boundary
 custom = get_option(varargin,'custom');
-delta = get_option(varargin,'delta');
+delta = get_option(varargin,'delta',delta);
 if isa(custom,'vector3d')
    criterion = angle(custom(Dl),custom(Dr)) < delta;
 else
