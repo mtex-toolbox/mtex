a8ba0fc7c504dccb587aa7bf4fe353efe71f4cbc
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-06 09:00:58 +0100

    another minor speed improvement in vector3d/plus

--- geometry/@vector3d/plus.m
+++ geometry/@vector3d/plus.m
@@ -1,15 +1,20 @@
-function v = plus(v1,v2)
+function v1 = plus(v1,v2)
 % poitwise addition
 
 try %#ok<TRYNC>
-  v = vector3d(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z);
+  v1.x = v1.x+v2.x;
+  v1.y = v1.y+v2.y;
+  v1.z = v1.z+v2.z;
+  
+  v1.isNormalized = false;
+  v1.opt = struct;
   return
 end
 
 if isnumeric(v1)
-  v = vector3d(v1 + v2.x,v1 + v2.y,v1 + v2.z);
+  v1 = vector3d(v1 + v2.x,v1 + v2.y,v1 + v2.z);
 elseif isnumeric(v2)
-  v = vector3d(v2 + v1.x,v2 + v1.y,v2 + v1.z);
+  v1 = vector3d(v2 + v1.x,v2 + v1.y,v2 + v1.z);
 else
-  v = plus(v2,v1);
+  v1 = plus(v2,v1);
 end
