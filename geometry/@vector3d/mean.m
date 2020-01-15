5821f21a996cadaea31ef5a72b6eee0c2c8dbb21
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-15 10:20:19 +0100

    * add option robust to vector3d/mean * do not normalize vector3d before taking the mean !!!

--- geometry/@vector3d/mean.m
+++ geometry/@vector3d/mean.m
@@ -19,46 +19,46 @@ function m = mean(v,varargin)
 %
 % Options
 %  antipodal - include <VectorsAxes.html antipodal symmetry>
+%  robust    - robust mean (with respect to outliers)
 %
 
-v = v.normalize;
+%v = v.normalize;
 if check_option(varargin,'antipodal') || v.antipodal
 
   varargin = delete_option(varargin,'antipodal');
 
   if check_option(varargin,'weights')
-    
-    w = get_option(varargin,'weights');
-    
-    xx = mean(w .* v.x.^2,  varargin{:});
-    xy = mean(w .* v.x.*v.y,varargin{:});
-    xz = mean(w .* v.x.*v.z,varargin{:});
-    yy = mean(w .* v.y.^2,  varargin{:});
-    yz = mean(w .* v.y.*v.z,varargin{:});
-    zz = mean(w .* v.z.^2,  varargin{:});
-    
-  else
-    
-    xx = mean(v.x.^2,  varargin{:});
-    xy = mean(v.x.*v.y,varargin{:});
-    xz = mean(v.x.*v.z,varargin{:});
-    yy = mean(v.y.^2,  varargin{:});
-    yz = mean(v.y.*v.z,varargin{:});
-    zz = mean(v.z.^2,  varargin{:});
-    
+    v = v .* sqrt(get_option(varargin,'weights'));
   end
-
+    
+  xx = mean(v.x.^2,  varargin{:});
+  xy = mean(v.x.*v.y,varargin{:});
+  xz = mean(v.x.*v.z,varargin{:});
+  yy = mean(v.y.^2,  varargin{:});
+  yz = mean(v.y.*v.z,varargin{:});
+  zz = mean(v.z.^2,  varargin{:});
+  
   [m,~] = eig3(xx,xy,xz,yy,yz,zz,'largest');
-
 else
   
   if check_option(varargin,'weights')
     v = v .* get_option(varargin,'weights');
   end
     
-  m = sum(v,varargin{:});
+  m = sum(v);
 end
 
 m = m .normalize;
+
+if check_option(varargin,'robust') && length(v)>4
+  omega = angle(m,v);
+  id = omega < quantile(omega,0.8)*(1+1e-5);
+  if ~any(id), return; end
+  varargin = delete_option(varargin,'robust');
+  varargin = delete_option(varargin,'weights',1);
+  
+  m = mean(v.subSet(id),varargin{:});
+  
+end
 
 end
