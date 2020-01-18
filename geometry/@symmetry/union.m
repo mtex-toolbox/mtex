a7a86b77dea7bf002847b10f595e7036484000ef
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-18 22:43:06 +0100

    not sure this makes sense

--- geometry/@symmetry/union.m
+++ geometry/@symmetry/union.m
@@ -1,4 +1,4 @@
-function u = union(s1,s2)
+function s = union(s1,s2)
 % returns the union of two symmetry groups
 %
 % 
@@ -7,14 +7,7 @@ if ~isa(s2,'symmetry') && isa(s2,'quaternion')
   s2 = [s2,rotation.id];
 end
 
-u  = s1 * s2;
-
-u = unique(u(:));
-
-% take the equal ones
-s1 = quaternion(u(~u.i));
-s2 = quaternion(u(u.i));
-s = [rotation(unique(s1,'antipodal'));-rotation(unique(s2,'antipodal'))];
+s = unique(s1 * s2);
 
 % find a symmetry that exactly contains s
 for i=1:45 % check all Laue groups
@@ -22,7 +15,7 @@ for i=1:45 % check all Laue groups
   ss = crystalSymmetry('pointId',i);
   
   if length(ss) == length(s) && all(any(isappr(abs(dot_outer(s,ss)),1)))
-    u = ss;
+    s = ss;
     return
   end
   
