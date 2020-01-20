eb4e049de08d7207924ec85a9e1cca2fa301884c
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-20 09:03:01 +0100

    bug fixes

--- geometry/geometry_tools/localOrientationGrid.m
+++ geometry/geometry_tools/localOrientationGrid.m
@@ -65,7 +65,7 @@ function ind = fundamental_region2(q,center,cs,ss)
 %  ind    -
 
 % symmetrise
-c_sym = ss *  center * cs;
+c_sym = symmetrise(center,ss.properGroup,cs.properGroup);
 omega = angle(c_sym * inv(center)); %#ok<MINV>
 %[~,i] = min(omega,[],2);
 %c_sym = c_sym(sub2ind(sie(c_sym,i));
