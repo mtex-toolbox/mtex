583b8d9103c36bd8581a644d962cd6778ecebd3d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-20 11:30:57 +0100

    bug fix

--- geometry/@symmetry/union.m
+++ geometry/@symmetry/union.m
@@ -3,7 +3,15 @@ function s = union(s1,s2)
 %
 % 
 
-rot = unique(s1.rot * s2.rot);
+if isa(s1,'symmetry')
+  rot = s1.rot;
+else
+  rot = s1;
+end
+
+if isa(s2,'symmetry'), s2 = s2.rot; end
+
+rot = unique(rot * s2);
 
 % find a symmetry that exactly contains s
 for i=1:45 % check all Laue groups
@@ -11,9 +19,21 @@ for i=1:45 % check all Laue groups
   s = crystalSymmetry('pointId',i);
   
   if numSym(s) == length(rot) && all(any(isappr(abs(dot_outer(rot,s.rot)),1)))   
+    
+    try %#ok<TRYNC>
+      s.axes = s1.axes;       % coordinate system
+      s.mineral = s1.mineral; % mineral name
+      s.color  = s1.color;    % color used for EBSD / grain plotting
+    end
     return
   end
   
 end
 
 s = crystalSymmetry(rot);
+
+try %#ok<TRYNC>
+  s.axes = s1.axes;       % coordinate system
+  s.mineral = s1.mineral; % mineral name
+  s.color  = s1.color;    % color used for EBSD / grain plotting
+end
