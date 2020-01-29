3fa4a4d328676f2aa3d2a8591c58a08853db2004
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-01-29 10:06:22 +0100

    fix #448

--- plotting/orientationPlot/orientationPlot.m
+++ plotting/orientationPlot/orientationPlot.m
@@ -73,11 +73,11 @@ classdef orientationPlot < handle
       % plot orientations into 3d space
 
       % ensure correct symmetry
-      if isa(ori,'orientation') && ~check_option(varargin,'noSymmetryCheck')
-        ori = oP.CS1.ensureCS(ori);
-      else
-        ori = orientation(ori,oP.CS1,oP.CS2);
-      end
+      %if isa(ori,'orientation') && ~check_option(varargin,'noSymmetryCheck')
+      %  ori = oP.CS1.ensureCS(ori);
+      %else
+      ori = orientation(ori,oP.CS1,oP.CS2);
+      %end
       ori.antipodal = oP.antipodal;
       
       % extract data
