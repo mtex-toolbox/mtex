fad617c289f413fc5f3030682656268a5fff4ba9
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-02-07 20:10:04 +0100

    use fast eig3 for symmetric tensors

--- TensorAnalysis/@tensor/eig.m
+++ TensorAnalysis/@tensor/eig.m
@@ -1,4 +1,4 @@
-function [V,E] = eig(T)
+function [V,E] = eig(T,varargin)
 % compute the eigenvalues and eigenvectors of a tensor
 %
 % Syntax
@@ -18,15 +18,19 @@ switch T.rank
 
   case 1
   case 2
-    E = zeros(3,length(T));
-    V = zeros(3,3,length(T));
-    for i = 1:length(T)
-      [V(:,:,i),E(:,i)] = eig(T.M(:,:,i),'vector');
+    if all(T.isSymmetric(:))
+      [V,E] = eig3(T.M,varargin{:});
+    else
+      E = zeros(3,length(T));
+      V = zeros(3,3,length(T));
+      for i = 1:length(T)
+        [V(:,:,i),E(:,i)] = eig(T.M(:,:,i),'vector');
+      end
     end
   case 3
   case 4
     E = zeros(6,length(T));
-V = zeros(6,6,length(T));
+    V = zeros(6,6,length(T));
     M = tensor42(T.M,T.doubleConvention);
     for i = 1:length(T)
       [V(:,:,i),E(:,i)] = eig(M(:,:,i),'vector');
@@ -35,6 +39,6 @@ end
 
 if nargout <= 1
   V = E;
-else
+elseif isa(V,'double')
   V = reshape(vector3d(V(1,:),V(2,:),V(3,:),'antipodal'),3,[]);
 end
