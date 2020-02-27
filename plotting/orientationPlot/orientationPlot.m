d06700d1d338aa172a6f26c196f8954f9d3d57cd
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-02-27 06:57:16 +0100

    try to ensure right symmetry in orientation section plot

--- plotting/orientationPlot/orientationPlot.m
+++ plotting/orientationPlot/orientationPlot.m
@@ -73,11 +73,13 @@ classdef orientationPlot < handle
       % plot orientations into 3d space
 
       % ensure correct symmetry
-      %if isa(ori,'orientation') && ~check_option(varargin,'noSymmetryCheck')
-      %  ori = oP.CS1.ensureCS(ori);
+      if isa(ori,'orientation') && ~check_option(varargin,'noSymmetryCheck')
+        try
+          ori = oP.CS1.ensureCS(ori);
+        end
       %else
-      ori = orientation(ori,oP.CS1,oP.CS2);
-      %end
+      %  ori = orientation(ori,oP.CS1,oP.CS2);
+      end
       ori.antipodal = oP.antipodal;
       
       % extract data
