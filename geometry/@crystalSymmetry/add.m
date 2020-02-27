c8c0a8995cc537755d39fb8bcba0204cb8f842a2
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-02-27 06:59:53 +0100

    adapt symmetry.add to handle class

--- geometry/@crystalSymmetry/add.m
+++ geometry/@crystalSymmetry/add.m
@@ -1,10 +1,6 @@
-function csNew = add(cs,mori,varargin)
+function cs = add(cs,mori,varargin)
 % add additional symmetry operations to a crystal symmetry
 
-
-sym = cs*mori;
-
-csNew = crystalSymmetry(sym,'mineral',cs.mineral);
-csNew.axes = cs.axes;
+cs = crystalSymmetry.byElements([mori;cs.rot(:)],'mineral',cs.mineral);
 
 end
