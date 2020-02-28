42d0970903057872d73f97ed5ffd47b39ee7362b
kilir <kiliru02@gmail.com>
2020-02-28 21:38:19 +0100

    Simplify grain2d/paris

--- EBSDAnalysis/@grain2d/paris.m
+++ EBSDAnalysis/@grain2d/paris.m
@@ -13,46 +13,15 @@ function p = paris(grains)
 % Output
 %  p - double
 %
+% 
 
-p = zeros(size(grains));
+hgrains = hull(grains);
+hullp = hgrains.perimeter;
+p = 200 * (grains.perimeter - hullp)./hullp;
 
-% store this in local variables for speed reasons
-X = grains.V(:,1);
-Y = grains.V(:,2);
+% sanity check - maybe some grains are too small,
+% not sufficiently smoothed etc...
+p(p==inf)=NaN; p(p<0)=NaN;
 
-poly = grains.poly;
-% remove inclusions
-incl = grains.inclusionId;
-for i = find(incl>0).'
-  poly{i} = poly{i}(1:end-incl(i));
-end
-
-% compute convex hull perimeters
-for id = 1:length(grains)
-  
-  % for small grains there is no difference
-  if length(poly{id}) <= 7, continue; end
-  
-  % extract coordinates
-  xGrain = X(poly{id});
-  yGrain = Y(poly{id});
-  
-  % compute perimeter
-  perimeterGrain = sum(sqrt(...
-    (xGrain(1:end-1) - xGrain(2:end)).^2 + ...
-    (yGrain(1:end-1) - yGrain(2:end)).^2));
-   
-  % compute convex hull
-  ixy = convhull(xGrain,yGrain);
-  
-  % compute perimenter
-  perimeterHull = sum(sqrt(...
-    (xGrain(ixy(1:end-1)) - xGrain(ixy(2:end))).^2 + ...
-    (yGrain(ixy(1:end-1)) - yGrain(ixy(2:end))).^2));
-  
-  % paris is the relative difference between convex hull perimenter and true
-  % perimeter
-  p(id) = 200*(perimeterGrain - perimeterHull)./perimeterHull;
-  
 end
 
