710a4532267a834aa015363b3f1eeda4bbdc388b
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-04-03 11:40:15 +0200

    default fontSize will also effect innerPlotSpacing

--- tools/option_tools/setMTEXpref.m
+++ tools/option_tools/setMTEXpref.m
@@ -5,3 +5,12 @@ group = getappdata(0,'mtex');
 group.(pref) = value;
 
 setappdata(0,'mtex',group);
+
+switch lower(pref)
+  case 'fontsize'
+    set(0,'DefaultAxesFontSize',value);
+    set(0,'DefaultLegendFontSize',value);
+    group.innerPlotSpacing = 1.5*value;
+    setappdata(0,'mtex',group);
+end
+  
