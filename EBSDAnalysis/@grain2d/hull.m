fdc29d135f85fba0a080a7474e325c2a7fb3cb59
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-05-29 22:12:36 +0200

    convex hull of grains should have proper boundary

--- EBSDAnalysis/@grain2d/hull.m
+++ EBSDAnalysis/@grain2d/hull.m
@@ -2,13 +2,25 @@ function grains = hull(grains)
 % replace grains by its convex hull
 
 V = grains.V;
+F = [];
 
 for i = 1:length(grains)
   
   ind = convhulln(V(grains.poly{i},:));
     
-  grains.poly{i} = grains.poly{i}([ind(:,1);ind(1,1)].');
+  p = grains.poly{i}([ind(:,1);ind(1,1)].');
+  grains.poly{i} = p;
+   
+  F = [F;[p(1:end-1).',p(2:end).']];
   
 end
+
+% update boundary
+grains.boundary.F = F;
+grains.boundary.ebsdId = NaN(size(F));
+bs = cellfun(@length,grains.poly);
+grains.boundary.grainId = repelem(grains.id,bs-1) * [1,0];
+grains.boundary.phaseId = [repelem(grains.phaseId,bs-1), ones(size(F,1),1)];
+grains.boundary.triplePoints = triplePointList;
 
 end
