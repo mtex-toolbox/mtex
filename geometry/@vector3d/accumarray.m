ac75739cedce98d0a0ef4c096e4f9bc8c1857b4a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-06-24 08:20:27 +0200

    vector3d/accumarray works now also for antipodal vectors

--- geometry/@vector3d/accumarray.m
+++ geometry/@vector3d/accumarray.m
@@ -6,13 +6,29 @@ function v = accumarray(subs,v,varargin)
 %
 % Input
 %  subs - 
-%  v - @vwector3d 
+%  v - @vector3d 
 %
 % Output
 %  v - @vector3d
 
-v.x = accumarray(subs,v.x,varargin{:});
-v.y = accumarray(subs,v.y,varargin{:});
-v.z = accumarray(subs,v.z,varargin{:});
+if v.antipodal
+  
+  % find a reference vector for each class
+  ref = accumarray(subs,1:length(v),[],@(x) x(1));
+  v_ref = v.subSet(ref(subs));
+  
+  flip = 1 - 2*(dot(v,v_ref,'noAntipodal') < 0);
+  
+  v.x = accumarray(subs,v.x .* flip,varargin{:});
+  v.y = accumarray(subs,v.y .* flip,varargin{:});
+  v.z = accumarray(subs,v.z .* flip,varargin{:});
+  
+else  
+  
+  v.x = accumarray(subs,v.x,varargin{:});
+  v.y = accumarray(subs,v.y,varargin{:});
+  v.z = accumarray(subs,v.z,varargin{:});
+  
+end  
 
 v.isNormalized = false;
