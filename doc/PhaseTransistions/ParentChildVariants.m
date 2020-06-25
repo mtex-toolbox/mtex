117e1fd16447fc724e78ed5319fbe9b9bd4bd8b1
Rüdiger Kilian <kilir@users.noreply.github.com>
2020-06-25 10:48:19 +0200

    Minor

--- doc/PhaseTransistions/ParentChildVariants.m
+++ doc/PhaseTransistions/ParentChildVariants.m
@@ -3,10 +3,10 @@
 %%
 % The crystallographic structure of most materials is dependend on external
 % conditions as temperature and pressure. When the external conditions
-% change the the crystals undergo a phase transition from the inital phase,
+% change the crystals may undergo a phase transition from the inital phase,
 % often called parent phase, into the child phase. While both phases still
 % have the same chemical composition their crystallographic structure might
-% quite different. A typical example are the alpha and beta phase of
+% be quite different. A typical example are the alpha and beta phase of
 % titanium. While the alpha phase is hexagonal
 
 csAlpha = crystalSymmetry('622',[3 3 4.7],'mineral','Ti (alpha)')
@@ -25,7 +25,7 @@ oriParent = orientation.rand(csAlpha)
 % be the orientation of the atomic lattice befor phase transition and
 % |oriChild| the orientation of the atomic lattice after the phase
 % transition. Since during a phase transition the atoms reorder with
-% respect to a minimal energy constraint both orientations |oriParent| and
+% respect to a minimal energy constraint, both orientations |oriParent| and
 % |oriChild| are in a specific orientation relationship with respect to
 % each other. In the case of alpha and beta Titanium the dominant
 % orientation relationship is described by the Euler angles
@@ -39,7 +39,7 @@ oriChild = oriParent * inv(alpha2beta)
 
 %%
 % This orientation relationship is characterised by the properties that it
-% alignes the c-axis direction of the hexagonal alpha phase with the (110)
+% alignes the c-axis direction of the hexagonal alpha phase with the [110]
 % direction of the cubic beta phase and the [2-1-10] direction of the alpha
 % phase with the  [-11-1] direction of the beta phase.
 
@@ -59,8 +59,8 @@ alpha2beta = orientation.map(Miller(0,0,0,1,csAlpha),Miller(1,1,0,csBeta),...
 
 %%
 % The advantage of the above definition by the alignment of different
-% crystal directions is that it independ of the
-% <CrystalReferenceSystem.html convention used for the hexagonal cyrstal
+% crystal directions is that it is independent of the
+% <CrystalReferenceSystem.html convention used for the hexagonal crystal
 % coordinate system>.
 %
 %% Child Variants
@@ -80,7 +80,7 @@ oriChild = oriParentSym * inv(alpha2beta)
 % However, with respect to cubic crystal symmetry the 12 child orientations
 % are not all symmetrically equivalent anymore. In fact, they form 6 pairs
 % of symmetrically equivalent orientations as depicted in the following
-% inverse pole figure
+% inverse pole figure.
 
 color = ind2color(14+(1:12));
 
@@ -106,7 +106,7 @@ hold off
 oriChild = variants(alpha2beta,oriParent,2:3)
 
 %%
-% Sometimes once faces in inverse question, i.e., determine the |variantId|
+% Sometimes one faces the inverse question, i.e., determine the |variantId|
 % from a parent and a child orientation or a pair of child orientations.
 % This can be done using the command <calcVariants.html |calcVariants|>
 % which is discussed in detail in the section
