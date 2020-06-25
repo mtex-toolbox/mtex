1d53fa561c70f1e6f2a4d3deb8b8109e1e22a28f
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-06-25 09:18:36 +0200

    docu

---
+++ doc/GrainBoundaries/QuadruplePoints.m
@@ -0,0 +1,53 @@
+
+
+
+
+cs = crystalSymmetry('1','mineral','test');
+
+
+id = [...
+  0 0 0 0 0 0; ...
+  0 1 1 1 1 0; ...
+  0 1 1 1 1 0; ...
+  0 1 0 0 1 0; ...
+  0 1 0 0 1 0; ...
+  0 1 1 1 0 0; ...
+  0 0 0 0 0 0]==1;
+
+rot = rotation.id(size(id));
+
+rot(id) = rotation.rand;
+
+
+ebsd = EBSDsquare(rot,2*ones(size(rot)),1:2,{'not indexed',cs},[1 1]);
+
+%%
+
+plot(ebsd,ebsd.orientations)
+
+%%
+
+grains = calcGrains(ebsd,'removeQuadruplePoints')
+
+%%
+
+gB = grains.boundary
+grains = merge(grains,gB(end),'calcMeanOrientation')
+
+%%
+
+grains = smooth(grains,1)
+
+%%
+
+plot(grains.boundary)
+
+%%
+
+id = 2;
+gB = grains(id).boundary;
+
+plot(gB,gB.curvature(10),'linewidth',6)
+
+mtexColorMap blue2red
+setColorRange(0.5*[-1,1])
