2d40e507d347f652bfdda7d84f875394c95906be
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-06-26 10:29:11 +0200

    more reasonable problem size :)

--- SO3Fun/laufzeittest.m
+++ SO3Fun/laufzeittest.m
@@ -1,14 +1,17 @@
 clear all
 
-rot = orientation.rand(100);
-SO3F = calcDensity(rot,'harmonic','bandwidth',25);
+rot = orientation.rand(1000);
+SO3F = calcDensity(rot,'harmonic','bandwidth',64,'halfwidth',2.5*degree);
 SO3F = SO3FunHarmonic(SO3F.components{1}.f_hat)
 
 
-ori=rotation.rand(10);
+ori=rotation.rand(1000000);
 
 
 %%
+tic
+    f0=eval(SO3F,ori);
+toc
 tic
     f1=eval2(SO3F,ori);
 toc
@@ -21,3 +24,4 @@ toc
 
 max(abs(f1-f2))
 max(abs(f1-f3))
+max(abs(f0-f3))
