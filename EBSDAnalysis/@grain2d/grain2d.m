5ab55ac2f6bd92909c6460aceda40bf5ef8fd709
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-07-05 09:06:08 +0200

    remove slow testing code

--- EBSDAnalysis/@grain2d/grain2d.m
+++ EBSDAnalysis/@grain2d/grain2d.m
@@ -94,54 +94,7 @@ classdef grain2d < phaseList & dynProp
         quadPoints = [];
       end
       
-      testing = 0;
-      if testing
-      
-        qAdded = 0;
-        for qP = quadPoints.'
-              
-          % add an additional vertex for each quad point
-          V = [V;V(qP,:)];
-        
-          % find the corresponding faces
-          iqF = find(any(F == qP,2));
-          qV = F(iqF,:).'; qV(qV==qP) = []; % the vertices of the quadruple point
-        
-          qOmega = atan2(V(qV,1) - V(qP,1),V(qV,2) - V(qP,2));
-          [~,qOrder] = sort(qOmega);
-        
-          iqD = find(all(I_FDext(iqF(qOrder([1,4])),:)) + all(I_FDext(iqF(qOrder([2,3])),:)));
-        
-          if length(iqD) < 2
-            qOrder = qOrder([2:end,1]);
-            iqD = find(all(I_FDext(iqF(qOrder([1,4])),:)) + all(I_FDext(iqF(qOrder([2,3])),:)));
-          end
-        
-          % set new vertex into face list
-          F(iqF(qOrder(1:2)),:) = [qV(qOrder(1:2)).',[size(V,1);size(V,1)]];
-        
-          % common D
-          %all(I_FDext(iqF(qOrder([1,4])),:))
-          %all(I_FDext(iqF(qOrder([2,3])),:))
-        
-          % if we have different grains - we need a new boundary
-          if find(I_DG(iqD(1),:)) ~= find(I_DG(iqD(2),:))
-        
-            % add new edge
-            F = [F; [qP,size(V,1)]];
-            qAdded = qAdded + 1;
-          
-            % new row to I_FDext
-            I_FDext = [I_FDext; ...
-              all(I_FDext(iqF(qOrder([1,4])),:)) + all(I_FDext(iqF(qOrder([2,3])),:))]; 
-        
-            % new row to I_FDext
-            I_FDint = [I_FDint; sparse(1,size(I_FDint,2))];
-          end
-        end
-      end
-            
-      if ~isempty(quadPoints) && ~testing
+      if ~isempty(quadPoints) 
       
         % find the 4 edges connected to the quadpoints
         I_FV = sparse(repmat((1:size(F,1)).',1,2),F,ones(size(F)));
@@ -225,7 +178,7 @@ classdef grain2d < phaseList & dynProp
         % new empty rows to I_FDint
         I_FDint = [I_FDint; sparse(qAdded,size(I_FDint,2))];
       
-      elseif ~testing
+      else
         qAdded = 0;
       end
       
