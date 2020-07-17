117e3fbfb5fba9be286c417f0790520983cedff3
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-07-17 21:29:37 +0200

    remove SO3Fun temporarily

--- SO3Fun/@SO3Fun/minus.m
+++
@@ -1,6 +0,0 @@
-function F = minus(F1,F2)
-% overload minus
-        
-F = plus(F1,-F2);
-        
-end
--- SO3Fun/@SO3FunHarmonic/length.m
+++
@@ -1,5 +0,0 @@
-function l = length(SO3F)
-% overloads length
-
-s = size(SO3F.fhat);
-l = prod(s(2:end));
