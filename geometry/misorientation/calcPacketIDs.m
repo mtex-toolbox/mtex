5cb7a753696bc643c47aa41b73112c6d921b9b76
Frank <contact@fniessen.com>
2020-09-07 16:45:46 +0200

    Updated function calcPacketIDs

--- geometry/misorientation/calcPacketIDs.m
+++ geometry/misorientation/calcPacketIDs.m
@@ -1,13 +1,13 @@
-function [packetId] = calcPacketIDs(hklParent,hklChild,p2c,variantIDs)
+function [packetIDs] = calcPacketIDs(hklParent,hklChild,p2c,variantIDs)
 %
 % Syntax
 %
-%  childId = calcPacketIDs(hklParent,hklChild,p2c,variantIDs)
+%  packetIDs = calcPacketIDs(hklParent,hklChild,p2c,variantIDs)
 %
 % Input
 %  hklParent  - Habit planes in the parent phase
 %  hklChild   - Habit plane in the child phase
-%  p2c        - parent to child mis@orientation
+%  p2c        - parent to child mis@orientation or list of variant mis@orientations
 %  variantIDs - List of identified variantIDs 
 %
 % Output
@@ -16,7 +16,19 @@ function [packetId] = calcPacketIDs(hklParent,hklChild,p2c,variantIDs)
 % Description
 %
 %
-  omega = dot(variants(p2c,hklParent),hklChild);
-  [~,packetId] = max(omega,[],2);  
-  packetId = packetId(variantIDs);
+
+%Determine lowest disorientations of parallel child planes
+if length(p2c) == length(p2c.variants) %List of variants
+    omega = dot(p2c*hklParent,hklChild);
+else %OR misorientation
+    omega = dot(variants(p2c,hklParent),hklChild);
+end
+%Determine packet IDs
+[~,packetId] = max(omega,[],2);  
+if nargin == 3
+   packetIDs = packetId; 
+else
+   packetIDs = nan(size(variantIDs));
+   packetIDs(~isnan(variantIDs))= packetId(variantIDs(~isnan(variantIDs)));
+end
   
