d9b152e7e2b063b5bd99adce568675a591b2fffb
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-09-24 22:11:56 +0200

    new option edgeAlpha

--- EBSDAnalysis/@grainBoundary/plot.m
+++ EBSDAnalysis/@grainBoundary/plot.m
@@ -10,13 +10,21 @@ function [h,mP] = plot(gB,varargin)
 %   % colorize segments according to some property
 %   plot(gB('Forsterite','Forsterite'),gB('Forsterite','Forsterite').misorientation.angle)
 %
+%   % colorize segments according to a list of RGB values
+%   plot(gB('Forsterite','Forsterite'),colorList)
+%
 % Input
 %  grains - @grain2d
 %  gB     - @grainBoundary
+%  colorList - n x 3 list of RGB values
 %  
 % Options
 %  linewidth - line width
 %  linecolor - line color
+%  edgeAlpha - (list of) transparency values between 0 and 1
+%  region    - [xmin xmax ymin ymax] plot only a subregion
+%  displayName - label to appear in the legend
+%  smooth      - try to make a smooth connections at the vertices
 %
 
 reg = get_option(varargin,'region');
@@ -32,8 +40,6 @@ if ~isempty(reg)
   gB = gB.subSet(ind);
   
 end
-
-
 
 % create a new plot
 mtexFig = newMtexFigure(varargin{:});
@@ -214,7 +220,6 @@ if nargin > 1 && isnumeric(varargin{1}) && ...
     
   end
   
-
   % this makes the line connectors more nice
   try
     pause(0.01)
@@ -267,7 +272,7 @@ if nargin > 1 && isnumeric(varargin{1}) && ...
   obj.EdgeColor = 'flat';
   color = squeeze(varargin{1});
   obj.FaceVertexCData = reshape(repmat(color,1,3)',size(color,2),[])';
-
+  
 else % color given directly
     
   obj.EdgeColor = str2rgb(get_option(varargin,{'linecolor','edgecolor','facecolor'},'k'));
@@ -275,6 +280,13 @@ else % color given directly
 end
 
 obj.hitTest = 'off';
+
+if check_option(varargin,'edgeAlpha')
+  obj.AlphaDataMapping = 'none';
+  obj.edgeAlpha = 'flat';
+  obj.FaceVertexAlphaData = get_option(varargin,'edgeAlpha');
+  varargin = delete_option(varargin,'edgeAlpha');
+end
 
 h = optiondraw(patch(obj),varargin{:});
 
