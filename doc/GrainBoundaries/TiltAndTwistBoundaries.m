8b223c555aa0a6221db02253964204174c89e797
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-09-25 21:54:16 +0200

    docu

--- doc/GrainBoundaries/TiltAndTwistBoundaries.m
+++ doc/GrainBoundaries/TiltAndTwistBoundaries.m
@@ -1,31 +1,30 @@
 %% Tilt and Twist Boundaries
 %
 %% Theory
-% If a material deforms through the movement of dislocations, re-
-% arrangement of dislocationsto a low-energy configuration may happen
-% during deformation (i.e. in slow, geologic deformation) or 
-% or afterwards (in many metals). In any case, the arrangement of
-% dislocation walls can lead to so-called subgrains boundaries. If such a
-% boundary is composed of edge dislocations, it is called a tilt
-% boundary and the rotation axis relating both parts of the grain at each
-% side can be expected to be within the boudnary plane (ideally parallel to
-% the edge dislocation line). If the boundary is composed of screw 
-% dislocations, the rotation axis should be normal to the boundary. 
-% Between those end-members, there are general boundaries where
-% the rotation axis is not easily related to the type of dislocations
-% unless further information is available.
+% If a material deforms through the movement of dislocations, rearrangement
+% of dislocationsto a low-energy configuration may happen during
+% deformation (i.e. in slow, geologic deformation) or or afterwards (in
+% many metals). In any case, the arrangement of dislocation walls can lead
+% to so-called subgrains boundaries. If such a boundary is composed of edge
+% dislocations, it is called a tilt boundary and the rotation axis relating
+% both parts of the grain at each side can be expected to be within the
+% boudnary plane (ideally parallel to the edge dislocation line). If the
+% boundary is composed of screw dislocations, the rotation axis should be
+% normal to the boundary. Between those end-members, there are general
+% boundaries where the rotation axis is not easily related to the type of
+% dislocations unless further information is available.
 
 %% Data import and grain detection
 % Lets start by loading an MTEX standard data set, reconstuct grains and
 % grain boundaries
 
 % import data
-mtexdata forsterite
+mtexdata forsterite silent
 
 % reconstruct grains
 [grains, ebsd.grainId] = calcGrains(ebsd);
 ebsd = ebsd(grains(grains.grainSize>10));
-[grains, ebsd.grainId, ebsd.mis2mean] = calcGrains(ebsd);
+[grains, ebsd.grainId, ebsd.mis2mean] = calcGrains(ebsd,'threshold',[1.5 15]*degree);
 
 % smooth grain boundaries to avoid stair casing effect
 grains = smooth(grains,5);
@@ -36,7 +35,6 @@ grains = grains(grains.grainSize>300,'fo')
 %%
 % Colorize the orientations according to their misorientation axis / angle
 % with respect to the grains mean orientation
-
 
 % sett the color key
 colorKey = axisAngleColorKey(ebsd('f').CS);
@@ -53,7 +51,6 @@ plot(grains('f').boundary,'lineWidth',2)
 
 % plot grain index
 text(grains('f'),arrayfun(@num2str,1:length(grains),'uniformOutput',false),'FontSize',18)
-
 
 % mark one specific grain
 ind = 73;
@@ -90,20 +87,16 @@ hold off
 % boundaries, let's segment at a small angle
 
 
-[subGrains, ebsdS.grainId,ebsdS.mis2mean] = calcGrains(ebsdS,'angle',1.25*degree,'boundary','tight');
+subGB = grains(ind).subBoundary;
 
-subGrains = smooth(subGrains,5);
-
-% gather all subgrain boundaries
-gbfo = [subGrains.boundary('f','f') subGrains.innerBoundary('f','f')];
 
 % colorize orientation according to their misorientation to the meanorientation
 plot(ebsdS,colorKey.orientation2color(ebsdS.orientations),'micronbar','off')
 hold on
 
 % colorize subgrain boundaries according to their misorientation angle
-plot(gbfo,gbfo.misorientation.angle./degree,'lineWidth',3)
-plot(subGrains.boundary('notIndexed'),'lineWidth',2)
+plot(subGB, subGB.misorientation.angle./degree,'lineWidth',3)
+
 hold off
 mtexColorbar('Title','Misorientation angle [\circ]','locacation','southoutside')
 mtexColorMap blue2red
@@ -114,7 +107,7 @@ mtexColorMap blue2red
 % coordinate system while colorizing them according to the misorientation
 % angle.
 
-plot(gbfo.misorientation.axis,gbfo.misorientation.angle./degree,...
+plot(subGB.misorientation.axis,subGB.misorientation.angle./degree,...
   'fundamentalRegion','MarkerEdgeColor','k')
 mtexColorMap white2black
 mtexColorbar('Title','Misorientation angle [\circ]','locacation','southoutside')
@@ -123,14 +116,13 @@ mtexColorbar('Title','Misorientation angle [\circ]','locacation','southoutside')
 % We observe that the misorientation axes have a strong tendency to be
 % perpendicular to [100].
 
-
 %% The misorientation axis in specimen coordinats
 % The computation of the misorientation axis in specimen coordinates is a
 % little bit more complicated as it is impossible using only the
 % misorientions. In fact we require the adjecent orientations on both sides
 % of the subgrain boundaries. We can find those by the command
 
-ori_boundary = ebsdS('id',gbfo.ebsdId).orientations
+ori_boundary = ebsd('id',subGB.ebsdId).orientations
 
 %%
 % which results in a Nx2 matrix of orientations with rows corresponding to
@@ -143,7 +135,7 @@ axS = axis(ori_boundary(:,1),ori_boundary(:,2),'antipodal')
 %%
 % and we may visualize them in a spherical plot using
 
-plot(axS,gbfo.misorientation.angle./degree,'upper','MarkerEdgeColor','k','MarkerSize',10)
+plot(axS,subGB.misorientation.angle./degree,'upper','MarkerEdgeColor','k','MarkerSize',10)
 mtexColorMap white2black
 mtexColorbar('Title','Misorientation angle [\circ]','locacation','southoutside')
 
@@ -153,34 +145,33 @@ mtexColorbar('Title','Misorientation angle [\circ]','locacation','southoutside')
 % First, we plot some crystal directions of the grain in a pole figure
 % so we understand the orientation of the grain a little better
 close all
-h = Miller({1,0,0},{0,1,0},{0,0,1},ebsd.CS);
-ori = ebsdS.orientations;
+h = Miller({1,0,0},{0,1,0},{0,0,1},grains(ind).CS);
+ori = ebsd(grains(ind)).orientations;
 plotPDF(ori,colorKey.orientation2color(ori),h,'MarkerSize',5)
 
 %%
 % now we plot the grain with subboundaries color coded with the direction
 % mapping according to the misorientation axes in crystal coordinates
 
-
-plot(ebsdS,colorKey.orientation2color(ori),'faceAlpha',0.3)
+plot(ebsd(grains(ind)),colorKey.orientation2color(ori),'faceAlpha',0.3)
 hold on
 axisKey = HSVDirectionKey(ori.CS);
-color = axisKey.direction2color(gbfo.misorientation.axis);
-plot(gbfo,'linewidth',6)
-plot(gbfo,color,'linewidth',4)
+color = axisKey.direction2color(subGB.misorientation.axis);
+plot(subGB,'linewidth',6)
+plot(subGB,color,'linewidth',4)
 hold off
-mtexTitle('misor crystal')
+mtexTitle('misorientation axis crystal')
 
 %%
 % Next we plot the grain with subboundaries colorcoded with the direction
 % mapping according to the misorientation axes in specimen coordinates
 
-plot(ebsdS,colorKey.orientation2color(ori),'faceAlpha',0.3)
+plot(ebsd(grains(ind)),colorKey.orientation2color(ori),'faceAlpha',0.3)
 hold on
 axisKey = HSVDirectionKey;
 color = axisKey.direction2color(axS);
-plot(gbfo,'linewidth',6)
-plot(gbfo,color,'linewidth',4)
+plot(subGB,'linewidth',6)
+plot(subGB,color,'linewidth',4)
 hold off
 mtexTitle('misor specimen')
 
