99fc27db96e4b9ffc1eb8d02d7793e1f838a8a81
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-10-01 21:52:32 +0200

    bug fix

--- EBSDAnalysis/@EBSD/project2FundamentalRegion.m
+++ EBSDAnalysis/@EBSD/project2FundamentalRegion.m
@@ -5,10 +5,13 @@ rot = ebsd.rotations;
 grainId = ebsd.grainId;
 
 CS = ebsd.CSList;
+isIndexed = grains.isIndexed;
 
 for i = 1:length(grains)
   
   id = grainId == grains.id(i);
+  
+  if nnz(id) < 2 || ~isIndexed(i), continue; end
   
   rot(id) = project2FundamentalRegion(rot(id),CS{grains.phaseId(i)},grains.meanRotation(i));
   
