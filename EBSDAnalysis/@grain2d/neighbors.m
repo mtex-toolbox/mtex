50f048cee602fbac69d44b6c4ec6889e02c7ff5a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-11-13 07:33:27 +0100

    neighbours(grains1,grains1)
    should work correct now

--- EBSDAnalysis/@grain2d/neighbors.m
+++ EBSDAnalysis/@grain2d/neighbors.m
@@ -33,12 +33,13 @@ if ~check_option(varargin,'full')
   if nargin > 1 && isa(varargin{1},'grain2d')
     
     isIn2 = ismember(pairs,varargin{1}.id);
+    isIn1 = ismember(pairs,grains.id);
     
     % second grain should aways be in second column
     pairs(isIn2(:,1),:) = fliplr(pairs(isIn2(:,1),:));
     
     % ensure second grain is present everywhere
-    pairs(~any(isIn2,2),:)=[];
+    pairs = pairs(all(isIn1 | isIn2,2) & any(isIn2,2),:);
     
   else
     pairs(any(~ismember(pairs,grains.id),2),:)=[];
