fe39c5b3f4398f49e857dad1f761cb05918fe64e
Rüdiger <kiliru02@gmail.com>
2020-11-14 20:31:34 +0100

    Attempt to align polar axes with mtex plotting convention and small bug fix for weights

--- geometry/@vector3d/histogram.m
+++ geometry/@vector3d/histogram.m
@@ -14,11 +14,39 @@ h = polarhistogram(rho,varargin{:});
 
 if ~isempty(weights)
   
-  [~,~,binId] = histcounts(h.Data,h.BinEdges);
-  
+  % oddly, matlab returns bins in the range -pi:pi and the data 
+  % within 0:2*pi with some data < 0
+  edges = h.BinEdges;
+  edges(edges<0)=pi-edges(edges<0);
+  data=mod(h.Data,2*pi);
+
+  [~,~,binId] = histcounts(data,sort(edges));
   h.BinCounts = accumarray(binId,weights,[length(h.BinEdges)-1 1],@nansum).' ./ sum(weights);
 end
+    
   
+% set plotting convention such that the plot coinices with a map
+x = getMTEXpref('xAxisDirection');
+switch x
+  case 'east'
+    h.Parent.ThetaZeroLocation='right';
+  case 'north'
+    h.Parent.ThetaZeroLocation='top';
+  case 'west'
+    h.Parent.ThetaZeroLocation='left';
+  case 'south'
+    h.Parent.ThetaZeroLocation='bottom';
+end
+
+z  = getMTEXpref('zAxisDirection');
+switch z
+  case 'intoPlane'
+    h.Parent.ThetaDir='clockwise';
+  case 'outOfPlane'
+    h.Parent.ThetaDir='counterclockwise';
+end
+
+
 if nargout == 0, clear h; end
 
 end
