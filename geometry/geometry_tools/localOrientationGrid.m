77aad2b82c333935768ee004ada3df930ef148aa
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-11-15 19:09:20 +0100

    important bug fix in ODF/volume with specimen symmetry and center close to the boundary of the fundamental region

--- geometry/geometry_tools/localOrientationGrid.m
+++ geometry/geometry_tools/localOrientationGrid.m
@@ -1,10 +1,15 @@
 function ori = localOrientationGrid(CS,SS,maxAngle,varargin)
 % define a equispaced grid localized to a center orientation
 %
+% Syntax
+%
+%   ori = localOrientationGrid(CS,SS,maxAngle,'center',center)
+% 
 % Input
 %  CS       - @symmetry
 %  SS       - @symmetry
-%  maxAngle - 
+%  maxAngle - radius of the ball of orientations
+%  center   - @orientation
 %
 % Output
 %
@@ -23,85 +28,33 @@ rotAngle = res/2:res:maxAngle;
 qId = quaternion();
 for i = 1:length(rotAngle)
   
-  dres = acos(max((cos(res/2)-cos(rotAngle(i)/2)^2)/...
+  dres = acos(max((cos(res/2) - cos(rotAngle(i)/2)^2)/...
     (sin(rotAngle(i)/2)^2),-1));  
   axes = equispacedS2Grid('resolution',dres);
   
   qId = [qId,axis2quat(axes,rotAngle(i))]; %#ok<AGROW>
 end
 
-% project center to fundamental region 
-% TODO: call project2FundamentalRegion ???
 center = quaternion(get_option(varargin,'center',rotation.id));
-sym_center = symmetrise(center,CS.properGroup,SS.properGroup);
-[~,ind] = min(angle(sym_center),[],1);
-center = sym_center(ind);
 
-% restrict to fundamental region
-% we do only have to respect specimen symmetr since we assume maxangle to
-% be sufficiently small
-q = quaternion;
-for i = 1:length(center)
-  cq = center(i) * qId(:);
-  if numSym(SS) > 1
-    ind = fundamental_region2(cq,center(i),CS,SS);
-    cq = cq(ind);
-  end
-  q = [q;cq(:)]; %#ok<AGROW>
+ori = orientation(qId * center,CS,SS);
+
+% ensure 
+if numSym(CS.properGroup) > 1 && numSym(SS.properGroup) > 1
+  
+  % in order to avoid centers that are exactly at the boundary of the
+  % fundamental region we distort the center slightly
+  sym_center = symmetrise(center * axis2quat(vector3d(3,2,1),0.001*degree), ...
+    CS.properGroup,SS.properGroup);
+  
+  % we have only to check for those sym_centers that are not to far away
+  delta = angle(sym_center(1),sym_center);
+  sym_center = sym_center(delta < 2*maxAngle);
+
+  % take only those orientations that are closest to center
+  [~,ind] = min(angle_outer(ori,sym_center,'noSymmetry'),[],2);
+  ori = ori(ind == 1);
+
 end
-
-ori = orientation(q,CS,SS);
-
-end
-
-function ind = fundamental_region2(q,center,cs,ss)
-%
-% Input
-%  q      - @quaternion to project
-%  center - center of fundamental region
-%  cs, ss - crystal and specimen @symmetry
-%
-% Output
-%  ind    -
-
-% symmetrise
-c_sym = symmetrise(center,ss.properGroup,cs.properGroup);
-omega = angle(c_sym * inv(center)); %#ok<MINV>
-%[~,i] = min(omega,[],2);
-%c_sym = c_sym(sub2ind(sie(c_sym,i));
-[~,c_sym] = selectMinbyRow(omega,c_sym);
-
-% convert to rodrigues space
-rq = Rodrigues(q); clear q;
-rc_sym = Rodrigues(c_sym); 
-
-% to remeber perpendicular planes for specimen symmetry case
-oldD = vector3d;
-
-% find rotation not part of the fundamental region
-ind = true(size(rq));
-for i = 2:length(rc_sym)
-  
-  d = rc_sym(i)-rc_sym(1);
-  if norm(d)<=1e-10 % find something that is orthogonal to rc_sym    
-  
-    if isempty(oldD)
-      d = orth(rc_sym(1));
-    elseif length(oldD) == 1
-      d = cross(oldD,rc_sym(1));
-      if norm(d)<1e-5, d = orth(oldD);end
-    else
-      continue
-    end
-    oldD = [oldD,d]; %#ok<AGROW>
-    nd = 0;
-  else
-    nd = norm(d).^2 /2;
-  end
-  
-  ind = ind & dot(rq-rc_sym(1),d) <= nd;
-  
-end
-
 
 end
