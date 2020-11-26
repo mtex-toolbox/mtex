8dff8f09d439712622fb2d8cd3e9d01b56080e70
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-11-26 12:17:20 +0100

    new option extrapolate to fill see #593

--- EBSDAnalysis/@EBSD/smooth.m
+++ EBSDAnalysis/@EBSD/smooth.m
@@ -1,8 +1,22 @@
 function [ebsd,filter] = smooth(ebsd,varargin)
 % smooth spatial EBSD 
 %
+% Syntax
+%
+%   ebsd = smooth(ebsd)
+%
+%   F = halfQuadraticFilter
+%   F.alpha = 2;
+%   ebsd = smooth(ebsd, F, 'fill', grains)
+%
 % Input
-%  ebsd - @EBSD
+%  ebsd   - @EBSD
+%  F      - @EBSDFilters
+%  grains - @grain2d if provided pixels at the boundary between grains are not filled
+%
+% Options
+%  fill        - fill missing values (this is different then not indexed values!)
+%  extrapolate - extrapolate up the the outer boundaries
 %
 % Example
 %   mtexdata forsterite;
