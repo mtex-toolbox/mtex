7c68f742114d719b8dead370e41cd24784b080a0
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-12-11 16:02:24 +0100

    minor

--- SO3Fun/@SO3FunRBF/SO3FunRBF.m
+++ SO3Fun/@SO3FunRBF/SO3FunRBF.m
@@ -3,7 +3,7 @@ classdef SO3FunRBF < SO3Fun
   properties
     c0 = 0               % constant portion
     center = orientation % center of the components
-    psi = deLaValeePoussinKernel % shape of the components
+    psi = SO3deLaValleePoussin % shape of the components
     weights = []         % coefficients
   end
 
