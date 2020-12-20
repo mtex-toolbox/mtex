c34a964aeed93287b7583af2dc581385b43aad57
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-12-20 16:56:04 +0100

    docu

--- EBSDAnalysis/@parentGrainReconstructor/mergeSimilar.m
+++ EBSDAnalysis/@parentGrainReconstructor/mergeSimilar.m
@@ -1,6 +1,16 @@
  function job = mergeSimilar(job, varargin)
+ %
+ % Syntax
+ %   job.mergeSimilar('threshold',5*degree)
+ %
+ % Input
+ %  job - @parentGrainReconstructor
+ %
+ % Options
+ %  threshold - misorientation angle up to which grains are merged
+ %
       
- [job.grains, mergeId] = merge(job.grains, varargin{:});
+ [job.grains, mergeId] = merge(job.grains, 'threshold',5*degree, varargin{:});
  job.mergeId = mergeId(job.mergeId); %#ok<*PROPLC>
  job.ebsd('indexed').grainId = mergeId(job.ebsd('indexed').grainId);
  
