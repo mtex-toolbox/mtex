a9cc345f15df369f6c62fb0d6218ef82bbdf545d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-12-28 07:57:55 +0100

    new function to compute fit between all child to child misorientations and job.p2c

---
+++ EBSDAnalysis/@parentGrainReconstructor/calcGBFit.m
@@ -0,0 +1,26 @@
+function [fit, c2cPairs] = calcGBFit(job,varargin)
+% fit between child to child misorientations and job.p2c
+%
+% Syntax
+%
+%   [fit, c2cPairs] = job.calcGBFit
+%
+% Input
+%  job - @parentGrainReconstructor
+%
+% Output
+%  fit      - fit between child to child misorientations and job.p2c
+%  c2cPairs - list of grainId of child to child neighbours
+
+% child to child boundaries
+c2cPairs = neighbors(job.childGrains);
+  
+% extract the corresponding mean orientations
+oriChild = job.grains('id',c2cPairs).meanOrientation;
+
+% child to child misorientation variants
+p2cV = job.p2c.variants; p2cV = p2cV(:);
+c2c = job.p2c .* inv(p2cV);
+  
+% misorientation to c2c variants
+fit = min(angle_outer(inv(oriChild(:,1)).*oriChild(:,2), c2c),[],2);
