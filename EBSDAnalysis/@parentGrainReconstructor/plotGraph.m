11c810220f5c4dd7040a852d29718909d1ee08c0
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2020-12-29 13:32:33 +0100

    minor

--- EBSDAnalysis/@parentGrainReconstructor/plotGraph.m
+++ EBSDAnalysis/@parentGrainReconstructor/plotGraph.m
@@ -9,4 +9,4 @@ v = full(job.graph(sub2ind(size(job.graph),grainPairs(:,1),grainPairs(:,2))));
       
 [gB,pairId] = job.grains.boundary.selectByGrainId(grainPairs);
 
-[varargout{1:nargout}] = plot(gB, v(pairId), varargin{:});
+[varargout{1:nargout}] = plot(gB, 'edgeAlpha', 1-v(pairId), varargin{:});
