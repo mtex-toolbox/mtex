ebdefed0fe42f7204e4cf62c1342ff6171c4caf8
Rüdiger <kiliru02@gmail.com>
2021-01-02 18:56:12 +0100

    Initial fix to issues related to https://github.com/mtex-toolbox/mtex/issues/659

--- ODFAnalysis/@ODF/export_mtex.m
+++ ODFAnalysis/@ODF/export_mtex.m
@@ -30,30 +30,30 @@ fprintf(fid,'%% specimen symmetry: %s\n',char(odf.SS));
 % TODO
 for i = 1:length(odf.components)
 
-  if check_option(odf(i),'UNIFORM') % uniform portion
+  if isa(odf(i).components{1},'uniformComponent') % uniform portion
 
     fprintf(fid,'%%\n%%%% uniform component\n');
-    fprintf(fid,'%% weight: %3.5f\n',odf(i).c);
+    fprintf(fid,'%% weight: %3.5f\n',odf(i).weights);
 
-  elseif check_option(varargin,'FOURIER') || check_option(odf(i),'FOURIER')
+  elseif isa(odf(i).components{1},'FourierComponent')
 
     fprintf(fid,'%%\n%%%% fourier component: currently not supported! \n');
 
-  elseif check_option(odf(i),'FIBRE') % fibre symmetric portion
-
+  elseif isa(odf(i).components{1},'fibreComponent') % fibre symmetric portion
+    
     fprintf(fid,'%%\n%%%% fibre component\n');
-    fprintf(fid,'%% weight: %3.5f\n',odf(i).c);
-    fprintf(fid,'%% kernel: %s\n',char(odf(i).psi));
-    fprintf(fid,'%% fibre h: %s\n',char(odf(i).center{1}));
-    fprintf(fid,'%% fibre r: %s\n',char(odf(i).center{2}));
+    fprintf(fid,'%% weight: %3.5f\n',odf(i).weights);
+    fprintf(fid,'%% kernel: %s\n',char(odf(i).components{1}.psi));
+    fprintf(fid,'%% fibre h: %s\n',char(odf(i).components{1}.h));
+    fprintf(fid,'%% fibre r: %s\n',char(odf(i).components{1}.r));
 
   else % radially symmetric portion
 
     fprintf(fid,'%%\n%%%% radial component:\n');
-    fprintf(fid,'%% kernel: %s\n',char(odf(i).psi));
+    fprintf(fid,'%% kernel: %s\n',char(odf(i).components{1}.psi));
 
     % build up matrix to be exported
-    d = Euler(odf(i).center,varargin{:});
+    d = Euler(odf(i).components{1}.center,varargin{:});
     if ~check_option(varargin,'radians'), d = d./degree;end
 
     % convention
@@ -62,7 +62,7 @@ for i = 1:length(odf.components)
     elseif check_option(varargin,{'ABG','ZYZ'})
       convention = 'ZYZ';
     elseif isa(odf(i).center,'SO3Grid')
-      convention = get_flag(get(odf(i).center,'options'),{'ZXZ','ZYZ'},'none');
+      convention = get_flag(get(odf(i).components{1}.center,'options'),{'ZXZ','ZYZ'},'none');
     else
       convention = getMTEXpref('EulerAngleConvention');
     end
@@ -74,7 +74,7 @@ for i = 1:length(odf.components)
       fprintf(fid,'%% alpha   beta    gamma   weight\n');
     end
 
-    fprintf(fid,'%3.5f %3.5f %3.5f %3.5f\n',[d,odf(i).c(:)].');
+    fprintf(fid,'%3.5f %3.5f %3.5f %3.5f\n',[d,odf(i).components{1}.weights].');
 
   end
 end
