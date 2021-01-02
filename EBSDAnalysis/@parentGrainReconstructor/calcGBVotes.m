c7fdd6c59df4529b15b464aa9d5859698c470e05
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-01-02 10:45:12 +0100

    compute votes with weights from the boundary length

--- EBSDAnalysis/@parentGrainReconstructor/calcGBVotes.m
+++ EBSDAnalysis/@parentGrainReconstructor/calcGBVotes.m
@@ -1,4 +1,5 @@
 function job = calcGBVotes(job,varargin)
+% compute votes from grain boundaries
 %
 % Syntax
 %
@@ -13,7 +14,7 @@ function job = calcGBVotes(job,varargin)
 % Options
 %  numFit - number of fits to be computed
 %  noC2C  - consider only parent / child grain boundaries
-%
+%  weights - store boundary as weights to votes
 
 numFit = get_option(varargin,'numFit',2);
 
@@ -32,6 +33,15 @@ if ~isempty(job.parentGrains)
   % store as table
   job.votes = table(grainPairs(:,2), parentId, fit, ...
     'VariableNames',{'grainId','parentId','fit'});
+  
+  % weight votes according to boundary length
+  if check_option(varargin,'weights')
+    
+    [~,pairId] = job.grains.boundary.selectByGrainId(grainPairs);
+    weights = accumarray(pairId,1,[size(grainPairs,1) 1]);
+    
+    job.votes.weights = weights;
+  end
 end
 
 % child-child - votes
@@ -44,10 +54,21 @@ if ~check_option(varargin,'noC2C')
   % compute for each parent/child pair of grains the best fitting parentId
   [parentId, fit] = calcParent(oriChild,job.p2c,'numFit',numFit,'id');
     
+  c2cVotes = table(grainPairs(:), reshape(parentId,[],numFit), repmat(fit,2,1), ...
+    'VariableNames',{'grainId','parentId','fit'});
+  
+  
+  % weight votes according to boundary length
+  if check_option(varargin,'weights')
+    
+    [~,pairId] = job.grains.boundary.selectByGrainId(grainPairs);
+    weights = accumarray(pairId,1,[size(grainPairs,1) 1]);
+    
+    c2cVotes.weights = [weights;weights];
+  end 
+  
   % add to table
-  job.votes = [job.votes; ...
-    table(grainPairs(:), reshape(parentId,[],numFit), repmat(fit,2,1), ...
-    'VariableNames',{'grainId','parentId','fit'})];
+  job.votes = [job.votes; c2cVotes];
 
 end
 
