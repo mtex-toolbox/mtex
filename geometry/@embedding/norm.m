1012cc60583ed30e4d483fd63bd828c8ed2e7d57
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-01-31 12:31:38 +0100

    normalize embedding norm to 1

--- geometry/@embedding/norm.m
+++ geometry/@embedding/norm.m
@@ -1,9 +1,14 @@
-function n = norm(obj)
+function n = norm(obj,varargin)
 
 n = 0;
 for i = 1:length(obj.u)
   n = n + norm(obj.u{i}).^2;
 end
 n = sqrt(n);
+
+if check_option(varargin,'normalized')
+  n = n ./ norm(embedding.id(obj.CS));
+end
+
 
 end
