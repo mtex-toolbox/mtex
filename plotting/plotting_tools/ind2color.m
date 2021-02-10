19e8985110049db38d4f1b33c5d02e87e7907954
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-02-10 15:53:47 +0100

    updated colors once again

--- plotting/plotting_tools/ind2color.m
+++ plotting/plotting_tools/ind2color.m
@@ -5,7 +5,10 @@ cmap = getMTEXpref('colors');
 
 if check_option(varargin,'ordered')
   order = [1,6,19,12,20,22,3,11,18,16,...
-    23,8,7,2,14,15,9,4,21,5,24,13,10,17];
+    23,8,7,2,14,15,9,5,21,4,24,13,10,17];
+  
+  iorder(order) = 1:24;
+  
   cmap = cmap(order,:);
 end
 
