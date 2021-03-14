216ab8ba9a8aa5da1c00a3e4502bc6d913d1db78
Erik Wünsche <erik.wuensche@s2015.tu-chemnitz.de>
2021-03-14 01:12:47 +0100

    bug fix

--- SO3Fun/@SO3Fun/plus.m
+++ SO3Fun/@SO3Fun/plus.m
@@ -1,7 +1,7 @@
 function SO3F = plus(SO3F1,SO3F2)
 % implements |SO3F1 + SO3F2|
 
-if isa(x,'SO3Fun') && isa(y,'SO3Fun')
+if isa(SO3F1,'SO3Fun') && isa(SO3F2,'SO3Fun')
   
   SO3F = SO3FunHandle(@(rot) SO3F1.eval(rot) + SO3F2.eval(rot));
   
