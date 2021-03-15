cba06ea1799a70a372621452d07eab8a5f9e52ea
Erik Wünsche <erik.wuensche@s2015.tu-chemnitz.de>
2021-03-15 20:16:37 +0100

    That is better for constructiong Wigner-D functions as example

--- SO3Fun/@SO3FunHarmonic/WignerDmap.m
+++ SO3Fun/@SO3FunHarmonic/WignerDmap.m
@@ -1,41 +1,46 @@
 function SO3F = WignerDmap(N,varargin)
 % create harmonic Representation of the Wigner-D function with harmonic
-% degree N
+% degree N and index k,l : D^n_{k,l}
 %
 % Syntax
-%   SO3F = WignerDmap(N)
+%   SO3F = WignerDmap(N,k,l)
 %   SO3F = WignerDmap(N,'full')
-%   SO3F = WignerDmap(N,'single',[k,l])      k,l \in (-N,N)
 %
 % Input
-%   N - harmonic degree of Wigner-D function
+%   N   - harmonic degree of Wigner-D function
+%   k,l - indices of Wigner-D function
 %
 % Output
 %   SO3F - @SO3FunHarmonic
 %
 % Options
-%   'full'    - creates f(x) = sum_{k,l} D_{k,l}^n (x)
-%   'single'  - creates a single of functions D_{k,l}^n for one index of k,l
+%   'full'    - creates f(x) = sum_{k,l} D_{k,l}^N (x), i.e. sum of all 
+%               Wigner-D functions with harmonic degree N
 %
 
-len = deg2dim(N+1)-deg2dim(N);
 
 if check_option(varargin,'full')
   
+  len = deg2dim(N+1)-deg2dim(N);
   fhat = [zeros(deg2dim(N),1);ones(len,1)];
   SO3F = SO3FunHarmonic(fhat);
   
-elseif check_option(varargin,'single')
-  
-  ind = get_option(varargin,'single')+N+1;
-  n = deg2dim(N)+(ind(2)-1)*(2*N+1)+ind(1);
-  v=zeros(deg2dim(N+1),1); v(n)=1;
-  SO3F=SO3FunHarmonic(v);
-  
 else
   
-  fhat = reshape([zeros(deg2dim(N),len);eye(len)],deg2dim(N+1),2*N+1,2*N+1);
+  s = size(N);
+  
+  n = N(:); k = varargin{1}(:); l = varargin{2}(:); 
+
+  % Probably point wise also in deg2dim
+  deg = n.*(2*n-1).*(2*n+1)/3;
+  
+  oneind = (deg+n+1+l+(2*n+1).*(n+k)) + (0:length(n)-1)'*deg2dim(max(n+1));
+  
+  fhat = zeros(deg2dim(max(n+1)),length(n));
+  fhat(oneind) = 1;
+
   SO3F = SO3FunHarmonic(fhat);
+  SO3F = reshape(SO3F,s);
   
 end
 
