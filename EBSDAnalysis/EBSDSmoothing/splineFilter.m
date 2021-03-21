d753c3ebf7dbe7e3413d49af1b9aacf572e781e0
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-03-21 11:23:07 +0100

    use embedding for smoothing / test only

--- EBSDAnalysis/EBSDSmoothing/splineFilter.m
+++ EBSDAnalysis/EBSDSmoothing/splineFilter.m
@@ -38,5 +38,26 @@ classdef splineFilter < EBSDFilter
       % project back to orientation space
       ori = orientation(expquat(vector3d(tq{:}),quaternion(qmean)),ori.CS,ori.SS);
     end
+    
+    function ori = smooth_Test(F,ori,quality)
+
+      ori(quality==0) = nan;
+      
+      % project to tangential space
+      e = embedding(ori);
+      
+      d = reshape(double(e),size(ori,1),size(ori,2),[]);
+      
+      % perform smoothing
+      if F.robust, rob = {'robust'}; else, rob = {}; end
+                  
+      [d,F.alpha] = smoothn({d(:,:,1),d(:,:,2),d(:,:,3),d(:,:,4),d(:,:,5),d(:,:,6),d(:,:,7),d(:,:,8),d(:,:,9)},F.alpha,rob{:});
+            
+      % project back to orientation space
+      e = setDouble(e,cat(3,d{:}));
+
+      ori = orientation(e);
+    end
+    
   end
 end
