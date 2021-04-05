5a91b6ab112071ce17602bf1765ae1d7c37c7124
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-05 22:47:09 +0200

    SO3FunRBF/mean

---
+++ SO3Fun/@SO3FunRBF/mean.m
@@ -0,0 +1,26 @@
+function value = mean(SO3F, varargin)
+% calculates the mean value for an univariate SO3Fun or calculates the mean
+% along a specified dimension fo a multimodal SO3Fun
+%
+% Syntax
+%   value = mean(SO3F)
+%   SO3F  = mean(SO3F, d)
+%
+% Input
+%  SO3F - @SO3FunRBF
+%  d - dimension to take the mean value over
+%
+% Output
+%  SO3F  - @SO3FunRBF
+%  value - double
+%
+% Description
+%
+% If sF is a 3x3 SO3Fun then |mean(sF)| returns a 3x3 matrix with the mean
+% values of each function mean(sF, 1) returns a 1x3 S2Fun wich contains the
+% pointwise means values along the first dimension
+%
+ 
+value = sum(SO3F.weights,varargin{:}) * SO3F.psi.A(1);
+
+end
