363977d7d57f8b832a1c2e76e7ab9efe6c1ce6b9
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-05 22:47:45 +0200

    generic volume

--- SO3Fun/@SO3Fun/volume.m
+++ SO3Fun/@SO3Fun/volume.m
@@ -1,4 +1,4 @@
-function v = volume(odf,center,radius,varargin)
+function v = volume(SO3F,center,radius,varargin)
 % ratio of orientations with a certain orientation
 %
 % Description
@@ -23,24 +23,32 @@ function v = volume(odf,center,radius,varargin)
 % ODF/fibreVolume ODF/entropy ODF/textureindex
 
 if isa(center,'fibre')
-  v = fibreVolume(odf,center.h,center.r,radius,varargin{:});
-  return
-end
-
-
-% check input
-argin_check(center,'quaternion');
-argin_check(radius,'double');
-
-v = 0;
-S3G = [];
-
-% cycle through components
-for i = 1:length(odf.components)
   
-  [iv,S3G] = volume(odf.components{i},center,radius,S3G,varargin{:});
-  v = v + odf.weights(i) * iv;
+  v = fibreVolume(SO3F,center.h,center.r,radius,varargin{:});  
   
+else
+  
+  % get resolution
+  res = get_option(varargin,'RESOLUTION',min(1.25*degree,radius/30),'double');
+
+  % discretisation
+  if nargin > 3 && isa(varargin{1},'orientation')
+    S3G = varargin{1};
+  else
+    S3G = equispacedSO3Grid(SO3F.CS,SO3F.SS,...
+      'maxAngle',radius,'center',center,'resolution',res,varargin{:});
+  end
+
+  % estimate volume portion of odf space
+  reference = 9897129 * 96 / numProper(SO3F.CS) / numProper(SO3F.SS);
+  f = min(1,length(S3G) * (res / 0.25 / degree)^3 / reference);
+  
+  % eval odf
+  if f == 0
+    v = 0;
+  else
+    v = mean(eval(SO3F,S3G)) * f;   
+  end 
 end
 
 end
