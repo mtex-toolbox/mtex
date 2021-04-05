ec64c1f9b631dfd157900ec6a479e0a048fe8497
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-05 22:50:57 +0200

    more nice output

--- SO3Fun/@SO3FunHarmonic/display.m
+++ SO3Fun/@SO3FunHarmonic/display.m
@@ -1,9 +1,13 @@
 function display(SO3F,varargin)
 % standard output
 
-displayClass(SO3F,inputname(1),varargin{:});
+if check_option(varargin,'skipHeader')
+  disp('  <strong>unimodal component</strong>');
+else
+  displayClass(SO3F,inputname(1),[],'moreInfo',symChar(SO3F),varargin{:});
+end
 
 if length(SO3F) > 1, disp([' size: ' size2str(SO3F)]); end
-disp([' bandwidth: ' num2str(SO3F.bandwidth)]);
+disp(['  bandwidth: ' num2str(SO3F.bandwidth)]);
 
 end
--- SO3Fun/@SO3FunRBF/display.m
+++ SO3Fun/@SO3FunRBF/display.m
@@ -4,11 +4,16 @@ function display(SO3F,varargin)
 if check_option(varargin,'skipHeader')
   disp('  <strong>unimodal component</strong>');
 else
-  displayClass(SO3F,inputname(1),varargin{:});
+  displayClass(SO3F,inputname(1),[],'moreInfo',symChar(SO3F),varargin{:});
 end
 
 disp(['  kernel: ',char(SO3F.psi)]);
-disp(['  center: ',char(SO3F.center)]);
+if isa(SO3F.center,'SO3Grid')
+  disp(['  center: ',char(SO3F.center)]);
+else
+  disp(['  center: ',num2str(length(SO3F.center)) ' orientations']);
+  Euler(SO3F.center)
+end
 if SO3F.c0 > 0, disp(['      c0: ',xnum2str(SO3F.c0)]); end
 
 end
