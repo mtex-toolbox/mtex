4885cf056ce184f12afa3d4b5dfe53dc0e3b6e90
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-06 09:49:56 +0200

    more nice output

--- SO3Fun/@SO3FunHarmonic/display.m
+++ SO3Fun/@SO3FunHarmonic/display.m
@@ -5,9 +5,10 @@ if check_option(varargin,'skipHeader')
   disp('  <strong>unimodal component</strong>');
 else
   displayClass(SO3F,inputname(1),[],'moreInfo',symChar(SO3F),varargin{:});
+  if SO3F.antipodal, disp('  antipodal: true'); end
+  if length(SO3F) > 1, disp([' size: ' size2str(SO3F)]); end
 end
 
-if length(SO3F) > 1, disp([' size: ' size2str(SO3F)]); end
 disp(['  bandwidth: ' num2str(SO3F.bandwidth)]);
 
 end
--- SO3Fun/@SO3FunRBF/display.m
+++ SO3Fun/@SO3FunRBF/display.m
@@ -1,19 +1,39 @@
 function display(SO3F,varargin)
 % called by standard output
 
-if check_option(varargin,'skipHeader')
-  disp('  <strong>unimodal component</strong>');
-else
+if ~check_option(varargin,'skipHeader')
   displayClass(SO3F,inputname(1),[],'moreInfo',symChar(SO3F),varargin{:});
+  if SO3F.antipodal, disp('  antipodal: true'); end
+  disp(' ');
 end
 
-disp(['  kernel: ',char(SO3F.psi)]);
-if isa(SO3F.center,'SO3Grid')
-  disp(['  center: ',char(SO3F.center)]);
-else
-  disp(['  center: ',num2str(length(SO3F.center)) ' orientations']);
-  Euler(SO3F.center)
+if SO3F.c0 > 0
+  disp('  <strong>uniform component</strong>');
+  if SO3F.c0 > 0, disp(['  weight: ',xnum2str(SO3F.c0)]); end
+  
+  if ~isempty(SO3F.center)
+    disp(' ');
+  else
+    return
+  end
 end
-if SO3F.c0 > 0, disp(['      c0: ',xnum2str(SO3F.c0)]); end
+  
+if ~isempty(SO3F.center)
+  
+  if length(SO3F.center)==1
+    disp('  <strong>unimodal component</strong>');
+  else
+    disp('  <strong>multimodal components</strong>');
+  end
+  disp(['  kernel: ',char(SO3F.psi)]);
+  if isa(SO3F.center,'SO3Grid')
+    disp(['  center: ',char(SO3F.center)]);
+    disp(['  weight: ',xnum2str(sum(SO3F.weights(:)))]);
+  else
+    disp(['  center: ',num2str(length(SO3F.center)) ' orientations']);
+    s.weight = SO3F.weights; 
+    Euler(SO3F.center,s)
+  end
+end
 
 end
