ded144b12a58377c92deda2bda3562ec83af5688
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-11 09:32:12 +0200

    EBSD/findbyOrientation should also work with fibre

--- EBSDAnalysis/@EBSD/findByOrientation.m
+++ EBSDAnalysis/@EBSD/findByOrientation.m
@@ -4,9 +4,13 @@ function ebsd = findByOrientation(ebsd,ori,epsilon)
 % Syntax
 %   ebsd = findByOrientation(ebsd,ori,epsilon)
 %
+%   f = fibre(Miller(1,1,1,cs),vector3d.Z)
+%   ebsd = findByOrientation(ebsd,f,epsilon)
+%
 % Input
 %  ebsd    - @EBSD
 %  ori     - @orientation 
+%  f       - @fibre 
 %  epsilon - misorientation angle threshold
 %
 % Output
@@ -18,7 +22,7 @@ function ebsd = findByOrientation(ebsd,ori,epsilon)
 if nargin == 2, epsilon = 1*degree; end
 
 % restrict to the right phase
-if isa(ori,'orientation')
+if isa(ori,'orientation') || isa(ori,'fibre')
   phaseId = cellfun(@(cs) isa(cs,'crystalSymmetry') & ori.CS == cs, ebsd.CSList);
   ebsd = subSet(ebsd,ismember(ebsd.phaseId,find(phaseId)));
 end
