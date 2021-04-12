a10d8b76db58baf24fd6932f59a01b5cbd188c5b
Erik Wünsche <erik.wuensche@s2015.tu-chemnitz.de>
2021-04-12 14:51:32 +0200

    move compute-ghat files to private folder in SO3FunHarmonic

--- SO3Fun/@SO3FunHarmonic/private/compute_ghat_matlab.m
+++ SO3Fun/@SO3FunHarmonic/private/compute_ghat_matlab.m
@@ -1,10 +1,11 @@
-function ghat = calghat(N,fhat,varargin)
- 
+function ghat = compute_ghat_matlab(N,fhat,varargin)
+% This function contains the matlab code which is used to compute the
+% fourier coefficients ghat in eval2 and evalfft by a faster C++ program.
 
 halfsize = get_flag(varargin,'isReal','notReal');
 even = get_flag(varargin,'makeeven','notmake');
 
-% (2N+2,2N+2,2N+2)
+% (2N+2,2N+2,2N+2)     -->     if ~SO3F.isReal in eval2
 if strcmpi(even,'makeeven') && ~strcmpi(halfsize,'isReal')
   
   ghat = zeros(2*N+2,2*N+2,2*N+2);
@@ -14,18 +15,19 @@ if strcmpi(even,'makeeven') && ~strcmpi(halfsize,'isReal')
     Fhat = reshape(fhat(deg2dim(n)+1:deg2dim(n+1)),2*n+1,2*n+1);
   
     d = Wigner_D(n,pi/2); d = d(:,1:n+1);
-%    
-%       if n==0
-%         dlmin2 = 1;
-%         d = dlmin2;
-%       elseif n==1
-%         dlmin1 = Wigner_D(1,pi/2); dlmin1 = dlmin1(:,1:2);
-%         d = dlmin1;
-%       else
-%         d =  Wigner_d_recursion(dlmin1,dlmin2,pi/2,'half'); 
-%         dlmin2 = dlmin1;
-%         dlmin1 =d;
-%       end
+
+%     % probabply use recursions formula for Wigner-d matrices
+%     if n==0
+%       dlmin2 = 1;
+%       d = dlmin2;
+%     elseif n==1
+%       dlmin1 = Wigner_D(1,pi/2); dlmin1 = dlmin1(:,1:2);
+%       d = dlmin1;
+%     else
+%       d =  Wigner_d_recursion(dlmin1,dlmin2,pi/2,'half');
+%       dlmin2 = dlmin1;
+%       dlmin1 =d;
+%     end
 
   
     D = permute(d,[1,3,2]) .* permute(d,[3,1,2]).* Fhat;
@@ -41,7 +43,7 @@ if strcmpi(even,'makeeven') && ~strcmpi(halfsize,'isReal')
   
 end
 
-% (2N+2,N+1+ind,2N+2)
+% (2N+2,N+1+ind,2N+2)     -->     if SO3F.isReal in eval2
 if strcmpi(even,'makeeven') && strcmpi(halfsize,'isReal')
   
   ind = mod(N+1,2);
@@ -53,6 +55,20 @@ if strcmpi(even,'makeeven') && strcmpi(halfsize,'isReal')
     Fhat = reshape(fhat(deg2dim(n)+1+n*(2*n+1):deg2dim(n+1)),2*n+1,n+1);
 
     d = Wigner_D(n,pi/2); d = d(:,1:n+1);
+    
+%     % probabply use recursions formula for Wigner-d matrices
+%     if n==0
+%       dlmin2 = 1;
+%       d = dlmin2;
+%     elseif n==1
+%       dlmin1 = Wigner_D(1,pi/2); dlmin1 = dlmin1(:,1:2);
+%       d = dlmin1;
+%     else
+%       d =  Wigner_d_recursion(dlmin1,dlmin2,pi/2,'half');
+%       dlmin2 = dlmin1;
+%       dlmin1 =d;
+%     end
+    
     D = permute(d,[1,3,2]) .* permute(d(n+1:end,:),[3,1,2]) .* Fhat;
 
     ghat(N+2+(-n:n),ind+(1:n+1),N+2+(-n:0)) = ...
@@ -68,7 +84,7 @@ if strcmpi(even,'makeeven') && strcmpi(halfsize,'isReal')
   
 end
 
-%(2N+1,2N+1,2N+1)
+%(2N+1,2N+1,2N+1)     -->     if ~SO3F.isReal in evalfft
 if ~strcmpi(even,'makeeven') && ~strcmpi(halfsize,'isReal')
 
   ghat = zeros(2*N+1,2*N+1,2*N+1);
@@ -77,7 +93,21 @@ if ~strcmpi(even,'makeeven') && ~strcmpi(halfsize,'isReal')
 
     Fhat = reshape(fhat(deg2dim(n)+1:deg2dim(n+1)),2*n+1,2*n+1);
 
-    d = Wigner_D(n,pi/2);  d = d(:,1:n+1);
+    d = Wigner_D(n,pi/2); d = d(:,1:n+1);
+    
+%     % probabply use recursions formula for Wigner-d matrices
+%     if n==0
+%       dlmin2 = 1;
+%       d = dlmin2;
+%     elseif n==1
+%       dlmin1 = Wigner_D(1,pi/2); dlmin1 = dlmin1(:,1:2);
+%       d = dlmin1;
+%     else
+%       d =  Wigner_d_recursion(dlmin1,dlmin2,pi/2,'half');
+%       dlmin2 = dlmin1;
+%       dlmin1 =d;
+%     end
+    
     D = permute(d,[1,3,2]) .* permute(d,[3,1,2]) .* Fhat;
 
     ghat(N+1+(-n:n),N+1+(-n:n),N+1+(-n:0)) = ...
@@ -92,7 +122,7 @@ end
 
 
 
-%(2N+1,N+1,2N+1)
+%(2N+1,N+1,2N+1)     -->     if SO3F.isReal in evalfft
 if ~strcmpi(even,'makeeven') && strcmpi(halfsize,'isReal')
   
   ghat = zeros(2*N+1,N+1,2*N+1);
@@ -102,6 +132,20 @@ if ~strcmpi(even,'makeeven') && strcmpi(halfsize,'isReal')
     Fhat = reshape(fhat(deg2dim(n)+1+n*(2*n+1):deg2dim(n+1)),2*n+1,n+1);
 
     d = Wigner_D(n,pi/2); d = d(:,1:n+1);
+    
+%     % probabply use recursions formula for Wigner-d matrices
+%     if n==0
+%       dlmin2 = 1;
+%       d = dlmin2;
+%     elseif n==1
+%       dlmin1 = Wigner_D(1,pi/2); dlmin1 = dlmin1(:,1:2);
+%       d = dlmin1;
+%     else
+%       d =  Wigner_d_recursion(dlmin1,dlmin2,pi/2,'half');
+%       dlmin2 = dlmin1;
+%       dlmin1 =d;
+%     end
+    
     D = permute(d,[1,3,2]) .* permute(d(n+1:end,:),[3,1,2]) .* Fhat;
 
     ghat(N+1+(-n:n),1:n+1,N+1+(-n:0)) =ghat(N+1+(-n:n),1:n+1,N+1+(-n:0))+D;
