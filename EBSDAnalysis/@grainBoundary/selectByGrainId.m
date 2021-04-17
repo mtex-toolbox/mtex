93b920b929d2bfb32dbeab6c47e071b5d95e315a
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-04-18 00:34:26 +0200

    grainBoundary.selectByGrainId guaranties the order specified by grainId

--- EBSDAnalysis/@grainBoundary/selectByGrainId.m
+++ EBSDAnalysis/@grainBoundary/selectByGrainId.m
@@ -14,11 +14,18 @@ function [gB, pairId, ind] = selectByGrainId(gB,grainId)
 %  ind    - gBSub = gB(ind)
 %
 
-% select by grainId
+% we sort both ids row-wise 
+gBFlip = diff(gB.grainId,[],2) < 0;
+idFlip = diff(grainId,[],2) < 0;
 gBgrainId = sort(gB.grainId,2);
 grainId = sort(grainId,2);
-  
+
+% find grainId in the boundary Ids 
 [ind, pairId] = ismember(gBgrainId,grainId,'rows');
   
+% remove boundary segments not in grainId
 gB = gB.subSet(ind);
 pairId = pairId(pairId>0);
+
+% flip to the order given by grainId
+gB = flip(gB,xor(gBFlip(ind),idFlip(pairId)));
