8e47f9f18cd1dcc2f22cf54c63de7051d57e23d1
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-06-29 06:08:16 +0200

    calcGrains('custom') should be called with option 'threshold'

--- EBSDAnalysis/@EBSD/private/gbc_custom.m
+++ EBSDAnalysis/@EBSD/private/gbc_custom.m
@@ -1,10 +1,17 @@
-function criterion = gbc_custom(ori,CS,Dl,Dr,delta,varargin)
+function criterion = gbc_custom(~,~,Dl,Dr,delta,varargin)
 % blue print for a self defined grain boundary criterion
-
 % check whether the two ebsd measurements are seperated by a grain boundary
+
+% get the custom property
 custom = get_option(varargin,'custom');
-delta = get_option(varargin,'delta',delta);
-if isa(custom,'vector3d')
+
+% get the threshold
+delta = get_option(varargin,{'delta','threshold'}, delta);
+
+% default threshold
+if length(delta)>1, delta = 0.5; end
+
+if isa(custom,'vector3d') || isa(custom,'quaternion') 
    criterion = angle(custom(Dl),custom(Dr)) < delta;
 else
    criterion = abs(custom(Dl)-custom(Dr)) < delta;
