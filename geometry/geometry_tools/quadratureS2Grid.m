38ed9eb2295c959cf2c4f9a441c2bb1f8f639d4d
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-07-09 07:28:43 +0200

    Revert "Add files via upload"
    This reverts commit 8c2cf5f9191f3542b242ee23a30ce0ce69da83d0.

--- geometry/geometry_tools/quadratureS2Grid.m
+++ geometry/geometry_tools/quadratureS2Grid.m
@@ -9,67 +9,42 @@ function [S2G, W, M2] = quadratureS2Grid(bandwidth, varargin)
 persistent S2G_p;
 persistent W_p;
 persistent M2_p;
-persistent N_p;
 
-
-
-
-if check_option(varargin, 'gauss') || check_option(varargin, 'chebyshev')
-  if check_option(varargin, 'gauss')
-    load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_gauss.mat'),'gridIndex');
-  elseif check_option(varargin, 'chebyshev')
-    load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_chebyshev.mat'),'gridIndex');
-  end
-  index = find(gridIndex.bandwidth >= bandwidth, 1);
-  if isempty(index)
-    index = size(gridIndex,1);
-    warning('M is too large, instead we are giving you the largest quadrature grid we got.');
-  end
-  M2 = gridIndex.bandwidth(index);
-  N = gridIndex.N(index);
+if check_option(varargin, 'gauss')
+  load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_gauss.mat'),'gridIndex');
 else
-  M2 = bandwidth+~iseven(bandwidth); % make the bandwidth multible of four
-  N = (2*bandwidth+2)*(bandwidth+1);
+  load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_chebyshev.mat'),'gridIndex');
+end
+index = find(gridIndex.bandwidth >= bandwidth, 1);
+if isempty(index)
+  index = size(gridIndex,1);
+  warning('M is too large, instead we are giving you the largest quadrature grid we got.');
 end
 
-if ~isempty(M2_p) && ~isempty(N_p) && M2_p == M2 && N_p == N
+M2 = gridIndex.bandwidth(index);
+
+if ~isempty(M2_p) && M2_p == M2
   S2G = S2G_p;
   W = W_p;
   M2 = M2_p;
   
 else
-  if check_option(varargin, 'gauss') || check_option(varargin, 'chebyshev')
-    name = cell2mat(gridIndex.name(index));
-    if check_option(varargin, 'gauss')
-      data = load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_gauss.mat'),name);
-    else
-      data = load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_chebyshev.mat'),name);
-    end
-    
-    data = data.(name);
-    S2G = vector3d.byPolar(data(:, 1), data(:, 2));
-
-    if check_option(varargin, 'gauss')
-      W = data(:,3);
-    else
-      W = 4*pi/size(data, 1) .* ones(size(S2G));
-    end  
+  name = cell2mat(gridIndex.name(index));
+  if check_option(varargin, 'gauss')
+    data = load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_gauss.mat'),name);
   else
-    bandwidth = bandwidth/2;
-    ph=(-bandwidth-1:bandwidth)/(2*bandwidth+2)*2*pi;
-    th=(0:bandwidth)/(bandwidth+1)*pi;
-    [ph,th]=meshgrid(ph, th);
-    S2G = vector3d.byPolar(th, ph);
-    S2G = S2G(:);
-    S2G.addOption('using_fsft');
-    % one could implement this by using the dct
-    v = 4./(1-4*(0:bandwidth/2).^2)';
-    v([1 end]) = v([1 end])/2;
-    W = 1/bandwidth*cos(2*(0:bandwidth)'*(0:bandwidth/2)*pi/bandwidth)*v;
-    W([1 end]) = W([1 end])/2;
-    W = 2*pi*W*ones(1, 2*bandwidth+2)/(2*bandwidth+2);
-    W = W(:);
+    data = load(fullfile(mtexDataPath,'vector3d','quadratureS2Grid_chebyshev.mat'),name);
   end
+
+  data = data.(name);
+  S2G = vector3d.byPolar(data(:, 1), data(:, 2));
+  
+  if check_option(varargin, 'gauss')
+    W = data(:,3);
+  else
+    W = 4*pi/size(data, 1) .* ones(size(S2G));
+  end  
+  
   
   % store the data
   S2G_p = S2G;
