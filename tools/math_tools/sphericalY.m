54857840e4f87fa4ab5ddaeb1135476a478993e7
Ralf Hielscher <Ralf.Hielscher@gmail.com>
2021-07-23 16:32:04 +0200

    sphericalY now working with NFFT option

--- tools/math_tools/sphericalY.m
+++ tools/math_tools/sphericalY.m
@@ -1,4 +1,4 @@
-function Y = sphericalY(l, v, varargin)
+function Y = sphericalY(n, v, varargin)
 % spherical harmonics of degree l
 %
 % Description
@@ -21,40 +21,45 @@ function Y = sphericalY(l, v, varargin)
 [theta,rho] = polar(v);
 
 % try NFSFT version
-if check_option(varargin,'nfsft') && l > 0
+if check_option(varargin,'nfsft') && n > 0
   
   % precomputation for nfsft
-  nfsft_precompute(l,1000);
-  plan = nfsft_init_advanced(l,1,NFSFT_NORMALIZED);
+  nfsftmex('precompute',n,1000,1,0);
+  plan = nfsftmex('init_advanced',n,length(v),1);
 
-  nfsft_set_x(plan,[rho;theta]);
+  nfsftmex('set_x',plan,[rho(:).';theta(:).']);
   
   % node-dependent precomputation
-  nfsft_precompute_x(plan);
+  nfsftmex('precompute_x',plan);
   
-  % Set Fourier coefficients.
-  nfsft_set_f(plan,1);
+  Y = zeros(length(v),2*n+1);
+  for k = -n:n
 
-  % transform
-  nfsft_adjoint(plan);
+    % set Fourier coefficients.
+    fhat = zeros((n+1)^2,1);
+    fhat(n^2+n+1+k) = 1;
+    nfsftmex('set_f_hat_linear',plan,fhat);
+
+    % transform
+    nfsftmex('trafo',plan);
   
-  % store results
-  Y = nfsft_get_f_hat_linear(plan);
-  Y = Y((end-2*l):end)';
-  
-  nfsft_finalize(plan);
+    % store results
+    Y(:,k+n+1) = nfsftmex('get_f',plan);
+    %Y = Y((end-2*n):end)';
+  end
+  nfsftmex('finalize',plan);
   
   return
 end
 
 % calculate assoziated legendre functions
-L = sqrt(2/(2*l+1))*reshape(legendre(l,cos(theta(:)),'norm').',numel(theta),l+1); % nodes x order
+L = sqrt(2/(2*n+1))*reshape(legendre(n,cos(theta(:)),'norm').',numel(theta),n+1); % nodes x order
 
 % expand to negative order
-if l>0
+if n>0
   L = [fliplr(L(:,2:end)),L];
 end
 
 % calcualte spherical harmonics
-Y = sqrt((2*l+1)/4/pi) .* L .* exp(1i*rho(:) * (-l:l));
+Y = sqrt((2*n+1)/4/pi) .* L .* exp(1i*rho(:) * (-n:n));
 
