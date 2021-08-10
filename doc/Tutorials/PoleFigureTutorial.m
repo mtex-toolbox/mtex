d679163dbd9765792698922ba30f975f0d783b4c
jhiscocks <j.hiscocks@queensu.ca>
2021-08-09 20:59:58 -0400

    Update PoleFigureTutorial.m
    Added some more details esp. for sampleSymmetry (please check wording, not familiar with this function).

--- doc/Tutorials/PoleFigureTutorial.m
+++ doc/Tutorials/PoleFigureTutorial.m
@@ -1,10 +1,10 @@
 %% Pole Figure Tutorial
 %
 %%
-% This tutorial explains the basic concepts on ananyzing x-ray, synchrotron
-% and neutron diffraction data.
+% This tutorial explains the basic concepts for ananyzing x-ray, synchrotron
+% and neutron diffraction pole figure data.
 %
-%% Import diffraction data
+%% Import pole figure diffraction data
 % Click on <matlab:import_wizard('PoleFigure') Import pole figure data> to
 % start the import wizard which is a GUI leading you through the import of
 % pole figure data. After finishing the wizard you will end up with a
@@ -16,10 +16,10 @@
 %
 % *Specify Crystal and Specimen Symmetries*
 
-% crystal symmetry
+% crystal symmetry for this ZnCuTi data is hexagonal.  Here we define the crystallographic unit cell and how it relates to cartesian xyz axes.
 CS = crystalSymmetry('6/mmm', [2.633 2.633 4.8], 'X||a*', 'Y||b', 'Z||c');
 
-% specimen symmetry
+% specimen symmetry tells MTEX if a certain symmetry should be present in the plotted pole figures.  The command used here selects triclinic, the most flexible option.
 SS = specimenSymmetry('1');
 
 % plotting convention
@@ -29,10 +29,10 @@ setMTEXpref('zAxisDirection','outOfPlane');
 %%
 % *Specify File Names*
 
-% path to files
+% path to files downloaded with the MTEX package
 pname = [mtexDataPath filesep 'PoleFigure' filesep 'ZnCuTi' filesep];
 
-% which files to be imported
+% which pole figure files are to be imported
 fname = {...
   [pname 'ZnCuTi_Wal_50_5x5_PF_002_R.UXD'],...
   [pname 'ZnCuTi_Wal_50_5x5_PF_100_R.UXD'],...
@@ -40,7 +40,7 @@ fname = {...
   [pname 'ZnCuTi_Wal_50_5x5_PF_102_R.UXD'],...
   };
 
-% defocusing
+% defocusing correction to compensate for the equipment-dependent loss of intensity at certain angles.
 fname_def = {...
   [pname 'ZnCuTi_defocusing_PF_002_R.UXD'],...
   [pname 'ZnCuTi_defocusing_PF_100_R.UXD'],...
@@ -51,6 +51,7 @@ fname_def = {...
 %%
 % *Specify Miller Indices*
 
+% These correspond to the files loaded, in order.
 h = { ...
   Miller(0,0,2,CS),...
   Miller(1,0,0,CS),...
@@ -64,10 +65,10 @@ h = { ...
 % create a Pole Figure variable containing the data
 pf = PoleFigure.load(fname,h,CS,SS,'interface','uxd');
 
-% defocusing
+% create a defocusing pole figure variable
 pf_def = PoleFigure.load(fname_def,h,CS,SS,'interface','uxd');
 
-% correct data
+% correct data by applying the defocusing compensation
 pf = correct(pf,'def',pf_def);
 
 %%
@@ -78,7 +79,7 @@ pf = correct(pf,'def',pf_def);
 plot(pf)
 
 %%
-% By default pole figures are plotted as intensity colored dots for any
+% By default pole figures are plotted as intensity-colored dots for every
 % data point. There are many options to specify the way pole figures are
 % plotted in MTEX. Have a look at the <PoleFigurePlot.html plotting
 % section> for more information.
@@ -92,21 +93,20 @@ plot(pf)
 %
 % * rotating pole figures
 % * scaling pole figures
-% * find outliers
-% * remove specific measurements
-% * superpose pole figures
+% * finding outliers
+% * removing specific measurements
+% * superposing pole figures
 %
 % As an example we set all negative intensities to zero
 
 pf(pf.intensities<0) = 0;
 plot(pf)
 
-%
 %% ODF Estimation
 %
-% Once your data are in a good shape, i.e. defocusing correction has been
-% done and only few outliers are left you can reconstruct an ODF out of
-% these data. This is done by the command <PoleFigure.calcODF.html
+% Once your data is in good shape, i.e. defocusing correction has been
+% done and few outliers are left you can reconstruct an ODF out of
+% this data. This is done by the command <PoleFigure.calcODF.html
 % calcODF>.
 
 odf = calcODF(pf,'silent')
@@ -114,25 +114,25 @@ odf = calcODF(pf,'silent')
 %%
 % Note that reconstructing an ODF from pole figure data is a severely ill-
 % posed problem, i.e., it does *not* provide a unique solution. A more
-% throughout discussion on the ambiguity of ODF reconstruction from
+% through discussion on the ambiguity of ODF reconstruction from
 % pole figure data can be found <PoleFigure2ODFAmbiguity.html here>. As a
-% rule of thumb: as more pole figures you have and as more consistent you
-% pole figure data are the better your reconstructed ODF will be.
+% rule of thumb: the more pole figures you have and the more consistent your
+% pole figure data the better your reconstructed ODF will be.
 %
 % To check how well your reconstructed ODF fits the measured pole figure
-% data do
+% data use
 
-plotPDF(odf,pf.h)
+figure;plotPDF(odf,pf.h)
 
 %%
 % Compare the recalculated pole figures with the measured data. 
-% A quantitative measure for the fitting are the so called RP values. They
-% can be computed by
+% A quantitative measure for the fitting is the so called RP value. They
+% can be computed for each imported pole figure with 
 
 calcError(odf,pf)
 
 %%
-% In the case of a bad fitting, you may want to tweak the reconstruction
+% In the case of a bad fit, you may want to tweak the reconstruction
 % algorithm. See <PoleFigure2ODF.html here> for more information.
 
 %% Visualize the ODF
@@ -140,7 +140,3 @@ calcError(odf,pf)
 
 plot(odf)
 mtexColorMap LaboTeX
-
-%%
-% restore old setting
-setMTEXpref('xAxisDirection','east');
