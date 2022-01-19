d2180b1ab635974cc8d196a0cb052b6a00711c1e
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-01-19 17:25:19 +0100

    Evaluate multidimensional SO3FunHarmonics
    The Code is restructured to allow a loop over the size of the SO3FunHarmonic

--- SO3Fun/@SO3FunHarmonic/eval2.m
+++ SO3Fun/@SO3FunHarmonic/eval2.m
@@ -1,32 +1,19 @@
 function f = eval2(SO3F,rot,varargin)
 
-N = SO3F.bandwidth;
+persistent keepPlan;
 
-% If SO3F is real valued we have the symmetry properties (*) and (**) for 
-% the Fourier coefficients. We will use this to speed up computation.
-if SO3F.isReal
-
-  %ind = mod(N+1,2);
-  % create ghat -> k x l x j
-  %   k = -N+1:N
-  %   l =    0:N+ind    -> use ghat(-k,-l,-j)=conj(ghat(k,l,j))        (*)
-  %   j = -N+1:N        -> use ghat(k,l,-j)=(-1)^(k+l)*ghat(k,l,j)     (**)
-  % we need to make it 2N+2 as the index set of the NFFT is -(N+1) ... N
-  % we use ind in 2nd dimension to get even number of fourier coefficients
-  % the additionally indices gives 0-columns in front of ghat
-  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^0+2^1+2^2); %'isReal','makeeven','normalize_fouriercoeffis'
-  
-else
-
-  % create ghat -> k x l x j
-  % we need to make it 2N+2 as the index set of the NFFT is -(N+1) ... N
-  % we can again use (**) to speed up  
-  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^1+2^2);  %'makeeven','normalize_fouriercoeffis'
-  
+% kill plan
+if check_option(varargin,'killPlan')
+  nfftmex('finalize',keepPlan);
+  keepPlan = [];
+  return
 end
 
+if isempty(rot), f = []; return; end
 
-% nfft
+
+N = SO3F.bandwidth;
+
 sz = size(rot);
 rot = rot(:);
 M = length(rot);
@@ -37,35 +24,80 @@ abg = (abg + [-0.25;0;0.25]);
 abg = [abg(2,:);abg(1,:);abg(3,:)];
 abg = mod(abg,1);
 
-% initialize nfft plan
-%plan = nfftmex('init_3d',2*N+2,2*N+2,2*N+2,M);
-NN = 2*N+2;
-N2 = size(ghat,2);
-FN = ceil(1.5*NN);
-FN2 = ceil(1.5*N2);
-plan = nfftmex('init_guru',{3,NN,N2,NN,M,FN,FN2,FN,4,int8(0),int8(0)});
-
-
-% set rotations as nodes in plan
-nfftmex('set_x',plan,abg);
-
-% node-dependent precomputation
-nfftmex('precompute_psi',plan);
-
-% set Fourier coefficients
-nfftmex('set_f_hat',plan,ghat(:));
-
-% fast fourier transform
-nfftmex('trafo',plan);
-
-% get function values from plan
-if SO3F.isReal
-  % use (*) and shift summation in 2nd index
-  f = 2*real((exp(-2*pi*1i*ceil(N/2)*abg(2,:)')).*(nfftmex('get_f',plan)));
+% create plan
+if check_option(varargin,'keepPlan')
+  plan = keepPlan;
 else
-  f = nfftmex('get_f',plan);
+  plan = [];
 end
 
-f = reshape(f,sz);
+if isempty(plan)
+
+  % initialize nfft plan
+  %plan = nfftmex('init_3d',2*N+2,2*N+2,2*N+2,M);
+  NN = 2*N+2;
+  N2 = N+1+mod(N+1,2);
+  FN = ceil(1.5*NN);
+  FN2 = ceil(1.5*N2);
+  plan = nfftmex('init_guru',{3,NN,N2,NN,M,FN,FN2,FN,4,int8(0),int8(0)});
+
+  % set rotations as nodes in plan
+  nfftmex('set_x',plan,abg);
+
+  % node-dependent precomputation
+  nfftmex('precompute_psi',plan);
+
+end
+
+f = zeros([length(rot) size(SO3F)]);
+for k = 1:length(SO3F)
+
+  % If SO3F is real valued we have the symmetry properties (*) and (**) for 
+  % the Fourier coefficients. We will use this to speed up computation.
+  if SO3F.isReal
+
+    %ind = mod(N+1,2);
+    % create ghat -> k x l x j
+    %   k = -N+1:N
+    %   l =    0:N+ind    -> use ghat(-k,-l,-j)=conj(ghat(k,l,j))        (*)
+    %   j = -N+1:N        -> use ghat(k,l,-j)=(-1)^(k+l)*ghat(k,l,j)     (**)
+    % we need to make it 2N+2 as the index set of the NFFT is -(N+1) ... N
+    % we use ind in 2nd dimension to get even number of fourier coefficients
+    % the additionally indices gives 0-columns in front of ghat
+    ghat = representationbased_coefficient_transform(N,SO3F.fhat(:,k),2^0+2^1+2^2); %'isReal','makeeven','normalize_fouriercoeffis'
+  
+  else
+
+    % create ghat -> k x l x j
+    % we need to make it 2N+2 as the index set of the NFFT is -(N+1) ... N
+    % we can again use (**) to speed up  
+    ghat = representationbased_coefficient_transform(N,SO3F.fhat(:,k),2^1+2^2);  %'makeeven','normalize_fouriercoeffis'
+  
+  end
+
+  % set Fourier coefficients
+  nfftmex('set_f_hat',plan,ghat(:));
+
+  % fast fourier transform
+  nfftmex('trafo',plan);
+
+  % get function values from plan
+  if SO3F.isReal
+    % use (*) and shift summation in 2nd index
+    f(:,k) = 2*real((exp(-2*pi*1i*ceil(N/2)*abg(2,:)')).*(nfftmex('get_f',plan)));
+  else
+    f(:,k) = nfftmex('get_f',plan);
+  end
+end
+
+
+% kill plan
+if check_option(varargin,'keepPlan')
+  keepPlan = plan;
+else
+  nfftmex('finalize',plan);
+end
+
+if numel(SO3F) == 1, f = reshape(f,sz); end
 
 end
