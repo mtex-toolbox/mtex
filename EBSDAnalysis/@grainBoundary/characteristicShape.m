b7afe379dcd6a71ec52dbc57b74e7dfff20ad5cc
kilir <ruediger.kilian@protonmail.com>
2022-03-21 11:56:29 +0100

    Add shape2d, however fake grain2d still faulty

--- EBSDAnalysis/@grainBoundary/characteristicShape.m
+++ EBSDAnalysis/@grainBoundary/characteristicShape.m
@@ -9,7 +9,7 @@ function cShape = characteristicShape(gb)
 %  gb   -  @grainBoundary 
 %
 % Output
-%  cShape - characteristic shape
+%  cShape - @shape2d
 %
 
 % xy coordinates shifted to originate at 0
@@ -30,20 +30,6 @@ xyn = cumsum(dxy);
 % shift again
 xyn = [xyn(:,1) - mean(xyn(:,1)) xyn(:,2) - mean(xyn(:,2))];
 
-% output
-% cShape = [atan2(xyn(:,2),xyn(:,1))  ...
-%          sqrt(xyn(:,2).^2 + xyn(:,1).^2)];
-
-prop = struct('x',1,'y',1,'grainId',1);
-ebsd = EBSD(rotation.nan,1,{'Hallo'},prop);
-
-n = size(xyn,1);
-F = [(1:n).',[(2:n).';1]];
-
-I_DG = 1;
-I_FD = sparse(ones(size(F,1),1));
-A_Db = 1;
-
-cShape = grain2d(ebsd,xyn,F,I_DG,I_FD,A_Db);
+cShape = shape2d(xyn);
 
 end
