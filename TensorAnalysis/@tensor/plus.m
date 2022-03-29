ac817a9746ac3bdbf8028e6f57fdae53c4bdc3a6
ralfHielscher <ralf.hielscher@gmail.com>
2022-03-29 17:07:04 +0200

    sum of complianceTensor should forget about density if those are different see: #1099

--- TensorAnalysis/@tensor/plus.m
+++ TensorAnalysis/@tensor/plus.m
@@ -6,6 +6,18 @@ if ~isa(T1,'tensor'), [T1,T2] = deal(T1,T2); end
 T = T1;
 if isa(T2,'tensor')
   T.M = T.M + T2.M;
+
+  if isfield(T1.prop,'density') && isfield(T2.prop,'density') ...
+      && T1.prop.density ~= T2.prop.density
+    
+    warning(['Tensors with different density have been summed! I have ' ...
+      'removed the density information from the resulting tensor. ' ...
+      'You need to reset the density your own by \n\n%s'], ...
+      '  T.density = (fak1 * T1.density + fak2 * T2.density) / (fak1 + fak2).')
+    T.prop = rmfield(T.prop,'density');
+
+  end
+
 else
   
   if T1.rank == 4 && size(T2,1) == 6 && size(T2,2) == 6
