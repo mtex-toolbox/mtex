0bd18a62cc8fd31246bd037d97700bf2134cd779
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-08 16:30:31 +0200

    some fixes

--- SO3Fun/@SO3Fun/radon.m
+++ SO3Fun/@SO3Fun/radon.m
@@ -1,9 +1,10 @@
 function S2F = radon(SO3F,h,r,varargin)
-% Radon transform of the SO(3) function
+% Radon transform of a SO(3) function
 %
 % Syntax
 %   S2F = radon(SO3F,h)
 %   S2F = radon(SO3F,[],r)
+%   S2F = radon(SO3F,h,r)
 %
 % Input
 %  SO3F - @SO3Fun
@@ -13,10 +14,11 @@ function S2F = radon(SO3F,h,r,varargin)
 % Output
 %  S2F - @S2FunHarmonic
 %
-% See also
 
 % S2Fun in h or r?
-if nargin<3 || isempty(r)
+if nargin<3, r = []; end
+
+if isempty(r)
   isPF = true;
 elseif isempty(h)
   isPF = false;
@@ -28,30 +30,38 @@ res = get_option(varargin,'RadonResolution',2*degree);
 
 if isPF % pole figure
 
-  
-  [r,W] = quadratureS2Grid(64);
+  [S2G,W] = quadratureS2Grid(64);
      
   for k = 1:length(h)
 
     % define fibres ->  max(length(h),length(r)) x resolution
-    ori = fibre2quat(h(k),r,'resolution',res);
+    ori = fibre2quat(h(k),S2G,'resolution',res);
     
     % evaluate ODF at these fibre
-    f = eval(SO3F,ori);
+    f = SO3F.eval(ori);
 
     % take the integral over the fibres
     pdf = mean(reshape(f,size(ori)),2);
-    S2F(k) = S2FunHarmonicSym.quadrature(r,pdf,'bandwidth',48); %#ok<AGROW>
-       
+    S2F(k) = S2FunHarmonicSym.quadrature(S2G,pdf,'bandwidth',48,'weights',W,SO3F.SS); %#ok<AGROW>
+
   end
   
 else % inverse pole figure
 
+  [S2G,W] = quadratureS2Grid(64);
+
   for k = 1:length(r)
-    sr = symmetrise(r(k),SO3F.SS,'unique');
-    S2F(k) = S2FunHarmonicSym.quadrature(inv(SO3F.center)*sr,...
-      repmat(SO3F.weights(:),1,length(sr)),SO3F.CS,...
-      'symmetrise','bandwidth',SO3F.psi.bandwidth) ./ length(sr); %#ok<AGROW>
+
+    % define fibres ->  max(length(h),length(r)) x resolution
+    ori = inv(fibre2quat(r(k),S2G,'resolution',res));
+    
+    % evaluate ODF at these fibre
+    f = SO3F.eval(ori);
+
+    % take the integral over the fibres
+    pdf = mean(reshape(f,size(ori)),2);
+    S2F(k) = S2FunHarmonicSym.quadrature(S2G,pdf,'bandwidth',48,'weights',W,SO3F.CS); %#ok<AGROW>
+
   end
   
 end
@@ -62,3 +72,16 @@ if check_option(varargin,'antipodal') || SO3F.CS.isLaue || ...
     (nargin > 2 && ~isempty(r) && r.antipodal)
   S2F.antipodal = true;
 end
+
+
+% evaluate S2Fun if needed
+if  ~isempty(r) &&  ~isempty(h)
+  if length(h) >= length(r)
+    S2F = S2F.eval(h);
+  else
+    S2F = S2F.eval(r).';
+  end
+end
+
+
+
