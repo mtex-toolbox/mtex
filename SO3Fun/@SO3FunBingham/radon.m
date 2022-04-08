0775261ee2fd56bf2a57f8f42f3802b9f98648b5
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-08 16:34:19 +0200

    update

--- SO3Fun/@SO3FunBingham/radon.m
+++ SO3Fun/@SO3FunBingham/radon.m
@@ -21,50 +21,45 @@ function S2F = radon(SO3F,h,r,varargin)
 %
 % See also
 
+if nargin<3, r = []; end
 
-% S2Fun in h or r?
-if nargin<3 || isempty(r)
-  isPF = true;
-elseif isempty(h)
-  isPF = false;
-else
-  isPF = length(h) < length(r);
+if isempty(h)
+%   S2F = S2FunHandle(@(h) radon(SO3F,v,r,varargin{:}));
+  S2F = S2FunHarmonicSym.quadrature(@(v) radon(SO3F,v,r,varargin{:}),SO3F.CS);
+  return
+end
+if isempty(r)
+%  S2F = S2FunHandle(@(r) radon(SO3F,h,v,varargin{:}));
+  S2F = S2FunHarmonicSym.quadrature(@(v) radon(SO3F,h,v,varargin{:}),SO3F.SS);
+  return
 end
 
-if isPF
-  S2F = S2FunHandle(@(r) reshape(eval_local(h,r),size(r)));
-else
-  S2F = S2FunHandle(@(h) reshape(eval_local(h,r),size(h)));
-end
 
-  function f = eval_local(h,r)
+f = zeros(length(h),length(r));
 
-    f = zeros(size(h) .* size(r));
+% symmetrise h and r
+hSym = symmetrise(h,SO3F.CS,varargin{:});
+rSym = symmetrise(r,SO3F.SS);
 
-    % symmetrise h and r
-    hSym = symmetrise(h,SO3F.CS,varargin{:});
-    rSym = symmetrise(r,SO3F.SS);
-
-
-    for ih = 1:size(hSym,1)
-      for ir = 1:size(rSym,1)
+for ih = 1:size(hSym,1)
+  for ir = 1:size(rSym,1)
     
-        q1 = hr2quat(hSym(ih,:),rSym(ir,:));
-        q2 = q1 .* axis2quat(hSym(ih,:),pi);
+    q1 = hr2quat(hSym(ih,:),rSym(ir,:));
+    q2 = q1 .* axis2quat(hSym(ih,:),pi);
     
-        A1 = dot_outer(q1,SO3F.A,'noSymmetry');
-        A2 = dot_outer(q2,SO3F.A,'noSymmetry');
+    A1 = dot_outer(q1,SO3F.A,'noSymmetry');
+    A2 = dot_outer(q2,SO3F.A,'noSymmetry');
     
-        a = (A1.^2 +  A2.^2) * SO3F.kappa ./2;
-        b = (A1.^2 -  A2.^2) * SO3F.kappa ./2;
-        c = (A1 .*  A2) * SO3F.kappa;
+    a = (A1.^2 +  A2.^2) * SO3F.kappa ./2;
+    b = (A1.^2 -  A2.^2) * SO3F.kappa ./2;
+    c = (A1 .*  A2) * SO3F.kappa;
 
-        bc = sqrt(b.^2 + c.^2);
-        f = f + reshape(exp(a) ./ SO3F.C0 .* besseli(0,bc),size(f)) ./ size(hSym,1) ./ size(rSym,1);
+    bc = sqrt(b.^2 + c.^2);
+    f = f + reshape(exp(a) ./ SO3F.C0 .* besseli(0,bc),size(f)) ./ size(hSym,1) ./ size(rSym,1);
               
-      end
-    end
-    f = f * SO3F.weight;
   end
+end
+S2F = f * SO3F.weight  ;
+
 end
 
