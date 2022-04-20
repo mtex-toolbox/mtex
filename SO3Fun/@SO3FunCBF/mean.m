5bf8af94bd74d15a15b394b7e0ec3dd7e5b8e492
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-20 20:24:58 +0200

    mean along dimension is not needed
    SO3FunComposition, SO3FunCBF are allways of size 1x1.

--- SO3Fun/@SO3FunCBF/mean.m
+++ SO3Fun/@SO3FunCBF/mean.m
@@ -1,28 +1,16 @@
 function value = mean(SO3F, varargin)
-% Calculates the mean value of a fibre SO3Fun or calculates the mean along
-% a specified dimension for a vector-valued SO3Fun.
+% Calculates the mean value of a fibre SO3Fun
 %
 % Syntax
 %   value = mean(SO3F)
-%   SO3F  = mean(SO3F, d)
 %
 % Input
 %  SO3F - @SO3FunCBF
-%  d - dimension to take the mean value over
 %
 % Output
-%  SO3F  - @SO3FunCBF
 %  value - double
 %
-% Description
-%
-% If SO3F is a 3x3 SO3Fun then 
-% |mean(SO3F)| returns a 3x3 matrix with the mean values of each function 
-% |mean(SO3F,1)| returns a 1x3 SO3Fun wich contains the pointwise mean values along the first dimension
-%
-
-% TODO: mean along specific dimension
  
-value = sum(SO3F.weights,varargin{:}) * SO3F.psi.A(1);
+value = SO3F.weights * SO3F.psi.A(1);
 
 end
--- SO3Fun/@SO3FunComposition/mean.m
+++ SO3Fun/@SO3FunComposition/mean.m
@@ -1,28 +1,16 @@
 function value = mean(SO3F,varargin)
-% Calculates the mean value of a SO3FunComposition or calculates the mean 
-% along a specified dimension for a vector-valued SO3Fun.
+% Calculates the mean value of a SO3FunComposition
 %
 % Syntax
 %   value = mean(SO3F)
-%   SO3F  = mean(SO3F, d)
 %
 % Input
 %  SO3F - @SO3FunComposition
-%  d - dimension to take the mean value over
 %
 % Output
-%  SO3F  - @SO3Fun
 %  value - double
 %
-% Description
-%
-% If SO3F is a 3x3 SO3Fun then 
-% |mean(SO3F)| returns a 3x3 matrix with the mean values of each function 
-% |mean(SO3F,1)| returns a 1x3 SO3Fun wich contains the pointwise mean values along the first dimension
-%
 
-% TODO: mean along specific dimension
-
-value = sum(cellfun(@(x) mean(x,varargin{:}),SO3F.components,'UniformOutput',true));
+value = sum(cellfun(@(x) mean(x),SO3F.components,'UniformOutput',true));
 
 end
