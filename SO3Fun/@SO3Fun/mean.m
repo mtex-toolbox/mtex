02ef59a013cf418419e4f52ae47ac2808aec5c66
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-20 20:26:27 +0200

    allow mean along dimension

--- SO3Fun/@SO3Fun/mean.m
+++ SO3Fun/@SO3Fun/mean.m
@@ -20,10 +20,31 @@ function value = mean(SO3F, varargin)
 % |mean(SO3F)| returns a 3x3 matrix with the mean values of each function 
 % |mean(SO3F, 1)| returns a 1x3 SO3Fun which contains the pointwise mean values along the first dimension
 %
+% Example 
+%   %generate SO3Funs
+%   SO3F1 = SO3Fun.dubna
+%   SO3F2 = SO3FunHandle(@(rot) SO3F1.eval(rot))
+%   A = ones(2,3);
+%   SO3F3 = SO3F2.*A
+% 
+%   %calculate mean values of SO3Funs
+%   mean(SO3F2)
+%   mean(SO3F3)
+%   mean(SO3F3,2)
+%   mean(SO3F3,1)
+%
 
-% TODO: mean along specific dimension
 
-nodes = equispacedSO3Grid(SO3F.SRight,SO3F.SLeft,'resolution',2.5*degree,varargin{:});
-value = mean(SO3F.eval(nodes(:)),varargin{:});
+% mean along specific dimension
+if nargin>1 && isnumeric(varargin{1})
+  value = SO3FunHandle(@(rot) mean(SO3F.eval(rot),varargin{1}+1),SO3F.SRight,SO3F.SLeft);
+  return
+end
+
+% mean value of a SO3Fun
+res = get_option(varargin,'resolution',2.5*degree);
+nodes = equispacedSO3Grid(SO3F.SRight,SO3F.SLeft,'resolution',res);
+value = mean(SO3F.eval(nodes(:)));
+value = reshape(value,size(SO3F));
 
 end
