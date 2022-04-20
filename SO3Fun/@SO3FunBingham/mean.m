2d4d23f759684151a18b0a19a753f0aef835436c
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-20 20:28:18 +0200

    mean for SO3FunBingham

---
+++ SO3Fun/@SO3FunBingham/mean.m
@@ -0,0 +1,15 @@
+function m=mean(SO3F,varargin)
+% Calculates the mean value of a SO3FunBingham
+%
+% Syntax
+%   value = mean(SO3F)
+%
+% Input
+%  SO3F - @SO3FunBingham
+%
+% Output
+%  value - double
+
+m = SO3F.weight;
+
+end
