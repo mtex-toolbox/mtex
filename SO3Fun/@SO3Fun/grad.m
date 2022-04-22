57454a35bb3be98d8ad94f6aa84192e118ffb663
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-22 15:34:50 +0200

    writing correction

--- SO3Fun/@SO3Fun/grad.m
+++ SO3Fun/@SO3Fun/grad.m
@@ -18,7 +18,7 @@ function g = grad(SO3F,varargin)
 % See also
 %
 
-% maybe we should return a function hand
+% maybe we should return a function handle
 if nargin == 1 || ~isa(varargin{1},'rotation')  
   g = SO3VectorFieldHandle(@(rot) SO3F.grad(rot,varargin{:}),SO3F.CS,SO3F.SS);
   return
