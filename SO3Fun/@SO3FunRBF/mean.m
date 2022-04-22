566150f2c4ec3efe3b7b021ab497bbd6643bc487
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-22 12:52:56 +0200

    correct mean

--- SO3Fun/@SO3FunRBF/mean.m
+++ SO3Fun/@SO3FunRBF/mean.m
@@ -1,10 +1,8 @@
 function value = mean(SO3F, varargin)
-% calculates the mean value for an univariate SO3Fun or calculates the mean
-% along a specified dimension for a multimodal SO3Fun
+% calculates the mean value for an SO3FunRBF
 %
 % Syntax
 %   value = mean(SO3F)
-%   SO3F  = mean(SO3F, d)
 %
 % Input
 %  SO3F - @SO3FunRBF
@@ -13,8 +11,6 @@ function value = mean(SO3F, varargin)
 %  value - double
 %
 
-% TODO: mean along specific dimension
- 
-value = sum(SO3F.weights(:),varargin{:}) * SO3F.psi.A(1) + SO3F.c0;
+  value = sum(SO3F.weights(:)) * SO3F.psi.A(1) + SO3F.c0;
 
 end
