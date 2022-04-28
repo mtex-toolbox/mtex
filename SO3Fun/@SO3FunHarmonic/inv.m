82807aea6f1632ab6804fb6e8f53a16c5981bb67
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-28 13:45:26 +0200

    exchange symmetries

--- SO3Fun/@SO3FunHarmonic/inv.m
+++ SO3Fun/@SO3FunHarmonic/inv.m
@@ -12,10 +12,17 @@ function SO3F = inv(SO3F)
 %  SO3F - @SO3FunHarmonic
 %  
 
+% exchange Symmetries
+sym = SO3F.SRight;
+SO3F.SRight = SO3F.SLeft;
+SO3F.SLeft = sym;
+
+% stop in simple case
 if SO3F.bandwidth == 0 || SO3F.antipodal==true
   return
 end
 
+% exchange wigner coefficients
 ind = zeros(deg2dim(SO3F.bandwidth+1),1);
 for l = 0:SO3F.bandwidth
   localind = reshape(deg2dim(l+1):-1:deg2dim(l)+1,2*l+1,2*l+1)';
