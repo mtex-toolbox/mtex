e625b40802406fe3e110df7f43f1b2f96c6e03ef
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-04-28 13:39:38 +0200

    correction of compared symmetries in convolution

--- SO3Fun/@SO3Fun/ensureCompatibleSymmetries.m
+++ SO3Fun/@SO3Fun/ensureCompatibleSymmetries.m
@@ -24,17 +24,22 @@ function ensureCompatibleSymmetries(SO3F1,SO3F2,varargin)
 %       If we are changing this, then also update the code in
 %       SO3FunComposition
 
-if isnumeric(SO3F1) || isnumeric(SO3F2)
-  return
+if isa(SO3F2,'S2FunHarmonicSym')
+  % compare symmetries in case of convolution with S2Fun
+  if SO3F1.SRight ~= SO3F2.s
+    error('By convolution of a @SO3Fun with a @S2Fun the symmetries have to be compatible.')
+  end
+elseif check_option(varargin,'conv_Left')
+  % compare symmetries in case of left sided convolution
+  em = SO3F1.SRight ~= SO3F2.SLeft;
+else
+  % compare all symmetrys in case of +, -, *, /, cat, subsasgn
+  em = (SO3F1.SRight ~= SO3F2.SRight) || (SO3F1.SLeft ~= SO3F2.SLeft);
 end
 
-if check_option(varargin,'conv')
-  sym = SO3F1.SRight;
-  SO3F1.SRight = SO3F1.SLeft;
-  SO3F1.SLeft = sym;
-end
 
-if (SO3F1.SRight ~= SO3F2.SRight) || (SO3F1.SLeft ~= SO3F2.SLeft)
+% error message
+if em
   error('Calculations with @SO3Fun''s are not supported if the symmetries are not compatible.')
 end
 
