8c6b0f059c2d271691f6cfd5dddb9a74ef577847
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-06-10 15:17:59 +0200

    create uniformODF with variable weight

--- ODFAnalysis/standardODFs/uniformODF.m
+++ ODFAnalysis/standardODFs/uniformODF.m
@@ -1,8 +1,13 @@
 function odf = uniformODF(varargin)
 
+v = 1;
+if nargin>0 && isnumeric(varargin{1}) && length(varargin{1})==1
+  v = varargin{1};
+end
+
 % get crystal and specimen symmetry
 [CS,SS] = extractSym(varargin);
                       
-odf = SO3FunRBF(orientation(CS,SS),SO3DeLaValleePoussinKernel,[],1);
+odf = SO3FunRBF(orientation(CS,SS),SO3DeLaValleePoussinKernel,[],v);
 
 end
