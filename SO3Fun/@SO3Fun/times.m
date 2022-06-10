2e241d9385c78d1d07b77060c0a7631c5669d53f
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-06-10 11:37:04 +0200

    allow + and .* for SO3Fun and constant part of uniformODF

--- SO3Fun/@SO3Fun/times.m
+++ SO3Fun/@SO3Fun/times.m
@@ -14,6 +14,16 @@ function SO3F = times(SO3F1,SO3F2)
 %  SO3F - @SO3Fun
 %
 
+% uniform component .* SO3Fun
+if isa(SO3F1,'SO3FunRBF') && isempty(SO3F1.center) && isa(SO3F2,'SO3Fun') 
+  SO3F = SO3F1.c0 .* SO3F2;
+  return
+end
+if isa(SO3F2,'SO3FunRBF') && isempty(SO3F2.center) && isa(SO3F1,'SO3Fun') 
+  SO3F = SO3F1 .* SO3F2.c0;
+  return
+end
+
 if isnumeric(SO3F1)
   dim = length(size(SO3F1));
   SO3F = SO3FunHandle(@(rot) permute(SO3F1,[dim+1 1:dim]) .* SO3F2.eval(rot),SO3F2.CS,SO3F2.SS);
