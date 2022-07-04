7dc29b7fb9068a107066c84c1c675ce224d03261
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-07-04 16:22:11 +0200

    doc

--- doc/SO3Functions/SO3FunMultivariate.m
+++ doc/SO3Functions/SO3FunMultivariate.m
@@ -48,8 +48,16 @@ y = [SO3Fun.dubna(nodes), (nodes.a.*nodes.b).^(1/4)];
 nodes.CS = SO3Fun.dubna.CS;
 %%
 % Now the actual command to get a 2x1 |SO3F1| of type 
-% <SO3FunHarmonic.SO3FunHarmonic |SO3FunHarmonic|>
+% <SO3FunHarmonic.SO3FunHarmonic |SO3FunHarmonic|> is
 SO3F1 = SO3FunHarmonic.approximation(nodes, y)
+
+%%
+% It is also possible to interpolate one component by an
+% <SO3FunHarmonic.SO3FunRBF |SO3FunRBF|>, that means
+SO3F2 = SO3FunHarmonic.interpolate(nodes,y(:,1))
+
+%%
+% This is only possible for univariate functions.
 
 %%
 % *Definition via function handle*
@@ -61,9 +69,9 @@ SO3F1 = SO3FunHarmonic.approximation(nodes, y)
 
 f = @(rot) [exp(rot.a+rot.b+rot.c)+50*(rot.b-cos(pi/3)).^3.*(rot.b-cos(pi/3) > 0), rot.a, rot.b, rot.c];
 %% 
-% Now we call the quadrature command to get 4x1 |SO3F2| of type 
+% Now we call the quadrature command to get 4x1 |SO3F3| of type 
 % <SO3FunHarmonic.SO3FunHarmonic |SO3FunHarmonic|>
-SO3F2 = SO3FunHarmonic.quadrature(f, 'bandwidth', 50)
+SO3F3 = SO3FunHarmonic.quadrature(f, 'bandwidth', 50,SO3F1.CS)
 
 %%
 % *Definition via Fourier-coefficients*
@@ -71,10 +79,10 @@ SO3F2 = SO3FunHarmonic.quadrature(f, 'bandwidth', 50)
 % If we already know the Fourier-coefficients, we can simply hand them in 
 % the format above to the constructor of |SO3FunHarmonic|.
 
-SO3F3 = SO3FunHarmonic(eye(10))
+SO3F4 = SO3FunHarmonic(eye(10))
 
 %%
-% * This command stores the ten first <WignerFunctions.html Wigner-D functions> in |SO3F3|
+% * This command stores the ten first <WignerFunctions.html Wigner-D functions> in |SO3F4|
 
 
 %% Operations which differ from an univariate SO3FunHarmonic
@@ -84,8 +92,8 @@ SO3F3 = SO3FunHarmonic(eye(10))
 %
 % You can concatenate and refer to functions as Matlab does with vectors and matrices
 
-SO3F4 = [SO3F1; SO3F2];
-SO3F4(2:3);
+SO3F5 = [SO3F1; SO3F3];
+SO3F5(2:4)
 
 %%
 % You can conjugate the Fourier-coefficients and transpose/ctranspose the 
@@ -99,8 +107,8 @@ SO3F1'
 % Some other operations
 
 length(SO3F1)
-size(SO3F2)
-SO3F3 = reshape(SO3F3, 2, [])
+size(SO3F3)
+SO3F4 = reshape(SO3F4, 2, [])
 
 %%
 % *|sum| and |mean|*
@@ -111,7 +119,7 @@ SO3F3 = reshape(SO3F3, 2, [])
 % dimension of a multivariate |SO3FunHarmonic|.
 
 sum(SO3F1, 1)
-mean(SO3F3, 2)
+mean(SO3F4, 2)
 
 %%
 % *min/max*
@@ -119,36 +127,39 @@ mean(SO3F3, 2)
 % If the |min| or |max| command gets a multivariate |SO3FunHarmonic| the 
 % pointwise minimum or maximum is calculated along the first non-singelton 
 % dimension if not specified otherwise.
-%
-
-min(SO3F3)
+% 
+% Therefore the function has to be real valued
+SO3F4.isReal = 1;
+min(SO3F4)
 
 %%
 % *Remark on the matrix product*
 %
 % At this point the matrix product is implemented pointwise and not as 
 % the usual matrix product.
+SO3F1.CS=specimenSymmetry;
 
-SO3F1 .* SO3F3
+SO3F1 .* SO3F4
 
 %% Visualization of multivariate SO3FunHarmonic
 %
 % Similary to the univariate case we also can look at the Fourier
 % coefficients of multivariate functions.
 
-plotSpektra(SO3F2)
+plotSpektra(SO3F3)
 
 %%
-% The section plot and the 3d plot are performed only for the first component
+% The section plot and the 3d plot are performed only for the first
+% component of a multivariate function
 
-plot(SO3F2)
+plot(SO3F3)
 
 %%
 
-plot3d(SO3F2)
+plot3d(SO3F3)
 
 %%
 % while the plot along a specific fibre includes all components.
 
-plotFibre(SO3F2,fibre.beta)
+plotFibre(SO3F3,fibre.beta)
 
