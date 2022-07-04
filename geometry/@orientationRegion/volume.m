7e240943b0f5b40bed38f69225df37da9e6cb8a7
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-07-04 13:13:05 +0200

    get volume of orientationRegion

--- geometry/@orientationRegion/volume.m
+++ geometry/@orientationRegion/volume.m
@@ -2,6 +2,8 @@ function v = volume(oR,varargin)
 % volume of an orientation region
 %
 
-% TODO
+% TODO: Do this faster
 
+if nargin == 1, S3G = equispacedSO3Grid(oR.CS1,oR.CS2,'resolution',5*degree); end
 
+v = nnz(oR.checkInside(S3G))/length(S3G);
