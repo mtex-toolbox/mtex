b26f6c1477618f97278db38b2a93359f9b540d7e
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-09-07 17:27:59 +0200

    dont waste storage by saving big grids

--- geometry/geometry_tools/quadratureSO3Grid.m
+++ geometry/geometry_tools/quadratureSO3Grid.m
@@ -9,27 +9,34 @@ function [SO3G, W, M2] = quadratureSO3Grid(bandwidth, varargin)
 %
 
 
-persistent SO3G_p;
-persistent W_p;
-persistent M2_p;
+% persistent SO3G_p;
+% persistent W_p;
+% persistent M2_p;
 
 if check_option(varargin, 'gauss') || check_option(varargin, 'chebyshev')
-  warning('quadrature of types gauss and chebychev are not implemented yet.')
+  warning('Quadrature of types gauss and chebychev are not implemented yet.')
   return
 end
 
 
-if check_option(varargin, 'ClenshawCurtis')
-  M2 = bandwidth;                           % Bandbreite
-  N =  (2*bandwidth+2)^2*(2*bandwidth+1);   % Anzahl Knoten
-end
-
-if ~isempty(M2_p) && M2_p == M2 && length(SO3G_p) == N
-  SO3G = SO3G_p;
-  W = W_p;
-  M2 = M2_p;
-  
-else
+% if check_option(varargin, 'ClenshawCurtis')
+%   M2 = bandwidth;                           % bandwidth
+%   % grid has to be full sized
+%   N =  (2*bandwidth+2)^2*(2*bandwidth+1);   % number of nodes
+% end
+% 
+% if ~isempty(M2_p) && M2_p == M2 && length(SO3G_p) == N
+%   SO3G = SO3G_p;
+%   if check_option(varargin,'Euler') && isa(SO3G,'rotation')
+%     SO3G = Euler(SO3G);
+%   elseif ~check_option(varargin,'Euler') && isnumeric(SO3G)
+%     [CS,SS] = extractSym(varargin);
+%     SO3G = orientation.byEuler(SO3G(:,:,:,1),SO3G(:,:,:,2),SO3G(:,:,:,3),'nfft',CS,SS);
+%   end
+%   W = W_p;
+%   M2 = M2_p;
+%   
+% else
   if check_option(varargin,'ClenshawCurtis')
     SO3G = regularSO3Grid('ClenshawCurtis','bandwidth',bandwidth,varargin{:});
 
@@ -42,11 +49,11 @@ else
     % W =     1/sqrt(8*pi^2)    *      (2*pi/(2*N+2))^2     *              w_b^N
     W = 1/(2*(size(SO3G,1))^2)*repmat(w',size(SO3G,1),1,size(SO3G,3));
   end
-  % store the data
-  SO3G_p = SO3G;
-  W_p = W;
-  M2_p = M2;
-end
+%   % store the data
+%   SO3G_p = SO3G;
+%   W_p = W;
+%   M2_p = M2;
+% end
 
 end
 
