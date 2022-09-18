be21dbea8a88f79731b1bee6a6e5ec36dcb14a9f
ralfHielscher <ralf.hielscher@gmail.com>
2022-09-18 20:26:41 +0200

    speedup

--- tools/misc_tools/trimMat.m
+++ tools/misc_tools/trimMat.m
@@ -1,8 +1,11 @@
 function B = trimMat(A)
 
-cMin = find(any(A,1),1,"first");
-cMax = find(any(A,1),1,"last");
-rMin = find(any(A,2),1,"first");
-rMax = find(any(A,2),1,"last");
+c = any(A,1);
+cMin = find(c,1,"first");
+cMax = find(c,1,"last");
+
+r = any(A(:,cMin:cMax),2);
+rMin = find(r,1,"first");
+rMax = find(r,1,"last");
 
 B = A(rMin:rMax,cMin:cMax);
