54814899be0ced9e5871045b847bf9b99a08f16e
ralfHielscher <ralf.hielscher@gmail.com>
2022-09-24 12:17:00 +0200

    better display

--- S2Fun/@S2FunHarmonicSym/display.m
+++ S2Fun/@S2FunHarmonicSym/display.m
@@ -1,20 +1,11 @@
 function display(sFs,varargin)
 % standard output
 
-displayClass(sFs,inputname(1),varargin{:});
+displayClass(sFs,inputname(1),'S2FunHarmonicSym','moreInfo',char(sFs.CS,'compact'));
+
+%displayClass(sFs,inputname(1),varargin{:});
 
 if length(sFs)> 1, disp([' size: ' size2str(sFs)]);end
-
-% display symmetry
-if isa(sFs.s,'crystalSymmetry')
-  if isempty(sFs.CS.mineral)
-    disp([' symmetry: ',char(sFs.CS,'verbose')]);
-  else
-    disp([' mineral: ',char(sFs.CS,'verbose')]);
-  end
-else
-    disp([' symmetry: ',char(sFs.SS)]);
-end
 
 disp([' bandwidth: ' num2str(sFs.bandwidth)]);
 if sFs.antipodal, disp(' antipodal: true'); end
