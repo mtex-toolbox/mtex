22e6d147c3cbf9edb5e7594e658c14653ce4610a
ralfHielscher <ralf.hielscher@gmail.com>
2022-09-25 10:44:35 +0200

    better fibre to fibre angle

--- geometry/@fibre/angle.m
+++ geometry/@fibre/angle.m
@@ -1,14 +1,30 @@
 function omega = angle(f,ori,varargin)
 % angle fibre to orientation or fibre to fibre
 %
+% Syntax
+% 
+%   omega = angle(f,ori) % angle orientation to fibre 
+%   omega = angle(f1,f2) % angle fibre to fibre
 %
+% Input
+%  f, f1, f2 - @fibre
+%  ori - @orientation
+%
+% Output
+%  omega - double
+%
+% see also 
+% orientation/angle
 
 if isa(ori,'orientation')
   omega = angle(ori .\ f.r,f.h,varargin{:});
+
 else
-  %omega = max(min(angle_outer(orientation(f),orientation(ori))));
-  omega = min(angle(f.h,ori.h) + angle(f.r,ori.r), ...
-          angle(f.h,-ori.h) + angle(f.r,-ori.r));
+  omega = max(angle(f,orientation(ori),varargin{:}));
+
+  % in the non symmetric case we have also
+  %omega = min(angle(f.h,ori.h) + angle(f.r,ori.r), angle(f.h,-ori.h) + angle(f.r,-ori.r));
+
 end
 
 end
