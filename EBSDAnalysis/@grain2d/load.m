fcc7150ca84f622a12f7f4045f4a1254bebd4ea6
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-09-26 15:53:26 +0200

    correcting indent size

--- EBSDAnalysis/@grain2d/load.m
+++ EBSDAnalysis/@grain2d/load.m
@@ -1,298 +1,298 @@
 function [grains] = load(filepath)
-% load tesselation data from neper files
-%
-% Description
-%
-% grain2d.load is a method to load the data from the tesselation files that
-% <neper.info/ neper> outputs (only 2 dimesional tesselations!)
-%
-% Syntax
-%   grains = grain2d.load('filepath/filename.tess')
-%
-% Input
-%  fname     - filename
-%
-% Output
-%  grain2d - @grain2d
-%
-% Example
-%
-%   
-%   grains = grain2d.load(fname)
-%
-% See also
-% readTessFile
+  % load tesselation data from neper files
+  %
+  % Description
+  %
+  % grain2d.load is a method to load the data from the tesselation files that
+  % <neper.info/ neper> outputs (only 2 dimesional tesselations!)
+  %
+  % Syntax
+  %   grains = grain2d.load('filepath/filename.tess')
+  %
+  % Input
+  %  fname     - filename
+  %
+  % Output
+  %  grain2d - @grain2d
+  %
+  % Example
+  %
+  %   
+  %   grains = grain2d.load(fname)
+  %
+  % See also
+  % readTessFile
 
-%filepath="2dslice.tess";
+  %filepath="2dslice.tess";
 
-%%
+  %%
 
-[dimension,V,poly,oriMatrix,crysym] = readTessFile(filepath);
+  [dimension,V,poly,oriMatrix,crysym] = readTessFile(filepath);
 
-if (dimension~=2)
+  if (dimension~=2)
     error 'format is not 2 - only two dimesional tesselations allowed'
-end
+  end
 
-CSList={'notIndexed',crystalSymmetry(crysym)};
-rot=rotation(quaternion(oriMatrix'));
-phaseList=2*ones(size(poly));
+  CSList={'notIndexed',crystalSymmetry(crysym)};
+  rot=rotation(quaternion(oriMatrix'));
+  phaseList=2*ones(size(poly));
 
-grains = grain2d(V,poly,rot,CSList,phaseList);
+  grains = grain2d(V,poly,rot,CSList,phaseList);
 
-%% check for clockwise poly's
+  %% check for clockwise poly's
 
-isNeg = (grains.area<0);
-grains.poly = cellfun(@fliplr, grains.poly(isNeg), 'UniformOutput', false);
+  isNeg = (grains.area<0);
+  grains.poly = cellfun(@fliplr, grains.poly(isNeg), 'UniformOutput', false);
 
-grains = grain2d(V,poly,rot,CSList,phaseList);
+  grains = grain2d(V,poly,rot,CSList,phaseList);
 end
 
 function [dimension,V, poly,oriMatrix,crysym] = readTessFile(filepath)
-% function for reading data from nepers tesselation files (.tess)
-%
-% Description
-%
-% readTessFile is a helping function used from the grain2d.load method to
-% read the data from the tesselation files that
-% <neper.info/ neper> outputs (only 2 dimesional tesselations!)
-%
-% Syntax
-%   [V,poly,oriMatrix,crysym] = readTessFile('filepath/filename.tess')
-%
-% Input
-%  fname     - filepath
-%
-% Output
-%  V - matrix[total_number_of_vertices,2] of vertices(x,y)
-%  poly - cell array of the polyhedrons, clockwise or against the clock
-%  oriMatrix - matrix[total_number_of_cells,4] containing the quaternion values
-%  crysym - crysym
-%
-% Example
-%
-%   
-%   [V,poly,oriMatrix,crysym] = readTessFile('fname')
-%
-% See also
-% grain2d.load
-%
+  % function for reading data from nepers tesselation files (.tess)
+  %
+  % Description
+  %
+  % readTessFile is a helping function used from the grain2d.load method to
+  % read the data from the tesselation files that
+  % <neper.info/ neper> outputs (only 2 dimesional tesselations!)
+  %
+  % Syntax
+  %   [V,poly,oriMatrix,crysym] = readTessFile('filepath/filename.tess')
+  %
+  % Input
+  %  fname     - filepath
+  %
+  % Output
+  %  V - matrix[total_number_of_vertices,2] of vertices(x,y)
+  %  poly - cell array of the polyhedrons, clockwise or against the clock
+  %  oriMatrix - matrix[total_number_of_cells,4] containing the quaternion values
+  %  crysym - crysym
+  %
+  % Example
+  %
+  %   
+  %   [V,poly,oriMatrix,crysym] = readTessFile('fname')
+  %
+  % See also
+  % grain2d.load
+  %
 
-%{
-variables:
-    -filepath
+  %{
+  variables:
+      -filepath
         - string - file that was read
-    -format
+      -format
         - string
-    -dimension
+      -dimension
         - int - dimension of the tesselation
-    -type
+      -type
         - string
-    -total_number_of_cells
+      -total_number_of_cells
         - int
-    -cell_ids
+      -cell_ids
         - vector
-    -crysym
+      -crysym
         - see output, string 
-    -seedsTable('seed_id', 'seed_x', 'seed_y', 'seed_z', 'seed_weight')
+      -seedsTable('seed_id', 'seed_x', 'seed_y', 'seed_z', 'seed_weight')
         - table 
-    -quaternion_descriptor
+      -quaternion_descriptor
         - string (1x1 cell)
-    -oriMatrix
+      -oriMatrix
         - see output 
-    -verticesTable('ver_id','ver_x', 'ver_y','ver_z','ver_state')
+      -verticesTable('ver_id','ver_x', 'ver_y','ver_z','ver_state')
 
-    -edgesTable('egde_id', 'vertex_1', 'vertex_2', 'edged_state')
-
-    -total_number_of_cells
+      -edgesTable('egde_id', 'vertex_1', 'vertex_2', 'edged_state')
+  
+      -total_number_of_cells
         - int
-    -total_number_of_vertices
+      -total_number_of_vertices
         - int
-    -total_number_of_edges
+      -total_number_of_edges
         - int
-    -total_number_of_faces
+      -total_number_of_faces
         - int
-    -I_FD
+      -I_FD
         - incidence matrix edges - grains/face
-    -V
+      -V
         - see output
-    -F
-    	- list of edges as indices to V
-    -poly
+      -F
+      	- list of edges as indices to V
+      -poly
         - see output
+  
+  %}
+  
+  %%
 
-%}
+  %open file
+  fid=fopen(filepath, 'r');
 
-%%
-
-%open file
-fid=fopen(filepath, 'r');
-
-if (fid==-1)
+  if (fid==-1)
     error 'file could not be opened'
-end
+  end
 
-%% ***tess
-% read file line for line
-buffer = fgetl(fid);
-if (~(strcmp(buffer, '***tess')))
+  %% ***tess
+  % read file line for line
+  buffer = fgetl(fid);
+  if (~(strcmp(buffer, '***tess')))
     error 'file has wrong structure. line 1 has to be "***tess"!';
-end
+  end
 
-%% **format
-buffer = fgetl(fid);
-if (~(strcmp(buffer, ' **format')))
+  %% **format
+  buffer = fgetl(fid);
+  if (~(strcmp(buffer, ' **format')))
     error 'file has wrong structure. line 2 has to be " **format"! Be careful with blank characters.';
-else
+  else
     disp("reading **format ...");
-end
-format = fscanf(fid, '%s', 1);       
-fgetl(fid);                %eliminate the \n character
+  end
+  format = fscanf(fid, '%s', 1);       
+  fgetl(fid);                %eliminate the \n character
 
-%% **general
-buffer = fgetl(fid);
-if (~(strcmp(buffer, ' **general')))
+  %% **general
+  buffer = fgetl(fid);
+  if (~(strcmp(buffer, ' **general')))
     error 'file has wrong structure. line 4 has to be " **general"! Be careful with blank characters.';
-else
+  else
     disp("reading **general ...");
-end
-dimension = fscanf(fid, '%d' , 1);
-if dimension~=2
+  end
+  dimension = fscanf(fid, '%d' , 1);
+  if dimension~=2
     error 'only two dimensional tesselations supported'
-end
-type = fscanf(fid, '%s', 1);
-fgetl(fid);                %eliminate the \n character
+  end
+  type = fscanf(fid, '%s', 1);
+  fgetl(fid);                %eliminate the \n character
 
-%% **cell
-buffer = fgetl(fid);
-if (~(strcmp(buffer, ' **cell')))
+  %% **cell
+  buffer = fgetl(fid);
+  if (~(strcmp(buffer, ' **cell')))
     error 'file has wrong structure. line 6 has to be " **cell"! Be careful with blank characters.';
-else
+  else
     disp("reading **cell ...");
-end
-total_number_of_cells = str2double(fgetl(fid));
+  end
+  total_number_of_cells = str2double(fgetl(fid));
 
-buffer=fgetl(fid);
-while (1)
+  buffer=fgetl(fid);
+  while (1)
     switch strtrim(buffer)
-        case "*id"
-            disp ("reading  *id ...")
-            cell_ids=fscanf(fid,'%u', inf);
-            buffer=fgetl(fid);
-        case "*crysym"
-            disp ("reading  *crysym ...");
-            buffer=fgetl(fid);
-            while(~isHeader(buffer))
-                crysym=strtrim(buffer);
-                buffer=fgetl(fid);
-            end
-        case "*seed"
-            disp ("reading  *seed ...");                    %output as table
-            seedsTable=fscanf(fid,'%u %f %f %f %f ', [5 inf]);
-            seedsTable=array2table(seedsTable');
-            seedsTable.Properties.VariableNames(1:5)={'seed_id', 'seed_x', 'seed_y', 'seed_z', 'seed_weight'};
-            buffer=fgetl(fid);
-        case "*seed (id x y z weigth )"                 %output as table
-            buffer="*seed";
-        case "*ori"
-            disp ("reading  *ori ...");
-            buffer=fgetl(fid);
-            quaternion_descriptor=split(buffer, ':');
-            if (strtrim(quaternion_descriptor(1))~="quaternion")
-                error 'orientation in wrong format. currently only available for quaternion';
-            end
-            quaternion_descriptor=strtrim(quaternion_descriptor(2));
-            oriMatrix=fscanf(fid,'%f %f %f %f ',[4 inf]);          %output as matrix
-            oriMatrix=oriMatrix';
-            buffer=fgetl(fid);
-        case "**vertex"
-            disp("reading **vertex ...")
-            break
-        otherwise
-            error 'reading cell parameters failed. file has wrong structure, expression not known'
+      case "*id"
+        disp ("reading  *id ...")
+          cell_ids=fscanf(fid,'%u', inf);
+          buffer=fgetl(fid);
+      case "*crysym"
+        disp ("reading  *crysym ...");
+        buffer=fgetl(fid);
+        while(~isHeader(buffer))
+          crysym=strtrim(buffer);
+          buffer=fgetl(fid);
+        end
+      case "*seed"
+        disp ("reading  *seed ...");                    %output as table
+        seedsTable=fscanf(fid,'%u %f %f %f %f ', [5 inf]);
+        seedsTable=array2table(seedsTable');
+        seedsTable.Properties.VariableNames(1:5)={'seed_id', 'seed_x', 'seed_y', 'seed_z', 'seed_weight'};
+        buffer=fgetl(fid);
+      case "*seed (id x y z weigth )"                 %output as table
+        buffer="*seed";
+      case "*ori"
+        disp ("reading  *ori ...");
+        buffer=fgetl(fid);
+        quaternion_descriptor=split(buffer, ':');
+        if (strtrim(quaternion_descriptor(1))~="quaternion")
+          error 'orientation in wrong format. currently only available for quaternion';
+        end
+        quaternion_descriptor=strtrim(quaternion_descriptor(2));
+        oriMatrix=fscanf(fid,'%f %f %f %f ',[4 inf]);          %output as matrix
+        oriMatrix=oriMatrix';
+        buffer=fgetl(fid);
+      case "**vertex"
+        disp("reading **vertex ...")
+        break
+      otherwise
+        error 'reading cell parameters failed. file has wrong structure, expression not known'
     end
-end
+  end
 
-%% **vertex
-total_number_of_vertices=str2double(fgetl(fid));
-verticesTable=fscanf(fid,'%u %f %f %f %d ',[5 inf]);
-verticesTable=array2table(verticesTable');          
-verticesTable.Properties.VariableNames(1:5)={'ver_id','ver_x', 'ver_y','ver_z','ver_state'};
+  %% **vertex
+  total_number_of_vertices=str2double(fgetl(fid));
+  verticesTable=fscanf(fid,'%u %f %f %f %d ',[5 inf]);
+  verticesTable=array2table(verticesTable');          
+  verticesTable.Properties.VariableNames(1:5)={'ver_id','ver_x', 'ver_y','ver_z','ver_state'};
 
-% rotate the 2dslice into xy-plane (z becomes zero)
-V=table2array(verticesTable(:,2:4));
-A=V(1,:);
-B=V(2,:);
-C=V(3,:);
+  % rotate the 2dslice into xy-plane (z becomes zero)
+  V=table2array(verticesTable(:,2:4));
+  A=V(1,:);
+  B=V(2,:);
+  C=V(3,:);
 
-v1=B-A;
-v2=C-A;
-z=[0,0,1];
+  v1=B-A;
+  v2=C-A;
+  z=[0,0,1];
 
-n=cross(v1,v2);
-n=1/norm(n)*n;
+  n=cross(v1,v2);
+  n=1/norm(n)*n;
 
-rot_angle=-asind(norm(cross(n,z))/(norm(n)*norm(z)));
-rot_angle=rot_angle/360*2*pi;
-rot_axis=cross(n,z);
-rot_axis=1/norm(rot_axis)*rot_axis;
+  rot_angle=-asind(norm(cross(n,z))/(norm(n)*norm(z)));
+  rot_angle=rot_angle/360*2*pi;
+  rot_axis=cross(n,z);
+  rot_axis=1/norm(rot_axis)*rot_axis;
 
-R=rotation.byAxisAngle(vector3d(rot_axis),rot_angle);
-
-Vrot=zeros(total_number_of_vertices,3);
-for i=1:total_number_of_vertices
+  R=rotation.byAxisAngle(vector3d(rot_axis),rot_angle);
+  
+  Vrot=zeros(total_number_of_vertices,3);
+  for i=1:total_number_of_vertices
     buffer=R*vector3d(V(i,:));
     Vrot(i,:)=buffer.xyz;
-end
-V=Vrot(:,1:2);
+  end
+  V=Vrot(:,1:2);
 
-clearvars A B C i n z R rot_angle rot_axis Vrot v1 v2
+  clearvars A B C i n z R rot_angle rot_axis Vrot v1 v2
 
-%% **edge
-skipEmptyLines(fid)
-buffer=fgetl(fid);
-if (strtrim(buffer)~="**edge")
+  %% **edge
+  skipEmptyLines(fid)
+  buffer=fgetl(fid);
+  if (strtrim(buffer)~="**edge")
     error ' **edge" not found'
-else
+  else
     disp("reading **edge ...");
-end
-skipEmptyLines(fid)
-total_number_of_edges=str2double(fgetl(fid));
-skipEmptyLines(fid)
-% reading in edges
-varTypes={'uint32', 'uint32', 'uint32', 'int32'};
-varNames={'egde_id', 'vertex_1', 'vertex_2', 'edged_state'};
-edgesTable=table('Size', [0 4], 'VariableTypes', varTypes , 'VariableNames',varNames);
-for i=0:total_number_of_edges-1                   %Hier Kanten einlesen Zeilenweise zur Tabelle hinzufüg
+  end
+  skipEmptyLines(fid)
+  total_number_of_edges=str2double(fgetl(fid));
+  skipEmptyLines(fid)
+  % reading in edges
+  varTypes={'uint32', 'uint32', 'uint32', 'int32'};
+  varNames={'egde_id', 'vertex_1', 'vertex_2', 'edged_state'};
+  edgesTable=table('Size', [0 4], 'VariableTypes', varTypes , 'VariableNames',varNames);
+  for i=0:total_number_of_edges-1                   %Hier Kanten einlesen Zeilenweise zur Tabelle hinzufüg
     buffer=textscan(fid,'%u %u %u %d', 1);
     edgesTable=[edgesTable;buffer];
     %fgetl(fid);
-end
+  end
 
-F=table2array(edgesTable(:, 2:3));
-clearvars varNames varTypes
-%% **face
-skipEmptyLines(fid)
-buffer=fgetl(fid);
-if (buffer~=" **face")
+  F=table2array(edgesTable(:, 2:3));
+  clearvars varNames varTypes
+  %% **face
+  skipEmptyLines(fid)
+  buffer=fgetl(fid);
+  if (buffer~=" **face")
     error '" **face" not found'
-else
+  else
     disp("reading **face ...");
-end
-skipEmptyLines(fid)
-total_number_of_faces=str2double(fgetl(fid));
+  end
+  skipEmptyLines(fid)
+  total_number_of_faces=str2double(fgetl(fid));
 
-I_FD=zeros(total_number_of_edges,total_number_of_faces);
-clear poly
-poly{total_number_of_cells,1}=[];
+  I_FD=zeros(total_number_of_edges,total_number_of_faces);  
+  clear poly
+  poly{total_number_of_cells,1}=[];
 
-% read in I_FD and poly
-for i=1:total_number_of_faces
+  % read in I_FD and poly
+  for i=1:total_number_of_faces
 
     buffer=fgetl(fid);
     buffer=split(buffer);
 
     for j=4:3+str2num(cell2mat(buffer(3)))
-        poly{i}=[poly{i,1} str2double(buffer(j))];
+      poly{i}=[poly{i,1} str2double(buffer(j))];
     end
     %A(length(A))
     poly{i}=[poly{i,1} str2double(buffer(4))];
@@ -301,35 +301,35 @@ for i=1:total_number_of_faces
     FD=split(buffer);
     sizeFD=str2double(FD(2));
     for j=3:sizeFD+2
-        I_FD(abs(str2double(FD(j))), i)=1;
+      I_FD(abs(str2double(FD(j))), i)=1;
     end
 
     fgetl(fid);
     fgetl(fid);
-end
+  end
 
-clearvars i j sizeFD FD
-%% close file
-fclose(fid);
-clearvars fid buffer
+  clearvars i j sizeFD FD
+  %% close file
+  fclose(fid);
+  clearvars fid buffer
 end 
 
 %%
 function skipEmptyLines(fid)
-% function to ignore/skip possible empty lines
-i=ftell(fid);
-skipEmptyLinesBuffer=fgetl(fid);
-while (skipEmptyLinesBuffer=="")
+  % function to ignore/skip possible empty lines
+  i=ftell(fid);
+  skipEmptyLinesBuffer=fgetl(fid);
+  while (skipEmptyLinesBuffer=="")
     i=ftell(fid);
     skipEmptyLinesBuffer=fgetl(fid);
-end
-fseek(fid, i, "bof");
+  end
+  fseek(fid, i, "bof");
 
-clearvars i skipEmptyLinesBuffer
+  clearvars i skipEmptyLinesBuffer
 end
 
 %%
 function returnvalue = isHeader(buffer)
-% function to check if buffer is Header (=starts with *)
-returnvalue=strncmp(strtrim(buffer), "*", 1);
+  % function to check if buffer is Header (=starts with *)
+  returnvalue=strncmp(strtrim(buffer), "*", 1);
 end
