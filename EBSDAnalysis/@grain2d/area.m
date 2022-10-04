12e448336bf4bc79d923fd60f3a4b336272bce1e
BjaJac <102985804+BjaJac@users.noreply.github.com>
2022-10-04 16:44:59 +0200

    grain2d.area can process planes in 3d now

--- EBSDAnalysis/@grain2d/area.m
+++ EBSDAnalysis/@grain2d/area.m
@@ -8,11 +8,28 @@ function A = area(grains,varargin)
 %  A  - list of areas (in measurement units)
 %
 
-A = zeros(length(grains.poly),1);
-
 poly = grains.poly;
 V = grains.V;
 
-for ig = 1:length(poly)
-  A(ig) = polySgnArea(V(poly{ig},1),V(poly{ig},2));
+A = zeros(length(poly),1);
+
+if (size(V,2)==2)        % plane in 2d
+  for ig = 1:length(poly)
+    A(ig) = polySgnArea(V(poly{ig},1),V(poly{ig},2));
+  end
+elseif (size(V,2)==3)    % plane in 3d 
+  % calculate the normal vector
+  X1=V(1,:);
+  X2=V(2,:);
+  X3=V(3,:);
+  nV=cross(X2-X1,X3-X1);
+  nV=1/norm(nV)*nV;
+
+  % calculate signed Area
+  for i=1:length(poly)
+    Ptlist=V(poly{i},:);
+    a=sum(cross(Ptlist(2:end,:),Ptlist(1:end-1,:)));
+    A(i)=dot(nV,a)/2;
+    %A(i)=norm(a)/2;        % unsigned area
+  end
 end
