144733fa952262146a8d77070fcb4ce5ac36bff3
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-11-11 08:22:07 +0100

    define antipodal and isReal options for Harmonics

--- SO3Fun/@SO3VectorFieldHarmonic/SO3VectorFieldHarmonic.m
+++ SO3Fun/@SO3VectorFieldHarmonic/SO3VectorFieldHarmonic.m
@@ -5,7 +5,6 @@ classdef (InferiorClasses = {?SO3FunHarmonic,?SO3FunBingham,?SO3FunCBF, ...
 
 properties
   SO3F
-  antipodal = false
 end
 
 properties(Dependent = true)
@@ -15,12 +14,14 @@ properties(Dependent = true)
   bandwidth
   SLeft
   SRight
+  antipodal
+  isReal
 end
 
 methods
 
   function SO3VF = SO3VectorFieldHarmonic(SO3F, varargin)
-    % initialize a spherical vector field
+    % initialize a rotational vector field
     
     if nargin == 0, return; end
     SO3VF.SO3F = SO3F(:);
@@ -29,6 +30,7 @@ methods
 
   function bw = get.bandwidth(SO3VF), bw = SO3VF.SO3F.bandwidth; end
   function SO3VF = set.bandwidth(SO3VF,bw), SO3VF.SO3F.bandwidth = bw; end
+  
   function x = get.x(SO3VF), x = SO3VF.SO3F(1); end
   function y = get.y(SO3VF), y = SO3VF.SO3F(2); end
   function z = get.z(SO3VF), z = SO3VF.SO3F(3); end
@@ -37,11 +39,14 @@ methods
   function SO3VF = set.z(SO3VF, z), SO3VF.SO3F(3) = z; end
 
   function SLeft = get.SLeft(SO3VF), SLeft = SO3VF.SO3F.SLeft; end
-  function SRight = get.SRight(SO3VF), SRight = SO3VF.SO3F.SRight; end
-  
+  function SRight = get.SRight(SO3VF), SRight = SO3VF.SO3F.SRight; end  
   function SO3VF = set.SLeft(SO3VF,SLeft), SO3VF.SO3F.SLeft = SLeft; end
   function SO3VF = set.SRight(SO3VF,SRight), SO3VF.SO3F.SRight = SRight; end
   
+  function a = get.antipodal(SO3VF), a = SO3VF.SO3F.antipodal; end
+  function SO3VF = set.antipodal(SO3VF,a), SO3VF.SO3F.antipodal = a; end
+  function r = get.isReal(SO3VF), r = SO3VF.SO3F.isReal; end  
+  function SO3VF = set.isReal(SO3VF,r), SO3VF.SO3F.isReal = r; end
   
 end
 
