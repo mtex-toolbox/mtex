0bde7c4be34e547c538dc3e1d435233d380178ee
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-11-11 14:25:12 +0100

    add power of SO3VectorField

---
+++ geometry/@vector3d/power.m
@@ -0,0 +1,15 @@
+function v = power(v,a)      
+% overloads v.^a componentwise for the coordinates of vector3d
+
+if numel(a)==1
+  a=a.*[1,1,1];
+end
+if numel(a)==3
+  v.x = (v.x).^a(1);
+  v.y = (v.y).^a(2);
+  v.z = (v.z).^a(3);
+else
+  error('Operator ''.^'' is not supported for operands of type ''vector3d''. Change the size of the exponent.')
+end
+
+end
