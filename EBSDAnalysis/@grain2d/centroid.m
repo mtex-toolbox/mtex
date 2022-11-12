12a28b1650e8fb1f42e396630a3f92f4397517b7
ralfHielscher <ralf.hielscher@gmail.com>
2022-11-12 09:11:22 +0100

    centroid fully working

--- EBSDAnalysis/@grain2d/centroid.m
+++ EBSDAnalysis/@grain2d/centroid.m
@@ -1,42 +1,44 @@
 function c = centroid(grains)
-% calculates the barycenters of the grain boundary
+% barycenters of the grains
+% 
+% Syntax
+%
+%   c = grains.centroid
+%
+% Input
+%  grains - @grain2d
+%
+% Output
+%  c - @vector3d
+%
+% See also
+% grain2d/fitEllipse
 %
 
-if dot(grains.N,zvector) ~= 1
-
-  [grains,rot] = rotate2Plane(grains);
-  c = centroid(grains);
-  c=(rot\c)';
-
-  return
-
+% extract vertices as vector3d -> grains.V should be vector3d!
+if size(grains.V,2)==2
+  V = vector3d(grains.V(:,1), grains.V(:,2),0);
+else
+  V = vector3d(grains.V(:,1), grains.V(:,2), grains.V(:,3));
 end
 
-% initalize x,y values
-x = zeros(size(grains)); y = x;
+% duplicate vertices according to their occurence in the grain2s
+V = V([grains.poly{:}]);
 
-faceOrder = [grains.poly{:}];
+% compute the relative area of the triangles between the edges an a
+% potential center (here (0,0,0) - but the center does not matter here)
+a = dot(cross(V(1:end-1),V(2:end)), grains.N);
 
-Vx = grains.V(faceOrder,1);
-Vy = grains.V(faceOrder,2);
+% weight the vertices according to the area
+aV = a .* (V(1:end-1)+V(2:end));
 
-dF = (Vx(1:end-1).*Vy(2:end)-Vx(2:end).*Vy(1:end-1));
-cx = (Vx(1:end-1) +Vx(2:end)).*dF;
-cy = (Vy(1:end-1) +Vy(2:end)).*dF;
-
+% average the weighte vertices for each polygon (grain)
 cs = [0; cumsum(cellfun('prodofsize',grains.poly))];
 
-for k=1:numel(x)
+c = vector3d.nan(length(grains),1);
+for k=1:length(c)
   ndx = cs(k)+1:cs(k+1)-1;
-  
-  a = sum(dF(ndx));
-  x(k) = sum(cx(ndx)) / 3 / a;
-  y(k) = sum(cy(ndx)) / 3 / a;
-end
-
-c=vector3d(x,y,grains.V(1,3));
-%compensate that grains are only turned parallel to xy plane, but possebly are situated over or under
-
+  c(k)  = sum(aV(ndx)) / 3 / sum(a(ndx));
 end
 
 % some test code
@@ -44,5 +46,5 @@ end
 % plot(ebsd)
 % grains = calcGrains(ebsd)
 % plot(grains(1806))
-% [x,y] = centroid(grains(1806));
-% hold on, plot(x,y,'o','color','b'); hold off
+% c = centroid(grains(1806));
+% hold on, plot(c.x,c.y,c.z,'o','color','b'); hold off
