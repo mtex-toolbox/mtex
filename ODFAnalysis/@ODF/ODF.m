f9ee4c5a059db5820c44227871254b421cf8a85c
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-11-29 15:14:23 +0100

    Preserve old classes to allow loading .mat files of ODFs

--- ODFAnalysis/@ODF/ODF.m
+++ ODFAnalysis/@ODF/ODF.m
@@ -1,9 +1,39 @@
-classdef ODF < dynOption
+classdef ODF
+% This class is obsolet since MTEX 6.0. Use the class @SO3Fun instead.
+% Anyway the class is preserved, so that saved @ODFs can be loaded.
   
   methods (Static = true)
     [odf,interface,options] = load(fname,varargin)
+  end
+
+  methods (Static = true, Hidden = true)
 
     function odf = loadobj(s)
+      % called by Matlab when an object is loaded from an .mat file
+      % this overloaded method ensures compatibility with older MTEX
+      % versions
+      
+      % maybe there is nothing to do
+      if isa(s,'SO3Fun'), odf = s; return; end
+      
+
+      components = s.components;
+
+      if isempty(components)
+        error('Unknown data typ in components.')
+      end
+
+      if isfield(s,'weights')
+        weights = s.weights;
+      else
+        weights = ones(numel(components),1);
+      end
+
+      odf = 0;
+      for k = 1:numel(components)
+        odf = odf + weights(k) * components{k};
+      end
+
     end
 
   end
--- obsolete/deLaValeePoussinKernel.m
+++ obsolete/deLaValeePoussinKernel.m
@@ -4,19 +4,18 @@ methods (Hidden = true)
 
   function psi = deLaValeePoussinKernel(varargin)
 
-    warning('The syntax "deLaValeePoussinKernel" is obsolete. Please use "SO3DeLaValleePoussinKernel" instead.')
+    warning(['The syntax "deLaValeePoussinKernel" is obsolete. ' ...
+      'Please use "SO3DeLaValleePoussinKernel" instead.'])
 
     psi = SO3DeLaValleePoussinKernel(varargin{:});
 
   end
 end
 
-methods (Static = true)
+methods (Static = true, Hidden=true)
   function psi = loadobj(s)
     psi = SO3DeLaValleePoussinKernel(s.kappa);
   end
 end
-
-
 
 end
