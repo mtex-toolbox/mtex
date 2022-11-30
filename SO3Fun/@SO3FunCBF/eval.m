097320d1b3b781355eb81d63e04092418bac81ec
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-11-30 16:26:37 +0100

    add weights

--- SO3Fun/@SO3FunCBF/eval.m
+++ SO3Fun/@SO3FunCBF/eval.m
@@ -3,16 +3,17 @@ function f = eval(SO3F,rot,varargin)
 %
 % sum_i \sum_j \sum_k  weights * psi(g*s_j h_i . s_k r_i)
 
-% TODO: Use weights
+s = size(rot);
+rot = rot(:);
 
 h = symmetrise(SO3F.h.normalize,'unqiue');
 
-% g x SS x CS x h
-gh = reshape((SO3F.SS.properGroup * rot).' * h, length(rot),[]);
+% rot x SS x CS x h
+gh = reshape((SO3F.SS.properGroup * rot).' * h, length(rot),[],length(SO3F.h));
 
-f = mean(SO3F.psi.eval(dot(gh,normalize(SO3F.r),'noSymmetry')),2);
+f = mean(SO3F.psi.eval(dot(gh,reshape(normalize(SO3F.r),1,1,[]),'noSymmetry')),2);
 
-f = reshape(f,size(rot));
+f = reshape(reshape(f,length(rot),[]) * SO3F.weights,s);
 
 if isalmostreal(f)
   f = real(f);
