8cc96e797a421333b211e3fb7a401b88ca98bc6d
ErikWünsche <erik.wuensche@math.tu-freiberg.de>
2022-12-08 13:05:33 +0100

    correct comments

--- SO3Fun/@SO3FunHarmonic/evalfft.m
+++ SO3Fun/@SO3FunHarmonic/evalfft.m
@@ -1,4 +1,103 @@
 function val = evalfft(SO3F,varargin)
+% Do fft for the fourier coefficients of SO3F and give the grid if there
+% are no evaluation points given.
+%
+% Syntax
+%   val = evalfft(SO3F)
+%
+% Input
+%  SO3F - @SO3FunHarmonic
+%
+% Output
+%  val.nodes  - @rotation : grid on SO3F 
+%  val.values - values at this grid points
+%
+% Example
+%   SO3F = SO3FunHarmonic(SO3Fun.dubna);
+%   v = SO3F.evalfft;
+%   v.values - SO3F.eval(v.nodes)
+% 
+
+N = SO3F.bandwidth;
+
+% get number of grid points
+H = ceil(get_option(varargin,'GridPointNum',2*N+1)/2-1);
+
+
+% % if there are no @rotation given, do dft
+% if ~isempty(varargin) && isa(varargin{1},'rotation')
+%   rot = varargin{1};
+%   dft = false;
+% else
+%   dft = true;
+% end
+% 
+% % get interpolation method
+% method = get_flag(varargin,{'nearest','trilinear'},'trilinear');
+% 
+% % give gridconstant h
+% h = pi/(H+1);
+
+
+% 1) calculate FFT of SO3F
+
+% if SO3F is real valued we have (*) and (**) for the Fourier coefficients
+% we will use this to speed up computation
+if SO3F.isReal
+
+  % create ghat -> k x j x l
+  % with  k = -N:N
+  %       l =  0:N      -> use ghat(-k,-l,-j)=conj(ghat(k,l,j))        (*)
+  %       j = -N:N      -> use ghat(k,l,-j)=(-1)^(k+l)*ghat(k,l,j)     (**)
+  % 2^0 -> fhat are the fourier coefficients of a real valued function
+  % 2^2 -> use L_2-normalized Wigner-D functions
+  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^0+2^2);
+
+  % correct ghat by (-1)^(k-l)
+  z = zeros(2*N+1,2*N+1,N+1)+(-N:N)'-reshape(0:N,1,1,[]);
+  ghat = ghat.*(1i).^z;
+
+else
+
+  % 2^2 -> use L_2-normalized Wigner-D functions
+  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^2);
+
+  % correct ghat by (-1)^(k-l)
+  z = zeros(2*N+1,2*N+1,2*N+1)+(-N:N)'-reshape(-N:N,1,1,[]);
+  ghat = ghat.*(1i).^z;
+
+end
+
+
+% fft
+f = fftn(ghat,[2*H+2,2*H+2,2*H+2]);
+
+f = f(:,1:H+2,:);                          % because beta is only in [0,pi]
+
+if SO3F.isReal
+  % need to shift summation of fft from [-N:N] to [0:2N]
+  z = (0:H+1)+(0:2*H+1)';
+  f = 2*real(exp(1i*pi*N/(H+1)*z).*f);     % shift summation & use (*)
+else
+  % need to shift summation of fft from [-N:N] to [0:2N]
+  z = (0:H+1)+(0:2*H+1)'+reshape(0:2*H+1,1,1,[]);
+  f = exp(1i*pi*N/(H+1)*z).*f;
+end
+
+
+% if dft
+  grid(:,[3,2,1]) = combvec(0:2*H+1,0:2*H+1,0:2*H+1)'*pi/(H+1);
+  grid = grid(grid(:,2)<=pi,:);
+  grid = orientation.byEuler(grid,'nfft',SO3F.CS,SO3F.SS);
+  val.nodes = reshape(grid,size(f));
+  val.values = f;
+%   return
+% end
+
+
+
+
+
 % evaluate harmonic SO3function in rotations by using fft
 %
 % Therefore the rotations are given by euler angles 'ZYZ'. This euler
@@ -25,145 +124,55 @@ function val = evalfft(SO3F,varargin)
 %   'nearest'      - use function value of nearest grid point
 %   'trilinear'    - trilinear interpolation by the neighbor points
 %
+%
+
+
+
+
+
+% % 2) evaluate SO3F in rot
 % 
-% Do fft for the fourier coefficients of SO3F and give the grid if there
-% are no evaluation points given.
-%
-% Syntax
-%   val = evalfft(SO3F)
-%
-% Input
-%   SO3F - @SO3FunHarmonic
-%
-% Output
-%   val - structure array composed of a grid and the SO3F values at this grid points
-%
-% Example
-%   SO3F = SO3FunHarmonic(SO3Fun.dubna);
-%   v = SO3F.evalfft;
-%   v.value - SO3F.eval(v.grid)
+% sz = size(rot);
+% rot = rot(:);
+% M = length(rot);
 % 
-
-% if there are no @rotation given, do dft
-if ~isempty(varargin) && isa(varargin{1},'rotation')
-  rot = varargin{1};
-  dft = false;
-else
-  dft = true;
-end
-
-N = SO3F.bandwidth;
-
-% get number of grid points
-H = ceil(get_option(varargin,'GridPointNum',2*N+1)/2-1);
-
-% get interpolation method
-method = get_flag(varargin,{'nearest','trilinear'},'trilinear');
-
-% give gridconstant h
-h = pi/(H+1);
-
-
-% 1) calculate FFT of SO3F
-
-% if SO3F is real valued we have (*) and (**) for the Fourier coefficients
-% we will use this to speed up computation
-if SO3F.isReal
-
-  % create ghat -> k x l x j
-  % with  k = -N:N
-  %       l =  0:N      -> use ghat(-k,-l,-j)=conj(ghat(k,l,j))        (*)
-  %       j = -N:N      -> use ghat(k,l,-j)=(-1)^(k+l)*ghat(k,l,j)     (**)
-  % 2^0 -> fhat are the fourier coefficients of a real valued function
-  % 2^2 -> use L_2-normalized Wigner-D functions
-  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^0+2^2);
-
-  % correct ghat by exp(-2*pi*i*(-1/4*l+1/4*k))
-  z = zeros(2*N+1,2*N+1,N+1)+(-N:N)'-reshape(0:N,1,1,[]);
-  ghat = ghat.*(1i).^z;
-
-else
-
-  % 2^2 -> use L_2-normalized Wigner-D functions
-  ghat = representationbased_coefficient_transform(N,SO3F.fhat,2^2);
-
-  % correct ghat by exp(-2*pi*i*(-1/4*l+1/4*k))
-  z = zeros(2*N+1,2*N+1,2*N+1)+(-N:N)'-reshape(-N:N,1,1,[]);
-  ghat = ghat.*(1i).^z;
-
-end
-
-
-% fft
-f = fftn(ghat,[2*H+2,2*H+2,2*H+2]);
-
-f = f(:,1:H+2,:);                          % because beta is only in [0,pi]
-
-if SO3F.isReal
-  % need to shift summation of fft from [-N:N] to [0:2N]
-  z = (0:H+1)+(0:2*H+1)';
-  f = 2*real(exp(1i*pi*N/(H+1)*z).*f);     % shift summation & use (*)
-else
-  % need to shift summation of fft from [-N:N] to [0:2N]
-  z = (0:H+1)+(0:2*H+1)'+reshape(0:2*H+1,1,1,[]);
-  f = exp(1i*pi*N/(H+1)*z).*f;
-end
-
-
-if dft
-  grid(:,[3,2,1]) = combvec(0:2*H+1,0:2*H+1,0:2*H+1)'*pi/(H+1);
-  grid = grid(grid(:,2)<=pi,:);
-  grid = orientation.byEuler(grid,'nfft',SO3F.CS,SO3F.SS);
-  val.grid = reshape(grid,size(f));
-  val.value = f;
-  return
-end
-
-
-
-% 2) evaluate SO3F in rot
-
-sz = size(rot);
-rot = rot(:);
-M = length(rot);
-
-% gridconst h and 2H+2 x H+2 x 2H+2 grid points in [0,2pi)x[0,pi]x[0,2pi)
-abg = Euler(rot,'nfft');
-abg = mod(abg/h,2*H+2);
-
-% interpolation
-if strcmpi(method,'nearest')
-
-  % basic := index of nearest grid point of rot
-  basic = round(abg);
-  ind = basic(:,1)*(2*H+2)*(H+2)+basic(:,2)*(2*H+2)+basic(:,3)+1;
-  val = f(ind);
-
-elseif strcmpi(method,'trilinear')
-  
-  % basic := index of next rounded down grid point of rot
-  basic = floor(abg);
-  % if beta = pi : we change basic point, to avoid domain error later
-  basic((basic(:,2)==H+1),2) = H;
-
-  % get the indices of the other grid points around rot
-  X = [0,0,0,0,1,1,1,1;0,0,1,1,0,0,1,1;0,1,0,1,0,1,0,1]';
-  gp = reshape(X,1,8,3)+reshape(basic,M,1,3);
-  gp(gp==2*H+2) = 0;
-
-  % get the indices of this grid points for f(:) from FFT
-  ind = gp(:,:,1)*(2*H+2)*(H+2)+gp(:,:,2)*(2*H+2)+gp(:,:,3)+1;
-
-  % transform coordinates in h^3 cube with gp as edges to [0,1]^3
-  pkt = abg-basic;
-  % calculate therefore a weightmatrix
-  w = prod(abs(reshape(X==0,1,8,3)-reshape(pkt,M,1,3)),3);
-
-  % get evaluation in rot
-  val = sum(w.*f(ind),2);
-
-end
-
-val = reshape(val,sz);
-
-end
+% % gridconst h and 2H+2 x H+2 x 2H+2 grid points in [0,2pi)x[0,pi]x[0,2pi)
+% abg = Euler(rot,'nfft');
+% abg = mod(abg/h,2*H+2);
+% 
+% % interpolation
+% if strcmpi(method,'nearest')
+% 
+%   % basic := index of nearest grid point of rot
+%   basic = round(abg);
+%   ind = basic(:,1)*(2*H+2)*(H+2)+basic(:,2)*(2*H+2)+basic(:,3)+1;
+%   val = f(ind);
+% 
+% elseif strcmpi(method,'trilinear')
+%   
+%   % basic := index of next rounded down grid point of rot
+%   basic = floor(abg);
+%   % if beta = pi : we change basic point, to avoid domain error later
+%   basic((basic(:,2)==H+1),2) = H;
+% 
+%   % get the indices of the other grid points around rot
+%   X = [0,0,0,0,1,1,1,1;0,0,1,1,0,0,1,1;0,1,0,1,0,1,0,1]';
+%   gp = reshape(X,1,8,3)+reshape(basic,M,1,3);
+%   gp(gp==2*H+2) = 0;
+% 
+%   % get the indices of this grid points for f(:) from FFT
+%   ind = gp(:,:,1)*(2*H+2)*(H+2)+gp(:,:,2)*(2*H+2)+gp(:,:,3)+1;
+% 
+%   % transform coordinates in h^3 cube with gp as edges to [0,1]^3
+%   pkt = abg-basic;
+%   % calculate therefore a weightmatrix
+%   w = prod(abs(reshape(X==0,1,8,3)-reshape(pkt,M,1,3)),3);
+% 
+%   % get evaluation in rot
+%   val = sum(w.*f(ind),2);
+% 
+% end
+% 
+% val = reshape(val,sz);
+% 
+% end
