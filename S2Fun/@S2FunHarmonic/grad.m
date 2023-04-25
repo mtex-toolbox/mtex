6f89bf842cdf32f6de5e9b78d0b153d09e2b6770
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2023-04-25 10:05:11 +0200

    fix

--- S2Fun/@S2FunHarmonic/grad.m
+++ S2Fun/@S2FunHarmonic/grad.m
@@ -14,6 +14,12 @@ function sVF = grad(sF, varargin)
 %    g - @vector3d
 %
 
+if check_option(varargin,'lazy')
+  sF = [sF.drho; sF.dthetasin];
+  sVF = S2VectorFieldHandle(@(v) localGrad(sF,v));
+  return
+end
+
 % Use Varshalovich 5.8.3(a) equation (10)&(11):
 
 % fallback to generic method
@@ -90,43 +96,23 @@ end
 end
 
 
-% 
-% sF = [sF.drho; sF.dthetasin];
-% 
-% if nargin > 1 && isa(varargin{1},'vector3d')
-%   
-%   sVF = localGrad(varargin{1});
-% 
-% elseif check_option(varargin,'lazy')
-% 
-%   sVF = S2VectorFieldHandle(@(v) localGrad(v));
-% 
-% else
-% 
-%   sVF = S2VectorFieldHarmonic.quadrature(@(v) localGrad(v),'bandwidth',sF.bandwidth);
-%   
-% 
-%   %sF = [sF.drho; sF.dtheta]; sVF = S2VectorFieldHarmonic(sF);
-% 
-% end
-% 
-%   function g = localGrad(v)
-%     
-%     gg = real(sF.eval(v));
-%     [th,rh] = polar(v);
-%     
-%     sinTh = max(1e-7,sin(th));
-% 
-%     x = gg(:,2) .* cos(rh) .* cos(th) - gg(:,1) .* sin(rh);
-%     y = gg(:,2) .* sin(rh) .* cos(th) + gg(:,1) .* cos(rh);
-%     z = -gg(:,2) .* sin(th);
-% 
-%     g = vector3d(x,y,z) ./ sinTh;
-%   
-%     %sVF = ...
-%     %  y(:, 1) ./ sin(v.theta) .* S2VectorField.rho(v)+ ...
-%     %  y(:, 2) ./ sin(v.theta) .* S2VectorField.theta(v);
-% 
-%     %g(isnan(g)) = vector3d([0 0 0]);
-% 
-%   end
+function g = localGrad(sF,v)
+
+gg = real(sF.eval(v));
+[th,rh] = polar(v);
+
+sinTh = max(1e-7,sin(th));
+
+x = gg(:,2) .* cos(rh) .* cos(th) - gg(:,1) .* sin(rh);
+y = gg(:,2) .* sin(rh) .* cos(th) + gg(:,1) .* cos(rh);
+z = -gg(:,2) .* sin(th);
+
+g = vector3d(x,y,z) ./ sinTh;
+
+%sVF = ...
+%  y(:, 1) ./ sin(v.theta) .* S2VectorField.rho(v)+ ...
+%  y(:, 2) ./ sin(v.theta) .* S2VectorField.theta(v);
+
+%g(isnan(g)) = vector3d([0 0 0]);
+
+end
