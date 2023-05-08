c5cadb0eb2d20455607b547bf5be61b82603074a
azdiarGazder <azdiar.gazder@gmail.com>
2023-05-08 16:36:13 +1000

    Update calcVariantId.m

--- geometry/misorientation/calcVariantId.m
+++ geometry/misorientation/calcVariantId.m
@@ -1,4 +1,4 @@
-function [childId, packetId] = calcVariantId(parentOri,childOri,p2c,varargin)
+function [childId, packetId,bainId] = calcVariantId(parentOri,childOri,p2c,varargin)
 % compute variantIds and packetId from parent / child orientation pairs
 %
 % Syntax
@@ -41,13 +41,13 @@ d = dot(childVariants,repmat(childOri(:),1,size(childVariants,2)));
 [~,childId] = max(d,[],2);
 
 % compute packetId if required
-if nargout == 2
+if nargout >= 2 % AAG EDIT
   % Get packet definition
   tmp = getClass(varargin,'cell');
   isMiller = [];
   for ii = 1:length(tmp); isMiller(ii) = ~isempty(getClass(tmp(ii),'Miller')); end
-  
-  if sum(isMiller) == 2 % definition given
+
+  if sum(isMiller) == 2 || sum(isMiller) == 4 % definition given % AAG EDIT
     ind = find(isMiller);
     h1 = tmp{ind(1)};  
     h2 = tmp{ind(2)};
@@ -64,5 +64,32 @@ if nargout == 2
   packetId = packetId(childId);
   
 end
+
+%% AAG ADDED SECTION
+% compute bainId if required
+if nargout == 3
+  % Get bain definition
+  tmp = getClass(varargin,'cell');
+  isMiller = [];
+  for ii = 1:length(tmp); isMiller(ii) = ~isempty(getClass(tmp(ii),'Miller')); end
+  
+  if sum(isMiller) == 2 || sum(isMiller) == 4 % definition given % AAG EDIT
+    ind = find(isMiller);
+    h1 = tmp{ind(3)};  
+    h2 = tmp{ind(4)};
+  else % definition assumed
+    warning('Bain ID calculation assuming {001}_p||{100}_c');
+    h1 = Miller({0,0,1},{1,0,0},{0,1,0},p2c.CS);
+    h2 = Miller(1,0,0,p2c.SS);
+  end
+  
+  omega = dot(variants(p2c,h1),h2);
+
+  [~,bainId] = max(omega,[],1);
+  
+  bainId = bainId(childId);
+  
+end
+%%
 
 end
