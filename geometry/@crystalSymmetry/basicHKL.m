2f1e4ae5b60f8d04a3503cb4a676e403f3adbd9a
Rüdiger Kilian <kilir@users.noreply.github.com>
2023-06-29 16:06:41 +0200

    allow for larger hkl, eleminate duplicates

--- geometry/@crystalSymmetry/basicHKL.m
+++ geometry/@crystalSymmetry/basicHKL.m
@@ -1,18 +1,30 @@
 function p = basicHKL(cs,varargin)
-% plot symmetry
+% generate a list of Millers within for permutations of -n,0,n, 
+% defaults to n = 1
 %
 % Input
-%  cs - symmetry
+%  cs  - symmetry
 %
 % Output
+%  p   - list of Millers
 %
 % Options
-%  antipodal      - include <VectorsAxes.html antipodal symmetry>
+%  maxhkl - maximum numeric value for h,k and l
 
-[h,k,l] = meshgrid(-1:1);
+n = get_option(varargin,'maxhkl',1);
+
+[h,k,l] = meshgrid(-n:n);
 
 p = Miller(h(:),k(:),l(:),cs);
 
 p(isinf(p.dspacing)) = [];
+
+if n>1 % eventually, this could be done more nicely
+    pl = p(abs(p.h) > 1  | abs(p.k) > 1  | abs(p.l) > 1 );
+    p1 = p(abs(p.h) <= 1 & abs(p.k) <= 1 & abs(p.l) <= 1);
+    idc = abs(angle_outer(p1,pl))<1e-6; % those are doubles
+    pl(any(idc)')=[];
+    p = [p1;pl];
+end
 
 p = unique(p.symmetrise,'noSymmetry');
