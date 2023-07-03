5cad0b7e6dccf85e3bb77de6f7f7b1b61dc9f3d6
ralfHielscher <ralf.hielscher@gmail.com>
2023-07-03 15:30:23 +0200

    minor

--- SO3Fun/@SO3FunHandle/size.m
+++ SO3Fun/@SO3FunHandle/size.m
@@ -1,7 +1,7 @@
 function d = size(SO3F,varargin)
 % size of @SO3FunHandle
 
-d = size(SO3F.fun(rotation.id));
+d = size(SO3F.fun(orientation.id(SO3F.CS,SO3F.SS)));
 d = d(2:end);
 if length(d) == 1, d = [d 1]; end
 if nargin > 1, d = d(varargin{1}); end
