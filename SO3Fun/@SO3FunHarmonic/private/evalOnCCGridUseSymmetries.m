4d67f930bda2a6ed3b89df8aadc0b6ffdd7abcd1
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2023-07-06 09:42:06 +0200

    fix

--- SO3Fun/@SO3FunHarmonic/private/evalOnCCGridUseSymmetries.m
+++ SO3Fun/@SO3FunHarmonic/private/evalOnCCGridUseSymmetries.m
@@ -9,16 +9,34 @@ function [values,nodes,weights] = evalOnCCGridUseSymmetries(SO3F,N,SRight,SLeft,
 
 LId = SLeft.id;
 RId = SRight.id;
-bothPerpZ = SRight.multiplicityPerpZ~=1 && SLeft.multiplicityPerpZ~=1;
 
 % ignore symmetry by using 'complete' grid
-if check_option(varargin,'complete') || (LId==1 && RId==1) || N<5
+if check_option(varargin,'complete') || (LId==1 && RId==1) || N<=5
   [nodes,weights] = quadratureSO3Grid(2*N,'ClenshawCurtis',SRight,SLeft,'complete','ABG');
   values = SO3F.eval(nodes);
 %   [a,b,c] = Euler(nodes,'nfft');
 %   nodes = cat(4,a,b,c);
   return
 end
+
+
+
+% check for suiting bandwidth
+[~,~,g] = fundamentalRegionEuler(SRight,SLeft,'ABG');
+t = N;
+LCM = lcm((1+double(round(2*pi/g/SRight.multiplicityZ) == 4))*SRight.multiplicityZ,SLeft.multiplicityZ);
+while mod(2*t+2,LCM)~=0
+  t = t+1;
+end
+if t~=N
+  error(['When trying to evaluate the function on the Clenshaw-Curtis quadrature',...
+         'grid with bandwidth %i using the symmetries, an error was detected.',...
+         'The specified bandwidth does not fit the symmetries. ' ...
+         'Use bandwidth %i instead.'],N,t);
+end
+
+
+
 
 % Get Clenshaw Curtis quadrature nodes suitable to the symmetries
 nodes = quadratureSO3Grid(2*N,'ClenshawCurtis',SRight,SLeft,'ABG');
@@ -49,11 +67,15 @@ addNodesG = false;
 % test for 2-fold mirror symmetry along alpha && next grid point is boundary of fundamental region
 if MirrorA && abs(ae+pi/(N+1)-a)<1e-3
   addNodesA = true;
+  warning off
   nodes = cat(3,nodes,rotation.byEuler(pi/(N+1),0,0).*nodes(:,:,end));
+  warning on
 end
 if MirrorG>0 && abs(ge+pi/(N+1)-g)<1e-3
   addNodesG = true;
+  warning off
   nodes = cat(1,nodes,nodes(end,:,:).*rotation.byEuler(pi/(N+1),0,0));
+  warning on
 end
 
 % TODO: Output this fundamental grid
@@ -112,21 +134,20 @@ for index = 1:num
       values_below = v(2:end,:,:);
     end
 
-    if ~( ismember(LId,19:21) && isa(SLeft,'crystalSymmetry') && ismember(RId,41:45) )
-      values_below = flip(values_below,3);
-      if (ismember(RId,3:8)) ...
-              || ( ismember(RId,19:21) && isa(SRight,'specimenSymmetry') ) ...
-              || ( ismember(RId,22:24) && isa(SRight,'crystalSymmetry') )
-        values_below = circshift(values_below,1,3);
-        values_below = flip(values_below,2);
-      elseif ismember(RId,[12:16,28:32,36:45])
-        values_below = flip(values_below,1);
-      elseif ismember(RId,19:21) && isa(SRight,'crystalSymmetry')
-        values_below = fftshift(values_below,3);
-        values_below = circshift(values_below,1,3);
-        values_below = flip(values_below,1);
-      end    
-    end
+    values_below = flip(values_below,3);
+    if (ismember(RId,3:8)) ...
+            || ( ismember(RId,19:21) && isa(SRight,'specimenSymmetry') ) ...
+            || ( ismember(RId,22:24) && isa(SRight,'crystalSymmetry') )
+      values_below = circshift(values_below,1,3);
+      values_below = flip(values_below,2);
+    elseif ismember(RId,[12:16,28:32,36:45])
+      values_below = flip(values_below,1);
+    elseif ismember(RId,19:21) && isa(SRight,'crystalSymmetry')
+      values_below = fftshift(values_below,3);
+      values_below = circshift(values_below,1,3);
+      values_below = flip(values_below,1);
+    end    
+
 
     v = cat(1,v,values_below);
 
@@ -155,10 +176,8 @@ for index = 1:num
     end
 
     if ( ismember(RId,[1:2,6:8,17:18]) ) ... 
-            || ( ismember(RId,[3:5,19:24]) && ismember(LId,6:8) ) ...
-            || ( ismember(RId,[3:5,19:21]) && ismember(LId,19:21) ) ...
-            || ( ismember(RId,19:21) && isa(SRight,'crystalSymmetry') && ismember(LId,[1:2,9:18,25:45]) ) ...
-            || ( ismember(RId,22:24) && ismember(LId,19:21) && isa(SLeft,'crystalSymmetry') )
+            || ( ismember(RId,[3:5,19:24]) && ismember(LId,[6:8,19:21]) ) ...
+            || ( ismember(RId,19:21) && isa(SRight,'crystalSymmetry') && ismember(LId,[1:2,9:18,25:45]) )
       values_right = fftshift(values_right,1);
     end
 
@@ -177,16 +196,12 @@ for index = 1:num
       values_below = v(:,:,2:end);
     end
     
-    if ( ismember(LId,22:24) ) || ( ismember(LId,3:5) && ~ismember(RId,41:45) )
+    if ismember(LId,[3:5,22:24])
       values_below = flip(values_below,2);
       values_below = flip(circshift(values_below,-1,1),1);
     elseif isa(SLeft,'specimenSymmetry') && ismember(LId,19:21)
       values_below = flip(values_below,2);
-      if ismember(RId,41:45)
-        values_below = flip(values_below,3);
-      else
         values_below = flip(circshift(values_below,-1,1),1);
-      end
     else
       values_below = flip(values_below,3);
       values_below = flip(circshift(values_below,-1,1),1);
@@ -220,6 +235,68 @@ values = reshape(values,[len s(2:end)]);
 % multiply the weights with r*s.
 % [nodes, weights] = quadratureSO3Grid(2*N,'ClenshawCurtis',SRight,SLeft,'Zfold','Euler');
 [nodes, weights] = quadratureSO3Grid(2*N,'ClenshawCurtis',SRight,SLeft,'Zfold','ABG');
+
+end
+
+
+function TEST
+
+clear
+rng(0)
+f = SO3FunRBF(orientation.rand(50,'Bunge'),SO3DeLaValleePoussinKernel(20));
+F = SO3FunHarmonic(f);
+
+S = {'1','211','121','112','222','3','321','312','4','422','6','622','23','432'};
+fprintf('\n New iteration \n')
+E = NaN(14,14,4);
+for s = 1:4
+for i = 1:14
+  for j = 1:14
+    switch s
+      case 1
+        CS = crystalSymmetry(S{i});
+        SS = crystalSymmetry(S{j});
+      case 2
+        CS = crystalSymmetry(S{i});
+        SS = specimenSymmetry(S{j});
+      case 3
+        CS = specimenSymmetry(S{i});
+        SS = crystalSymmetry(S{j});
+      case 4
+        CS = specimenSymmetry(S{i});
+        SS = specimenSymmetry(S{j});
+    end
+
+    if (CS.id==22 && isa(CS,'specimenSymmetry')) || (SS.id == 22 && isa(SS,'specimenSymmetry'))
+      continue;
+    end
+
+    f = F; f.CS=CS; f.SS=SS; f2=f.symmetrise;
+
+    [a,b,g] = fundamentalRegionEuler(CS,SS,'ABG');
+    bw = [];
+    for N = 10+(0:30)
+      t = N;
+      LCM = lcm((1+double(round(2*pi/g/CS.multiplicityZ) == 4))*CS.multiplicityZ,SS.multiplicityZ);
+      while mod(2*t+2,LCM)~=0
+        t = t+1;
+      end
+      bw = [bw,t];
+    end
+    bw = unique(bw);
+    for k=1:length(bw)
+      [values,nodes,~] = evalOnCCGridUseSymmetries(f2,bw(k),CS,SS);
+      A = f2.eval(nodes);
+      E(i,j,s) = sum([E(i,j,s) , norm(A(:)-values(:))],'omitnan');
+    end
+
+    w = waitbar((j+(i-1)*14+(s-1)*14*14)/(14*14*4));
+  end
+end
+end
+close(w);
+
+max(E,[],3,'omitnan')
 
 end
 
