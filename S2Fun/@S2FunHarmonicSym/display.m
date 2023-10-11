01bb15d2732c800091c65a69d736750bf378ef4a
ralfHielscher <ralf.hielscher@gmail.com>
2023-10-11 19:29:22 +0200

    mineral is displayed in the header

--- S2Fun/@S2FunHarmonicSym/display.m
+++ S2Fun/@S2FunHarmonicSym/display.m
@@ -5,17 +5,6 @@ displayClass(sFs,inputname(1),'moreInfo',char(sFs.CS,'compact'));
 
 if length(sFs)> 1, disp(['  size: ' size2str(sFs)]);end
 
-% display symmetry
-if isa(sFs.s,'crystalSymmetry')
-  if isempty(sFs.CS.mineral)
-    disp(['  symmetry: ',char(sFs.CS,'verbose')]);
-  else
-    disp(['  mineral: ',char(sFs.CS,'verbose')]);
-  end
-else
-    disp(['  symmetry: ',char(sFs.SS)]);
-end
-
 disp(['  bandwidth: ' num2str(sFs.bandwidth)]);
 if sFs.antipodal, disp('  antipodal: true'); end
 if sFs.isReal, disp('  isReal: true'); end
