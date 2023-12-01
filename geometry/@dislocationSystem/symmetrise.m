fac91c496686fce2189b690372c1492e17bf7a76
Rüdiger Kilian <kilir@users.noreply.github.com>
2023-12-02 00:50:21 +0100

    bugfix

--- geometry/@dislocationSystem/symmetrise.m
+++ geometry/@dislocationSystem/symmetrise.m
@@ -1,30 +1,30 @@
-function sS = symmetrise(sS,varargin)
+function dS = symmetrise(dS,varargin)
 % find all symmetrically equivalent slips systems
 
-if ~isa(sS.b,'Miller'), return; end
+if ~isa(dS.b,'Miller'), return; end
 
 b = [];
-n =  [];
-CRSS = [];
-for i = 1:length(sS)
+l =  [];
+u = [];
+for i = 1:length(dS)
 
   % find all symmetrically equivalent
-  [mm,~] = symmetrise(sS.b(i),varargin{:});
-  [nn,~] = symmetrise(sS.n(i),'antipodal'); %#ok<*PROP>
+  mm = symmetrise(dS.b(i),'unique',varargin{:});
+  ll = symmetrise(dS.l(i),'unique','antipodal'); %#ok<*PROP>
   
   % find those which have the same angles as the original system
   % for slip system this is of course 90 degree
-  [r,c] = find(isnull(dot(sS.n(i),sS.b(i),'noSymmetry')-...
-    dot_outer(mm,nn,'noSymmetry')));
+  [r,c] = find(isnull(dot(dS.l(i),dS.b(i),'noSymmetry')-...
+    dot_outer(mm,ll,'noSymmetry')));
 
   % restricht to the orthogonal ones
   b = [b;mm(r(:))]; %#ok<*AGROW>
-  n = [n;nn(c(:))];
-  CRSS = [CRSS;repmat(sS.CRSS(i),length(r),1)];
+  l= [l;ll(c(:))];
+  u = [u;repmat(dS.u(i),length(r),1)];
 end
 
-sS.b = b;
-sS.n = n;
-sS.CRSS = CRSS;  
+dS.b = b;
+dS.l = l;
+dS.u = u;  
 
 end
