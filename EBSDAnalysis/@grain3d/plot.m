3a8c56ea6d7f6705cf2a842dc5e8b2a488251c32
BjaJac <102985804+BjaJac@users.noreply.github.com>
2023-12-13 10:44:42 +0100

    bugfix

--- EBSDAnalysis/@grain3d/plot.m
+++ EBSDAnalysis/@grain3d/plot.m
@@ -1,5 +1,5 @@
 function varargout = plot(grains,varargin)
-% colorize grains, or more percise their faces
+% colorize 3 dimensional grains, or more percise their faces
 %
 % Only outer faces, the inner grains have the color gray [0.5 0.5 0.5].
 %
@@ -13,7 +13,7 @@ function varargout = plot(grains,varargin)
 %   plot(grains,grains.volume)
 %
 % Input
-%  grains  - @grain2d
+%  grains  - @grain3d
 %  property- n x 1 numeric or orientation
 %
 % See also
@@ -32,21 +32,27 @@ if nargin>1 && isa(varargin{1},'orientation')
   varargin{1} = [];
 
   faceColor = 0.5 .* ones(size(poly,1),3);
-  isouter = ~all(gB.grainId,2);
-  faceColor(isouter,:) = grainColor(nonzeros(gB.grainId(isouter,:)),:);
+
+  isouter = sum(grains.I_CF(:,gB.id) ,1)';
+  faceColor(isouter==1,:) = grainColor(gB.grainId(isouter==1,1),:);
+  faceColor(isouter==-1,:) = grainColor(gB.grainId(isouter==-1,2),:);
 
 elseif nargin>1 && isnumeric(varargin{1})
   % color by property
 
   grainColor = varargin{1};
   varargin{1} = [];
-  assert(numel(grainColor) == length(grains),...
+
+  assert(any(numel(grainColor) == length(grains) * [1,3]),...
     'Number of grains must be the same as the number of data');
 
-  faceColor = NaN(size(poly,1),1);
-  isouter = ~all(gB.grainId,2);
-  faceColor(isouter,:) = grainColor(nonzeros(gB.grainId(isouter,:)),:);
-  colorbar
+  faceColor = NaN(size(poly,1),size(grainColor,2));
+
+  isouter = sum(grains.I_CF(:,gB.id) ,1)';
+  faceColor(isouter==1,:) = grainColor(gB.grainId(isouter==1,1),:);
+  faceColor(isouter==-1,:) = grainColor(gB.grainId(isouter==-1,2),:);
+
+  if (numel(grainColor) == length(grains));  colorbar; end
 
 elseif check_option(varargin,'FaceColor')
   faceColor = get_option(varargin, 'FaceColor') .* ones(size(poly,1),3);
@@ -55,8 +61,10 @@ else
 
   grainColor = [1,1,1 ; grains.color];
   faceColor = 0.5 .* ones(size(poly,1),3);
-  isouter = ~all(gB.phaseId,2);
-  faceColor(isouter,:) = grainColor(nonzeros(gB.phaseId(isouter,:)),:);
+
+  isouter = sum(grains.I_CF(:,gB.id) ,1)';
+  faceColor(isouter==1,:) = grainColor(gB.phaseId(isouter==1,1),:);
+  faceColor(isouter==-1,:) = grainColor(gB.phaseId(isouter==-1,2),:);
 
 end
 
