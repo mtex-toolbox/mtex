cf11c4917b7bbdf716796abfb760377e7a72714e
BjaJac <102985804+BjaJac@users.noreply.github.com>
2024-01-24 13:02:50 +0100

    small improvements

--- doc/Grains/Grains3D.m
+++ doc/Grains/Grains3D.m
@@ -15,27 +15,26 @@ grains3 = job.simulateGrains(odf,numGrains)
 % or you can load an existing tesselation file
 %grains3 = grain3d.load('allgrains.tess','CS',cs)
 
-%%
+%% Plotting Grains
 
 % set camera
 how2plot = plottingConvention;
 how2plot.outOfScreen = vector3d(-10,-5,2);
 how2plot.east = vector3d(1,-2,0);
 
+% colorize by mean orientation
 plot(grains3,grains3.meanOrientation)
 setCamera(how2plot)
 
-%%
-
+%% 
+% colorize by volume
 plot(grains3,grains3.volume)
 setCamera(how2plot)
 
-
 %% 
-
+% plot single grains, color by phase
 figure
 grains = grains3(1:5)
-
 plot(grains)
 
 
@@ -49,25 +48,14 @@ P0 = vector3d(0.5,0.5,0.5);
 
 % Slice by plane normal and point
 N = vector3d(1,-1,0);
-% not working yet (produces a V with NaN)
-grains1_10= grains3.slice(N,P0)
+grains1_10 = grains3.slice(N,P0)
 
+how2plot = plottingConvention;
 how2plot.outOfScreen = N;
 plot(grains1_10,grains1_10.meanOrientation)
-
 setCamera(how2plot)
 
 %%
-
-grains = grains3(1:5)
-
-plot(grains,grains.meanOrientation)
-how2plot.outOfScreen = vector3d(-10,-4,1);
-how2plot.east = vector3d(2,-5,0);
-setCamera(how2plot)
-
-%%
-
 
 % Slice by matgeom plane
 N3 = vector3d(2,2,4);
@@ -91,3 +79,13 @@ how2plot = plottingConvention;
 how2plot.outOfScreen = vector3d(-10,-5,2);
 how2plot.east = vector3d(1,-2,0);
 setCamera(how2plot)
+
+%% 
+% Plot single 3D grains together with slices
+grains = grains3(1:5)
+
+plot(grains,grains.meanOrientation)
+how2plot.outOfScreen = vector3d(-10,-4,1);
+how2plot.east = vector3d(2,-5,0);
+setCamera(how2plot)
+
