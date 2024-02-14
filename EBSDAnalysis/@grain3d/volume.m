c86c1fc3b6f42654906be78d5349241d0c19875b
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-02-14 16:10:03 +0100

    minor speedup

--- EBSDAnalysis/@grain3d/volume.m
+++ EBSDAnalysis/@grain3d/volume.m
@@ -2,13 +2,16 @@ function volume = volume(grains, varargin)
 
 volume = zeros(size(grains.id));
 
+xyz = grains.boundary.allV.xyz;
+
 for i = 1:length(grains.id)
   Cells_Faces = grains.I_CF(i,:);
   Polys = grains.poly(grains.boundary.id2ind(find(Cells_Faces)));
+  
   % flip Polys, so normal direction pointing outwards
   Cells_Faces = nonzeros(Cells_Faces);
-  Polys(Cells_Faces == -1) = cellfun(@(c) fliplr(c), Polys(Cells_Faces == ...
-    -1), 'UniformOutput', false);
+  Polys(Cells_Faces == -1) = cellfun(@(c) fliplr(c), ...
+    Polys(Cells_Faces == -1), 'UniformOutput', false);
 
-  volume(i) = meshVolume(grains.boundary.allV.xyz, Polys);
+  volume(i) = meshVolume(xyz, Polys);
 end
