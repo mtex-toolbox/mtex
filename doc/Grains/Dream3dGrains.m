5f1e312e87e1d3b60281974beec9a2cc50ff85c0
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-02-23 14:45:39 +0100

    plot face normals to see that they are not correct :)

--- doc/Grains/Dream3dGrains.m
+++ doc/Grains/Dream3dGrains.m
@@ -9,4 +9,18 @@ grains = grain3d.load(fname)
 
 %%
 
+clf
+
+id = 2;
+plot(grains(id))
+
+c = grains(id).boundary.centroid;
+
+%hold on
+%scatter3(c.x,c.y,c.z,'.')
+%hold off
+
+hold on
+quiver3(c,grains(id).boundary.N,'arrowSize',0.15)
+hold off
 
