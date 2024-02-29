db5edc4c3a69072d9163f140c2fa311acab8fd9a
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-02-29 06:36:25 +0100

    minor

--- geometry/@embedding/normalize.m
+++ geometry/@embedding/normalize.m
@@ -1,4 +1,16 @@
 function obj = normalize(obj)
+% normalize to one
+%
+% Syntax
+%
+%   e = normalize(e)
+%
+% Input
+%  e - @embedding
+%
+% Output
+%  e - @embedding
+%
 
 obj = obj ./ norm(obj);
 
