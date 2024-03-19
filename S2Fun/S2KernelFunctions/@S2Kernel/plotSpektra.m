8024e48cee03fc6329a2e7ec7518e2a34a77d225
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-03-19 07:31:37 +0100

    minor

--- S2Fun/S2KernelFunctions/@S2Kernel/plotSpektra.m
+++ S2Fun/S2KernelFunctions/@S2Kernel/plotSpektra.m
@@ -2,15 +2,18 @@ function plotSpektra(psi,varargin)
 % plot the Legendre coefficients of the kernel function
   
 bw = get_option(varargin,'bandwidth',32);
+n = 0:bw;
+A = zeros(1,bw+1);
+
 bw = min(bw,length(psi.A)-1);
-A = reshape(psi.A(1:bw+1),1,[]);
+
+A(1:bw+1) = psi.A(1:bw+1);
+A = A./ (2*n+1);
 
 if check_option(varargin,'logarithmic')
-  optiondraw(loglog(0:bw,abs(A)./(2*(0:bw)+1),...
-    'marker','o','MarkerSize',5),varargin{:});
+  optiondraw(loglog(n, abs(A), 'marker','o','MarkerSize',5),varargin{:});
 else
-  optiondraw(semilogx(0:bw,A./(2*(0:bw)+1),...
-    'marker','o','MarkerSize',5),varargin{:});
+  optiondraw(semilogx(n, A, 'marker','o','MarkerSize',5),varargin{:});
 end
 set(gcf,'Name',['Legendre coefficients of the kernel ',inputname(1)]);
 
