c857810220dcf119795f1368608f9a14286c3445
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2024-03-21 16:48:22 +0100

    add docu grad curl div antiderivative

--- doc/SO3Functions/SO3FunVectorField.m
+++ doc/SO3Functions/SO3FunVectorField.m
@@ -133,22 +133,57 @@ plot(SO3VF1);
 clf;
 quiver3(SO3VF2);
 
-%% Differential operators (Gradient, Divergence, Curl)
+%% Differential operators (Gradient, Antiderivative, Curl, Divergence)
 %
 % The gradient of an @SO3Fun in some specific rotation is an element of 
-% the tangent space, i.e. an @SO3TangentVector. Hence 
+% the tangent space, i.e. an @SO3TangentVector. We are also able to compute
+% the gradient as rotational function, i.e. as @SO3VectorField.
+G = SO3F.grad
 
+%%
+% We can evaluate this to get the gradient of |SO3F| at some specifiv
+% rotations or we can directly compute this @SO3TangentVector
+G.eval(rot)
+SO3F.grad(rot)
 
+%%
+% Now one can perform an minimization by gradient descent method which 
+% internally uses the gradient.
 
+plot(SO3F,'phi2',(0:3)*30*degree)
+rot = orientation.byEuler(300*degree,125*degree,30*degree);
 
+% Do steepest descent
+m = steepestDescent(SO3F,rot)
 
+% For Visualisation lets do single gradient steps and plot them
+for i=1:10
+  rot(i+1) = steepestDescent(SO3F,rot(i),'maxIter',1);
+end
+annotate(rot)
 
+%%
+% An @SO3VectorField is conservativ, i.e. it is the gradient of some
+% @SO3Fun if its curl (rotor) is 0.
 
+% curl of SO3VF1
+c1 = curl(SO3VF1)
+sqrt(sum(norm(c1.SO3F).^2))
 
+% curl of G
+c2 = curl(G)
+sqrt(sum(norm(c2.SO3F).^2))
 
+%%
+% Hence |SO3VF1| is not conservativ, but |G| is conservativ.
+% So we are able to compute the antiderivative of |G|.
+A = G.antiderivative
 
+%%
+% Now |A| is again the same as |SO3F| with respect to some constant:
+norm(SO3F-A-1)
 
-
-
-
+%%
+% Similarly we can compute the divergence of an @SO3VectorField.
+G.div
 
