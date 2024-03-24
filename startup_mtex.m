7ed5883e2c900522dfc47d0f5d3bfa98523cbed9
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-03-24 12:10:40 +0100

    omit warning with 'rotation' beein twice in search path

--- startup_mtex.m
+++ startup_mtex.m
@@ -157,7 +157,7 @@ end
 % set MTEX search path
 function setMTEXPath(local_path)
 
-exclPath = {'data','makeDoc','templates','nfft','ODF_Components_old','kernelFunctions'};
+exclPath = {'data','makeDoc','templates','nfft','ODF_Components_old','kernelFunctions','deprecated'};
 
 if ~MATLABverLessThan('8.4'), exclPath = [exclPath,'8.4']; end
 if ~MATLABverLessThan('8.5'), exclPath = [exclPath,'8.5']; end
@@ -380,7 +380,7 @@ saSubDirs = saSubDirs([saSubDirs.isdir]); %Handles files without extensions that
 %Loop through the directory list and recursively call this function.
 for intDirIndex = 1 : length(saSubDirs)
   strThisDirName = saSubDirs(intDirIndex).name;
-  blnIgnoreDir = any(strcmpi(strThisDirName, { 'private', 'CVS', '.', '..', caStrsIgnoreDirs{:} }));
+  blnIgnoreDir = any(strcmpi(strThisDirName, [ {'private'}, {'CVS'}, {'.'}, {'..'}, caStrsIgnoreDirs(:)' ]));
   blnDirBegins = any(strncmp(strThisDirName, {'@', '.','+'}, 1));
   if ~(blnIgnoreDir || blnDirBegins)
     strThisStartDir = sprintf('%s%s%s', strStartDir, strFileSep, strThisDirName);
