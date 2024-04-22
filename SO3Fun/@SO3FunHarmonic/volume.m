88de3d9bb78f5c2ee7c67e6ea6357c2c582de8dd
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2024-04-22 15:15:34 +0200

    symmetries around some orientation

--- SO3Fun/@SO3FunHarmonic/volume.m
+++ SO3Fun/@SO3FunHarmonic/volume.m
@@ -26,17 +26,11 @@ function v = volume(SO3F,center,radius,varargin)
 % See also
 % SO3Fun/volume SO3Fun/fibreVolume SO3Fun/entropy SO3Fun/textureindex
 
-% TODO: antipodal is not considered up to now
-if SO3F.antipodal
-  warning(['The antipodal property is not considered up to now by the volume' ...
-     ' computation of an SO3Fun.'])
-end
-
 if isa(center,'fibre')
-  
   v = fibreVolume(SO3F,center.h,center.r,radius,varargin{:});  
   return
 end
+
 
 % In case of symmetries the computation of the volume by the chebychev 
 % series only works for a small radius
@@ -50,33 +44,14 @@ else
   minAngle = pi;
 end
 minAngle = min(minAngle(minAngle>0.01));
-if cs ~= crystalSymmetry && ss ~= specimenSymmetry || radius>minAngle
+if (cs ~= crystalSymmetry && ss ~= specimenSymmetry) || radius>minAngle || SO3F.antipodal
   v = volume@SO3Fun(SO3F,center,radius);
   return
 end
 
-% get an rotational function by symmetrizing
-SO3Fs = SO3F; SO3Fs.fhat = 0;
-N = SO3F.bandwidth;
-SO3Fs.bandwidth = N;
 
-% rotate SO3F such that center -> rotation.id
-if center ~= rotation.id
-  SO3F = rotate(SO3F,inv(center));
-end
-  
-% set all Fourier coefficients f_hat(n,k,l)=0 for k ~= l and compute the
-% mean in each Fourier matrix
-p = 0;
-for n=0:N
-  ind = deg2dim(n)+1:2*n+2:deg2dim(n+1);
-  p(n+1) = mean(SO3F.fhat(ind));
-end
-psi = SO3Kernel(real(p).'.*sqrt(2*(0:N)'+1));
-
-if SO3F.antipodal
-  radius = min(radius,90*degree);
-end
+% get an rotational function
+[~,psi] = SO3F.symmetrise(center);
 
 % integrate by trapezoidal rule
 len = 1000;
@@ -87,9 +62,6 @@ y = w.*y;
 v = radius/len * (0.5*y(1)+sum(y(2:end-1))+0.5*y(end));
 
 % Symmetry
-if SO3F.antipodal
-  v = 2*v;
-end
 v = numProper(cs)*numProper(ss)*v;
 
 end
