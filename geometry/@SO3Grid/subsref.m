cccadaa77b5616989f479da2bce51b7767b43162
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2024-05-30 17:24:26 +0200

    get single Euler angles of rotations by commands

--- geometry/@SO3Grid/subsref.m
+++ geometry/@SO3Grid/subsref.m
@@ -2,6 +2,14 @@ function varargout = subsref(S3G,varargin)
 % overloads subsref
 
 try
+  s = varargin{:};
+  if strcmp(s.type,'.') && strcmp(s.subs,'gamma')
+    [varargout{1:nargout}] = S3G.gamma;
+    return
+  end
+end
+
+try
   % subindexing SO3Grid is orientation!!
   [varargout{1:nargout}] = subsref(orientation(S3G),varargin{:});
 catch %#ok<CTCH>
