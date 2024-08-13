b3a93c320cd055341f9424be00d68a929bfdcdcf
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-08-13 10:48:51 +0200

    when doing grains3(ind).meanOrientation access the meanorientation directly

--- EBSDAnalysis/@grain3d/subsref.m
+++ EBSDAnalysis/@grain3d/subsref.m
@@ -10,6 +10,44 @@ function varargout = subsref(grains3,s)
 %  grains - @grain2d
 %  cond   - logical array with same size as grains
 %
+
+% some special cases to speed things up
+if strcmp(s(1).type,'()') && ...
+    length(s)>1 && strcmp(s(2).type,'.') && strcmp(s(2).subs,'meanOrientation')
+  
+  if strcmp(s(1).type,'{}')
+    ind = grains3.id2ind(s(1).subs{1});
+  else
+    ind = subsind(grains3,s(1).subs);
+  end
+  
+  phId = unique(grains3.phaseId(ind));
+
+  if length(phId) > 1
+    error('MTEX:MultiplePhases',['\n' ...
+      '----------------------------------------------------------------\n'...
+      'Your variable contains the phases: ' ...
+      grains3.mineralList{phId(1)} ', ' grains3.mineralList{phId(2)} '\n\n' ...
+      'However, you are executing a command that is only permitted for a single phase!\n\n' ...
+      'Please read the chapter ' doclink('EBSDSelect','"select EBSD data"')  ...
+      ' for how to restrict grains to a single phase.\n' ...
+      '----------------------------------------------------------------\n']);
+  end
+  if isempty(ind)
+    ori = orientation;
+  else
+    ori = orientation(grains3.prop.meanRotation(ind),grains3.CSList{phId});
+  end
+    
+  if numel(s)>2
+    [varargout{1:nargout}] = builtin('subsref',ori,s(3:end));
+  else
+    varargout{1} = ori;
+  end
+  return
+end
+
+
 if strcmp(s(1).type,'()') || strcmp(s(1).type,'{}')
   
   if strcmp(s(1).type,'{}')
