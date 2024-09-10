3a51bbb68dc1cfd4060a8e56ac987c9c3bbad12c
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-09-10 09:47:04 +0200

    bugfix in tensor/sym

--- TensorAnalysis/@tensor/sym.m
+++ TensorAnalysis/@tensor/sym.m
@@ -1,5 +1,9 @@
-function S = sym(T,varargin)
+function T = sym(T,varargin)
 % the symmetric part S = 0.5*(T + T') of a tensor
+%
+% Description
+% This function can also be used to fill NaN values to generate a symmetric
+% tensor
 %
 % Syntax
 %   S = sym(T)
@@ -11,32 +15,42 @@ function S = sym(T,varargin)
 %  S - @tensor
 %
 
-if T.rank < 2
-  S = T;
-  return;
-elseif T.rank == 2
-  S = 0.5 * (T + T');
-  return
-end
-
 S = T;
-dims = T.rank+1 : ndims(T.M);
-allP = perms(1:T.rank);
 
-if any(isnan(S.M(:)))
-  
-  % take the mean over all permutations of the dimensions
-  for p = allP.'
-    S.M = max(S.M, permute(T.M,[p;dims]),'omitnan');
-  end
-  
-else
-  S.M = S.M .* 0;
+switch T.rank
 
-  % take the mean over all permutations of the dimensions
-  for p = allP.'
-    S.M = S.M + permute(T.M,[p;dims]);
-  end
-  S.M = S.M ./ size(allP,1);
+  case 0 % nothing to do
+  case 1 % nothing to do
+  case 2 
   
+    if any(isnan(T.M(:)))
+      S.M = max(S.M, permute(S.M,[2 1 3:ndims(S.M)]));
+    else
+      S.M = 0.5 * (S.M + permute(S.M,[2 1 3:ndims(S.M)]));
+    end
+    
+  otherwise
+
+    dims = (T.rank+1 : ndims(T.M)).';
+    allP = perms(1:T.rank);
+
+    if any(isnan(S.M(:)))
+  
+      % take the mean over all permutations of the dimensions
+      for p = allP.'
+        S.M = max(S.M, permute(T.M,[p;dims]),'omitnan');
+      end
+  
+    else
+      S.M = S.M .* 0;
+
+      % take the mean over all permutations of the dimensions
+      for p = allP.'
+        S.M = S.M + permute(T.M,[p;dims]);
+      end
+      S.M = S.M ./ size(allP,1);
+
+    end
+end
+ 
 end
