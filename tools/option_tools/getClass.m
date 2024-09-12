646b9cdbb86b83af6ed18e72f7693837000d600d
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-09-12 13:53:36 +0200

    minor

--- tools/option_tools/getClass.m
+++ tools/option_tools/getClass.m
@@ -1,4 +1,5 @@
 function [out,list] = getClass(list,className,default,varargin)
+% returns the first element of the requested class from a list
 
 if nargin == 2, default = [];end
 
@@ -10,7 +11,7 @@ else
   out = list{match};
 end
 
-% remove all occurence of this class in the list
+% remove all occurrence of this class in the list
 if nargout > 1
   list(cellfun(@(x) isa(x,className),list)) = [];
 end
