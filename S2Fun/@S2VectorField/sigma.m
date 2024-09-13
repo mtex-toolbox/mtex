f5563f573c7199273558359ac9b4ff75d4d64478
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-09-13 08:48:49 +0200

    bug fix

--- S2Fun/@S2VectorField/sigma.m
+++ S2Fun/@S2VectorField/sigma.m
@@ -4,7 +4,7 @@ function vF = sigma(varargin)
 % we want the singular point to be always at the lower hemisphere
 pC = getClass(varargin,'plottingConvention',plottingConvention.default);
 
-vF = S2VectorFieldHandle(@(r) pC.rot * local(inv(pC.rot) * r));
+vF = S2VectorFieldHandle(@(r) pC.rot .* local(inv(pC.rot) .* r));
 
   function v = local(r)
     [theta,rho] = polar(r); %#ok<POLAR>
