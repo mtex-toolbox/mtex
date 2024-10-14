cf23c9034e697a4c87512113837c033cbda81320
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-10-14 16:29:39 +0200

    principleAxis for arbitrary polyhedron + important bug fix

--- EBSDAnalysis/@grain3d/principalComponents.m
+++ EBSDAnalysis/@grain3d/principalComponents.m
@@ -27,10 +27,10 @@ if isnumeric(grains.F)
   % the unnormalized normals
   N = cross(B-A,C-A,2);
 
-  % volume of each grain via divergence theorem
+  % volume and centroid of each grain via divergence theorem
   vol = grains.I_GF * sum(A .* N,2) / 6;
-  c = grains.centroid.xyz;
-
+  c = grains.I_GF * (N.*((A+B).^2 + (B+C).^2 + (C+A).^2)) / 48 ./ vol;
+  c = c.xyz;
 
   % the quadrature points
   P1 = (1/3 * A + 1/3 * B + 1/3 * C);
@@ -53,23 +53,7 @@ if isnumeric(grains.F)
   Vxyz = grains.I_GF * (N .* Ixyz) / 2;
   Vxyz = Vxyz - vol .* [c(:,2) .* c(:,3), c(:,1) .* c(:,3), c(:,1) .* c(:,2)];
 
-  % compute the principle components of the cross correlation matrix
-  [v,lambda] = eig3(Vx2y2z2(:,1),Vxyz(:,3),Vxyz(:,2),Vx2y2z2(:,2),Vxyz(:,1),Vx2y2z2(:,3));
-  v = (v .* lambda).';
-
-  % scale to volume
-  v = v .* (vol ./ (4/3*pi*prod(norm(v),2))).^(1/3);
-
-  if nargout > 1
-    a = v(:,3);
-    b = v(:,2);
-    c = v(:,1);
-  else
-    a = v;
-  end
-
-  
-else
+else % implicitly do the triangulation
 
   % the number of triangles per face
   numTetra = cellfun(@numel,grains.F)-3;
@@ -101,5 +85,44 @@ else
   c = vector3d.byXYZ(N.*((A+B).^2 + (B+C).^2 + (C+A).^2) / 48);
 
   c = grains.I_GF * accumarray(id,c) ./ vol;
+  c = c.xyz;
+
+  % the quadrature points
+  P1 = (1/3 * A + 1/3 * B + 1/3 * C);
+  P2 = (3/5 * A + 1/5 * B + 1/5 * C);
+  P3 = (1/5 * A + 3/5 * B + 1/5 * C);
+  P4 = (1/5 * A + 1/5 * B + 3/5 * C);
+
+  % the integrals of x^3, y^3, z^3 over the triangles
+  Ix3y3z3 = -9/16 * P1.^3 + 25/48 * (P2.^3 + P3.^3 + P4.^3);
+  % the integral of x*y*z over the triangles
+  Ixyz = -9/16 * prod(P1,2) + 25/48 * (prod(P2,2) + prod(P3,2) + prod(P4,2));
   
+  % the volume integrals of x.^2, y.^2, z.^2
+  Vx2y2z2 = grains.I_GF * accumarray(id,vector3d.byXYZ(N .* Ix3y3z3)) / 6;
+    
+  % I (x-mx)^2 = I x^2 - mx^2
+  Vx2y2z2 = Vx2y2z2.xyz - vol .* c.^2;
+  
+  % the volume integrals of yz, xz, xy
+  Vxyz = grains.I_GF * accumarray(id,vector3d.byXYZ(N .* Ixyz)) / 2;
+  Vxyz = Vxyz.xyz - vol .* [c(:,2) .* c(:,3), c(:,1) .* c(:,3), c(:,1) .* c(:,2)];
+
+end
+
+%Vx2y2z2 = sqrt(Vx2y2z2); Vxyz = sqrt(Vxyz);
+
+% compute the principle components of the cross correlation matrix
+[v,lambda] = eig3(Vx2y2z2(:,1),Vxyz(:,3),Vxyz(:,2),Vx2y2z2(:,2),Vxyz(:,1),Vx2y2z2(:,3));
+v = (v .* sqrt(lambda)).';
+
+% scale to volume
+v = v .* (vol ./ (4/3*pi*prod(norm(v),2))).^(1/3);
+
+if nargout > 1
+  a = v(:,3);
+  b = v(:,2);
+  c = v(:,1);
+else
+  a = v;
 end
