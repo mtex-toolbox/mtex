82f6c3a0ed98bc2c5ab12e2a2d3eb164d59486aa
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-11-07 12:32:45 +0100

    plotTDF should be a polar plot

--- tools/misc_tools/plotTDF.m
+++ tools/misc_tools/plotTDF.m
@@ -13,9 +13,7 @@ function p1 = plotTDF(bc,pd,varargin)
 %  linestyle -
 %
 
-[mtexFig,isNew] = newMtexFigure(varargin{:});
-
-p1 = polar(bc,pd,'parent',mtexFig.gca);
+p1 = polarplot(bc,pd);
 % set linewidth
 p1.LineWidth = get_option(varargin,'linewidth',2);
 % set linecolor
@@ -30,34 +28,10 @@ p1.Tag='doNotDelete';
 % fix FontSize
 txA = findall(gca,'type','text');
 for k=1:length(txA)
-    txA(k).FontSize=getMTEXpref('FontSize')-4;
+  txA(k).FontSize = getMTEXpref('FontSize')-4;
 end
 
-if check_option(varargin,'nogrid')
-    % find all of the lines in the polar plot
-    h = findall(gcf,'type','line');
-    for k=1:length(h)
-        id_del(k) = ~strcmp(h(k).Tag,'doNotDelete');
-        % delete all other lines excet our plot
-    end
-    delete(h(id_del));
-end
 
-if check_option(varargin,'nolabels')
-    % find and remove the radial text labels in the polar plot
-    tx=findall(gcf,'type','text');
-    for k=1:length(tx)
-        id_delt(k) = strcmp(tx(k).HorizontalAlignment,'left');
-    end
-    delete(tx(id_delt));
-end
 
-% although it's inside an mtexFigure we need to set plotting conventions
-mtexFig.setCamera('xAxisDirection',getMTEXpref('xAxisDirection'));
-mtexFig.setCamera('zAxisDirection',getMTEXpref('zAxisDirection'));
 
-% 
-if isNew, mtexFig.drawNow(varargin{:});end
-% 
-% 
 end
