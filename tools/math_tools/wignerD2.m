5452cbd05ded7f13ae25721dc9b088d423c11b25
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2024-11-13 10:54:51 +0100

    make suitable to other Wigner functions

--- tools/math_tools/wignerD2.m
+++ tools/math_tools/wignerD2.m
@@ -1,6 +1,8 @@
 function D = wignerD2(l, g)
 % spherical harmonics of degree l
 %
+% $$ D_n^{k,l}(R) = \int_{S^2} Y_n^k(\xi) \, \overline{Y_n^l(R\cdot\xi)} d{\xi} $$
+%
 % Input
 %  l     - degree
 %  theta - azimuth angle
@@ -10,11 +12,11 @@ function D = wignerD2(l, g)
 %  Y - (2l+1) x numel(theta,rho) matrix of function values
 %
 % See also
-% wignerD sphericalY
+% WignerD sphericalY Wigner_D
 
 r = equispacedS2Grid('resolution',1.5*degree);
 
 Y1 = sphericalY(l,r);
 Y2 = sphericalY(l,g*r);
 
-D = Y1' * Y2./length(r)*pi*4;
+D = Y1.' * conj(Y2)./length(r)*pi*4;
