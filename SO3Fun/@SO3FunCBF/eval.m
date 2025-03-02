a78c23fb11c6c7389ff5d822a788cbfec9d28a6d
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2025-03-02 14:34:51 +0100

    Revert "SO3FunCBF should return a Nx1 list of values"
    This reverts commit f4486c302baad7e741d31361a5561332e5580ae1.

--- SO3Fun/@SO3FunCBF/eval.m
+++ SO3Fun/@SO3FunCBF/eval.m
@@ -8,6 +8,7 @@ function f = eval(SO3F,rot,varargin)
 %   ensureCompatibleSymmetries(SO3F,rot)
 % end
 
+s = size(rot);
 rot = rot(:);
 
 h = symmetrise(SO3F.h.normalize,'unique');
@@ -17,7 +18,7 @@ gh = reshape((SO3F.SS.properGroup * rot).' * h, length(rot),[],length(SO3F.h));
 
 f = mean(SO3F.psi.eval(dot(gh,reshape(normalize(SO3F.r),1,1,[]),'noSymmetry')),2);
 
-f = reshape(f,length(rot),[]) * SO3F.weights;
+f = reshape(reshape(f,length(rot),[]) * SO3F.weights,s);
 
 if isalmostreal(f)
   f = real(f);
