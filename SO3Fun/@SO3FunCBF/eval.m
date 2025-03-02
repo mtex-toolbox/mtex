f4486c302baad7e741d31361a5561332e5580ae1
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2025-03-02 14:12:30 +0100

    SO3FunCBF should return a Nx1 list of values

--- SO3Fun/@SO3FunCBF/eval.m
+++ SO3Fun/@SO3FunCBF/eval.m
@@ -8,7 +8,6 @@ function f = eval(SO3F,rot,varargin)
 %   ensureCompatibleSymmetries(SO3F,rot)
 % end
 
-s = size(rot);
 rot = rot(:);
 
 h = symmetrise(SO3F.h.normalize,'unique');
@@ -18,7 +17,7 @@ gh = reshape((SO3F.SS.properGroup * rot).' * h, length(rot),[],length(SO3F.h));
 
 f = mean(SO3F.psi.eval(dot(gh,reshape(normalize(SO3F.r),1,1,[]),'noSymmetry')),2);
 
-f = reshape(reshape(f,length(rot),[]) * SO3F.weights,s);
+f = reshape(f,length(rot),[]) * SO3F.weights;
 
 if isalmostreal(f)
   f = real(f);
