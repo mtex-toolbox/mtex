749d406c0a3242b4b522da06c5a7190c492b8426
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2025-03-12 01:04:31 +0100

    makeGrid should be possible for any spherical projection

--- plotting/sphericalProjections/eareaProjection.m
+++ plotting/sphericalProjections/eareaProjection.m
@@ -43,61 +43,6 @@ classdef eareaProjection < sphericalProjection
       v = sP.pC.rot * v;
     end
     
-    function S2G = makeGrid(sP, varargin)
-      
-      % get resolution
-      res = get_option(varargin,'resolution',1*degree);
-
-      for iP = 1:length(sP)
-
-        % rotate the spherical region
-        sR = inv(sP(iP).pC.rot) * sP(iP).sR;
-
-        [rhoMin,rhoMax] = rhoRange(sR);
-        rho = linspace(rhoMin(1),rhoMax(1),round(1+(rhoMax(1)-rhoMin(1))/res));
-        for i = 2:length(rhoMin)
-          rho = [rho,nan,linspace(rhoMin(i),rhoMax(i),round(1+(rhoMax(i)-rhoMin(i))/res))]; %#ok<AGROW>
-        end
-
-        [thetaMin,thetaMax] = thetaRange(sR,rho);
-
-        % remove values out of region
-        ind = (thetaMax > 1e-5) & (thetaMin < pi - 1e-5);
-        
-        ind(end) = ind(end-1); ind(1) = ind(2);
-        
-        % we should put some nans to seperate regions
-        rho(diff(ind) == 1) = nan;
-        ind(diff(ind) == 1) = true;
-
-        rho(~ind) = []; thetaMin(~ind) = []; thetaMax(~ind) = [];
-
-        if isempty(rho)
-          v = vector3d;
-        else % generate grid
-
-          dtheta = thetaMax - thetaMin;
-
-          % ensure an odd number of points to have some points at the equator
-          ntheta = max(3,2*round(max(dtheta./res./2))+1);
-
-          theta = linspace(0,1,ntheta).' * dtheta + repmat(thetaMin,ntheta,1);
-
-          rho = repmat(rho,ntheta,1);
-
-          v = vector3d.byPolar(theta,rho);
-        end
-
-        v = v.setOption('plot',true,'resolution',res,'region',sP(iP).sR);
-        % the above procdure does not work so well if we have a full sphere
-        % and the theta region is not connected
-        % thatswhy we have to check once again
-        v(~sR.checkInside(v)) = nan;
-
-        S2G{iP} = sP(iP).pC.rot .* v;
-      end
-    end
-
   end
   
 end
