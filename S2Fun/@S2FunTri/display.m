bf05b82f5e86b238e60ee11ca5b583335d41b78a
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-03-16 15:38:27 +0100

    doc

--- S2Fun/@S2FunTri/display.m
+++ S2Fun/@S2FunTri/display.m
@@ -6,3 +6,4 @@ displayClass(sF,inputname(1),varargin{:});
 disp(' ');
 disp(['  ' varlink([inputname(1),'.vertices'],'vertices') ': ',size2str(sF.vertices)])
 disp(['  ' varlink([inputname(1),'.values'],'values') ':   ',size2str(sF.values)])
+disp(' ');
