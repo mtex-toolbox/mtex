6cea96e5427fa037bff6c83871edfaef1f06b825
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-03-16 14:00:56 +0100

    fix S2FunTri-class (no antipodal and no symmetry)

--- S2Fun/@S2FunTri/times.m
+++ S2Fun/@S2FunTri/times.m
@@ -16,25 +16,25 @@ function sF = times(sF1,sF2)
        
 if isnumeric(sF1)
   sF = sF2;
-  
-  sF.values = sF2.values;
-  
   sF.values = times(sF1, sF.values);
-  
+  return
 elseif isnumeric(sF2)
-            
   sF = sF1;
-  
-  sF.values = sF1.values;
-  
   sF.values = times(sF1.values, sF2);
-            
-else
-            
-  sF=sF1;
-            
-  sF.values = times(sF1.values, sF2.values);
-        
+  return
 end
-        
+
+if ~isa(sF2,'S2FunTri')
+  sF2 = S2FunTri(sF1.vertices,sF2.eval(sF1.vertices));
+end
+
+if sF1.tri==sF2.tri
+  sF = sF1;
+  sF.values = sF1.values .* sF2.values;
+else
+  v = [sF1.vertices(:);sF2.vertices(:)];
+  n = [sF1.values(:).*sF2.eval(sF1.vertices(:)); sF1.eval(sF2.vertices(:)).*sF2.values(:)];
+  sF = S2FunTri(v,n);
+end
+
 end
