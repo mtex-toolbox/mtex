a3c39787155ac028d4e6578febdfa9aaeb342743
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2025-03-16 23:09:16 +0100

    minor

--- EBSDAnalysis/@EBSD3square/EBSD3square.m
+++ EBSDAnalysis/@EBSD3square/EBSD3square.m
@@ -16,13 +16,7 @@ classdef EBSD3square < EBSD3
   properties (Dependent = true)    
     gradientX       % orientation gradient in x
     gradientY       % orientation gradient in y
-    gradientZ       % orientation gradient in z
-    xmin
-    xmax
-    ymin
-    ymax
-    zmin
-    zmax
+    gradientZ       % orientation gradient in z    
   end
   
   methods
@@ -76,33 +70,6 @@ classdef EBSD3square < EBSD3
       newId = (1:length(ebsd)).';
     end
            
-    % --------------------------------------------------------------
-    
-    function out = get.xmin(ebsd)
-      out = ebsd.pos.x(1);
-    end
-    
-    function out = get.xmax(ebsd)
-      out = ebsd.pos.x(end);
-    end
-    
-    function out = get.ymin(ebsd)
-      out = ebsd.pos.y(1);
-    end
-    
-    function out = get.ymax(ebsd)
-      out = ebsd.pos.y(end);
-    end
-
-    function out = get.zmin(ebsd)
-      out = ebsd.pos.z(1);
-    end
-    
-    function out = get.zmax(ebsd)
-      out = ebsd.pos.z(end);
-    end
-    
-    
     function gX = get.gradientX(ebsd)
       % gives the gradient in X direction with respect to specimen
       % coordinate system
@@ -115,8 +82,8 @@ classdef EBSD3square < EBSD3
       gX(:,end) = - gX(:,end);
       
       % ignore grain boundaries if possible
-      try
-        gX(ebsd.grainId ~= ebsd.grainId(:,[2:end end-1],:)) = NaN;
+      if isfield(ebsd.prop.grainId)
+        gX(ebsd.prop.grainId ~= ebsd.prop.grainId(:,[2:end end-1],:)) = NaN;
       end
       
     end
@@ -133,8 +100,8 @@ classdef EBSD3square < EBSD3
       gY(end,:) = - gY(end,:);
       
       % ignore grain boundaries if possible
-      try
-        gY(ebsd.grainId ~= ebsd.grainId([2:end end-1],:,:)) = NaN;
+      if isfield(ebsd.prop.grainId)
+        gY(ebsd.prop.grainId ~= ebsd.prop.grainId([2:end end-1],:,:)) = NaN;
       end
       
     end
@@ -152,39 +119,19 @@ classdef EBSD3square < EBSD3
       gZ(end,:) = - gZ(end,:);
       
       % ignore grain boundaries if possible
-      try
-        gZ(ebsd.grainId ~= ebsd.grainId(:,:,[2:end end-1])) = NaN;
+      if isfield(ebsd.prop.grainId)
+        gZ(ebsd.prop.grainId ~= ebsd.prop.grainId(:,:,[2:end end-1])) = NaN;
       end
       
     end
 
-    function h = gridBoundary(ebsd)
-
-      x = ebsd.xmin:ebsd.dx:ebsd.xmax;
-      y = ebsd.ymin-ebsd.dy:ebsd.dy:ebsd.ymax+ebsd.dy;
-      z = ebsd.zmin-ebsd.dz:ebsd.dz:ebsd.zmax+ebsd.dz; %NOT SURE THIS IS CORRECT
-
-% TO BE IMPLEMENTED
-      h = [];
-
-%       h= [
-%         repmat(ebsd.xmin-ebsd.dx, numel(y),1), y.' ; ...
-%         x.', repmat(ebsd.ymin-ebsd.dy, numel(x), 1) ; ...
-%         x.', repmat(ebsd.ymax+ebsd.dy, numel(x), 1) ; ...
-%         repmat(ebsd.xmax+ebsd.dx, numel(y),1), y.'];
-    
+    function e = end(ebsd3,i,n)
+      if n==1
+        e = size(ebsd3.id,1);
+      else
+        e = size(ebsd3.id,i);
+      end
     end
-
-    % some testing code - gradient can be either in specimen coordinates or
-    % in crystal coordinates 
-    % 
-    % cs = crystalSymmetry('321')
-    % ori1 = orientation.rand(cs)
-    % ori2 = orientation.rand(cs)
-    %
-    % the following output should be constant
-    % gO = log(ori1,ori2.symmetrise,SO3TangentSpace.leftVector) % true for this
-    % gO = log(ori1.symmetrise,ori2,SO3TangentSpace.leftVector) % true for this
        
   end
       
