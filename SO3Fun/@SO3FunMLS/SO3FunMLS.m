42df0d3a02a3e2b70d956d61c4b53ef2a4d7f93a
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-04-30 18:04:19 +0200

    adjust SO3FunMLS

--- SO3Fun/@SO3FunMLS/SO3FunMLS.m
+++ SO3Fun/@SO3FunMLS/SO3FunMLS.m
@@ -1,112 +1,105 @@
 classdef SO3FunMLS < SO3Fun
-  % a class representing a function on the sphere
+% a class representing a function on the rotation group
+% Syntax
+%   SO3F = SO3FunMLS(nodes,values)
+%   SO3F = SO3FunMLS(nodes,values,N,__)
+%   SO3F = SO3FunMLS(nodes,values,eps,__)
+%   SO3F = SO3FunMLS(nodes,values,eps,w,__)
+%
+% Input
+%  nodes  - @orientation,@rotation (interpolation points)
+%  values - array of function values
+%  N      - specified number of neighbors used for local interpolation
+%  eps    - support radius of the weight function
+%  w      - @function_handle (weight function)
+%
+% Output
+%  SO3F - @SO3FunMLS
+%
+% Options
+%  degree - the polynomial degree used for approximation
+%
+% Flags
+%  all_degrees  - use even AND odd degrees up to degree if true
+%  centered     - only evaluate the basis near the pole if true
+%  tangent      - use polynomials on the tangent space
+%  monomials    -
+%  hat          - use hat function as weight function
+%  squared_hat  - use squared-hat function as weight function
+%  indicator    - use indicator function as weight function
+%
+
 
   properties
-    nodes       = [];      % orientations where the function values are known
-    values      = [];      % the corresponding values
-    degree      = 3;       % the polynomial degree used for approximation
-    delta       = 0;       % support radius of the weight function
-    nn          = 0;       % specified number of neighbors to use 
-    w           = @(t)(max(1-t, 0).^4 .* (4*t+1)); % wendland weight function
-    all_degrees = false;   % use even AND odd degrees up to degree if true
-    centered    = false;   % only evaluate the basis near the pole if true
-    tangent     = false;   % use polynomials on the tangent space
-    SLeft    	             % defined in SO3Fun
-    SRight   	             % defined in SO3Fun
-    antipodal	             % defined in SO3Fun
-    bandwidth	             % defined in SO3Fun
-    s = crystalSymmetry('1');   % TODO: symmetry
+    nodes       = [];   % orientations where the function values are known
+    values      = [];   % the corresponding values
+    degree      = 3     % the polynomial degree used for approximation
+    delta       = 0     % support radius of the weight function
+    nn          = 0     % specified number of neighbors to use 
+    w           = @(t)(max(1-t, 0).^4 .* (4*t+1));        % wendland weight function
+    all_degrees = false % use even AND odd degrees up to degree if true
+    centered    = false % only evaluate the basis near the pole if true
+    tangent     = false % use polynomials on the tangent space
+    % monomials
+    bandwidth   = getMTEXpref('maxSO3Bandwidth');
   end
 
   properties (Dependent)
     dim;
+    antipodal
+    SLeft
+    SRight
   end
 
   methods
-    % initialize a spherical function
-    function sF = SO3FunMLS(nodes, values, varargin)
-      % set the mandatory inputs
-      % some grids in mtex are provided as row vectors 
-      if size(nodes, 1) < size(nodes, 2) 
-        nodes = orientation(nodes(':'));
-      end
-      sF.nodes = nodes;
-      sF.values = values;
+    % TODO: decide Bandwidth
+    
+    function SO3F = SO3FunMLS(nodes, values, varargin)
 
-      % set degree if given
-      if nargin >= 3 && isnumeric(varargin{1})
-        sF.degree = varargin{1};
-        varargin(1) = [];
-      end
-      % set delta or k if given
-      if nargin >= 4 && isnumeric(varargin{1})
-        temp = varargin{1};
-        % if the input is a whole number, assume that nn is specified
-        if (floor(temp) == temp)
-          sF.nn = temp;
-        else
-          sF.delta = temp;
-        end
-        varargin(1) = [];
-      % otherwise set k = 2 * dim
-      else
-        sF.nn = 2 * sF.dim;
-      end
+      if nargin == 0, return; end
+ 
+      SO3F.nodes = nodes; % preserve grid structure
+      SO3F.values = reshape(values,length(nodes),[]); % allow vector fields
 
+      % set optional arguments
+      SO3F.degree = get_option(varargin,'degree',3);
+      
       % apply flags in the function arguments and remove them afterwards
-      alldeg_specifier_pos = find(strcmp(varargin, 'all_degrees'), 1);
-      if ~isempty(alldeg_specifier_pos)
-        sF.all_degrees = true;
-        varargin(alldeg_specifier_pos) = [];
-      end
-
-center_specifier_pos = find(strcmp(varargin, 'centered'), 1);
-      if ~isempty(center_specifier_pos)
-        sF.centered = true;
-        varargin(center_specifier_pos) = [];
-      end
-
-      tangent_specifier_pos = find(strcmp(varargin, 'tangent'), 1);
-      if ~isempty(tangent_specifier_pos)
-        sF.tangent = true;
-        % this is the same as using only even/odd centered monomials and
-        % setting the z-coordinate to 1
-        sF.centered = true;
-        sF.monomials = true;
-        varargin(tangent_specifier_pos) = [];
+      SO3F.all_degrees = check_option(varargin,'all_degrees');
+      SO3F.centered = check_option(varargin,'centered');
+      if check_option(varargin,'tangent')
+        SO3F.tangent = true;
+        SO3F.centered = true;
+        % SO3F.monomials = true;
       end
 
       % get the weight function if one is specified
-      if numel(varargin) > 0
-        parser = inputParser;
-        addParameter(parser, "weight", sF.w);
-        parse(parser, varargin{:});
-        weight_arg = string(parser.Results.weight);
-        if isa(weight_arg, 'string')
-          if strcmp(weight_arg, 'hat')
-            sF.w = @(t)(max(1-t, 0));
-          elseif  strcmp(weight_arg, 'squared hat')
-            sF.w = @(t)(max(1-t, 0).^2);
-          elseif strcmp(weight_arg, 'indicator')
-            sF.w = @(t)(t .* (t < 1));
-          end
-        elseif isa(weight_arg, "function_handle")
-          sF.w = parser.results.weight;
-        end
+      if check_option(varargin,'hat')
+        SO3F.w = @(t)(max(1-t, 0));
+      elseif check_option(varargin,'squared hat')
+        SO3F.w = @(t)(max(1-t, 0).^2);
+      elseif check_option(varargin,'indicator')
+        SO3F.w = @(t)(t .* (t < 1));
       end
+      wendland = @(t)(max(1-t, 0).^4 .* (4*t+1));
+      SO3F.w = getClass(varargin,'function_handle',wendland);
 
-      if (sF.nn < sF.dim)
-        sF.nn = 2 * sF.dim;
-        warning(sprintf(...
-          ['The specified number of neighbors nn was less than the dimension dim.\n\t ' ...
-          'nn has been set to 2 * dim.']));
+      % set delta or k if given
+      if nargin > 2 && isnumeric(varargin{1})
+        temp = varargin{1};
+      else
+        temp = 2 * SO3F.dim;
       end
-
-      % set delta, if it has not been set yet. if nn is given, knnsearch is preferred
-      if (sF.delta == 0)
-        sF.delta = guess_delta(sF);
+      % if the input is a whole number, assume that nn is specified
+      if (floor(temp) == temp)
+        warning('The specified number of neighbors nn was less than the dimension dim. nn has been set to 2 * dim.');
+        SO3F.nn = max(temp,2*SO3F.dim);
+        SO3F.delta = guess_delta(SO3F);
+      else
+        SO3F.nn = 2 * SO3F.dim;
+        SO3F.delta = temp;
       end
-
+      
     end
 
     function dimension = get.dim(sF)
@@ -120,6 +113,43 @@ center_specifier_pos = find(strcmp(varargin, 'centered'), 1);
     function d = guess_delta(sF)
       d = acos(1 - 2 * sF.dim / numel(sF.nodes));
     end
+
+    function SO3F = set.SRight(SO3F,S)
+      SO3F.nodes.CS = S;
+    end
+
+    function S = get.SRight(SO3F)
+      try
+        S = SO3F.nodes.CS;
+      catch
+        S = specimenSymmetry;
+      end
+    end
+
+    function SO3F = set.SLeft(SO3F,S)
+      SO3F.nodes.SS = S;
+    end
+
+    function S = get.SLeft(SO3F)
+      try
+        S = SO3F.nodes.SS;
+      catch
+        S = specimenSymmetry;
+      end
+    end
+
+    function SO3F = set.antipodal(SO3F,antipodal)
+      SO3F.nodes.antipodal = antipodal;
+    end
+
+    function antipodal = get.antipodal(SO3F)
+      try
+        antipodal = SO3F.nodes.antipodal;
+      catch
+        antipodal = false;
+      end
+    end
+
 
   end
 
