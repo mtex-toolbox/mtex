07a8b97e9f461b9c881c5c0b048181a46ea2f6ce
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-05-07 12:26:52 +0200

    fix

--- SO3Fun/@SO3FunRBF/reshape.m
+++ SO3Fun/@SO3FunRBF/reshape.m
@@ -1,11 +1,10 @@
 function SO3F = reshape(SO3F, varargin)
 % overloads reshape
 
+SO3F.c0 = reshape(SO3F.c0, varargin{:});
 if length(varargin{1}) > 1 % varargin is matrix
-  SO3F.c0 = reshape(SO3F.c0, [numel(SO3F.center) varargin{:}]);
   SO3F.weights = reshape(SO3F.weights, [numel(SO3F.center) varargin{:}]);
 else % varargin is cell array
-  SO3F.c0 = reshape(SO3F.c0, varargin{:});
   SO3F.weights = reshape(SO3F.weights, length(SO3F.center), varargin{:});
 end
 
