c0f1828723f75bbc514fdb48d7a69062f4c699cd
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-06 15:08:45 +0200

    fix norm methods

--- SO3Fun/@SO3VectorFieldHarmonic/normSquare.m
+++ SO3Fun/@SO3VectorFieldHarmonic/normSquare.m
@@ -11,7 +11,12 @@ function SO3F = normSquare(SO3VF)
 %  SO3F - @SO3FunHarmonic
 %
 
+if SO3VF.bandwidth == 0
+  SO3F = SO3FunHarmonic(0,SO3VF.hiddenCS,SO3VF.hiddenSS);
+  return
+end
+
 SO3F = SO3FunHarmonic.quadrature(@(rot) norm(SO3VF.eval(rot)).^2,...
-  SO3VF.CS,SO3VF.SS,'bandwidth',min(getMTEXpref('maxSO3Bandwidth'),2*SO3VF.bandwidth));
+  SO3VF.hiddenCS,SO3VF.hiddenSS,'bandwidth',min(getMTEXpref('maxSO3Bandwidth'),2*SO3VF.bandwidth));
 
 end
