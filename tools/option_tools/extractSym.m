85a16fcfd270f48863e8622b50074fbecb368d16
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-06 15:06:18 +0200

    doc

--- tools/option_tools/extractSym.m
+++ tools/option_tools/extractSym.m
@@ -1,5 +1,8 @@
 function [SRight,SLeft] = extractSym(list)
-% extract crystal (SRight) and specimen (SLeft) symmetry from list of input arguments
+% extract crystal (SRight) and specimen (SLeft) symmetry from list of input 
+% arguments. The first 2 symmetries of the list are returned. If there is
+% none or just one symmetry in the list, the remaining outputs are set to 
+% standard specimen symmetry.
 
 SLeft = specimenSymmetry;
 SRight = SLeft;
