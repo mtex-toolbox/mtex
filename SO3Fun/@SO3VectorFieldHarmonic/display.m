81bf3978c915ed45e423695ab097409b07128047
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-07 15:14:39 +0200

    just for testing

--- SO3Fun/@SO3VectorFieldHarmonic/display.m
+++ SO3Fun/@SO3VectorFieldHarmonic/display.m
@@ -2,12 +2,14 @@ function display(SO3VF,varargin)
 % standard output
 displayClass(SO3VF,inputname(1),[],'moreInfo',symChar(SO3VF),varargin{:});
 
-disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
-
 if ~SO3VF.SO3F.isReal, disp('  isReal: false'); end
 disp(['  bandwidth: ' num2str(SO3VF.SO3F.bandwidth)]);
 disp(['  tangent space: ' , char(SO3VF.tangentSpace)]);
 
+disp(' ')
+
+disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
+disp( [' inner tangent space: ' char(SO3VF.internTangentSpace)] );
 disp(' ')
 
 end
