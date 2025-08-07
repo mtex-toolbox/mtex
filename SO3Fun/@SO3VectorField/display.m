0da33dc9857e701340316026511b83a8c59f11c0
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-07 15:14:39 +0200

    just for testing

--- SO3Fun/@SO3VectorField/display.m
+++ SO3Fun/@SO3VectorField/display.m
@@ -6,4 +6,8 @@ disp(['  tangent space: ' , char(SO3VF.tangentSpace)]);
 
 disp(' ')
 
+disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
+disp( [' inner tangent space: ' char(SO3VF.internTangentSpace)] );
+disp(' ')
+
 end
--- SO3Fun/@SO3VectorFieldRBF/display.m
+++ SO3Fun/@SO3VectorFieldRBF/display.m
@@ -1,5 +1,6 @@
 function display(SO3VF,varargin)
 % standard output
+
 displayClass(SO3VF,inputname(1),[],'moreInfo',symChar(SO3VF),varargin{:});
 
 disp(['  kernel: ',char(SO3VF.SO3F.psi)]);
@@ -13,5 +14,11 @@ end
 disp(['  tangent space: ' , char(SO3VF.tangentSpace)]);
 
 disp(' ')
+
+
+disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
+disp( [' inner tangent space: ' char(SO3VF.internTangentSpace)] );
+disp(' ')
+
 
 end
