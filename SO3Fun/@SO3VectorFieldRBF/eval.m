2d1a58e45a6253add4f68fc70c5845f0e328a62a
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-07 15:13:23 +0200

    update SO3VectorFieldRBF class

--- SO3Fun/@SO3VectorFieldRBF/eval.m
+++ SO3Fun/@SO3VectorFieldRBF/eval.m
@@ -2,7 +2,7 @@ function f = eval(SO3VF,rot,varargin)
 % evaluate the SO3VectorFieldRBF in rotations
 % 
 % Syntax
-%   f = eval(SO3VF,rot)         % left tangent vector
+%   f = eval(SO3VF,rot) 
 %
 % Input
 %   rot - @rotation
@@ -11,20 +11,27 @@ function f = eval(SO3VF,rot,varargin)
 %   f - @vector3d
 %
 % See also
-% 
+% SO3FunRBF/eval
+
 
 % if isa(rot,'orientation')
 %   ensureCompatibleSymmetries(SO3VF,rot)
 % end
 
-xyz = SO3VF.SO3F.eval(rot);
 
-% generate tangentspace vector
-f = SO3TangentVector(xyz.',rot(:),SO3VF.internTangentSpace);
+% check whether the hidden symmetries match the symmetries of the
+% underlying SO3Fun dependent on the tangent space representation
+check_symmetry(SO3VF)
+
+% evaluation method
+xyz = SO3VF.SO3F.eval(rot,varargin{:});
+
+% generate tangent space vector
+f = SO3TangentVector(xyz.',rot(:),SO3VF.internTangentSpace,SO3VF.hiddenCS,SO3VF.hiddenSS);
 f = reshape(f,size(rot));
 
+% Maybe change tangent space
 tS = SO3TangentSpace.extract(varargin{:},SO3VF.tangentSpace);
-
 f = transformTangentSpace(f,tS);
 
 end
