72385786a93c295d01b0e16388ef7ab075567727
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-08 17:21:30 +0200

    doc

--- geometry/@SO3TangentVector/plus.m
+++ geometry/@SO3TangentVector/plus.m
@@ -2,7 +2,7 @@ function v = plus(v1,v2)
 % overload plus +
 
 if ~isa(v1,'SO3TangentVector') || ~isa(v2,'SO3TangentVector')
-  error('For tangent vectors, it is only possible to calculate the cross product with tangent vectors.')
+  error('For tangent vectors, it is only possible to calculate the sum with other tangent vectors.')
 end
 tS = v1.tangentSpace;
 v2 = transformTangentSpace(v2,tS);
