f8f352e323839474df44909be20ba6a428c08587
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-08 17:21:30 +0200

    doc

--- geometry/@SO3TangentVector/cross.m
+++ geometry/@SO3TangentVector/cross.m
@@ -2,7 +2,7 @@ function v = cross(v1,v2,varargin)
 % overload cross
 
 if ~isa(v1,'SO3TangentVector') ||  ~isa(v2,'SO3TangentVector')
-  error('For tangent vectors, it is only possible to calculate the cross product with tangent vectors.')
+  error('For tangent vectors, it is only possible to calculate the cross product with other tangent vectors.')
 end
 tS = v1.tangentSpace;
 v2 = transformTangentSpace(v2,tS);
--- geometry/@SO3TangentVector/dot_outer.m
+++ geometry/@SO3TangentVector/dot_outer.m
@@ -2,7 +2,7 @@ function v = dot_outer(v1,v2,varargin)
 % overload dot_outer
 
 if ~isa(v1,'SO3TangentVector') ||  ~isa(v2,'SO3TangentVector')
-  error('For tangent vectors, it is only possible to calculate the cross product with tangent vectors.')
+  error('For tangent vectors, it is only possible to calculate the dot product with other tangent vectors.')
 end
 tS = v1.tangentSpace;
 v2 = transformTangentSpace(v2,tS);
