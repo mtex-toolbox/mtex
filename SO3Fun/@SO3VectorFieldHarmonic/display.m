6834016c7d1510fe723ab4ca3cef22972c94e4d4
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-13 10:06:03 +0200

    doc

--- SO3Fun/@SO3VectorFieldHarmonic/display.m
+++ SO3Fun/@SO3VectorFieldHarmonic/display.m
@@ -2,14 +2,22 @@ function display(SO3VF,varargin)
 % standard output
 displayClass(SO3VF,inputname(1),[],'moreInfo',symChar(SO3VF),varargin{:});
 
+% harmonic structure
 if ~SO3VF.SO3F.isReal, disp('  isReal: false'); end
 disp(['  bandwidth: ' num2str(SO3VF.SO3F.bandwidth)]);
+
+% Tangent Space
 disp(['  tangent space: ' , char(SO3VF.tangentSpace)]);
 
+% hidden properties
+if SO3VF.CS~=SO3VF.hiddenCS || SO3VF.SS ~= SO3VF.hiddenSS
+  disp( ['  intern symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
+end
+if SO3VF.tangentSpace ~= SO3VF.internTangentSpace
+  disp( ['  intern tangent space: ' char(SO3VF.internTangentSpace)] );
+end
+
 disp(' ')
 
-disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
-disp( [' inner tangent space: ' char(SO3VF.internTangentSpace)] );
-disp(' ')
 
 end
--- SO3Fun/@SO3VectorFieldRBF/display.m
+++ SO3Fun/@SO3VectorFieldRBF/display.m
@@ -3,21 +3,25 @@ function display(SO3VF,varargin)
 
 displayClass(SO3VF,inputname(1),[],'moreInfo',symChar(SO3VF),varargin{:});
 
+% RBF-structure
 disp(['  kernel: ',char(SO3VF.SO3F.psi)]);
-
 if isa(SO3VF.SO3F.center,'SO3Grid')
   disp(['  center: ',char(SO3VF.SO3F.center)]);
 else
   disp(['  center: ',num2str(length(SO3VF.SO3F.center)), ' orientations']);
 end
 
+% Tangent space
 disp(['  tangent space: ' , char(SO3VF.tangentSpace)]);
 
-disp(' ')
+% hidden properties
+if SO3VF.CS~=SO3VF.hiddenCS || SO3VF.SS ~= SO3VF.hiddenSS
+  disp( ['  intern symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
+end
+if SO3VF.tangentSpace ~= SO3VF.internTangentSpace
+  disp( ['  intern tangent space: ' char(SO3VF.internTangentSpace)] );
+end
 
-
-disp( [' inner symmetries: ' char(SO3VF.hiddenCS,'compact') ' ' char(8594) ' ' char(SO3VF.hiddenSS,'compact')] );
-disp( [' inner tangent space: ' char(SO3VF.internTangentSpace)] );
 disp(' ')
 
 
