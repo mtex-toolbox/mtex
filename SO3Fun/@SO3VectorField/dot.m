a277c78580e45b8dc79b05f188587424afd6d955
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-13 08:42:24 +0200

    minor

--- SO3Fun/@SO3VectorField/dot.m
+++ SO3Fun/@SO3VectorField/dot.m
@@ -14,11 +14,7 @@ function SO3F = dot(SO3VF1, SO3VF2, varargin)
 if ~isa(SO3VF1,'SO3VectorField') ||  ~isa(SO3VF2,'SO3VectorField') 
   error('For SO3VectorFields, it only make sense to calculate the pointwise dot product with other SO3VectorFields.')
 end
-% tS = v1.tangentSpace;
-% v2 = transformTangentSpace(v2,tS);
-% 
-% ensureCompatibleTangentSpaces(v1,v2,'equal');
-% v = dot@vector3d(v1,v2,varargin{:});
+
 
 % ensure compatible symmetries
 em = (SO3VF1.hiddenCS ~= SO3VF2.hiddenCS) || (SO3VF1.hiddenSS ~= SO3VF2.hiddenSS);
--- geometry/@SO3TangentVector/cat.m
+++ geometry/@SO3TangentVector/cat.m
@@ -3,11 +3,20 @@ function SO3TV = cat(dim,varargin)
 
 [~,ind] = find(cellfun(@(v) isa(v,'SO3TangentVector'),varargin));
 v = varargin{ind(1)};
+tS = v.tangentSpace;
 r = [];
+
 for i = ind(1:end)
-  tS = ensureCompatibleTangentSpaces(v,varargin{i});
-  v.tangentSpace = tS;
+
+  % ensure compatible symmetries
+  em = (v.hiddenCS ~= varargin{i}.hiddenCS) || (v.hiddenSS ~= varargin{i}.hiddenSS);
+  if em
+    error('The symmetries are not compatible. (Calculations with @SO3TangentVector''s needs suitable intern symmetries.)')
+  end
+
+  varargin{i} = transformTangentSpace(varargin{i},tS);
   r = cat(dim,r,varargin{i}.rot);
+
 end
 
 v = cat@vector3d(dim,varargin{:});
