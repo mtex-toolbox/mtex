f8aa6e55ee5c0416efd93b61b44b5a187a3dd201
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-08-20 09:16:56 +0200

    allow dot for tangent vectors and vector3d

--- geometry/@SO3TangentVector/dot.m
+++ geometry/@SO3TangentVector/dot.m
@@ -1,6 +1,11 @@
 function v = dot(v1,v2,varargin)
 % overload dot
 
+if ( isa(v1,'vector3d') && ~isa(v1,'SO3TangentVector') ) || ( isa(v2,'vector3d') && ~isa(v2,'SO3TangentVector') )
+  v = dot@vector3d(v1,v2,varargin{:});
+  return
+end
+
 if ~isa(v1,'SO3TangentVector') ||  ~isa(v2,'SO3TangentVector')
   error('For tangent vectors, it is only possible to calculate the dot product with other tangent vectors.')
 end
