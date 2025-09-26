9e7d768a21a8f1a076ece881e3ff639997ef7148
kilir <ruediger.kilian@protonmail.com>
2025-09-26 23:02:04 +0200

    fix

--- EBSDAnalysis/@neper/simulateGrains.m
+++ EBSDAnalysis/@neper/simulateGrains.m
@@ -1,4 +1,4 @@
-function varargout = simulateGrains(varargin)
+function grains = simulateGrains(numGrains, varargin)
 % generating 3d neper tessellations
 %
 % Syntax
@@ -22,68 +22,40 @@ function varargout = simulateGrains(varargin)
 %  grains - @grain3d
 %
 
-
 this = neper.instance;
 
 assert(nargin>0,'too few input arguments')
-
-if isnumeric(varargin{1})
-  numGrains = varargin{1};
-  ori = varargin{2}.discreteSample(numGrains);
-else
-  ori=varargin{1};
-  numGrains=length(varargin{1});
-end
-
-% generate Neper symmetry names
-CS = ori.CS.LaueName;
-switch CS
-  case '2/m11'
-    CS='2/m';
-  case '12/m1'
-    CS='2/m';
-  case '112/m'
-    CS='2/m';
-  case '-3m1'
-    CS='-3m';
-  case '-31m'
-    CS='-3m';
-  otherwise
-end
-
-% save ori to file
-oriFilename='ori_in.txt';
-fid=fopen([this.filePath oriFilename],'w');
-fprintf(fid,'%f %f %f\n',ori.Rodrigues.xyz.');
-fclose(fid);
 
 if check_option(varargin,'silent')
   output2file = ['>> ' this.filePath 'neper.log'];
 else
   output2file = '';
 end
+
 system([this.cmdPrefix 'neper -T -n ' num2str(numGrains) ...
   ' -id ' num2str(this.id) ' -morpho "' this.morpho '" ' ...
   ' -domain "cube(' num2str(this.cubeSize(1)) ',' num2str(this.cubeSize(2)) ',' num2str(this.cubeSize(3)) ')"' ...
   ' -morphooptistop "itermax=' num2str(this.iterMax) '" ' ... % decreasing the iterations makes things go a bit faster for testing
   ' ' num2str(this.varNeperopts) ' '... %add additional neper options, full syntax
-  ' -oricrysym "' CS '" '...
-  ' -ori "file(' [this.filePathUnix oriFilename] ')" ' ... % read orientations from file, default rodrigues
   ' -statpoly faceeqs ' ... % some statistics on the faces
   ' -o ' [this.filePathUnix this.fileName3d] ' ' ... % output file name
-  ' -oridescriptor rodrigues ' ... % orientation format in output file
-  ' -oriformat plain ' ...
-  ' -format tess,ori' ... % outputfiles
+  ' -format tess' ... % outputfiles
   output2file ...
   ' && ' ...
   ...
   this.cmdPrefix 'neper -V ' [this.filePathUnix this.fileName3d] '.tess' output2file]);
 
-%% return value
-if nargout >= 1
-  varargout = {grain3d.load([this.filePath this.fileName3d '.tess'],'CS',ori.CS)};
+grains = grain3d.load([this.filePath this.fileName3d '.tess']);
+
+
+% set orientations
+if ~isa(varargin{1},'orientation')
+  ori = varargin{1}.discreteSample(length(grains)); % if neper regularization is used, length(grains) might be different from numGrains
 else
-  varargout = {};
+  ori=varargin{1};
 end
+
+grains.CSList{2} =ori.CS;
+grains.meanOrientation = ori;
 
 end
