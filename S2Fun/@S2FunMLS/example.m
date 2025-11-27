04b76907dbcdf5960cb6a1714d4b29994b71d512
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2025-11-27 15:51:04 +0100

    minor

--- S2Fun/@S2FunMLS/example.m
+++ S2Fun/@S2FunMLS/example.m
@@ -2,9 +2,9 @@ function sF = example(varargin)
 % Construct example for an S2FunTri.
 
       odf = SantaFe;
-      v = equispacedS2Grid;
+      v = equispacedS2Grid('points',1000);
       values = odf.calcPDF(Miller(1,0,0,odf.CS),v);
       
-      sF = S2FunMLS(v,1i*values);
+      sF = S2FunMLS(v,values);
 
 end
