e09543c8c1dcb9e999e5267a7ce3403a1b6d16fe
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-03-01 08:44:07 +0100

    added sqrt function

---
+++ S2Fun/@S2Fun/sqrt.m
@@ -0,0 +1,18 @@
+function sF = sqrt(sF, varargin)
+% absolute value of a function
+% Syntax
+%   sF = sqrt(sF)
+%
+% Input
+%  sF - @S2Fun
+%
+% Output
+%  sF - @S2FunHandle
+%
+% Options
+%  bandwidth - minimal degree of the spherical harmonic
+%
+
+sF = S2FunHandle(@(v) sqrt(sF.eval(v)),sF.s);
+
+end
