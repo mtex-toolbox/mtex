2f8fab03c1764e8f79ce60b372de4ab362010a15
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-03-01 08:46:16 +0100

    minor fixc

--- geometry/@orientation/private/extractSym.m
+++ geometry/@orientation/private/extractSym.m
@@ -1,14 +1,18 @@
 function [sym1,sym2] = extractSym(obj)
 % extract left and right symmetry from various objects
 
+sym2 = [];
 if isa(obj,'symmetry')
   sym1 = obj;
   sym2 = obj;
 elseif isa(obj,'orientation')
   sym1 = obj.SS;
   sym2 = obj.CS;
+elseif isa(obj,'Miller')
+  sym1 = obj.CS;
+elseif isa(obj,'quaternion') || isa(obj,'vector3d')
+  sym1 = [];
 else
-  sym2 = [];
   try
     sym1 = obj.CS;
   catch
