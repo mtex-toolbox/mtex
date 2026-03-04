a9184fb1a0f38784672f9564add73b6c514d95a5
Erik Wünsche <erik.wuensche@math.tu-freiberg.de>
2026-03-04 14:28:39 +0100

    Fix SO3Fun/max for SO3Fun with high left symmetry
    mtexdata emsland
    grains = ebsd.calcGrains('angle',10*degree)
    gBAF = grains.boundary('a','f')
    mori = gBAF.misorientation;
    mdf = calcDensity(mori,'halfwidth',3*degree)
    [mval,mpos]= max(mdf)
    plot(mdf,'3d')
    annotate(mpos)

--- SO3Fun/@SO3Fun/max.m
+++ SO3Fun/@SO3Fun/max.m
@@ -142,7 +142,7 @@ while res > targetRes
   res = res / 1.25;
 
   % neighborhood search
-  S3Glocal = localOrientationGrid(SO3F.SLeft,SO3F.SLeft,2*res,'resolution',res/2);
+  S3Glocal = localOrientationGrid(SO3F.SLeft,SO3F.SLeft,2*res,'resolution',res/2,'doNotRespectFR');
   newModes = (S3Glocal * modes).';
   f = reshape(eval(SO3F,newModes,varargin{:}),size(newModes));
 
