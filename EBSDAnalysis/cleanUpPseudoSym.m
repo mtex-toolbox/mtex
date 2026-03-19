5e63ba0f018628217769f84508c53c025c44b962
PhilKro <56341570+PhilKro@users.noreply.github.com>
2026-03-19 11:56:19 +0100

    Fix: Added proper Burgers vector lengths to screw dislocaitons in dislocationSystem, these were ill-defined for hexagonal. Updated cleanUpPseudoSym.m.

--- EBSDAnalysis/cleanUpPseudoSym.m
+++ EBSDAnalysis/cleanUpPseudoSym.m
@@ -1,18 +1,35 @@
-function [ebsd,grainsM] = cleanUpPseudoSym(ebsd,grains,mori,varargin)
+function [ebsd,grainsM,numChanged] = cleanUpPseudoSym(ebsd,grains,mori,varargin)
+% cleanUpPseudoSym Corrects pseudo-symmetry artifacts using tortuosity.
 %
 % Syntax
+%   [ebsd, grainsM, numChanged] = cleanUpPseudoSym(ebsd, grains, mori, varargin)
 %
 % Input
+%   ebsd   - @EBSD object
+%   grains - @grain2d object
+%   mori   - @orientation (can be an array of pseudo-symmetries)
+%
+% Options
+%   'delta'     - Tolerance for boundary misorientation angle to match pseudo-symmetry (default: 2*degree)
+%   'threshold' - Minimum tortuosity (boundary length / straight distance) to trigger merge (default: 1.5)
 %
 % Output
-%
+%   ebsd       - updated @EBSD object
+%   grainsM    - merged @grain2d object
+%   numChanged - number of pixels that changed orientation
+
+% initialize output counter
+numChanged = 0;
 
 % select grain boundaries with the correct symmetry
-gB = grains.boundary(mori.CS,mori.CS);
+gB = grains.boundary(mori(1).CS, mori(1).CS);
 
 % select grain boundaries with the mori
 tol = get_option(varargin,'delta',2*degree);
-gB =  gB(angle(gB.misorientation,mori) < tol);
+
+% evaluate all moris and keep boundary if any match is within tolerance
+isMatch = any(angle(gB.misorientation, mori) < tol, 2);
+gB = gB(isMatch);
 
 % extent of each component
 xmin = accumarray(gB.componentId,gB.midPoint.x,[],@min);
@@ -26,9 +43,7 @@ l = accumarray(gB.componentId,gB.segLength);
 
 % tortuosity projected back to each segment
 tortuosity = l(gB.componentId)./d(gB.componentId);
-
 maxT = get_option(varargin,'threshold',1.5);
-
 cond = tortuosity > maxT & gB.componentSize > 4;
 
 % merge grains
@@ -39,17 +54,40 @@ cond = tortuosity > maxT & gB.componentSize > 4;
 ind = ebsd.grainId > 0;
 ebsd.grainId(ind) = parentId(grains.id2ind(ebsd.grainId(ind)));
 
-% update EBSD orientations
-for k=newInd
-  
-  ind = find(ebsd.grainId == grainsM.id(k));
-  
-  ori = ebsd.orientations(ind);
+% array of all possible operators: [identity, mori_1, mori_2, ...]
+id_op = orientation.id(mori(1).CS,mori(1).CS); 
+all_ops = [id_op, transpose(mori(:))];
 
-  swap = angle(ori,grainsM.meanOrientation(k) * inv(mori)) ...
-    < angle(ori,grainsM.meanOrientation(k));
-  
-  ori(swap) = ori(swap) * mori;
-  ebsd.orientations(ind) = ori;
+% identify ebsd points belonging to the newly merged grains
+updated_grain_ids = grainsM.id(newInd);
+is_merged_pt = ismember(ebsd.grainId, updated_grain_ids);
+merged_pts = find(is_merged_pt);
 
+if ~isempty(merged_pts)
+  % extract orientations of merged points and their new grain mean orientations
+  ori = ebsd.orientations(merged_pts);
+  
+  grain_inds = grainsM.id2ind(ebsd.grainId(merged_pts));
+  mean_oris = grainsM.meanOrientation(grain_inds);
+  
+  % calculate distances for all operators simultaneously
+  dists = angle(ori .* all_ops, mean_oris);
+  
+  % find the operator that minimizes the misorientation distance
+  [~, best_op_idx] = min(dists, [], 2);
+  
+  % count pixels that will change (index 1 is the identity operator)
+  numChanged = sum(best_op_idx > 1);
+  
+  % apply the best operator (skip index 1 since it is the identity)
+  for m = 1:length(mori)
+      swap = (best_op_idx == m + 1);
+      if any(swap)
+          ori(swap) = ori(swap) * mori(m);
+      end
+  end
+  
+  % update EBSD orientations
+  ebsd.orientations(merged_pts) = ori;
+end
 end
