44e4b13b74c87f2530b53171c558111c7b7d7333
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-04-02 10:25:12 +0200

    bug fix

--- EBSDAnalysis/@EBSDsquare/plotUnitCells.m
+++ EBSDAnalysis/@EBSDsquare/plotUnitCells.m
@@ -3,16 +3,6 @@ function h = plotUnitCells(ebsd,d,varargin)
 
 d = reshape(d,size(ebsd,1),size(ebsd,2),[]);
 
-if check_option(varargin,'region')  
-  ext = get_option(varargin,'region');
-    
-  ind = ebsd.pos.x > ext(1) & ebsd.pos.x < ext(2) & ...
-    ebsd.pos.y > ext(3) & ebsd.pos.y < ext(4);
-     
-  d = submatrix(d,ind);
-
-end
-
 h = plotSurf(ebsd.pos,d,ebsd.unitCell,varargin{:});
 
 end
