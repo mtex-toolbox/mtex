daef81879a3893929581992a1efcddeb7f7791c2
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-05-11 10:25:44 +0200

    allow to run checkInterface on arbitrary folders

--- tests/checkInterfaces.m
+++ tests/checkInterfaces.m
@@ -1,23 +1,33 @@
 function  checkInterfaces(type)
 
-% find all interfaces
-interfaces = dir([mtex_path '/interfaces/load' type '_*.m']);
-interfaces = {interfaces.name};
-% do not use interfaces generic
-ind = cellfun('isempty',strfind(interfaces,'generic'));
-interfaces = interfaces(ind);
+if nargin == 1
+  % find all interfaces
+  interfaces = dir([mtex_path '/interfaces/load' type '_*.m']);
+  interfaces = {interfaces.name};
+  % do not use interfaces generic
+  ind = cellfun('isempty',strfind(interfaces,'generic'));
+  interfaces = interfaces(ind);
 
+  path = [mtexDataPath filesep type];
+  
+else
+  interfaces = {};
+  path = pwd;
+  type = 'EBSD';
+end
 
-files = dir([mtexDataPath filesep type]);
-
+files = dir(path);
+files = files(~[files.isdir]);
 int = {};
 
-for i = 4:length(files)
+for i = 1:length(files)
   
   try    
-    [tmp,interface] = feval([type '.load'  ],fullfile(mtexDataPath,type,files(i).name));    
+    [tmp,interface] = feval([type '.load'  ],fullfile(path,files(i).name),'wizard');    
     disp([files(i).name, repmat(' ',1,20-length(files(i).name)), ' - ', interface]);
-    interfaces(strcmpi(interfaces,['load' type '_' interface '.m'])) = [];
+    if ~isempty(interfaces)
+      interfaces(strcmpi(interfaces,['load' type '_' interface '.m'])) = [];
+    end
   catch
     disp([files(i).name, repmat(' ',1,20-length(files(i).name)), ' - FAILED']);
     %disp(lasterr);
