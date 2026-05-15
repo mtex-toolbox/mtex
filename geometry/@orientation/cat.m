1da921e1877fdbd80e8c4c7e775bb150fa47d8d1
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-05-15 17:33:55 +0200

    minor fixes

--- geometry/@orientation/cat.m
+++ geometry/@orientation/cat.m
@@ -14,13 +14,15 @@ function ori = cat(dim,varargin)
 % See also
 % rotation/horzcat, rotation/vertcat
 
-if varargin{1}.CS ~= varargin{2}.CS
-  warning("The symmetries " + char(varargin{1}.CS) + " and " + char(varargin{2}.CS) + ...
-    " of the orientations to be concatenated do not match");
-end
-if varargin{1}.SS ~= varargin{2}.SS
-  warning("The symmetries " + char(varargin{1}.SS) + " and " + char(varargin{2}.SS) + ...
-    " of the orientations to be concatenated do not match");
+if ~isempty(varargin{1}) && ~isempty(varargin{2})
+  if varargin{1}.CS ~= varargin{2}.CS
+    warning("The symmetries " + char(varargin{1}.CS) + " and " + char(varargin{2}.CS) + ...
+      " of the orientations to be concatenated do not match");
+  end
+  if varargin{1}.SS ~= varargin{2}.SS
+    warning("The symmetries " + char(varargin{1}.SS) + " and " + char(varargin{2}.SS) + ...
+      " of the orientations to be concatenated do not match");
+  end
 end
 
 ori = cat@rotation(dim,varargin{:});
