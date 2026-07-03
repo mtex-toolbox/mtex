8d10e545c76c166d19b1897855ee2015546f0a0a
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2026-07-03 15:32:32 +0200

    speed up of loadHelper

--- interfaces/tools/loadHelper.m
+++ interfaces/tools/loadHelper.m
@@ -2,60 +2,63 @@ classdef loadHelper < handle
 % helps to load data-matrix with ColumnNames
 
 properties
-  data % (n x m) matrix
+  data         % (n x M) numeric matrix, UNveraendert (copy-on-write geteilt)
+  colMap       % 1 x m: physische Spalte in data je logischer Spalte
+  columnNames  % 1 x m cellstr (stripws + lower)
   unit
   opts
 end
 
 methods
-
   function loader = loadHelper(d, varargin)
-    % Options
-    %  KeepNaN -
-    %  Radians -
-    %  passive rotation -
-
     columnNames = stripws(lower(get_option(varargin,'ColumnNames')));
     columns = get_option(varargin,'Columns',1:length(columnNames));
-
     assert(length(columnNames) == length(columns), ...
       'Length of ColumnNames and Columns differ');
 
     [columnNames, m] = unique(columnNames);
-    columns = columns(m);
 
-    loader.data = array2table(d(:,columns),'variableNames',columnNames);
+    loader.data        = d;            % <-- KEINE Kopie (copy-on-write)
+    loader.colMap      = columns(m);   % nur die Index-Zuordnung
+    loader.columnNames = columnNames;
 
-    % check some flags
     loader.unit = degree + (1-degree)*check_option(varargin,{'radians','radiant','radiand'});
-
-    varargin = delete_option(varargin,{'ColumnNames','Columns'},1);
+    varargin  = delete_option(varargin,{'ColumnNames','Columns'},1);
     loader.opts = delete_option(varargin,{'radians','radiant','radiand'});
-    
   end
 
+  % --- interne Helfer -------------------------------------------------
+  function idx = colIndex(loader,names)   % logische Indizes (in columnNames/colMap)
+    names = stripws(lower(ensurecell(names)));
+    [~,idx] = ismember(names,loader.columnNames);
+  end
+
+  function removeColumns(loader,idx)      % billig: nur Zuordnung kuerzen
+    loader.colMap(idx)      = [];
+    loader.columnNames(idx) = [];
+  end
+  % --------------------------------------------------------------------
+
   function out = hasColumns(loader,names)
-    out = all(cellfun(@(name) any(strcmpi(loader.data.Properties.VariableNames,name)),...
-      stripws(ensurecell(names))));
+    names = stripws(lower(ensurecell(names)));
+    out = all(ismember(names,loader.columnNames));
   end
 
   function out = findColumn(loader,names)
-    out = find(cellfun(@(x) any(strcmpi(stripws(ensurecell(names)),x)), ...
-      loader.data.Properties.VariableNames),1);
+    names = stripws(lower(ensurecell(names)));
+    out = find(ismember(loader.columnNames,names),1);
   end
 
   function d = getColumnData(loader,colname)
-    try
-      pos = loader.findColumn(colname);
-      d =  table2array(loader.data(:,pos));
-      loader.data(:,pos) = [];
-    catch
+    pos = loader.findColumn(colname);
+    if isempty(pos)
       error(['No data of type ' colname ' found'])
     end
+    d = loader.data(:, loader.colMap(pos));
+    loader.removeColumns(pos);
   end
 
   function rot = getRotations(loader)
-
     conventions = {...
       {'Euler 1' 'Euler 2' 'Euler 3'};
       {'phi1','Phi','phi2'};
@@ -64,31 +67,25 @@ methods
       {'Psi','Theta','phi'};
       {'omega','Theta','phi'};
       {'Quat real','Quat i','Quat j','Quat k'}; };
-    
     conventions = cellfun(@(s) stripws(lower(s)),conventions,'UniformOutput',false);
+    convNames = {'Euler','Bunge','Matthies','Roe','Kocks','Canova','Quaternion'};
 
-    convNames  = {...
-      'Euler','Bunge', 'Matthies', 'Roe', 'Kocks', 'Canova', 'Quaternion' };
-
-    type = find(cellfun(@(convention) loader.hasColumns(convention),conventions),1,'first');
-
+    type = find(cellfun(@(c) loader.hasColumns(c),conventions),1,'first');
     assert(~isempty(type),'At least specify three Euler angles or four quaternion components should be specified!');
 
-    cols = conventions{type};
+    idx  = loader.colIndex(conventions{type});
+    phys = loader.colMap(idx);
 
-    % set rows where angle is 4*pi to nan
-    ind = abs(table2array(loader.data(:,cols(1)))*loader.unit - 4*pi)<1e-3;
-    loader.data(ind,cols) = {nan};
+    % kleiner Ausschnitt statt Schreibzugriff auf die grosse Matrix
+    rotData = loader.data(:,phys);
+    ind = abs(rotData(:,1)*loader.unit - 4*pi) < 1e-3;
+    rotData(ind,:) = nan;
 
-    % eliminate nans
-    rotData = table2array(loader.data(:,cols));
-    if type <=6
-      flag = extract_option([convNames{type} loader.opts],convNames);     
-      
-      if max(rotData(~isnan(rotData))) > 15
+    if type <= 6
+      flag = extract_option([convNames{type} loader.opts],convNames);
+      if any(any(rotData>15))
         rotData = rotData * pi/180;
       end
-
       rot = rotation.byEuler(rotData, flag{:});
     else
       rot = rotation(quaternion(rotData));
@@ -98,79 +95,69 @@ methods
       rot = inv(rot);
     end
 
-    % remove columns
-    loader.data(:,cols) = [];
-
+    loader.removeColumns(idx);
   end
 
   function v = getVector3d(loader)
-
     conventions = {...
-      {'x','y','z'};...
+      {'x','y','z'};
       {'Polar' 'Azimuth'};
       {'Polar Angle' 'Azimuth Angle'};
       {'Colatitude','Longitude'};
-      {'Colattitude','Longitude'}; % for historical reasons
+      {'Colattitude','Longitude'};   % historisch
       {'Latitude','Longitude'};
-      {'Lattitude','Longitude'};   % for historical reasons
-      };
+      {'Lattitude','Longitude'}; };   % historisch
     conventions = cellfun(@(s) stripws(lower(s)),conventions,'UniformOutput',false);
 
-    type = find(cellfun(@(convention) loader.hasColumns(convention),conventions),1,'first');
+    type = find(cellfun(@(c) loader.hasColumns(c),conventions),1,'first');
+    assert(~isempty(type),'Columns for cartesian or spherical coordinates not specified!');
 
-    assert(~isempty(type),'Columns for cartesian or spherical coordinates not specoified!');
+    idx  = loader.colIndex(conventions{type});
+    phys = loader.colMap(idx);
 
-    cols = conventions{type};
-
-    % eliminate nans
     if ~check_option(loader.opts,'keepNaN')
-      loader.data(any(isnan(table2array(loader.data(:,cols))),2),:) = [];
-    end
-
-    % specimen directions
-    if type == 1  % xyz
-      v = vector3d(table2array(loader.data(:,cols)));
-    else % spherical
-
-      theta = table2array(loader.data(:,cols(1))) * loader.unit;
-      rho   = table2array(loader.data(:,cols(2))) * loader.unit;
-            
-      if type >= 6, theta = pi/2 - theta; end % latitude
-
-      v = vector3d.byPolar(theta,rho);
-
-    end
-
-    % remove columns
-    loader.data(:,cols) = [];
-
-  end
-
-  function options = getOptions(loader,varargin)    
-
-    % maybe we should ignore some columns
-    cols = ensurecell(get_option(varargin,'ignoreColumns',[]));
-    for k = 1:length(cols)
-      try
-        loader.data(:,cols{k}) = [];
+      bad = any(isnan(loader.data(:,phys)),2);
+      if any(bad)
+        loader.data(bad,:) = [];   % einzige echte Kopie, nur falls Zeilen wegfallen
       end
     end
 
-    options = table2struct(loader.data,'ToScalar',true);
+    if type == 1  % xyz
+      v = vector3d(loader.data(:,phys));
+    else          % spherical
+      theta = loader.data(:,phys(1)) * loader.unit;
+      rho   = loader.data(:,phys(2)) * loader.unit;
+      if type >= 6, theta = pi/2 - theta; end
+      v = vector3d.byPolar(theta,rho);
+    end
+
+    loader.removeColumns(idx);
+  end
+
+  function options = getOptions(loader,varargin)
+    cols = ensurecell(get_option(varargin,'ignoreColumns',[]));
+    for k = 1:length(cols)
+      idx = loader.findColumn(cols{k});
+      if ~isempty(idx), loader.removeColumns(idx); end
+    end
+    options = struct();
+    for k = 1:numel(loader.columnNames)
+      options.(loader.columnNames{k}) = loader.data(:, loader.colMap(k));
+    end
   end
 
   function xyz = getPos(loader)
-
     if loader.hasColumns({'x','y','z'})
-      xyz = vector3d(table2array(loader.data(:,{'x','y','z'})));
-      loader.data(:,{'x','y','z'}) = [];
+      idx = loader.colIndex({'x','y','z'});
+      xyz = vector3d(loader.data(:, loader.colMap(idx)));
+      loader.removeColumns(idx);
     elseif loader.hasColumns({'x','y'})
-      xyz = vector3d(table2array(loader.data(:,'x')), table2array(loader.data(:,'y')),0);
-      loader.data(:,{'x','y'}) = [];
-    else 
+      ix = loader.colIndex('x');  iy = loader.colIndex('y');
+      xyz = vector3d(loader.data(:, loader.colMap(ix)), loader.data(:, loader.colMap(iy)), 0);
+      loader.removeColumns([ix iy]);
+    else
       error('No columns with "x" and "y" coordinates found!')
     end
   end
 end
-
 end
