d9869b64b7ea0afc46a78530a4c38a2d4bcfd513
kilir <ruediger.kilian@protonmail.com>
2026-07-09 20:04:20 +0200

    bug fix

--- tools/math_tools/chi2inv.m
+++ tools/math_tools/chi2inv.m
@@ -1,7 +1,6 @@
 function x = chi2inv(p,nu)
 % Inverse-chi-squared distribution
 % x = Gamma(nu/2,1/(2*p)) / Gammma(nu/2)
-
-x = gammainc(1/(2*p),nu/2) / gamma(nu/2);
-
+% x = gammainc(1/(2*p),nu/2) / gamma(nu/2);
+x = 2 * gammaincinv(p, nu/2, 'lower');
 end
